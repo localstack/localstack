2376f9149d674338abc9a6a793916a1d2e4edc8c
Rio Goodman <rgoodman@atlassian.com>
2017-01-03 16:27:32 -0800

    python things

--- localstack/mock/infra.py
+++ localstack/mock/infra.py
@@ -368,7 +368,7 @@ def update_apigateway(method, path, data, headers, response=None, return_forward
 
 def update_kinesis(method, path, data, headers, response=None, return_forward_info=False):
     if return_forward_info:
-        if constants.KINESIS_RETURN_ERRORS:
+        if KINESIS_RETURN_ERRORS:
             return 500
         else:
             return True
--- tests/test_kin.py
+++ tests/test_kin.py
@@ -1,14 +1,14 @@
 import __init__
 import boto3
-from localstack.constants import ENV_DEV, LAMBDA_TEST_ROLE
+from localstack.constants import *
 from localstack.utils.common import *
 from localstack.mock import infra
 from localstack.utils.kinesis import kinesis_connector
 from localstack.utils.aws import aws_stack
 from .lambdas import lambda_integration
-from localstack import constants
 
 TEST_STREAM_NAME = lambda_integration.KINESIS_STREAM_NAME
+
 
 def start_test(env=ENV_DEV):
     try:
@@ -18,17 +18,17 @@ def start_test(env=ENV_DEV):
         kinesis = aws_stack.connect_to_service('kinesis', env=env)
         stream = aws_stack.create_kinesis_stream(TEST_STREAM_NAME)
 
-        records = [ 
-            { 
+        records = [
+            {
                 'Data': '0',
                 'ExplicitHashKey': '0',
                 'PartitionKey': '0'
             }
         ]
 
-        constants.KINESIS_RETURN_ERRORS = True
+        KINESIS_RETURN_ERRORS = True
         kinesis.put_records(StreamName='test-stream-1', Records=records)
-        constants.KINESIS_RETURN_ERRORS = False
+        KINESIS_RETURN_ERRORS = False
 
     except KeyboardInterrupt, e:
         infra.KILLED = True
