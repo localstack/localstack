27fb4050c8a14f303409882bbfc440d4a91e9af1
Waldemar Hummer <whummer@atlassian.com>
2017-04-24 09:30:45 +1000

    add initial support for CORS headers for S3

--- localstack/mock/proxy/s3_listener.py
+++ localstack/mock/proxy/s3_listener.py
@@ -2,6 +2,7 @@ import re
 import urlparse
 import logging
 import json
+import xmltodict
 import xml.etree.ElementTree as ET
 from requests.models import Response
 from localstack.constants import *
@@ -10,6 +11,9 @@ from localstack.utils.common import timestamp, TIMESTAMP_FORMAT_MILLIS
 
 # mappings for S3 bucket notifications
 S3_NOTIFICATIONS = {}
+
+# mappings for bucket CORS settings
+BUCKET_CORS = {}
 
 # set up logger
 LOGGER = logging.getLogger(__name__)
@@ -93,17 +97,64 @@ def get_xml_text(node, name, ns=None, default=None):
     return child.text
 
 
+def get_cors(bucket_name):
+    response = Response()
+    cors = BUCKET_CORS.get(bucket_name)
+    if not cors:
+        # TODO: check if bucket exists, otherwise return 404-like error
+        cors = {
+            'CORSConfiguration': []
+        }
+    body = xmltodict.unparse(cors)
+    response._content = body
+    response.status_code = 200
+    return response
+
+
+def set_cors(bucket_name, cors):
+    # TODO: check if bucket exists, otherwise return 404-like error
+    if isinstance(cors, basestring):
+        cors = xmltodict.parse(cors)
+    BUCKET_CORS[bucket_name] = cors
+    response = Response()
+    response.status_code = 200
+    return response
+
+
+def delete_cors(bucket_name):
+    # TODO: check if bucket exists, otherwise return 404-like error
+    BUCKET_CORS.pop(bucket_name, {})
+    response = Response()
+    response.status_code = 200
+    return response
+
+
+def append_cors_headers(bucket_name, request_method, request_headers, response):
+    cors = BUCKET_CORS.get(bucket_name)
+    if not cors:
+        return
+    origin = request_headers.get('Origin', '')
+    for rule in cors['CORSConfiguration']['CORSRule']:
+        allowed_methods = rule.get('AllowedMethod', [])
+        if request_method in allowed_methods:
+            allowed_origins = rule.get('AllowedOrigin', [])
+            for allowed in allowed_origins:
+                if origin in allowed or re.match(allowed.replace('*', '.*'), origin):
+                    response.headers['Access-Control-Allow-Origin'] = origin
+                    break
+
+
 def update_s3(method, path, data, headers, response=None, return_forward_info=False):
     if return_forward_info:
         parsed = urlparse.urlparse(path)
         query = parsed.query
         path = parsed.path
+        bucket = path.split('/')[1]
         query_map = urlparse.parse_qs(query)
         if method == 'PUT' and (query == 'notification' or 'notification' in query_map):
             tree = ET.fromstring(data)
             queue_config = tree.find('{%s}QueueConfiguration' % XMLNS_S3)
             if len(queue_config):
-                bucket = path[1:]
                 S3_NOTIFICATIONS[bucket] = {
                     'Id': get_xml_text(queue_config, 'Id'),
                     'Event': get_xml_text(queue_config, 'Event', ns=XMLNS_S3),
@@ -111,9 +162,22 @@ def update_s3(method, path, data, headers, response=None, return_forward_info=Fa
                     'Topic': get_xml_text(queue_config, 'Topic', ns=XMLNS_S3),
                     'CloudFunction': get_xml_text(queue_config, 'CloudFunction', ns=XMLNS_S3)
                 }
+        if query == 'cors' or 'cors' in query_map:
+            if method == 'GET':
+                return get_cors(bucket)
+            if method == 'PUT':
+                return set_cors(bucket, data)
+            if method == 'DELETE':
+                return delete_cors(bucket)
         return True
+    # get subscribers and send bucket notifications
     if method in ('PUT', 'DELETE') and '/' in path[1:]:
         parts = path[1:].split('/', 1)
         bucket_name = parts[0]
         object_path = '/%s' % parts[1]
         send_notifications(method, bucket_name, object_path)
+    # append CORS headers to response
+    if response:
+        parsed = urlparse.urlparse(path)
+        bucket_name = parsed.path.split('/')[0]
+        append_cors_headers(bucket_name, request_method=method, request_headers=headers, response=response)
