e34ee38c50a027eafa2a39219fe01d8450052d76
tim <tim@tims-Air.lan>
2017-05-12 17:05:29 +0800

    Fix issue #105 - ElasticSearch hangs when querying using GET.  Update proxy to pass GET request data when present.

---
+++ tests/integration/test_elasticsearch.py
@@ -0,0 +1,94 @@
+import boto3
+from nose.tools import assert_raises, assert_equal, assert_true
+from botocore.exceptions import ClientError
+from localstack import config
+import json
+import requests
+import time
+from localstack.utils.aws import aws_stack
+
+es_url = aws_stack.get_local_service_url('elasticsearch')
+test_index = 'megacorp'
+test_doc_id = 1
+common_headers = {
+    'content-type': 'application/json',
+    'Accept-encoding': 'identity'
+}
+
+
+def setUp():
+    document = {
+        'first_name': 'Jane',
+        'last_name': 'Smith',
+        'age': 32,
+        'about': 'I like to collect rock albums',
+        'interests': ['music']
+    }
+    resp = add_document(test_doc_id, document)
+    assert_equal(
+        201,
+        resp.status_code,
+        msg='Request failed({}): {}'.format(
+            resp.status_code,
+            resp.text))
+
+
+def tearDown():
+    delete_document(test_doc_id)
+
+
+def add_document(id, document):
+    article_path = '{}/{}/employee/{}?pretty'.format(es_url, test_index, id)
+    resp = requests.put(
+        article_path,
+        data=json.dumps(document),
+        headers=common_headers)
+    # Pause to allow the document to be indexed
+    time.sleep(1)
+    return resp
+
+
+def delete_document(id):
+    article_path = '{}/{}/employee/{}?pretty'.format(es_url, test_index, id)
+    resp = requests.delete(
+        article_path,
+        headers=common_headers)
+    # Pause to allow the document to be indexed
+    time.sleep(1)
+    return resp
+
+
+def test_elasticsearch_get_document():
+    article_path = '{}/{}/employee/{}?pretty'.format(
+        es_url, test_index, test_doc_id)
+    resp = requests.get(
+        article_path,
+        headers=common_headers)
+
+    assert_true(
+        'I like to collect rock albums' in resp.text,
+        msg='Document not found({}): {}'.format(
+            resp.status_code, resp.text))
+
+
+def test_elasticsearch_search():
+    search_path = '{}/{}/employee/_search?pretty'.format(es_url, test_index)
+
+    search = {
+        'query': {
+            'match': {
+                'last_name': 'Smith'
+            }
+        }
+    }
+
+    resp = requests.get(
+        search_path,
+        data=json.dumps(search),
+        headers=common_headers)
+
+    assert_true(
+        'I like to collect rock albums' in resp.text,
+        msg='Search failed({}): {}'.format(
+            resp.status_code,
+            resp.text))
