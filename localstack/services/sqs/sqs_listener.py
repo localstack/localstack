0227d1a89a40ca762efb3defefa1b999b6aae3cf
Waldemar Hummer <waldemar.hummer@gmail.com>
2017-08-05 20:39:55 +1000

    support unicode names for S3; fix SQS Java tests using SSL

--- localstack/services/sqs/sqs_listener.py
+++ localstack/services/sqs/sqs_listener.py
@@ -1,5 +1,9 @@
+import re
 import xmltodict
 from six.moves.urllib import parse as urlparse
+from requests.models import Response
+from localstack import config
+from localstack.utils.common import to_str
 from localstack.utils.analytics import event_publisher
 from localstack.services.generic_proxy import ProxyListener
 
@@ -23,7 +27,16 @@ class ProxyListenerSQS(ProxyListener):
             if event_type:
                 event_publisher.fire_event(event_type, payload={'u': event_publisher.get_hash(queue_url)})
 
-        return True
+            # patch the response and return https://... if we're supposed to use SSL
+            if config.USE_SSL and action in ('CreateQueue', 'GetQueueUrl'):
+                content_str = to_str(response.content)
+                if '<QueueUrl>http://' in content_str:
+                    new_response = Response()
+                    new_response.status_code = response.status_code
+                    new_response.headers = response.headers
+                    new_response._content = re.sub(r'<QueueUrl>\s*http://', '<QueueUrl>https://', content_str)
+                    new_response.headers['content-length'] = len(new_response._content)
+                    return new_response
 
 
 # instantiate listener
