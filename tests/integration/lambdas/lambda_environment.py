5227fc5cfaa6dec8f726f289c0e0e8a15e5184e9
Waldemar Hummer <waldemar.hummer@gmail.com>
2017-08-31 14:26:54 +1000

    add test for AWS_PROXY Lambda integration

--- tests/integration/lambdas/lambda_environment.py
+++ tests/integration/lambdas/lambda_environment.py
@@ -2,6 +2,5 @@ import os
 
 
 def handler(event, context):
-    '''
-    '''
+    ''' Simple Lambda function that returns the value of the "Hello" environment variable '''
     return {'Hello': os.environ.get('Hello')}
