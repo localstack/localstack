2791f75a676425585cba94d3140bb1eb0bc9b7ff
Jacky Leung <jacky@fishpond.co.nz>
2017-12-27 16:06:26 +1300

    Fix list index out of range

--- localstack/services/sns/sns_listener.py
+++ localstack/services/sns/sns_listener.py
@@ -60,9 +60,9 @@ class ProxyListenerSNS(ProxyListener):
                 sqs_client = aws_stack.connect_to_service('sqs')
                 for subscriber in SNS_SUBSCRIPTIONS[topic_arn]:
                     if subscriber['Protocol'] == 'sqs':
-                        queue_name = subscriber['Endpoint'].split(':')[5]
                         queue_url = subscriber.get('sqs_queue_url')
                         if not queue_url:
+                            queue_name = subscriber['SubscriptionArn'].split(':')[5]
                             queue_url = aws_stack.get_sqs_queue_url(queue_name)
                             subscriber['sqs_queue_url'] = queue_url
                         sqs_client.send_message(QueueUrl=queue_url,
