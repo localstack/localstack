d73eb5d9d4ee42a968d600a2831623e00842c8c4
Paul-Albert Chmilevski <albert.chmilevski@gmail.com>
2018-01-23 15:03:39 +0200

    Cloudformation ListStackResources returns QueueUrl for PhysicalResourceId

--- tests/integration/test_cloudformation.py
+++ tests/integration/test_cloudformation.py
@@ -8,8 +8,10 @@ from botocore.parsers import ResponseParserError
 
 THIS_FOLDER = os.path.dirname(os.path.realpath(__file__))
 TEST_TEMPLATE_1 = os.path.join(THIS_FOLDER, 'templates', 'template1.yaml')
+TEST_TEMPLATE_2 = os.path.join(THIS_FOLDER, 'templates', 'template2.yaml')
 
 TEST_STACK_NAME = 'test-cf-stack-1'
+TEST_STACK_NAME_2 = 'test-cf-stack-2'
 
 
 def bucket_exists(name):
@@ -28,6 +30,12 @@ def queue_exists(name):
             return True
 
 
+def queue_url_exists(queue_url):
+    sqs_client = aws_stack.connect_to_service('sqs')
+    queues = sqs_client.list_queues()
+    return queue_url in queues['QueueUrls']
+
+
 def stream_exists(name):
     kinesis_client = aws_stack.connect_to_service('kinesis')
     streams = kinesis_client.list_streams()
@@ -36,10 +44,30 @@ def stream_exists(name):
 
 def get_stack_details(stack_name):
     cloudformation = aws_stack.connect_to_service('cloudformation')
-    stacks = cloudformation.describe_stacks(StackName=TEST_STACK_NAME)
+    stacks = cloudformation.describe_stacks(StackName=stack_name)
     for stack in stacks['Stacks']:
         if stack['StackName'] == stack_name:
             return stack
+
+
+def describe_stack_resource(stack_name, resource_logical_id):
+    cloudformation = aws_stack.connect_to_service('cloudformation')
+    response = cloudformation.describe_stack_resources(StackName=stack_name)
+    for resource in response['StackResources']:
+        if resource['LogicalResourceId'] == resource_logical_id:
+            return resource
+
+
+def list_stack_resources(stack_name):
+    cloudformation = aws_stack.connect_to_service('cloudformation')
+    response = cloudformation.list_stack_resources(StackName=stack_name)
+    return response['StackResourceSummaries']
+
+
+def get_queue_urls():
+    sqs = aws_stack.connect_to_service('sqs')
+    response = sqs.list_queues()
+    return response['QueueUrls']
 
 
 class CloudFormationTest(unittest.TestCase):
@@ -64,6 +92,9 @@ class CloudFormationTest(unittest.TestCase):
         assert queue_exists('cf-test-queue-1')
         # assert that stream has been created
         assert stream_exists('cf-test-stream-1')
+        # assert that queue has been created
+        resource = describe_stack_resource(TEST_STACK_NAME, 'SQSQueueNoNameProperty')
+        assert queue_exists(resource['PhysicalResourceId'])
 
     def test_validate_template(self):
         cloudformation = aws_stack.connect_to_service('cloudformation')
@@ -82,3 +113,20 @@ class CloudFormationTest(unittest.TestCase):
             if isinstance(err, ClientError):
                 assert err.response['ResponseMetadata']['HTTPStatusCode'] == 400
                 assert err.response['Error']['Message'] == 'Template Validation Error'
+
+    def test_list_stack_resources_returns_queue_urls(self):
+        cloudformation = aws_stack.connect_to_resource('cloudformation')
+        template = template_deployer.template_to_json(load_file(TEST_TEMPLATE_2))
+        cloudformation.create_stack(StackName=TEST_STACK_NAME_2, TemplateBody=template)
+
+        def check_stack():
+            stack = get_stack_details(TEST_STACK_NAME_2)
+            assert stack['StackStatus'] == 'CREATE_COMPLETE'
+
+        retry(check_stack, retries=3, sleep=2)
+
+        list_stack_summaries = list_stack_resources(TEST_STACK_NAME_2)
+        queue_urls = get_queue_urls()
+
+        for resource in list_stack_summaries:
+            assert resource['PhysicalResourceId'] in queue_urls
