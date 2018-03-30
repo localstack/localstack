485f6a78c18b6c4124a94c02c48a192e544fb78e
Eric Abruzzese <eric.abruzzese@gmail.com>
2018-03-30 14:43:15 -0400

    Bump Elasticsearch version to 6.2.0
    The latest supported AWS version is 6.2, and the AWS docs recommend
    using the latest supported version.

--- localstack/constants.py
+++ localstack/constants.py
@@ -69,7 +69,7 @@ APPLICATION_JSON = 'application/json'
 LAMBDA_TEST_ROLE = 'arn:aws:iam::%s:role/lambda-test-role' % TEST_AWS_ACCOUNT_ID
 
 # installation constants
-ELASTICSEARCH_JAR_URL = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.3.0.zip'
+ELASTICSEARCH_JAR_URL = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.0.zip'
 DYNAMODB_JAR_URL = 'https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.zip'
 ELASTICMQ_JAR_URL = 'https://s3-eu-west-1.amazonaws.com/softwaremill-public/elasticmq-server-0.13.8.jar'
 STS_JAR_URL = 'http://central.maven.org/maven2/com/amazonaws/aws-java-sdk-sts/1.11.14/aws-java-sdk-sts-1.11.14.jar'
