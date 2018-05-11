0def11f42a975726827a58cd78487b2a16f29bdf
Alan Justino <alan.justino@yahoo.com.br>
2018-05-11 16:06:04 -0300

    Fix zipfile entries with 0 as permission
    On OSX with serverless, all hidden (starting with .) files do have
    0 as external_attr on the uploaded zipfile

--- localstack/utils/common.py
+++ localstack/utils/common.py
@@ -511,7 +511,7 @@ def _unzip_file_entry(zip_ref, file_entry, target_dir):
     zip_ref.extract(file_entry.filename, path=target_dir)
     out_path = os.path.join(target_dir, file_entry.filename)
     perm = file_entry.external_attr >> 16
-    os.chmod(out_path, perm)
+    os.chmod(out_path, perm or 0o777)
 
 
 def is_jar_archive(content):
