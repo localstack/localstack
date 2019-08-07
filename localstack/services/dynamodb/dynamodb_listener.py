e53f548481c38938db5eeac2b71c8619e459a93c
Ben Iofel <ben@flow.io>
2019-08-07 09:37:15 -0400

    use error_response()

--- localstack/services/dynamodb/dynamodb_listener.py
+++ localstack/services/dynamodb/dynamodb_listener.py
@@ -53,24 +53,16 @@ class ProxyListenerDynamoDB(ProxyListener):
             # Check if table exists, to avoid error log output from DynamoDBLocal
             table_names = ddb_client.list_tables()['TableNames']
             if to_str(data['TableName']) not in table_names:
-                response = Response()
-                response.status_code = 400
-                response._content = json.dumps({
-                    '__type': 'com.amazonaws.dynamodb.v20120810#ResourceNotFoundException',
-                    'message': 'Cannot do operations on a non-existent table'
-                })
+                response = error_response(message='Cannot do operations on a non-existent table',
+                                          error_type='ResourceNotFoundException')
                 fix_headers_for_updated_response(response)
                 return response
         elif action == '%s.DeleteTable' % ACTION_PREFIX:
             # Check if table exists, to avoid error log output from DynamoDBLocal
             table_names = ddb_client.list_tables()['TableNames']
             if to_str(data['TableName']) not in table_names:
-                response = Response()
-                response.status_code = 400
-                response._content = json.dumps({
-                    '__type': 'com.amazonaws.dynamodb.v20120810#ResourceNotFoundException',
-                    'message': 'Cannot do operations on a non-existent table'
-                })
+                response = error_response(message='Cannot do operations on a non-existent table',
+                                          error_type='ResourceNotFoundException')
                 fix_headers_for_updated_response(response)
                 return response
         elif action == '%s.BatchWriteItem' % ACTION_PREFIX:
