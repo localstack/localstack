6f9442390af6eca799941eee765161797918dac7
Waldemar Hummer <waldemar.hummer@gmail.com>
2019-11-19 19:07:04 +0100

    fix cloudformation template downloads from S3 URLs (#1776)

--- localstack/utils/common.py
+++ localstack/utils/common.py
@@ -289,7 +289,9 @@ class CaptureOutput(object):
 # ----------------
 
 
-def is_string(s, include_unicode=True):
+def is_string(s, include_unicode=True, exclude_binary=False):
+    if isinstance(s, six.binary_type) and exclude_binary:
+        return False
     if isinstance(s, str):
         return True
     if include_unicode and isinstance(s, six.text_type):
