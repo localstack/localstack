fcfd2aa9a74dbd7b7766b7f28b6e756ded02035d
Waldemar Hummer <waldemar.hummer@gmail.com>
2020-01-03 20:31:25 +0100

    extract command to start SFN into separate function

--- localstack/services/stepfunctions/stepfunctions_starter.py
+++ localstack/services/stepfunctions/stepfunctions_starter.py
@@ -11,12 +11,7 @@ LOG = logging.getLogger(__name__)
 MAX_HEAP_SIZE = '256m'
 
 
-def start_stepfunctions(port=None, asynchronous=False, update_listener=None):
-    port = port or config.PORT_STEPFUNCTIONS
-    install.install_stepfunctions_local()
-    backend_port = DEFAULT_PORT_STEPFUNCTIONS_BACKEND
-    # TODO: local port is currently hard coded in Step Functions Local :/
-    backend_port = 8083
+def get_command():
     lambda_endpoint = aws_stack.get_local_service_url('lambda')
     dynamodb_endpoint = aws_stack.get_local_service_url('dynamodb')
     sns_endpoint = aws_stack.get_local_service_url('sns')
@@ -27,6 +22,16 @@ def start_stepfunctions(port=None, asynchronous=False, update_listener=None):
            '--sqs-endpoint %s --aws-region %s --aws-account %s --step-functions-endpoint %s') % (
         install.INSTALL_DIR_STEPFUNCTIONS, MAX_HEAP_SIZE, lambda_endpoint, dynamodb_endpoint,
         sns_endpoint, sqs_endpoint, aws_stack.get_region(), TEST_AWS_ACCOUNT_ID, sfn_endpoint)
+    return cmd
+
+
+def start_stepfunctions(port=None, asynchronous=False, update_listener=None):
+    port = port or config.PORT_STEPFUNCTIONS
+    install.install_stepfunctions_local()
+    backend_port = DEFAULT_PORT_STEPFUNCTIONS_BACKEND
+    # TODO: local port is currently hard coded in Step Functions Local :/
+    backend_port = 8083
+    cmd = get_command()
     print('Starting mock StepFunctions (%s port %s)...' % (get_service_protocol(), port))
     start_proxy_for_service('stepfunctions', port, backend_port, update_listener)
     return do_run(cmd, asynchronous)
