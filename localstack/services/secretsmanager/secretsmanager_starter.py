078c3077e1abb3a445c0b1ede288b6b406f8eb60
Phan Duong <duongpv@outlook.com>
2020-03-14 23:23:36 +0700

    #1866 Each call to SecretsManager list-secrets returns a different ARN for the same secret * Add secretsmanager_starter * Patch moto function: secret_arn * Add integration test

---
+++ localstack/services/secretsmanager/secretsmanager_starter.py
@@ -0,0 +1,34 @@
+from moto.secretsmanager import models as secretsmanager_models
+from localstack import config
+from localstack.constants import DEFAULT_PORT_SECRETSMANAGER
+from localstack.services.infra import start_moto_server
+
+SECRET_ARN_STORAGE = {}
+
+
+def apply_patches():
+    secret_arn_orig = secretsmanager_models.secret_arn
+
+    def secretsmanager_urls_secret_arn(region, secret_id):
+        k = '{}_{}'.format(region, secret_id)
+        if k not in SECRET_ARN_STORAGE:
+            arn = secret_arn_orig(region, secret_id)
+            SECRET_ARN_STORAGE[k] = arn
+
+        return SECRET_ARN_STORAGE[k]
+
+    secretsmanager_models.secret_arn = secretsmanager_urls_secret_arn
+
+
+def start_secretsmanager(port=None, asynchronous=None):
+    port = port or config.PORT_SECRETSMANAGER
+
+    apply_patches()
+
+    return start_moto_server(
+        key='secretsmanager',
+        name='Secrets Manager',
+        port=port,
+        backend_port=DEFAULT_PORT_SECRETSMANAGER,
+        asynchronous=asynchronous
+    )
