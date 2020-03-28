e375cbac5141df9ace65989d6b3250ddbc3b8134
Phan Duong <duongpv@outlook.com>
2020-03-29 00:17:16 +0700

    #1215 Lambda event does not get SQS MessageAttributes. * Remove unused unit test

--- tests/unit/test_sqs.py
+++ tests/unit/test_sqs.py
@@ -1,41 +1,8 @@
 import unittest
 from localstack.services.sqs import sqs_listener
-from localstack.services.sqs.sqs_listener import format_message_attributes
 
 
 class SQSListenerTest (unittest.TestCase):
-    def test_sqs_format_message_attrs(self):
-        request_data = {
-            'Action': ['SendMessage'],
-            'MessageAttribute.1.Name': ['attr_1'],
-            'MessageAttribute.1.Value.DataType': ['String'],
-            'MessageAttribute.1.Value.StringValue': ['attr_1_value'],
-            'MessageAttribute.2.Name': ['attr_2'],
-            'MessageAttribute.2.Value.DataType': ['Custom'],
-            'MessageAttribute.2.Value.StringValue': ['attr_2_value'],
-            'MessageBody': ['body message'],
-            'QueueUrl': ['http://localhost:4576/queue/foo-queue'],
-            'Version': ['2012-11-05']
-        }
-
-        expected = {
-            'attr_2': {
-                'dataType': 'Custom',
-                'stringValue': 'attr_2_value',
-                'stringListValues': [],
-                'binaryListValues': []
-            },
-            'attr_1': {
-                'dataType': 'String',
-                'stringValue': 'attr_1_value',
-                'stringListValues': [],
-                'binaryListValues': []
-            }
-        }
-
-        result = format_message_attributes(request_data)
-        self.assertEqual(result, expected)
-
     def test_sqs_message_attrs_md5(self):
         msg_attrs = {
             'MessageAttribute.1.Name': ['timestamp'],
