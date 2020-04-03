2e18d318ba210652dbe8db0c4a8dc3ec3229da61
usmankb <usman@krazybee.com>
2020-04-04 03:26:39 +0530

    #2150 handling describe_credit_instance_specifications,ec2 tests

--- localstack/services/ec2/ec2_starter.py
+++ localstack/services/ec2/ec2_starter.py
@@ -1,5 +1,5 @@
 from moto.ec2 import models as ec2_models
-from moto.ec2.exceptions import InvalidPermissionNotFoundError
+from moto.ec2.exceptions import InvalidPermissionNotFoundError,MotoNotImplementedError
 from localstack import config
 from localstack.constants import DEFAULT_PORT_EC2_BACKEND
 from localstack.services.infra import start_moto_server
@@ -18,8 +18,22 @@ def patch_ec2():
                     return '_ignore_'
         return revoke_security_group_egress
 
+    def patch_describe_instance_credit_specifications(backend):
+        describe_instance_credit_specifications_orig = backend.describe_instance_credit_specifications
+
+        def describe_instance_credit_specifications(**kwargs):
+            try:
+                return describe_instance_credit_specifications_orig(**kwargs)
+            except MotoNotImplementedError:
+                # this will happen because the moto doesnt support the describe_instance_credit_specifications fucnction.
+                if kwargs[0] == []:
+                    return '_ignore_'
+        return describe_instance_credit_specifications
+
+
     for region, backend in ec2_models.ec2_backends.items():
         backend.revoke_security_group_egress = patch_revoke_security_group_egress(backend)
+        backend.describe_instance_credit_specifications = patch_describe_instance_credit_specifications(backend)
 
 
 def start_ec2(port=None, asynchronous=False, update_listener=None):
---
+++ tests/integration/test_ec2.py
@@ -0,0 +1,63 @@
+import unittest
+from localstack.utils.aws import aws_stack
+
+INSTANCE_ID = 'i-d3dd5cba602b5c0ab'
+IMAGE_ID = 'ami-e4c81199'
+DUMMY_INSTANCE_ID = 'ABC'
+GROUP_ID = 'sg-1a2b3c4d'
+
+class Ec2Test(unittest.TestCase):
+
+    def setUp(self):
+        self.ec2_client = aws_stack.connect_to_service('ec2')
+
+    def test_create_image(self):
+        result = self.ec2_client.create_image(InstanceId=INSTANCE_ID,Name = 'TEST_IMAGE')
+        actual_image_id = result['ImageId']
+        self.assertIsNotNone(actual_image_id)
+
+    def test_run_instances(self):
+        result_instance = self.ec2_client.run_instances(ImageId=IMAGE_ID,InstanceType='t2.micro',MaxCount=1,MinCount=1)
+        actual_instance_id = result_instance['Instances'][0]['InstanceId']
+        self.assertIsNotNone(actual_instance_id)
+
+    def test_run_and_describe_instances(self):
+        result_instance = self.ec2_client.run_instances(ImageId=IMAGE_ID, InstanceType='t2.micro', MaxCount=1,MinCount=1)
+        actual_instance_id = result_instance['Instances'][0]['InstanceId']
+        self.assertIsNotNone(actual_instance_id)
+
+        result = self.ec2_client.describe_instances(InstanceIds=[actual_instance_id])
+        instance_id = result['Reservations'][0]['Instances'][0]['InstanceId']
+        instance_type = result['Reservations'][0]['Instances'][0]['InstanceType']
+        self.assertEqual(actual_instance_id, instance_id)
+        self.assertEqual(instance_type,'t2.micro')
+
+    def test_stop_instances(self):
+        result_instance = self.ec2_client.run_instances(ImageId=IMAGE_ID, InstanceType='t2.micro', MaxCount=1,
+                                                        MinCount=1)
+        actual_instance_id = result_instance['Instances'][0]['InstanceId']
+        self.assertIsNotNone(actual_instance_id)
+
+        result = self.ec2_client.stop_instances(InstanceIds=[actual_instance_id])
+        current_state = result['StoppingInstances'][0]['CurrentState']['Name']
+        previous_state = result['StoppingInstances'][0]['PreviousState']['Name']
+        self.assertEqual(current_state, 'stopping')
+        self.assertEqual(previous_state, 'running')
+
+
+    def test_start_instances(self):
+        result_instance = self.ec2_client.run_instances(ImageId=IMAGE_ID, InstanceType='t2.micro', MaxCount=1,
+                                                        MinCount=1)
+        actual_instance_id = result_instance['Instances'][0]['InstanceId']
+        self.assertIsNotNone(actual_instance_id)
+
+        result = self.ec2_client.start_instances(InstanceIds=[actual_instance_id])
+        current_state = result['StartingInstances'][0]['CurrentState']['Name']
+        self.assertEqual(current_state, 'pending')
+
+
+
+
+
+
+
