6f4f62d10b837941af370ded490112f9c3fd5ab8
Phan Duong <duongpv@outlook.com>
2020-04-19 00:35:02 +0700

    Fix build error

--- tests/integration/lambdas/lambda_send_message.py
+++ tests/integration/lambdas/lambda_send_message.py
@@ -4,7 +4,7 @@ import os
 
 def handler(event, context):
     protocol = 'https' if os.environ.get('USE_SSL') else 'http'
-    sqs = boto3.client('sqs', endpoint_url='{}://{}:4576'.format(protocol, os.environ['LOCALSTACK_HOSTNAME']))
+    sqs = boto3.client('sqs', verify=False, endpoint_url='{}://{}:4576'.format(protocol, os.environ['LOCALSTACK_HOSTNAME']))
 
     queue_url = sqs.get_queue_url(QueueName=event['queue_name'])['QueueUrl']
     rs = sqs.send_message(
