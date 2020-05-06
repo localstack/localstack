802fc532afadd687c10ea389c3b9a692ef3ac598
Phan Duong <duongpv@outlook.com>
2020-05-06 15:25:03 +0700

    Add integration test from CW metric data * Put metric data * List metrics * Get metric statistics
    Issue fixed:
    #712 cloudwatch internal server errors

--- tests/integration/test_cloudwatch.py
+++ tests/integration/test_cloudwatch.py
@@ -1,35 +1,55 @@
 import unittest
+
+from datetime import datetime
+from dateutil.tz import tzutc
+
 from localstack.utils.aws import aws_stack
+from localstack.utils.common import short_uid
 
 
 class CloudWatchTest(unittest.TestCase):
-
     def test_put_metric_data(self):
+        metric_name = 'metric-%s' % short_uid()
+        namespace = 'namespace-%s' % short_uid()
+
         client = aws_stack.connect_to_service('cloudwatch')
 
+        # Put metric data without value
         data = [
             {
-                'MetricName': 'm1',
+                'MetricName': metric_name,
                 'Dimensions': [{
                     'Name': 'foo',
                     'Value': 'bar'
                 }],
-                'Value': 123.45,
-                'StatisticValues': {
-                    'SampleCount': 123.0,
-                    'Sum': 123.0,
-                    'Minimum': 123.0,
-                    'Maximum': 123.0
-                },
-                'Values': [
-                    123.0,
-                ],
-                'Counts': [
-                    123.0,
-                ],
-                'Unit': 'Seconds',
-                'StorageResolution': 123
-            },
+                'Timestamp': datetime(2019, 1, 3, tzinfo=tzutc()),
+                'Unit': 'Seconds'
+            }
         ]
-        response = client.put_metric_data(Namespace='string', MetricData=data)
-        self.assertEquals(response['ResponseMetadata']['HTTPStatusCode'], 200)
+        rs = client.put_metric_data(
+            Namespace=namespace,
+            MetricData=data
+        )
+        self.assertEquals(rs['ResponseMetadata']['HTTPStatusCode'], 200)
+
+        # Get metric statistics
+        rs = client.get_metric_statistics(
+            Namespace=namespace,
+            MetricName=metric_name,
+            StartTime=datetime(2019, 1, 1),
+            EndTime=datetime(2019, 1, 10),
+            Period=120,
+            Statistics=[
+                'Average'
+            ]
+        )
+        self.assertEqual(rs['Label'], metric_name)
+        self.assertEqual(len(rs['Datapoints']), 1)
+        self.assertEqual(rs['Datapoints'][0]['Timestamp'], data[0]['Timestamp'])
+
+        rs = client.list_metrics(
+            Namespace=namespace,
+            MetricName=metric_name
+        )
+        self.assertEqual(len(rs['Metrics']), 1)
+        self.assertEqual(rs['Metrics'][0]['Namespace'], namespace)
