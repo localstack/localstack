f782dccbe9333b463f262a0d5b2a21ce4833d20b
Phan Duong <duongpv@outlook.com>
2020-05-30 00:33:54 +0700

    Implement IAM SimulatePrincipalPolicy * Patch moto IAM implementation * Add integration test
    Issue fixed:
    #1674 IAM SimulatePrincipalPolicy returns XML error

--- localstack/services/iam/iam_starter.py
+++ localstack/services/iam/iam_starter.py
@@ -1,10 +1,99 @@
+import json
 import uuid
+
 from moto.iam.responses import IamResponse, GENERIC_EMPTY_TEMPLATE
 from moto.iam.models import (
-    iam_backend as moto_iam_backend, aws_managed_policies, AWSManagedPolicy, IAMNotFoundException, User
+    iam_backend as moto_iam_backend, aws_managed_policies, AWSManagedPolicy, IAMNotFoundException, Policy, User
 )
 from localstack import config
 from localstack.services.infra import start_moto_server
+
+
+USER_RESPONSE_TEMPLATE = """<{{ action }}UserResponse>
+   <{{ action }}UserResult>
+      <User>
+         <Path>{{ user.path }}</Path>
+         <UserName>{{ user.name }}</UserName>
+         <UserId>{{ user.id }}</UserId>
+         <Arn>{{ user.arn }}</Arn>
+         <CreateDate>{{ user.created_iso_8601 }}</CreateDate>
+         <Tags>
+            {% for tag in user.tags %}<member>
+                <Key>{{ tag.Key }}</Key>
+                <Value>{{ tag.Value }}</Value>
+            </member>{% endfor %}
+         </Tags>
+     </User>
+   </{{ action }}UserResult>
+   <ResponseMetadata>
+      <RequestId>{{request_id}}</RequestId>
+   </ResponseMetadata>
+</{{ action }}UserResponse>"""
+
+
+ADDITIONAL_MANAGED_POLICIES = {
+    'AWSLambdaExecute': {
+        'Arn': 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+        'Path': '/',
+        'CreateDate': '2017-10-20T17:23:10+00:00',
+        'DefaultVersionId': 'v4',
+        'Document': {
+            'Version': '2012-10-17',
+            'Statement': [
+                {
+                    'Effect': 'Allow',
+                    'Action': [
+                        'logs:*'
+                    ],
+                    'Resource': 'arn:aws:logs:*:*:*'
+                },
+                {
+                    'Effect': 'Allow',
+                    'Action': [
+                        's3:GetObject',
+                        's3:PutObject'
+                    ],
+                    'Resource': 'arn:aws:s3:::*'
+                }
+            ]
+        },
+        'UpdateDate': '2019-05-20T18:22:18+00:00',
+    }
+}
+
+
+SIMULATE_PRINCIPAL_POLICY_RESPONSE = """
+<SimulatePrincipalPolicyResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
+  <SimulatePrincipalPolicyResult>
+    <IsTruncated>false</IsTruncated>
+    <EvaluationResults>
+      {% for eval in evaluations %}
+      <member>
+        <MatchedStatements>
+          <member>
+            <SourcePolicyId>PolicyInputList.1</SourcePolicyId>
+            <EndPosition>
+              <Column>4</Column>
+              <Line>7</Line>
+            </EndPosition>
+            <StartPosition>
+              <Column>16</Column>
+              <Line>3</Line>
+            </StartPosition>
+          </member>
+        </MatchedStatements>
+        <MissingContextValues/>
+        <EvalResourceName>{{eval.resourceName}}</EvalResourceName>
+        <EvalDecision>{{eval.decision}}</EvalDecision>
+        <EvalActionName>{{eval.actionName}}</EvalActionName>
+      </member>
+      {% endfor %}
+    </EvaluationResults>
+  </SimulatePrincipalPolicyResult>
+  <ResponseMetadata>
+    <RequestId>004d7059-4c14-11e5-b121-bd8c7EXAMPLE</RequestId>
+  </ResponseMetadata>
+</SimulatePrincipalPolicyResponse>"""
 
 
 def apply_patches():
@@ -68,6 +157,58 @@ def apply_patches():
 
     moto_iam_backend.detach_role_policy = iam_backend_detach_role_policy
 
+    policy_init_orig = Policy.__init__
+
+    def iam_response_simulate_principal_policy(self):
+        def build_evaluation(action_name, resource_name, policy_statements):
+            for statement in policy_statements:
+                # TODO Implement evaluation logic here
+                if action_name in statement['Action'] \
+                        and resource_name in statement['Resource'] \
+                        and statement['Effect'] == 'Allow':
+
+                    return {
+                        'actionName': action_name,
+                        'resourceName': resource_name,
+                        'decision': 'allowed',
+                        'matchedStatements': []
+                    }
+
+            return {
+                'actionName': action_name,
+                'resourceName': resource_name,
+                'decision': 'explicitDeny'
+            }
+
+        policy = moto_iam_backend.get_policy(self._get_param('PolicySourceArn'))
+        policy_statements = json.loads(policy.document)['Statement']
+        actions = self._get_multi_param('ActionNames.member')
+        resource_arns = self._get_multi_param('ResourceArns.member')
+        evaluations = []
+        for action in actions:
+            for resource_arn in resource_arns:
+                evaluations.append(build_evaluation(action, resource_arn, policy_statements))
+
+        template = self.response_template(SIMULATE_PRINCIPAL_POLICY_RESPONSE)
+        return template.render(evaluations=evaluations)
+
+    def policy__init__(
+            self,
+            name,
+            default_version_id=None,
+            description=None,
+            document=None,
+            path=None,
+            create_date=None,
+            update_date=None
+    ):
+        policy_init_orig(self, name, default_version_id, description, document, path, create_date, update_date)
+        self.document = document
+
+    Policy.__init__ = policy__init__
+
+    IamResponse.simulate_principal_policy = iam_response_simulate_principal_policy
+
 
 def start_iam(port=None, asynchronous=False, update_listener=None):
     port = port or config.PORT_IAM
@@ -75,56 +216,3 @@ def start_iam(port=None, asynchronous=False, update_listener=None):
     apply_patches()
     return start_moto_server('iam', port, name='IAM',
         asynchronous=asynchronous, update_listener=update_listener)
-
-
-USER_RESPONSE_TEMPLATE = """<{{ action }}UserResponse>
-   <{{ action }}UserResult>
-      <User>
-         <Path>{{ user.path }}</Path>
-         <UserName>{{ user.name }}</UserName>
-         <UserId>{{ user.id }}</UserId>
-         <Arn>{{ user.arn }}</Arn>
-         <CreateDate>{{ user.created_iso_8601 }}</CreateDate>
-         <Tags>
-            {% for tag in user.tags %}<member>
-                <Key>{{ tag.Key }}</Key>
-                <Value>{{ tag.Value }}</Value>
-            </member>{% endfor %}
-         </Tags>
-     </User>
-   </{{ action }}UserResult>
-   <ResponseMetadata>
-      <RequestId>{{request_id}}</RequestId>
-   </ResponseMetadata>
-</{{ action }}UserResponse>"""
-
-
-ADDITIONAL_MANAGED_POLICIES = {
-    'AWSLambdaExecute': {
-        'Arn': 'arn:aws:iam::aws:policy/AWSLambdaExecute',
-        'Path': '/',
-        'CreateDate': '2017-10-20T17:23:10+00:00',
-        'DefaultVersionId': 'v4',
-        'Document': {
-            'Version': '2012-10-17',
-            'Statement': [
-                {
-                    'Effect': 'Allow',
-                    'Action': [
-                        'logs:*'
-                    ],
-                    'Resource': 'arn:aws:logs:*:*:*'
-                },
-                {
-                    'Effect': 'Allow',
-                    'Action': [
-                        's3:GetObject',
-                        's3:PutObject'
-                    ],
-                    'Resource': 'arn:aws:s3:::*'
-                }
-            ]
-        },
-        'UpdateDate': '2019-05-20T18:22:18+00:00',
-    }
-}
