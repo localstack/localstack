04ee425329f978a64a75124420c532c18a180d3f
usmangani1 <sgosman_chem@yahoo.com>
2020-07-08 02:32:20 +0530

    fix invocation of destination Lambda for Logs subscription filters (#2678)

--- tests/integration/test_logs.py
+++ tests/integration/test_logs.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-
 import unittest
-
 from localstack.constants import APPLICATION_AMZ_JSON_1_1
 from localstack.utils.aws import aws_stack
-from localstack.utils.common import short_uid
+from localstack.utils import testutil
+from localstack.utils.common import short_uid, retry
+from localstack.services.awslambda.lambda_api import LAMBDA_RUNTIME_PYTHON36, func_arn
+from tests.integration.test_lambda import TEST_LAMBDA_PYTHON3, TEST_LAMBDA_NAME_PY3, TEST_LAMBDA_LIBS
 
 
 class CloudWatchLogsTest(unittest.TestCase):
@@ -83,3 +84,43 @@ class CloudWatchLogsTest(unittest.TestCase):
         self.logs_client.delete_log_group(
             logGroupName=group
         )
+
+    def test_put_subscribe_policy(self):
+        lambda_client = aws_stack.connect_to_service('lambda')
+        log_client = aws_stack.connect_to_service('logs')
+
+        testutil.create_lambda_function(
+            handler_file=TEST_LAMBDA_PYTHON3, libs=TEST_LAMBDA_LIBS,
+            func_name=TEST_LAMBDA_NAME_PY3, runtime=LAMBDA_RUNTIME_PYTHON36)
+
+        lambda_client.invoke(
+            FunctionName=TEST_LAMBDA_NAME_PY3, Payload=b'{}')
+
+        log_group_name = '/aws/lambda/{}'.format(TEST_LAMBDA_NAME_PY3)
+
+        log_client.put_subscription_filter(
+            logGroupName=log_group_name,
+            filterName='test',
+            filterPattern='',
+            destinationArn=func_arn(TEST_LAMBDA_NAME_PY3),
+        )
+        stream = 'ls-%s' % short_uid()
+        log_client.create_log_stream(logGroupName=log_group_name, logStreamName=stream)
+
+        log_client.put_log_events(
+            logGroupName=log_group_name,
+            logStreamName=stream,
+            logEvents=[
+                {'timestamp': 0, 'message': 'test'},
+                {'timestamp': 0, 'message': 'test 2'},
+            ],
+        )
+
+        resp2 = log_client.describe_subscription_filters(logGroupName=log_group_name)
+        self.assertEqual(len(resp2['subscriptionFilters']), 1)
+
+        def check_invocation():
+            events = testutil.get_lambda_log_events(TEST_LAMBDA_NAME_PY3)
+            self.assertEqual(len(events), 2)
+
+        retry(check_invocation, retries=6, sleep=3.0)
