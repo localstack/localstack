f01f73b106fe0ebd7acf67a5553b0f57d22e1601
Nevil Macwan <macnev2013@gmail.com>
2020-09-24 10:59:12 +0530

    fix "TypeName" as optional param for es7

--- localstack/services/firehose/firehose_api.py
+++ localstack/services/firehose/firehose_api.py
@@ -61,7 +61,10 @@ def put_records(stream_name, records):
         if 'ESDestinationDescription' in dest:
             es_dest = dest['ESDestinationDescription']
             es_index = es_dest['IndexName']
-            es_type = es_dest['TypeName']
+            try:
+                es_type = es_dest['TypeName']
+            except KeyError:
+                es_type = None
             es = connect_elasticsearch()
             for record in records:
                 obj_id = uuid.uuid4()
