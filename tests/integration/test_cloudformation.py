78454a18ef818bc507318aa2b45b933ec692ef9e
Phan Duong <duongpv@outlook.com>
2020-10-06 14:56:12 +0700

    Fix issue when update stack with same template * Patch moto function * Add integration test
    Issue fixed:
    #2444 Not working for same cloud-formation "No updates are to be performed."

--- tests/integration/test_cloudformation.py
+++ tests/integration/test_cloudformation.py
@@ -825,6 +825,8 @@ TEST_UPDATE_LAMBDA_FUNCTION_TEMPLATE = {
     }
 }
 
+SQS_TEMPLATE = os.path.join(THIS_FOLDER, 'templates', 'fifo_queue.json')
+
 
 def bucket_exists(name):
     s3_client = aws_stack.connect_to_service('s3')
@@ -2048,3 +2050,26 @@ class CloudFormationTest(unittest.TestCase):
         )
 
         cloudformation.delete_stack(StackName='myteststack')
+
+    def test_update_stack_with_same_template(self):
+        stack_name = 'stack-%s' % short_uid()
+        template_data = load_file(SQS_TEMPLATE)
+        cloudformation = aws_stack.connect_to_service('cloudformation')
+
+        params = {
+            'StackName': stack_name,
+            'TemplateBody': template_data
+        }
+        cloudformation.create_stack(**params)
+
+        with self.assertRaises(Exception) as ctx:
+            cloudformation.update_stack(**params)
+            waiter = cloudformation.get_waiter('stack_update_complete')
+            waiter.wait(StackName=stack_name)
+
+        error_message = str(ctx.exception)
+        self.assertIn('UpdateStack', error_message)
+        self.assertIn('No updates are to be performed.', error_message)
+
+        # clean up
+        cloudformation.delete_stack(StackName=stack_name)
