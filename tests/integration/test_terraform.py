b91e7724ebbf73c54aa10c5b66e62d8a5052fbb2
root <root@ubunu2004.linuxvmimagrs.local>
2020-10-16 12:38:46 +0530

    minor fix

--- tests/integration/test_terraform.py
+++ tests/integration/test_terraform.py
@@ -16,11 +16,12 @@ class TestTerraform(unittest.TestCase):
         run('cd %s; terraform plan -out=tfplan -input=false' % (base_dir))
         run('cd %s; terraform apply -input=false tfplan' % (base_dir))
 
+    @classmethod
     def tearDownClass(cls):
         base_dir = os.path.join(os.path.dirname(__file__), 'terraform')
         run('cd %s; terraform destroy -auto-approve' % (base_dir))
 
     def test_bucket_exists(self):
         s3_client = aws_stack.connect_to_service('s3')
-        response = s3_client.list_buckets(Name=BUCKET_NAME)
-        self.assertEqual(response['Buckets'][0]['Name'], BUCKET_NAME)
+        response = s3_client.head_bucket(Bucket=BUCKET_NAME)
+        self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
