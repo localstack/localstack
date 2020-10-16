3278e330d0565c6e18dc5d110cebf993432f3d95
Waldemar Hummer <waldemar.hummer@gmail.com>
2020-10-17 08:19:05 +1100

    release version 0.12.0; fix import issue for Windows (#3118)

--- localstack/utils/common.py
+++ localstack/utils/common.py
@@ -1,7 +1,6 @@
 import io
 import os
 import re
-import pwd
 import grp
 import sys
 import json
@@ -602,6 +601,7 @@ def ensure_readable(file_path, default_perms=None):
 
 def chown_r(path, user):
     """ Recursive chown """
+    import pwd  # keep this import here for Windows compatibility
     uid = pwd.getpwnam(user).pw_uid
     gid = grp.getgrnam(user).gr_gid
     os.chown(path, uid, gid)
