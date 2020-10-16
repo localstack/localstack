f6d58705a525f72136bc9c2d78f93b137a50e077
macnev2013 <root@ubunu2004.linuxvmimagrs.local>
2020-10-16 23:41:17 +0530

    merge upstream to master

--- tests/integration/terraform/s3.tf
+++ tests/integration/terraform/s3.tf
@@ -1,17 +1,9 @@
 resource "aws_s3_bucket" "b" {
-<<<<<<< HEAD
-  bucket = "test-bucket"
-  acl    = "public-read"
-
-  tags = {
-    Name        = "test-bucket"
-=======
   bucket = "tf-bucket"
   acl    = "public-read"
 
   tags = {
     Name        = "tf-bucket"
->>>>>>> 65d4433e215bc383c49fcd12cdc5b09f3259b392
     Environment = "Dev"
   }
 }
--- tests/integration/test_terraform.py
+++ tests/integration/test_terraform.py
@@ -3,11 +3,7 @@ import unittest
 from localstack.utils.aws import aws_stack
 from localstack.utils.common import run
 
-<<<<<<< HEAD
-BUCKET_NAME = 'test-bucket'
-=======
 BUCKET_NAME = 'tf-bucket'
->>>>>>> 65d4433e215bc383c49fcd12cdc5b09f3259b392
 
 
 class TestTerraform(unittest.TestCase):
@@ -20,20 +16,12 @@ class TestTerraform(unittest.TestCase):
         run('cd %s; terraform plan -out=tfplan -input=false' % (base_dir))
         run('cd %s; terraform apply -input=false tfplan' % (base_dir))
 
-<<<<<<< HEAD
-=======
     @classmethod
->>>>>>> 65d4433e215bc383c49fcd12cdc5b09f3259b392
     def tearDownClass(cls):
         base_dir = os.path.join(os.path.dirname(__file__), 'terraform')
         run('cd %s; terraform destroy -auto-approve' % (base_dir))
 
     def test_bucket_exists(self):
         s3_client = aws_stack.connect_to_service('s3')
-<<<<<<< HEAD
-        response = s3_client.list_buckets(Name=BUCKET_NAME)
-        self.assertEqual(response['Buckets'][0]['Name'], BUCKET_NAME)
-=======
         response = s3_client.head_bucket(Bucket=BUCKET_NAME)
         self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
->>>>>>> 65d4433e215bc383c49fcd12cdc5b09f3259b392
