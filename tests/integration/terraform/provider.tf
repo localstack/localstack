e6674fee084d4bf4e4c0aee4a308543cd09068f8
Nevil Macwan <macnev2013@gmail.com>
2020-10-17 16:28:13 +0530

    initial support for lambda tf

--- tests/integration/terraform/provider.tf
+++ tests/integration/terraform/provider.tf
@@ -8,5 +8,7 @@ provider "aws" {
   s3_force_path_style         = true
   endpoints {
     s3 = "http://localhost:4566"
+    lambda = "http://localhost:4566"
+    iam = "http://localhost:4566"
   }
 }
