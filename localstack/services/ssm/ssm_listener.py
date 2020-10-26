539036d3d1fb3e0d67780f18d84a6bea91ad5562
usmankb <usman@krazybee.com>
2020-10-26 15:03:15 +0530

    Fix:SSM:secrets get parameter

--- localstack/services/ssm/ssm_listener.py
+++ localstack/services/ssm/ssm_listener.py
@@ -1,6 +1,7 @@
 import json
 from requests.models import Request
 from localstack.utils.common import to_str
+from localstack.utils.aws import aws_stack
 from localstack.utils.persistence import PersistingProxyListener
 
 ACTION_PUT_PARAM = 'AmazonSSM.PutParameter'
@@ -14,6 +15,18 @@ def normalize_name(param_name):
     if '/' in param_name:
         param_name = '/%s' % param_name
     return param_name
+
+
+def get_secrets_information(name, resource_name):
+    client = aws_stack.connect_to_service('secretsmanager')
+    secret_info = client.get_secret_value(SecretId=resource_name)
+    del secret_info['ResponseMetadata']
+
+    result = {'Parameter': {'SourceResult': secret_info, 'Name': name, 'Value':
+              secret_info.get('SecretString'), 'Type': 'SecureString',
+                            'LastModifiedDate': secret_info.get('CreatedDate')}}
+
+    return result
 
 
 class ProxyListenerSSM(PersistingProxyListener):
@@ -37,6 +50,16 @@ class ProxyListenerSSM(PersistingProxyListener):
             elif target in [ACTION_PUT_PARAM, ACTION_GET_PARAM]:
                 name = data.get('Name') or ''
                 data['Name'] = normalize_name(name)
+
+                if target == ACTION_GET_PARAM:
+                    details = name.split('/')
+
+                    if len(details) > 4:
+                        service = details[3]
+                        resource_name = details[4]
+
+                        if service == 'secretsmanager':
+                            return get_secrets_information(name, resource_name)
 
             data = json.dumps(data)
             if data != data_orig:
