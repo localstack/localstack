711229640da9898daa81e6ffdb0cf17d0116132b
usmangani1 <sgosman_chem@yahoo.com>
2020-11-02 03:06:58 +0530

    add test for S3 precondition check (#3169)

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -1370,6 +1370,23 @@ class S3ListenerTest(unittest.TestCase):
         client.delete_object(Bucket=BUCKET, Key=OBJECT_KEY)
         client.delete_bucket(Bucket=BUCKET)
 
+    def test_precondition_failed_error(self):
+        bucket = 'bucket-%s' % short_uid()
+        client = self._get_test_client()
+
+        client.create_bucket(Bucket=bucket)
+        client.put_object(Bucket=bucket, Key='foo', Body=b'{"foo": "bar"}')
+
+        # this line makes localstack crash:
+        try:
+            client.get_object(Bucket=bucket, Key='foo', IfMatch='"not good etag"')
+        except ClientError as e:
+            self.assertEqual(e.response['Error']['Code'], 'PreconditionFailed')
+            self.assertEqual(e.response['Error']['Message'], 'At least one of the pre-conditions you '
+                                                             'specified did not hold')
+
+        client.delete_object(Bucket=bucket, Key='foo')
+        client.delete_bucket(Bucket=bucket)
     # ---------------
     # HELPER METHODS
     # ---------------
