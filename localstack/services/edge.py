b65a130a1efff96bc3bc6068ddccb4cebe3634d3
Kanishk Godha <kanishk.godha@live.com>
2020-11-12 14:18:42 +0000

    fix

--- localstack/services/edge.py
+++ localstack/services/edge.py
@@ -223,7 +223,7 @@ def get_api_from_custom_rules(method, path, data, headers):
 
     if b'Action=AssumeRoleWithWebIdentity' in data_bytes or 'Action=AssumeRoleWithWebIdentity' in path:
         return 'sts', config.PORT_STS
-        
+
     # TODO: move S3 public URLs to a separate port/endpoint, OR check ACLs here first
     stripped = path.strip('/')
     if method in ['GET', 'HEAD'] and '/' in stripped:
