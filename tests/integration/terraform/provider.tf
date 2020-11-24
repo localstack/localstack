2bcb055fef22efc79dc3a1081da168c47c5b7d96
Nevil Macwan <macnev2013@gmail.com>
2020-11-24 15:30:39 +0000

    minor fix

--- tests/integration/terraform/provider.tf
+++ tests/integration/terraform/provider.tf
@@ -13,5 +13,6 @@ provider "aws" {
     lambda = "http://localhost:4566"
     iam    = "http://localhost:4566"
     apigateway = "http://localhost:4566"
+    route53 = "http://localhost:4566"
   }
 }
