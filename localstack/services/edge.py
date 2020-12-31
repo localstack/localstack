59180e8fc1b0ff05e67d645b39502a67157d596c
Cristopher Pinzón <cristopher.pinzon@gmail.com>
2020-12-31 13:21:46 -0500

    add gzip decoding to edge service to support zipped request payloads (#3412)

--- localstack/services/edge.py
+++ localstack/services/edge.py
@@ -34,6 +34,10 @@ HEADER_KILL_SIGNAL = 'x-localstack-kill'
 # lock obtained during boostrapping (persistence restoration) to avoid concurrency issues
 BOOTSTRAP_LOCK = threading.RLock()
 
+GZIP_ENCODING = 'GZIP'
+IDENTITY_ENCODING = 'IDENTITY'
+S3 = 's3'
+
 
 class ProxyListenerEdge(ProxyListener):
 
@@ -86,6 +90,11 @@ class ProxyListenerEdge(ProxyListener):
         headers['Host'] = host
         if isinstance(data, dict):
             data = json.dumps(data)
+
+        encoding_type = headers.get('content-encoding') or ''
+        if encoding_type.upper() == GZIP_ENCODING and api is not S3:
+            headers.set('content-encoding', IDENTITY_ENCODING)
+            data = gzip.decompress(data)
 
         lock_ctx = BOOTSTRAP_LOCK
         if persistence.API_CALLS_RESTORED or is_internal_call_context(headers):
