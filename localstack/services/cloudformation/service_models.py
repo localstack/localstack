4a86cdd819bb743ae256690af7ac11da897eab50
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-01-11 11:54:24 +0100

    minor: add missing CF resource props for DDB/SQS resources

--- localstack/services/cloudformation/service_models.py
+++ localstack/services/cloudformation/service_models.py
@@ -789,10 +789,22 @@ class DynamoDBTable(GenericBaseModel):
     def cloudformation_type():
         return 'AWS::DynamoDB::Table'
 
+    def get_physical_resource_id(self, attribute=None, **kwargs):
+        table_name = self.props.get('TableName')
+        if attribute in REF_ID_ATTRS:
+            return table_name
+        return aws_stack.dynamodb_table_arn(table_name)
+
     def fetch_state(self, stack_name, resources):
         table_name = self.props.get('TableName') or self.resource_id
         table_name = self.resolve_refs_recursively(stack_name, table_name, resources)
         return aws_stack.connect_to_service('dynamodb').describe_table(TableName=table_name)
+
+
+class QueuePolicy(GenericBaseModel):
+    @staticmethod
+    def cloudformation_type():
+        return 'AWS::SQS::QueuePolicy'
 
 
 class SSMParameter(GenericBaseModel):
