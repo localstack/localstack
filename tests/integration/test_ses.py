044849014a24c415825fb918f084b1cc5276205c
gattes <migag9@gmail.com>
2021-01-19 02:27:30 +0100

    Fixing test

--- tests/integration/test_ses.py
+++ tests/integration/test_ses.py
@@ -14,30 +14,21 @@ TEST_TEMPLATE_ATTRIBUTES = {
 class SESTest(unittest.TestCase):
 
     def test_list_templates(self):
-
         client = aws_stack.connect_to_service('ses')
         client.create_template(Template=TEST_TEMPLATE_ATTRIBUTES)
-        # Should not fail after 2 consecutive tries
-        client.list_templates()
         templ_list = client.list_templates()['TemplatesMetadata']
         self.assertEqual(1, len(templ_list))
+        created_template = templ_list[0]
+        self.assertEqual(
+            created_template['Name'], TEST_TEMPLATE_ATTRIBUTES['TemplateName'])
+        self.assertTrue(
+            type(created_template['CreatedTimestamp']) is (date, datetime))
 
-        template = templ_list[0]
-        self.assertEqual(template['TemplateName'],
+        # Should not fail after 2 consecutive tries
+        templ_list = client.list_templates()['TemplatesMetadata']
+        self.assertEqual(1, len(templ_list))
+        created_template = templ_list[0]
+        self.assertEqual(created_template['Name'],
                          TEST_TEMPLATE_ATTRIBUTES['TemplateName'])
-        self.assertEqual(template['SubjectPart'],
-                         TEST_TEMPLATE_ATTRIBUTES['SubjectPart'])
-        self.assertEqual(template['TextPart'],
-                         TEST_TEMPLATE_ATTRIBUTES['TextPart'])
-        self.assertEqual(template['HtmlPart'],
-                         TEST_TEMPLATE_ATTRIBUTES['HtmlPart'])
-
-        self.assertTrue(type(template['Timestamp']) is str)
-        self.assertFalse(type(template['Timestamp']) is (date, datetime))
-
-        # Validate Java SDKv2 Timestamp
-        try:
-            strptime_format = '%Y-%m-%dT%H:%M:%S.%fZ'
-            datetime.strptime(template['Timestamp'], strptime_format)
-        except ValueError:
-            return False
+        self.assertTrue(
+            type(created_template['CreatedTimestamp']) is (date, datetime))
