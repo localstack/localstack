18d9c8b18161de37d16b1c7b028eef7bf2f93cac
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-02-05 00:45:20 +0100

    add support for Route53 AssociateVPCWithHostedZone (#3559)

--- localstack/services/route53/route53_listener.py
+++ localstack/services/route53/route53_listener.py
@@ -1,7 +1,15 @@
 import xmltodict
-from requests.models import Response
 from six.moves.urllib.parse import urlparse
+from localstack.utils.common import short_uid, to_str, timestamp_millis
 from localstack.utils.persistence import PersistingProxyListener
+from localstack.services.generic_proxy import RegionBackend
+from localstack.utils.aws.aws_responses import requests_response
+
+
+class Route53Backend(RegionBackend):
+    def __init__(self):
+        # maps zone ID to association details
+        self.vpc_hosted_zone_associations = {}
 
 
 class ProxyListenerRoute53(PersistingProxyListener):
@@ -12,8 +20,47 @@ class ProxyListenerRoute53(PersistingProxyListener):
         parsed_url = urlparse(path)
         action = parsed_url.path.split('/')[2]
 
+        is_associate = path.endswith('/associatevpc')
+        if is_associate or path.endswith('/disassociatevpc'):
+            path_parts = path.split('/')
+            zone_id = path_parts[3]
+            req_data = xmltodict.parse(to_str(data))
+            region_details = Route53Backend.get()
+            zone_details = region_details.vpc_hosted_zone_associations.get(zone_id) or []
+            if is_associate:
+                assoc_id = short_uid()
+                zone_data = req_data.get('AssociateVPCWithHostedZoneRequest', {})
+                zone_data['Id'] = assoc_id
+                zone_data['HostedZoneId'] = zone_id
+                zone_details.append(zone_data)
+                response_entry = {
+                    'ChangeInfo': {
+                        'Id': assoc_id,
+                        'Status': 'INSYNC',
+                        'SubmittedAt': timestamp_millis()
+                    }
+                }
+            else:
+                def _match(z):
+                    return z['HostedZoneId'] == zone_id and z['VPC']['VPCId'] == zone_data['VPC']['VPCId']
+                zone_data = req_data.get('DisassociateVPCFromHostedZoneRequest', {})
+                response_entry = [z for z in zone_details if _match(z)]
+                zone_details = [z for z in zone_details if not _match(z)]
+                if not response_entry:
+                    return 404
+                response_entry = response_entry[0]
+
+            region_details.vpc_hosted_zone_associations[zone_id] = zone_details
+
+            response_tag = '%sVPCWithHostedZoneResponse' % ('Associate' if is_associate else 'Disassociate')
+            response = {
+                response_tag: response_entry
+            }
+            body = xmltodict.unparse(response)
+            response = requests_response(body)
+            return response
+
         if action == 'change':
-            response = Response()
             if method == 'GET':
                 resource_id = parsed_url.path.split('/')[-1]
                 change_response = {
@@ -21,15 +68,13 @@ class ProxyListenerRoute53(PersistingProxyListener):
                         'ChangeInfo': {
                             'Id': resource_id,
                             'Status': 'INSYNC',
-                            'SubmittedAt': '2010-09-10T01:36:41.958Z'
+                            'SubmittedAt': timestamp_millis()
                         }
                     }
                 }
-
                 body = xmltodict.unparse(change_response)
-                response._content = body
-                response.status_code = 200
-            return response
+                response = requests_response(body)
+                return response
 
         return True
 
--- tests/integration/test_route53.py
+++ tests/integration/test_route53.py
@@ -11,3 +11,25 @@ class TestRoute53(unittest.TestCase):
         self.assertEqual(201, response['ResponseMetadata']['HTTPStatusCode'])
 
         response = route53.get_change(Id='string')
+        self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
+
+    def test_associate_vpc_with_hosted_zone(self):
+        ec2 = aws_stack.connect_to_service('ec2')
+        route53 = aws_stack.connect_to_service('route53')
+
+        response = route53.create_hosted_zone(Name='zone123', CallerReference='ref123')
+        zone_id = response['HostedZone']['Id']
+
+        vpc = ec2.create_vpc(CidrBlock='10.0.0.0/16')
+        vpc_id = vpc['Vpc']['VpcId']
+        result = route53.associate_vpc_with_hosted_zone(HostedZoneId=zone_id,
+            VPC={'VPCRegion': aws_stack.get_region(), 'VPCId': vpc_id}, Comment='test 123')
+        self.assertTrue(result['ChangeInfo'].get('Id'))
+
+        result = route53.disassociate_vpc_from_hosted_zone(
+            HostedZoneId=zone_id, VPC={'VPCRegion': aws_stack.get_region(), 'VPCId': vpc_id}, Comment='test2')
+        self.assertIn(response['ResponseMetadata']['HTTPStatusCode'], [200, 201])
+        # subsequent call (after disassociation) should fail with 404 error
+        with self.assertRaises(Exception):
+            route53.disassociate_vpc_from_hosted_zone(
+                HostedZoneId=zone_id, VPC={'VPCRegion': aws_stack.get_region(), 'VPCId': vpc_id})
