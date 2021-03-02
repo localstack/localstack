8e5585ff5f5bbaa2f92167500ae4b9edd849607d
gattes <migag9@gmail.com>
2021-03-02 17:51:00 +0100

    Feat: Adding SES delete template function

--- localstack/services/ses/ses_starter.py
+++ localstack/services/ses/ses_starter.py
@@ -82,11 +82,19 @@ def apply_patches():
     email_responses.list_templates = list_templates
 
     def delete_template(self):
-        email_templates = ses_backend.list_templates()
-        for template in email_templates:
-            if template["template_name"] == template_data.get("._name", ""):
-                ses_backend.list_templates().remove(template)
-        return email_templates(self)
+        template_name = self._get_param('TemplateName')
+        templates = ses_backend.templates
+        if template_name in templates:
+            del templates[template_name]
+        ses_backend.templates = templates
+        DELETE_IDENTITY_RESPONSE = """<DeleteTemplateResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
+            <DeleteTemplateResult/>
+            <ResponseMetadata>
+              <RequestId>d96bd875-9bf2-11e1-8ee7-c98a0037a2b6</RequestId>
+            </ResponseMetadata>
+        </DeleteTemplateResponse>"""
+        template = self.response_template(DELETE_IDENTITY_RESPONSE)
+        return template.render()
 
     email_responses.delete_template = delete_template
 
