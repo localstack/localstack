3d30ed98d4225d06e05371d78d619512d0994339
Mohit Alonja <alonjamohit77@gmail.com>
2021-03-13 08:07:58 +0000

    FIX: localstack is running inside the docker config flag

--- localstack/config.py
+++ localstack/config.py
@@ -259,11 +259,12 @@ def in_docker():
             any(['docker-' in file_names for file_names in os.listdir('/sys/fs/cgroup/system.slice/')]),
         ]):
             return False
-    except Exception:
+    except FileNotFoundError:
         pass
-    os_hostname = open('/etc/hostname', 'rt').read().strip()
-        content = ifh.read()
-        if os_hostname in content or 'docker' in content:
+    with open('/proc/1/cgroup', 'rt') as ifh:
+        os_hostname = open('/etc/hostname', 'rt').read().strip()
+        lines_list = ifh.read().split('\n')
+        if any([os_hostname in element for element in lines_list]) or 'docker' in ifh.read():
             return True
     return False
 
