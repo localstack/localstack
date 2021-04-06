4c29364abc5e553ff43ba090fc8c76a295e9fdcf
gattes <migag9@gmail.com>
2021-04-06 18:42:19 +0200

    Feat: Adding AWS Support API port, bumping up version

---
+++ localstack/services/support/support_starter.py
@@ -0,0 +1,7 @@
+from moto.support import responses as support_responses
+from localstack.services.infra import start_moto_server
+
+
+def start_support(port=None, asynchronous=False):
+    port = port
+    return start_moto_server('support', port, name='Support', asynchronous=asynchronous)
