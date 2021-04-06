90765e8caa0f3187a3ceb2e54c4e6cc3558aa882
Phan Duong <duongpv@outlook.com>
2021-04-07 00:13:38 +0700

    Add Security Group Rule IPv6 range support
    Issue fixed:
    #3785 Creation of AWS Security Group Rule hangs using terraform

--- tests/integration/terraform/ec2.tf
+++ tests/integration/terraform/ec2.tf
@@ -11,3 +11,17 @@ resource "aws_security_group" "test-sg-5249" {
   description = "Test Security Group test-sg-5249"
   vpc_id      = aws_vpc.main-vpc.id
 }
+
+resource "aws_security_group_rule" "allow_redshift_connection" {
+  from_port         = 5439
+  protocol          = "TCP"
+  security_group_id = aws_security_group.test-sg-5249.id
+  to_port           = 5439
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  ipv6_cidr_blocks = [
+    "::/0"
+  ]
+  type = "ingress"
+}
