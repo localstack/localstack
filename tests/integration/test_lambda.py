a2e1202dc18b19e5dbe50c8b549a8325c404a0cb
Miguel Angel Gagliardo <miguel.gagliardo@n26.com>
2021-05-05 02:42:23 +0200

    Duplicating rs in order to get ARN and partial ARN

--- tests/integration/test_lambda.py
+++ tests/integration/test_lambda.py
@@ -133,6 +133,7 @@ class LambdaTestBase(unittest.TestCase):
         client = aws_stack.connect_to_service('lambda')
         client.create_function(**kwargs)
 
+        rs = client.get_function(FunctionName=func_name)
         function_arn = rs['Configuration'].get('FunctionArn')
         partial_function_arn = ':'.join(function_arn.split(':')[3:])
 
