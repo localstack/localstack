9715977f081d42b41d15fb966e8c6c75162de353
usmangani1 <sgosman_chem@yahoo.com>
2021-05-07 23:10:41 +0530

    Fixed review comments

--- localstack/services/resourcegroups/rg_starter.py
+++ localstack/services/resourcegroups/rg_starter.py
@@ -5,5 +5,5 @@ from localstack import config
 def start_rg(port=None, asynchronous=False, update_listener=None):
     port = port or config.PORT_RESOURCE_GROUPS
 
-    return start_moto_server('resource-groups', port, name='Resource Groups Tagging API',
+    return start_moto_server('resource-groups', port, name='Resource Groups API',
         asynchronous=asynchronous, update_listener=update_listener)
--- tests/integration/test_resourcegroups.py
+++ tests/integration/test_resourcegroups.py
@@ -4,12 +4,9 @@ from localstack.utils.aws import aws_stack
 from localstack.utils.common import short_uid
 
 
-class TestRESROUCEGROUPIntegrations(unittest.TestCase):
+class TestResourceGroups(unittest.TestCase):
     def setUp(self):
         self.resource_group_client = aws_stack.connect_to_service('resource-groups')
-
-    def cleanUp(self, name):
-        self.resource_group_client.delete_group(GroupName=name)
 
     def test_create_group(self):
         name = 'resource_group-{}'.format(short_uid())
@@ -32,78 +29,17 @@ class TestRESROUCEGROUPIntegrations(unittest.TestCase):
         self.assertEqual(response['Group']['Name'], name)
         self.assertEqual(response['ResourceQuery']['Type'], 'TAG_FILTERS_1_0')
         self.assertEqual(response['Tags']['resource_group_tag_key'], 'resource_group_tag_value')
-        self.cleanUp(name)
 
-    def test_delete_group(self):
-        name = 'resource_group-{}'.format(short_uid())
-        self.resource_group_client.create_group(
-            Name=name,
-            Description='description',
-            ResourceQuery={
-                'Type': 'TAG_FILTERS_1_0',
-                'Query': json.dumps(
-                    {
-                        'ResourceTypeFilters': ['AWS::AllSupported'],
-                        'TagFilters': [
-                            {'Key': 'resources_tag_key', 'Values': ['resources_tag_value']}
-                        ],
-                    }
-                ),
-            },
-            Tags={'resource_group_tag_key': 'resource_group_tag_value'},
-        )
+        response = self.resource_group_client.get_group(GroupName=name)
+        self.assertEqual(response['Group']['Description'], 'description')
+
+        response = self.resource_group_client.list_groups()
+        self.assertEqual(len(response['GroupIdentifiers']), 1)
+        self.assertEqual(len(response['Groups']), 1)
+
         response = self.resource_group_client.delete_group(GroupName=name)
         self.assertEqual(response['Group']['Name'], name)
 
         response = self.resource_group_client.list_groups()
         self.assertEqual(len(response['GroupIdentifiers']), 0)
         self.assertEqual(len(response['Groups']), 0)
-
-    def test_get_group(self):
-        name = 'resource_group-{}'.format(short_uid())
-        self.resource_group_client.create_group(
-            Name=name,
-            Description='description',
-            ResourceQuery={
-                'Type': 'TAG_FILTERS_1_0',
-                'Query': json.dumps(
-                    {
-                        'ResourceTypeFilters': ['AWS::AllSupported'],
-                        'TagFilters': [
-                            {'Key': 'resources_tag_key', 'Values': ['resources_tag_value']}
-                        ],
-                    }
-                ),
-            },
-            Tags={'resource_group_tag_key': 'resource_group_tag_value'},
-        )
-
-        response = self.resource_group_client.get_group(GroupName=name)
-        self.assertEqual(response['Group']['Description'], 'description')
-
-        self.cleanUp(name)
-
-    def test_list_groups(self):
-        name = 'resource_group-{}'.format(short_uid())
-        self.resource_group_client.create_group(
-            Name=name,
-            Description='description',
-            ResourceQuery={
-                'Type': 'TAG_FILTERS_1_0',
-                'Query': json.dumps(
-                    {
-                        'ResourceTypeFilters': ['AWS::AllSupported'],
-                        'TagFilters': [
-                            {'Key': 'resources_tag_key', 'Values': ['resources_tag_value']}
-                        ],
-                    }
-                ),
-            },
-            Tags={'resource_group_tag_key': 'resource_group_tag_value'},
-        )
-
-        response = self.resource_group_client.list_groups()
-        self.assertEqual(len(response['GroupIdentifiers']), 1)
-        self.assertEqual(len(response['Groups']), 1)
-
-        self.cleanUp(name)
