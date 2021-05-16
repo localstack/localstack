8e2e8d8763c29f6a7400162b1e5877004d026ae8
Nevil Macwan <macnev2013@gmail.com>
2021-05-16 16:56:05 +0000

    added test case forr presign url

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -1,5 +1,6 @@
 import io
 import os
+import shutil
 import ssl
 import boto3
 import gzip
@@ -23,8 +24,10 @@ from localstack.utils.aws import aws_stack
 from localstack.services.s3 import s3_listener, s3_utils
 
 from localstack.utils.common import (
-    short_uid, retry, get_service_protocol, to_bytes, safe_requests, to_str, new_tmp_file, rm_rf, load_file)
-from localstack.services.awslambda.lambda_utils import LAMBDA_RUNTIME_PYTHON36
+    new_tmp_dir, short_uid, retry, get_service_protocol, to_bytes, safe_requests, to_str, new_tmp_file, rm_rf,
+    load_file, run)
+from localstack.services.awslambda.lambda_api import use_docker
+from localstack.services.awslambda.lambda_utils import LAMBDA_RUNTIME_PYTHON36, LAMBDA_RUNTIME_NODEJS14X
 
 TEST_BUCKET_NAME_WITH_POLICY = 'test-bucket-policy-1'
 TEST_QUEUE_FOR_BUCKET_WITH_NOTIFICATION = 'test_queue_for_bucket_notification_1'
@@ -2093,6 +2096,37 @@ class TestS3(unittest.TestCase):
 
         # Cleanup
         self._delete_bucket(bucket, key_by_path)
+
+    def test_s3_lambda_integration(self):
+        if not use_docker():
+            return
+        temp_folder = new_tmp_dir()
+        handler_file = os.path.join(THIS_FOLDER, 'lambdas', 'lambda_s3_integration.js')
+        shutil.copy(handler_file, temp_folder)
+        run('cd %s; npm install @aws-sdk/client-s3; npm install @aws-sdk/s3-request-presigner;' % temp_folder)
+
+        function_name = 'func-integration-%s' % short_uid()
+        lambda_client = aws_stack.connect_to_service('lambda')
+        s3_client = aws_stack.connect_to_service('s3')
+
+        testutil.create_lambda_function(
+            func_name=function_name,
+            zip_file=testutil.create_zip_file(temp_folder, get_content=True),
+            runtime=LAMBDA_RUNTIME_NODEJS14X,
+            handler='lambda_s3_integration.handler'
+        )
+        s3_client.create_bucket(Bucket=function_name)
+
+        response = lambda_client.invoke(FunctionName=function_name)
+        presigned_url = response['Payload'].read()
+        presigned_url = json.loads(to_str(presigned_url))['body'].strip('"')
+
+        response = requests.put(presigned_url, verify=False)
+        response = s3_client.head_object(Bucket=function_name, Key='key.png')
+        self.assertEqual(response['ResponseMetadata']['HTTPStatusCode'], 200)
+
+        s3_client.delete_object(Bucket=function_name, Key='key.png')
+        s3_client.delete_bucket(Bucket=function_name)
 
     def test_terraform_request_sequence(self):
 
