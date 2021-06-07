b0f71ab689aba14921ec4d26eed60e37ae885bd1
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-06-07 11:55:35 +0200

    refactor API GW impl. for better compatibility with Terraform

--- localstack/services/apigateway/apigateway_starter.py
+++ localstack/services/apigateway/apigateway_starter.py
@@ -54,27 +54,12 @@ def apply_patches():
         resource = self.get_resource(function_id, resource_id)
         method = resource.get_method(method_type)
         if not method:
-            raise NoIntegrationDefined()
-
+            return
         return resource.resource_methods.pop(method_type)
-
-    def apigateway_models_resource_get_method(self, method_type):
-        method = self.resource_methods.get(method_type)
-        if not method:
-            raise NoIntegrationDefined()
-
-        return method
-
-    def apigateway_models_resource_get_integration(self, method_type):
-        resource_method = self.resource_methods.get(method_type, {})
-        if 'methodIntegration' not in resource_method:
-            raise NoIntegrationDefined()
-
-        return resource_method['methodIntegration']
 
     def apigateway_models_resource_delete_integration(self, method_type):
         if method_type in self.resource_methods:
-            return self.resource_methods[method_type].pop('methodIntegration')
+            return self.resource_methods[method_type].pop('methodIntegration', {})
 
         return {}
 
@@ -189,24 +174,33 @@ def apply_patches():
 
     def apigateway_response_integrations(self, request, *args, **kwargs):
         result = apigateway_response_integrations_orig(self, request, *args, **kwargs)
-        timeout_milliseconds = self._get_param('timeoutInMillis')
-        request_parameters = self._get_param('requestParameters') or {}
-        cache_key_parameters = self._get_param('cacheKeyParameters') or []
-        content_handling = self._get_param('contentHandling')
+
+        if self.method not in ['PUT', 'PATCH']:
+            return result
+
+        url_path_parts = self.path.split('/')
+        function_id = url_path_parts[2]
+        resource_id = url_path_parts[4]
+        method_type = url_path_parts[6]
+
+        integration = self.backend.get_integration(function_id, resource_id, method_type)
+        if not integration:
+            return result
 
         if self.method == 'PUT':
-            url_path_parts = self.path.split('/')
-            function_id = url_path_parts[2]
-            resource_id = url_path_parts[4]
-            method_type = url_path_parts[6]
+            timeout_milliseconds = self._get_param('timeoutInMillis')
+            request_parameters = self._get_param('requestParameters') or {}
+            cache_key_parameters = self._get_param('cacheKeyParameters') or []
+            content_handling = self._get_param('contentHandling')
+            integration['timeoutInMillis'] = timeout_milliseconds
+            integration['requestParameters'] = request_parameters
+            integration['cacheKeyParameters'] = cache_key_parameters
+            integration['contentHandling'] = content_handling
+            return 200, {}, json.dumps(integration)
 
-            integration_response = self.backend.get_integration(function_id, resource_id, method_type)
-
-            integration_response['timeoutInMillis'] = timeout_milliseconds
-            integration_response['requestParameters'] = request_parameters
-            integration_response['cacheKeyParameters'] = cache_key_parameters
-            integration_response['contentHandling'] = content_handling
-            return 200, {}, json.dumps(integration_response)
+        if self.method == 'PATCH':
+            patch_operations = self._get_param('patchOperations')
+            apply_json_patch_safe(integration, patch_operations, in_place=True)
 
         return result
 
@@ -249,6 +243,19 @@ def apply_patches():
 
         return result
 
+    def apigateway_models_backend_update_deployment(self, function_id, deployment_id, patch_operations):
+        rest_api = self.get_rest_api(function_id)
+        deployment = rest_api.get_deployment(deployment_id)
+        deployment = deployment or {}
+        apply_json_patch_safe(deployment, patch_operations, in_place=True)
+        return deployment
+
+    def apigateway_models_resource_get_integration(self, method_type):
+        resource_method = self.resource_methods.get(method_type, {})
+        if 'methodIntegration' not in resource_method:
+            raise NoIntegrationDefined()
+        return resource_method['methodIntegration']
+
     if not hasattr(apigateway_models.APIGatewayBackend, 'put_rest_api'):
         apigateway_response_restapis_individual_orig = APIGatewayResponse.restapis_individual
         APIGatewayResponse.restapis_individual = apigateway_response_restapis_individual
@@ -256,6 +263,9 @@ def apply_patches():
 
     if not hasattr(apigateway_models.APIGatewayBackend, 'delete_method'):
         apigateway_models.APIGatewayBackend.delete_method = apigateway_models_backend_delete_method
+
+    if not hasattr(apigateway_models.APIGatewayBackend, 'update_deployment'):
+        apigateway_models.APIGatewayBackend.update_deployment = apigateway_models_backend_update_deployment
 
     apigateway_models_RestAPI_to_dict_orig = apigateway_models.RestAPI.to_dict
 
@@ -291,11 +301,23 @@ def apply_patches():
 
         return 200, {}, rest_api
 
-    apigateway_models.Resource.get_method = apigateway_models_resource_get_method
+    def individual_deployment(self, request, full_url, headers, *args, **kwargs):
+        result = individual_deployment_orig(self, request, full_url, headers, *args, **kwargs)
+        if self.method == 'PATCH' and len(result) >= 3 and result[2] in ['null', None, str(None)]:
+            url_path_parts = self.path.split('/')
+            function_id = url_path_parts[2]
+            deployment_id = url_path_parts[4]
+            patch_operations = self._get_param('patchOperations')
+            deployment = self.backend.update_deployment(function_id, deployment_id, patch_operations)
+            return 200, {}, json.dumps(deployment)
+        return result
+
     apigateway_models.Resource.get_integration = apigateway_models_resource_get_integration
     apigateway_models.Resource.delete_integration = apigateway_models_resource_delete_integration
     apigateway_response_resource_methods_orig = APIGatewayResponse.resource_methods
     APIGatewayResponse.resource_methods = apigateway_response_resource_methods
+    individual_deployment_orig = APIGatewayResponse.individual_deployment
+    APIGatewayResponse.individual_deployment = individual_deployment
     apigateway_response_integrations_orig = APIGatewayResponse.integrations
     APIGatewayResponse.integrations = apigateway_response_integrations
     apigateway_response_integration_responses_orig = APIGatewayResponse.integration_responses
