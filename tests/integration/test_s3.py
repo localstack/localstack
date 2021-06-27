291c6cb2abc72463e3981b8111546e428a241ec3
Macwan Nevil <macnev2013@gmail.com>
2021-06-27 15:03:02 +0530

    add support for V4 S3 presigned URLs with custom headers (#4157)

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -1625,6 +1625,36 @@ class TestS3(unittest.TestCase):
         finally:
             config.S3_SKIP_SIGNATURE_VALIDATION = old_config
 
+    # Note: This test may have side effects (via `s3_client.meta.events.register(..)`) and
+    # may not be suitable for parallel execution
+    def test_presign_with_query_params(self):
+        def add_query_param(self, request, **kwargs):
+            request.url += 'requestedBy=abcDEF123'
+
+        bucket_name = short_uid()
+        s3_client = aws_stack.connect_to_service('s3')
+        s3_presign = boto3.client('s3',
+            endpoint_url=config.get_edge_url(),
+            aws_access_key_id='test',
+            aws_secret_access_key='test',
+            config=Config(signature_version='s3v4')
+        )
+
+        s3_client.create_bucket(Bucket=bucket_name)
+        s3_client.put_object(Body='test-value', Bucket=bucket_name, Key='test')
+        response = s3_client.head_object(Bucket=bucket_name, Key='test')
+        s3_client.meta.events.register('before-sign.s3.GetObject', add_query_param)
+        try:
+            presign_url = s3_presign.generate_presigned_url(
+                ClientMethod='get_object',
+                Params={'Bucket': bucket_name, 'Key': 'test'},
+                ExpiresIn=86400
+            )
+            response = requests.get(presign_url)
+            self.assertEqual(response._content, b'test-value')
+        finally:
+            s3_client.meta.events.unregister('before-sign.s3.GetObject', add_query_param)
+
     def run_presigned_url_signature_authentication(self):
 
         client = boto3.client('s3', endpoint_url=config.get_edge_url(),
