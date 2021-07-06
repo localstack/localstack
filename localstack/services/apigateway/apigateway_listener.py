64b0bde3c89979b1dddb7d72965c792501824ccd
unknown <ranjithshakrish94@gmail.com>
2021-07-06 21:00:15 +0200

    Rebase master and fix lint errors

--- localstack/services/apigateway/apigateway_listener.py
+++ localstack/services/apigateway/apigateway_listener.py
@@ -451,6 +451,7 @@ def invoke_rest_api_integration_backend(
 
             # apply custom request template
             data_str = data
+            isBase64Encoded = False
             try:
                 data_str = json.dumps(data) if isinstance(data, (dict, list)) else to_str(data)
                 data_str = apply_template(
@@ -462,7 +463,10 @@ def invoke_rest_api_integration_backend(
                     headers=headers,
                 )
             except Exception:
-                pass
+                # Temporary fix to send binary data to Lambda api
+                # To do identify all content-type which needs to base64 encoded
+                data_str = base64.b64encode(data_str)
+                isBase64Encoded = True
 
             # Sample request context:
             # https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-create-api-as-simple-proxy-for-lambda.html#api-gateway-create-api-as-simple-proxy-for-lambda-test
@@ -484,6 +488,7 @@ def invoke_rest_api_integration_backend(
                 stage,
                 api_id,
                 headers,
+                isBase64Encoded=isBase64Encoded,
                 path_params=path_params,
                 query_string_params=query_string_params,
                 method=method,
