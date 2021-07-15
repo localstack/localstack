9eb7f9880a5bf65970d47aed21203a82cd43bbd7
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2021-07-15 12:28:05 -0500

    local golang lambda execution test

---
+++ tests/integration/lambdas/golang/go.mod
@@ -0,0 +1,5 @@
+module example/handler
+
+go 1.16
+
+require github.com/aws/aws-lambda-go v1.24.0
---
+++ tests/integration/lambdas/golang/handler.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
