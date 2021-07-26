a5e90e7f709a2ab3eca089d415fc3eade5fc2cc2
Miguel Angel Gagliardo <miguel.gagliardo@n26.com>
2021-07-26 13:25:37 +0200

    Chaanging variable name

--- localstack/services/sqs/sqs_listener.py
+++ localstack/services/sqs/sqs_listener.py
@@ -302,16 +302,16 @@ class ProxyListenerSQS(PersistingProxyListener):
 
             elif action == "CreateQueue":
                 if SQS_BACKEND_IMPL == "elasticmq":
-                    values_matched = []
+                    keys_matched = []
                     for k, v in req_data.items():
                         match = re.match(r"^Tag\.(\d+)\.Key", k)
                         if match:
                             index = match.group(1)
                             tag_val = "Tag.{}.Value".format(index)
                             if tag_val not in req_data.keys():
-                                values_matched.append(tag_val)
-                    if values_matched:
-                        for tag_val in values_matched:
+                                keys_matched.append(tag_val)
+                    if keys_matched:
+                        for tag_val in keys_matched:
                             req_data[tag_val] = ""
                 changed_attrs = _fix_dlq_arn_in_attributes(req_data)
                 if changed_attrs:
