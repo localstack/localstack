fbf20c174af2cc09214042bed40e34abc7ada8f5
Thomas Rausch <thomas@thrau.at>
2021-08-26 22:53:26 +0200

    remove relative import in localstack.cli to avoid runtime warning

--- localstack/cli/__init__.py
+++ localstack/cli/__init__.py
@@ -1,12 +1,10 @@
 from .console import console
-from .localstack import create_with_plugins
 from .plugin import LocalstackCli, LocalstackCliPlugin
 
 name = "cli"
 
 __all__ = [
     "console",
-    "create_with_plugins",
     "LocalstackCli",
     "LocalstackCliPlugin",
 ]
