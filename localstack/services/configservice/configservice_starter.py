4d490dec3db8b0e4b6d106b5df9c451eb47551ce
Miguel Gagliardo <migag9@gmail.com>
2021-08-27 03:46:46 +0200

    Pointing to right moto service

--- localstack/services/configservice/configservice_starter.py
+++ localstack/services/configservice/configservice_starter.py
@@ -2,4 +2,4 @@ from localstack.services.infra import start_moto_server
 
 
 def start_configservice(port=None, asynchronous=False):
-    return start_moto_server("configservice", port, name="Config Service", asynchronous=asynchronous)
+    return start_moto_server("config", port, name="Config Service", asynchronous=asynchronous)
