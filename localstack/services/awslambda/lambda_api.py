d3e6416763fd2fe19b7ab33c42031513022b23ce
Serkan ÖZAL <serkan@thundra.io>
2021-09-02 20:46:51 +0300

    Fixed lint issue

--- localstack/services/awslambda/lambda_api.py
+++ localstack/services/awslambda/lambda_api.py
@@ -1197,7 +1197,9 @@ def forward_to_external_url(func_details, event, context, invocation_type):
     LOG.debug("Forwarding Lambda invocation to LAMBDA_FORWARD_URL: %s" % config.LAMBDA_FORWARD_URL)
     result = safe_requests.post(url, data, headers=headers)
     if result.status_code >= 400:
-        raise Exception("Received error status code %s from external Lambda invocation" % result.status_code)
+        raise Exception(
+            "Received error status code %s from external Lambda invocation" % result.status_code
+        )
     content = run_safe(lambda: to_str(result.content)) or result.content
     LOG.debug(
         "Received result from external Lambda endpoint (status %s): %s"
