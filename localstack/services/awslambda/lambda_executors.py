4a30313bb872e728d84e8fbf576aaf4bed29d07c
Cristopher Pinzón <cristopher.pinzon@gmail.com>
2021-09-15 12:26:06 -0500

    pass event details as environment variables in local execution of Golang Lambdas (#4579)

--- localstack/services/awslambda/lambda_executors.py
+++ localstack/services/awslambda/lambda_executors.py
@@ -1193,12 +1193,14 @@ class LambdaExecutorLocal(LambdaExecutor):
                 os.environ.pop(env_name, None)
 
     def execute_go_lambda(self, event, context, main_file, func_details=None):
-        event_json_string = "%s" % (json.dumps(event) if event else "{}")
-        cmd = "AWS_LAMBDA_FUNCTION_HANDLER=%s AWS_LAMBDA_EVENT_BODY='%s' %s" % (
-            main_file,
-            event_json_string,
-            GO_LAMBDA_RUNTIME,
-        )
+        cmd = GO_LAMBDA_RUNTIME
+
+        if func_details:
+            func_details.envvars["AWS_LAMBDA_FUNCTION_HANDLER"] = main_file
+            func_details.envvars["AWS_LAMBDA_EVENT_BODY"] = json.dumps(json_safe(event))
+        else:
+            LOG.warn("Function details are empty")
+
         LOG.info(cmd)
         result = self._execute_in_custom_runtime(cmd, func_details=func_details)
         return result
