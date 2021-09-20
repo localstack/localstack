c6b17bdeeeba6cb0f628354bbddc522351d1469a
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-09-20 18:47:29 +0200

    add/re-enable util function for creating simple SSL proxy servers

--- localstack/utils/server/proxy_server.py
+++ localstack/utils/server/proxy_server.py
@@ -1,9 +1,13 @@
-import asyncio
+import logging
 import select
 import socket
 
 from localstack.constants import BIND_HOST, LOCALHOST_IP
-from localstack.utils.common import is_number, run_safe, start_worker_thread
+from localstack.utils.async_utils import ensure_event_loop
+from localstack.utils.common import TMP_THREADS, is_number, run_safe, start_worker_thread
+from localstack.utils.run import FuncThread
+
+LOG = logging.getLogger(__name__)
 
 BUFFER_SIZE = 2 ** 10  # 1024
 
@@ -69,18 +73,30 @@ def start_tcp_proxy(src, dst, handler, **kwargs):
             pass
 
 
-def start_ssl_proxy(port, target, target_ssl=False):
+def start_ssl_proxy(port, target, target_ssl=False, asynchronous=False):
+    def _run(*args):
+        return _do_start_ssl_proxy(port, target, target_ssl=target_ssl)
+
+    if not asynchronous:
+        return _run()
+    proxy = FuncThread(_run)
+    TMP_THREADS.append(proxy)
+    proxy.start()
+    return proxy
+
+
+def _do_start_ssl_proxy(port, target, target_ssl=False):
     import pproxy
 
     from localstack.services.generic_proxy import GenericProxy
 
     if ":" not in str(target):
         target = "127.0.0.1:%s" % target
-    print("Starting SSL proxy server %s -> %s" % (port, target))
+    LOG.debug("Starting SSL proxy server %s -> %s" % (port, target))
 
     # create server and remote connection
     server = pproxy.Server("secure+tunnel://0.0.0.0:%s" % port)
-    target_proto = "secure+tunnel" if target_ssl else "tunnel"
+    target_proto = "ssl+tunnel" if target_ssl else "tunnel"
     remote = pproxy.Connection("%s://%s" % (target_proto, target))
     args = dict(rserver=[remote], verbose=print)
 
@@ -89,7 +105,7 @@ def start_ssl_proxy(port, target, target_ssl=False):
     for context in pproxy.server.sslcontexts:
         context.load_cert_chain(cert_file_name, key_file_name)
 
-    loop = asyncio.get_event_loop()
+    loop = ensure_event_loop()
     handler = loop.run_until_complete(server.start_server(args))
     try:
         loop.run_forever()
--- tests/unit/test_proxy.py
+++ tests/unit/test_proxy.py
@@ -1,11 +1,19 @@
 import logging
 import unittest
 
-from localstack_ext.bootstrap.local_daemon import get_free_tcp_port
+import requests
 
 from localstack import config
+from localstack.constants import LOCALHOST_HOSTNAME
+from localstack.services.generic_proxy import ProxyListener, start_proxy_server
 from localstack.services.infra import start_proxy_for_service
-from localstack.utils.common import is_port_open, poll_condition
+from localstack.utils.common import (
+    get_free_tcp_port,
+    is_port_open,
+    poll_condition,
+    wait_for_port_open,
+)
+from localstack.utils.server.proxy_server import start_ssl_proxy
 
 LOG = logging.getLogger(__name__)
 
@@ -48,3 +56,37 @@ class TestProxyServer(unittest.TestCase):
         server.join(timeout=15)
 
         self.assertFalse(is_port_open(proxy_port))
+
+
+def test_ssl_proxy_server():
+    class MyListener(ProxyListener):
+        def forward_request(self, *args, **kwargs):
+            invocations.append((args, kwargs))
+            return 200
+
+    invocations = []
+
+    # start SSL proxy
+    listener = MyListener()
+    port = get_free_tcp_port()
+    server = start_proxy_server(port, update_listener=listener, use_ssl=True)
+    wait_for_port_open(port)
+
+    # start SSL proxy
+    proxy_port = get_free_tcp_port()
+    proxy = start_ssl_proxy(proxy_port, port, asynchronous=True)
+    wait_for_port_open(proxy_port)
+
+    # invoke SSL proxy server
+    url = f"https://{LOCALHOST_HOSTNAME}:{proxy_port}"
+    num_requests = 3
+    for i in range(num_requests):
+        response = requests.get(url)
+        assert response.status_code == 200
+
+    # assert backend server has been invoked
+    assert len(invocations) == num_requests
+
+    # clean up
+    proxy.stop()
+    server.stop()
