5bbc71e54aeb01d53ee50f2c98d5fa9d590160d2
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2021-09-27 17:41:20 +0200

    Add tests for KMS grants

--- tests/integration/test_kms.py
+++ tests/integration/test_kms.py
@@ -1,28 +1,77 @@
-import unittest
+import botocore.exceptions
+import pytest
 
 from localstack import config
 from localstack.constants import TEST_AWS_ACCOUNT_ID
-from localstack.utils.aws import aws_stack
 
 
-class KMSTest(unittest.TestCase):
-    def test_create_key(self):
-        client = aws_stack.connect_to_service("kms")
+class TestKMS:
+    def test_create_key(self, kms_client):
 
-        response = client.list_keys()
-        self.assertEqual(200, response["ResponseMetadata"]["HTTPStatusCode"])
+        response = kms_client.list_keys()
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
         keys_before = response["Keys"]
 
-        response = client.create_key(
+        response = kms_client.create_key(
             Policy="policy1", Description="test key 123", KeyUsage="ENCRYPT_DECRYPT"
         )
-        self.assertEqual(200, response["ResponseMetadata"]["HTTPStatusCode"])
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
         key_id = response["KeyMetadata"]["KeyId"]
 
-        response = client.list_keys()
-        self.assertEqual(len(keys_before) + 1, len(response["Keys"]))
+        response = kms_client.list_keys()
+        assert len(response["Keys"]) == len(keys_before) + 1
 
-        response = client.describe_key(KeyId=key_id)["KeyMetadata"]
-        self.assertEqual(key_id, response["KeyId"])
-        self.assertIn(":%s:" % config.DEFAULT_REGION, response["Arn"])
-        self.assertIn(":%s:" % TEST_AWS_ACCOUNT_ID, response["Arn"])
+        response = kms_client.describe_key(KeyId=key_id)["KeyMetadata"]
+        assert response["KeyId"] == key_id
+        assert ":%s:" % config.DEFAULT_REGION in response["Arn"]
+        assert ":%s:" % TEST_AWS_ACCOUNT_ID in response["Arn"]
+
+    def test_create_grant_with_invalid_key(self, kms_client):
+        with pytest.raises(botocore.exceptions.ClientError):
+            kms_client.create_grant(
+                KeyId="invalid",
+                GranteePrincipal="arn:aws:iam::000000000000:role/test",
+                Operations=["Decrypt", "Encrypt"],
+            )
+
+    def test_list_grants_with_invalid_key(self, kms_client):
+        with pytest.raises(botocore.exceptions.ClientError):
+            kms_client.list_grants(
+                KeyId="invalid",
+            )
+
+    def test_create_grant_with_valid_key(self, kms_client, kms_key):
+        key_id = kms_key["KeyMetadata"]["KeyId"]
+
+        grants_before = kms_client.list_grants(KeyId=key_id)["Grants"]
+
+        grant = kms_client.create_grant(
+            KeyId=key_id,
+            GranteePrincipal="arn:aws:iam::000000000000:role/test",
+            Operations=["Decrypt", "Encrypt"],
+        )
+        assert "GrantId" in grant
+        assert "GrantToken" in grant
+
+        grants_after = kms_client.list_grants(KeyId=key_id)["Grants"]
+        assert len(grants_after) == len(grants_before) + 1
+
+    def test_revoke_grant(self, kms_client, kms_grant_and_key):
+        grant = kms_grant_and_key[0]
+        key_id = kms_grant_and_key[1]["KeyMetadata"]["KeyId"]
+        grants_before = kms_client.list_grants(KeyId=key_id)["Grants"]
+
+        kms_client.revoke_grant(KeyId=key_id, GrantId=grant["GrantId"])
+
+        grants_after = kms_client.list_grants(KeyId=key_id)["Grants"]
+        assert len(grants_after) == len(grants_before) - 1
+
+    def test_retire_grant(self, kms_client, kms_grant_and_key):
+        grant = kms_grant_and_key[0]
+        key_id = kms_grant_and_key[1]["KeyMetadata"]["KeyId"]
+        grants_before = kms_client.list_grants(KeyId=key_id)["Grants"]
+
+        kms_client.retire_grant(GrantToken=grant["GrantToken"])
+
+        grants_after = kms_client.list_grants(KeyId=key_id)["Grants"]
+        assert len(grants_after) == len(grants_before) - 1
