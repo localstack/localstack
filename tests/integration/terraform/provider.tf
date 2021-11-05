098f6ee3f88b748323bb10310fe00fb3945ececb
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-11-05 17:11:58 +0100

    upgrade terraform version in base image

--- tests/integration/terraform/provider.tf
+++ tests/integration/terraform/provider.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_providers {
-    # Note: pinning version for now, as 3.64.0 seems to be causing issues in our CI builds
-    version = "3.63.0"
-  }
-}
-
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "aws_mock_key"
