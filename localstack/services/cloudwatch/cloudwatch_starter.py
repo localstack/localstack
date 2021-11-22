d9c094c44a3d450531b8b59189ac40619cd4929c
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-11-22 22:52:24 +0100

    fix tests and remove obsolete patch for CloudWatch metrics filtering (#4989)

--- localstack/services/cloudwatch/cloudwatch_starter.py
+++ localstack/services/cloudwatch/cloudwatch_starter.py
@@ -1,9 +1,7 @@
-import moto.cloudwatch.models as cloudwatch_models
 import moto.cloudwatch.responses as cloudwatch_responses
 
 from localstack import config
 from localstack.services.infra import start_moto_server
-from localstack.utils.common import to_unique_items_list
 
 
 def apply_patches():
@@ -15,22 +13,6 @@ def apply_patches():
                 "</AlarmName><TreatMissingData>{{ alarm.treat_missing_data }}</TreatMissingData>",
             )
         )
-
-    def get_all_metrics(self, *args, **kwargs):
-        # Filter results to return only unique combinations of (Namespace, MetricName, Dimensions)
-        # TODO: This is hugely inefficient (!), especially as the number of metric data is growing.
-        #       Should be fixed upstream, or we should roll our own implementation!
-        def comparator(i1, i2):
-            i1 = (i1.namespace, i1.name, set((d.name, d.value) for d in i1.dimensions))
-            i2 = (i2.namespace, i2.name, set((d.name, d.value) for d in i2.dimensions))
-            return i1 == i2
-
-        result = get_all_metrics_orig(self, *args, **kwargs)
-        result = to_unique_items_list(result, comparator=comparator)
-        return result
-
-    get_all_metrics_orig = cloudwatch_models.CloudWatchBackend.get_all_metrics
-    cloudwatch_models.CloudWatchBackend.get_all_metrics = get_all_metrics
 
     # add put_composite_alarm
 
