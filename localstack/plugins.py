3ca55c361a9f8f6c60ea04ffa8f2a64dabff6b61
Thomas Rausch <thomas@thrau.at>
2021-12-15 16:50:28 +0100

    reduce loglevel of edge port configuration hook

--- localstack/plugins.py
+++ localstack/plugins.py
@@ -9,7 +9,7 @@ LOG = logging.getLogger(__name__)
 @hooks.configure_localstack_container()
 def configure_edge_port(container):
     ports = [config.EDGE_PORT, config.EDGE_PORT_HTTP]
-    LOG.info("configuring container with edge ports: %s", ports)
+    LOG.debug("configuring container with edge ports: %s", ports)
     for port in ports:
         if port:
             container.ports.add(port)
