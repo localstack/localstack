f924892e3744b1a7729baef07b6d2e2e8c0bceed
Dominik Schubert <dominik.schubert91@gmail.com>
2021-12-16 20:49:48 +0100

    fix deletion of event invoke config when lambda is already deleted

--- localstack/services/awslambda/lambda_api.py
+++ localstack/services/awslambda/lambda_api.py
@@ -2124,6 +2124,9 @@ def delete_function_event_invoke_config(function):
     region = LambdaRegion.get()
     try:
         function_arn = func_arn(function)
+        if function_arn not in region.lambdas:
+            msg = f"Function not found: {function_arn}"
+            return not_found_error(msg)
         lambda_obj = region.lambdas[function_arn]
     except Exception as e:
         return error_response(str(e), 400)
