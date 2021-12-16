a21b4c805bff002ae3a6964634b62cb08525bb98
Dominik Schubert <dominik.schubert91@gmail.com>
2021-12-16 19:34:54 +0100

    test lambda invocation from sqs/sns destination

--- tests/integration/cloudformation/test_cloudformation_integration.py
+++ tests/integration/cloudformation/test_cloudformation_integration.py
@@ -3,7 +3,7 @@ from localstack.utils.generic.wait_utils import wait_until
 from tests.integration.cloudformation.test_cloudformation_changesets import load_template_raw
 
 
-def test_s3_sns_lambda(
+def test_events_sqs_sns_lambda(
     cfn_client,
     logs_client,
     cleanup_stacks,
@@ -11,7 +11,6 @@ def test_s3_sns_lambda(
     is_change_set_created_and_available,
     is_stack_created,
     events_client,
-    sqs_client,
 ):
     stack_name = f"stack-{short_uid()}"
     change_set_name = f"change-set-{short_uid()}"
@@ -32,41 +31,47 @@ def test_s3_sns_lambda(
         describe_response = cfn_client.describe_stacks(StackName=stack_id)
         stack = describe_response["Stacks"][0]
         assert stack["StackStatus"] == "CREATE_COMPLETE"
-        assert len(stack["Outputs"]) == 6
-        topic_arn = [o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "TopicArn"][0]
-        assert topic_arn
-        assert "us-gov" in topic_arn
-        queue_url = [o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "QueueUrl"][0]
-        event_bus_name = [
-            o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "EventBusName"
-        ][0]
+        assert len(stack["Outputs"]) == 7
+        lambda_name = [o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "FnName"][0]
+        bus_name = [o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "EventBusName"][
+            0
+        ]
         events_client.put_events(
             Entries=[
                 {
-                    "DetailType": "test",
-                    "Detail": '{"something": "otherthing"}',
-                    "EventBusName": event_bus_name,
+                    "DetailType": "test-detail-type",
+                    "Detail": '{"app": "localstack"}',
+                    "EventBusName": bus_name,
                 },
             ]
         )
 
-        # queue_url = [o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "QueueUrl"][0]
-        # event_bus_name = [
-        #     o["OutputValue"] for o in stack["Outputs"] if o["OutputKey"] == "EventBusName"
-        # ][0]
-        # events_client.put_events(Entries=[
-        #     {
-        #         "Source": 'enterprise-test',
-        #         "DetailType": 'test',
-        #         "Detail": "something",
-        #         "EventBusName": event_bus_name,
-        #     },
-        # ])
+        # verifying lambdas have been called and the respective log groups/streams were created
+        def _check_lambda_invocations():
+            groups = logs_client.describe_log_groups(
+                logGroupNamePrefix=f"/aws/lambda/{lambda_name}"
+            )
+            streams = logs_client.describe_log_streams(
+                logGroupName=groups["logGroups"][0]["logGroupName"]
+            )
+            assert (
+                0 < len(streams) <= 2
+            )  # should be 1 or 2 because of the two potentially simultaneous calls
 
-        msgs = sqs_client.receive_message(QueueUrl=queue_url)
-        assert len(msgs["Messages"]) > 0
+            all_events = []
+            for s in streams["logStreams"]:
+                events = logs_client.get_log_events(
+                    logGroupName=groups["logGroups"][0]["logGroupName"],
+                    logStreamName=s["logStreamName"],
+                )["events"]
+                all_events.extend(events)
+
+            assert [e for e in all_events if "enterprise-topic" in e["message"]]
+            assert [e for e in all_events if "enterprise-queue" in e["message"]]
+            return True
+
+        wait_until(_check_lambda_invocations)
 
     finally:
-        pass
-        # cleanup_changesets([change_set_id])
-        # cleanup_stacks([stack_id])
+        cleanup_changesets([change_set_id])
+        cleanup_stacks([stack_id])
