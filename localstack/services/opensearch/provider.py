d80d4cab80ec1f72f9f5f4ea7f352da686a11edb
Silvio <s.vasiljevic@warrify.com>
2021-12-21 12:50:42 +0100

    Add dummy response to create-domain

--- localstack/services/opensearch/provider.py
+++ localstack/services/opensearch/provider.py
@@ -1,17 +1,24 @@
 from localstack.aws.api import RequestContext
 from localstack.aws.api.opensearch import (
     AdvancedOptions,
+    AdvancedSecurityOptions,
     AdvancedSecurityOptionsInput,
+    AutoTuneOptions,
     AutoTuneOptionsInput,
+    AutoTuneOptionsOutput,
+    AutoTuneState,
     ClusterConfig,
     CognitoOptions,
+    ColdStorageOptions,
     CreateDomainResponse,
     DeleteDomainResponse,
+    DeploymentStatus,
     DescribeDomainResponse,
     DescribeDomainsResponse,
     DomainEndpointOptions,
     DomainName,
     DomainNameList,
+    DomainStatus,
     EBSOptions,
     EncryptionAtRestOptions,
     EngineType,
@@ -23,10 +30,14 @@ from localstack.aws.api.opensearch import (
     NextToken,
     NodeToNodeEncryptionOptions,
     OpensearchApi,
+    OpenSearchPartitionInstanceType,
     PolicyDocument,
+    ServiceSoftwareOptions,
     SnapshotOptions,
     TagList,
+    TLSSecurityPolicy,
     VersionString,
+    VolumeType,
     VPCOptions,
 )
 
@@ -52,12 +63,61 @@ class OpensearchProvider(OpensearchApi):
         tag_list: TagList = None,
         auto_tune_options: AutoTuneOptionsInput = None,
     ) -> CreateDomainResponse:
-        return CreateDomainResponse()
+        status = DomainStatus(
+            DomainId=f"0000000000000/{domain_name}",
+            DomainName=domain_name,
+            Created=True,
+            Deleted=False,
+            Processing=True,
+            UpgradeProcessing=False,
+            EngineVersion="OpenSearch_1.0",
+            ClusterConfig=ClusterConfig(
+                InstanceType=OpenSearchPartitionInstanceType.t2_medium_search,
+                InstanceCount=1,
+                DedicatedMasterEnabled=False,
+                ZoneAwarenessEnabled=False,
+                WarmEnabled=False,
+                ColdStorageOptions=ColdStorageOptions(Enabled=False),
+            ),
+            EBSOptions=EBSOptions(EBSEnabled=True, VolumeType=VolumeType.gp2, VolumeSize=10),
+            AccessPolicies="",
+            SnapshotOptions=SnapshotOptions(AutomatedSnapshotStartHour=0),
+            CognitoOptions=CognitoOptions(Enabled=False),
+            EncryptionAtRestOptions=EncryptionAtRestOptions(Enabled=False),
+            NodeToNodeEncryptionOptions=NodeToNodeEncryptionOptions(Enabled=False),
+            AdvancedOptions={
+                "override_main_response_version": "false",
+                "rest.action.multi.allow_explicit_index": "true",
+            },
+            ServiceSoftwareOptions=ServiceSoftwareOptions(
+                CurrentVersion="",
+                NewVersion="",
+                UpdateAvailable=False,
+                Cancellable=False,
+                UpdateStatus=DeploymentStatus.COMPLETED,
+                Description="There is no software update available for this domain.",
+                AutomatedUpdateDate="0.0",
+                OptionalDeployment=True,
+            ),
+            DomainEndpointOptions=DomainEndpointOptions(
+                EnforceHTTPS=False,
+                TLSSecurityPolicy=TLSSecurityPolicy.Policy_Min_TLS_1_0_2019_07,
+                CustomEndpointEnabled=False,
+            ),
+            AdvancedSecurityOptions=AdvancedSecurityOptions(
+                Enabled=False, InternalUserDatabaseEnabled=False
+            ),
+            AutoTuneOptions=AutoTuneOptionsOutput(State=AutoTuneState.ENABLE_IN_PROGRESS),
+        )
+        return CreateDomainResponse(DomainStatus=status)
 
     def delete_domain(
         self, context: RequestContext, domain_name: DomainName
     ) -> DeleteDomainResponse:
-        return DeleteDomainResponse()
+        status = DomainStatus(
+            DomainId=f"0000000000000/{domain_name}", DomainName=domain_name, Deleted=True
+        )
+        return DeleteDomainResponse(DomainStatus=status)
 
     def describe_domain(
         self, context: RequestContext, domain_name: DomainName
