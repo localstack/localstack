a02e72ad9eec4bff9d58e87f88fbcea302558307
Daniel Fangl <daniel.fangl@localstack.cloud>
2022-02-02 20:13:41 +0100

    remove now unnecessary debug output, add debug output for installing elasticsearch plugins, persist test reports in workspace

--- localstack/services/install.py
+++ localstack/services/install.py
@@ -153,21 +153,9 @@ def get_elasticsearch_install_dir(version: str) -> str:
     if version == get_elasticsearch_install_version(
         ELASTICSEARCH_DEFAULT_VERSION
     ) and not os.path.exists(MARKER_FILE_LIGHT_VERSION):
-        LOG.debug(
-            "Chose to install to static_libs, version %s, path light exists: %s, path light: %s",
-            version,
-            os.path.exists(MARKER_FILE_LIGHT_VERSION),
-            MARKER_FILE_LIGHT_VERSION,
-        )
         # install the default version into a subfolder of the code base
         install_dir = os.path.join(dirs.static_libs, "elasticsearch")
     else:
-        LOG.debug(
-            "Chose to install to var_libs, version %s, path light exists: %s, path light: %s",
-            version,
-            os.path.exists(MARKER_FILE_LIGHT_VERSION),
-            MARKER_FILE_LIGHT_VERSION,
-        )
         # put all other versions into the TMP_FOLDER
         install_dir = os.path.join(config.dirs.var_libs, "elasticsearch", version)
 
@@ -209,7 +197,8 @@ def install_elasticsearch(version=None):
                 LOG.info("Installing Elasticsearch plugin %s", plugin)
 
                 def try_install():
-                    safe_run([plugin_binary, "install", "-b", plugin])
+                    output = safe_run([plugin_binary, "install", "-b", plugin])
+                    LOG.debug("Plugin installation output: %s", output)
 
                 # We're occasionally seeing javax.net.ssl.SSLHandshakeException -> add download retries
                 download_attempts = 3
