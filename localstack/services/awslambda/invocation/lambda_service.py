493f9cd94d7f02c2a9e7341124ad342dcd9c88fc
Daniel Fangl <daniel.fangl@localstack.cloud>
2022-02-04 10:12:47 +0100

    add some service code

--- localstack/services/awslambda/invocation/lambda_service.py
+++ localstack/services/awslambda/invocation/lambda_service.py
@@ -1,2 +1,14 @@
+from typing import Dict
+
+from localstack.services.awslambda.invocation.runtime_api import LambdaRuntimeAPI
+from localstack.services.awslambda.invocation.version_manager import LambdaVersionManager
+
+
 class LambdaService:
-    pass
+    # mapping from qualified ARN to version manager
+    lambda_version_managers: Dict[str, LambdaVersionManager]
+    lambda_runtime_api: LambdaRuntimeAPI
+
+    def __init__(self, lambda_runtime_api: LambdaRuntimeAPI):
+        self.lambda_version_managers = {}
+        self.lambda_runtime_api = lambda_runtime_api
--- localstack/services/awslambda/invocation/runtime_api.py
+++ localstack/services/awslambda/invocation/runtime_api.py
@@ -1,2 +1,46 @@
-class RuntimeAPI:
-    pass
+import logging
+
+from flask import Flask
+
+from localstack.utils.serving import Server
+
+RUNTIME_API_PREFIX = "/2018-06-01/runtime"
+LOG = logging.getLogger(__name__)
+
+runtime_api = Flask("runtime_api")
+
+
+class LambdaRuntimeAPI:
+    @runtime_api.route(f"{RUNTIME_API_PREFIX}/invocation/next", methods=["GET"])
+    def next_invocation(self):
+        return 200, {}
+
+    @runtime_api.route(f"{RUNTIME_API_PREFIX}/invocation/<req_id>/response", methods=["POST"])
+    def invocation_response(self, req_id: str):
+        return 202, ""
+
+    @runtime_api.route(f"{RUNTIME_API_PREFIX}/init/error", methods=["POST"])
+    def init_error(self):
+        return {"invocation": "init_error"}
+
+    @runtime_api.route(
+        f"{RUNTIME_API_PREFIX}/invocation/<req_id>/error",
+        methods=["POST"],
+    )
+    def invocation_error(self, req_id: str):
+        return {"invocation": f"invocation_error to invocation {req_id}"}
+
+
+class RuntimeAPIServer(Server):
+    def __init__(self, port: int, host: str = "0.0.0.0"):
+        super().__init__(port, host)
+
+    def do_run(self):
+        runtime_api.run(self.host, self.port)
+
+    def do_shutdown(self):
+        self._thread.stop()
+
+    def cleanup(self):
+        if not self.is_running():
+            return
