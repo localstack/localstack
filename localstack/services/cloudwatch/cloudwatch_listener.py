5296d253e14d7d7186c8a05ce54d1eda1cdccaf2
Thomas Rausch <thomas@thrau.at>
2022-02-09 23:03:35 +0100

    remove superfluous cloudwatch response processing

--- localstack/services/cloudwatch/cloudwatch_listener.py
+++ localstack/services/cloudwatch/cloudwatch_listener.py
@@ -2,7 +2,7 @@ from moto.cloudwatch.models import cloudwatch_backends
 
 from localstack.services.generic_proxy import ProxyListener
 from localstack.utils.aws import aws_responses, aws_stack
-from localstack.utils.common import parse_request_data, replace_response_content
+from localstack.utils.common import parse_request_data
 from localstack.utils.tagging import TaggingService
 
 XMLNS_CLOUDWATCH = ""
@@ -45,30 +45,6 @@ class ProxyListenerCloudWatch(ProxyListener):
             arn = aws_stack.cloudwatch_alarm_arn(name)
             tags = aws_responses.extract_tags(req_data)
             TAGS.tag_resource(arn, tags)
-
-        # Fix Incorrect date format to the correct format
-        # the dictionary contains the tag as the key and the value is a
-        # tuple (pattern, replacement)
-
-        regexes1 = (r"<{}>([^<]+) ([^<+]+)(\+[^<]*)?</{}>", r"<{}>\1T\2Z</{}>")
-        regexes2 = (r"<{}>([^<]+) ([^<+.]+)(\.[^<]*)?</{}>", r"<{}>\1T\2Z</{}>")
-        timestamp_tags = {
-            "AlarmConfigurationUpdatedTimestamp": regexes1,
-            "StateUpdatedTimestamp": regexes1,
-            "member": regexes2,
-        }
-
-        for tag, value in timestamp_tags.items():
-            pattern, replacement = value
-            self.fix_date_format(response, tag, pattern, replacement)
-        response.headers["Content-Length"] = len(response.content)
-        return response
-
-    def fix_date_format(self, response, timestamp_tag, pattern, replacement):
-        """Normalize date to correct format"""
-        pattern = pattern.format(timestamp_tag, timestamp_tag)
-        replacement = replacement.format(timestamp_tag, timestamp_tag)
-        replace_response_content(response, pattern, replacement)
 
 
 # instantiate listener
