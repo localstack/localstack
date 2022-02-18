c4435220b5d74a1581b0b6e0015af3d2ef042bdd
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-02-18 19:08:38 +0100

    fix loopback address assertion

--- tests/unit/utils/test_net_utils.py
+++ tests/unit/utils/test_net_utils.py
@@ -4,6 +4,6 @@ from localstack.utils.net_utils import resolve_hostname
 
 
 def test_resolve_hostname():
-    assert "127.0." in resolve_hostname(LOCALHOST)
+    assert "127." in resolve_hostname(LOCALHOST)
     assert resolve_hostname("example.com")
     assert resolve_hostname(f"non-existing-host-{short_uid()}") is None
