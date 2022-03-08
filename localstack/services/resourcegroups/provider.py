725c28be038df32a6d2d29ff3c20d8a12fda0237
Giovanni Grano <me@giograno.com>
2022-03-08 19:00:52 +0100

    PR nit

--- localstack/services/resourcegroups/provider.py
+++ localstack/services/resourcegroups/provider.py
@@ -1,7 +1,5 @@
-from abc import ABC
-
 from localstack.aws.api.resource_groups import ResourceGroupsApi
 
 
-class ResourceGroupsProvider(ResourceGroupsApi, ABC):
+class ResourceGroupsProvider(ResourceGroupsApi):
     pass
