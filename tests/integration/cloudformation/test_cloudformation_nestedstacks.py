1806a9cd1ee0261ec07b10ca990fa99543c34c54
Dominik Schubert <dominik.schubert91@gmail.com>
2022-03-09 07:58:21 +0100

    cleanup bucket in test

--- tests/integration/cloudformation/test_cloudformation_nestedstacks.py
+++ tests/integration/cloudformation/test_cloudformation_nestedstacks.py
@@ -3,24 +3,23 @@ import os
 from localstack.utils.strings import short_uid
 
 
-def test_nested_stack_output_refs(cfn_client, deploy_cfn_template, s3_client):
+def test_nested_stack_output_refs(cfn_client, deploy_cfn_template, s3_client, s3_create_bucket):
     """test output handling of nested stacks incl. referencing the nested output in the parent stack"""
-    bucket = f"test-bucket-{short_uid()}"
-    nested_bucket = f"test-bucket-nested-{short_uid()}"
+    bucket_name = s3_create_bucket()
+    nested_bucket_name = f"test-bucket-nested-{short_uid()}"
     key = f"test-key-{short_uid()}"
-    create_response = s3_client.create_bucket(Bucket=bucket)
     s3_client.upload_file(
         os.path.join(
             os.path.dirname(__file__), "../templates/nested-stack-output-refs.nested.yaml"
         ),
-        Bucket=bucket,
+        Bucket=bucket_name,
         Key=key,
     )
     result = deploy_cfn_template(
         template_file_name="nested-stack-output-refs.yaml",
         template_mapping={
-            "s3_bucket_url": f"{create_response['Location']}/{key}",
-            "nested_bucket_name": nested_bucket,
+            "s3_bucket_url": f"/{bucket_name}/{key}",
+            "nested_bucket_name": nested_bucket_name,
         },
     )
 
@@ -29,9 +28,9 @@ def test_nested_stack_output_refs(cfn_client, deploy_cfn_template, s3_client):
     nested_stack_outputs = nested_stack_details["Stacks"][0]["Outputs"]
     assert "InnerCustomOutput" not in result.outputs
     assert (
-        nested_bucket
+        nested_bucket_name
         == [
             o["OutputValue"] for o in nested_stack_outputs if o["OutputKey"] == "InnerCustomOutput"
         ][0]
     )
-    assert f"{nested_bucket}-suffix" == result.outputs["CustomOutput"]
+    assert f"{nested_bucket_name}-suffix" == result.outputs["CustomOutput"]
