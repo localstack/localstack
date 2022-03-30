96f1cbac813bab1928730622da73371102b30110
Viren Nadkarni <viren.nadkarni@localstack.cloud>
2022-03-30 15:49:43 +0530

    Merge helper functionality

--- localstack/services/ses/provider.py
+++ localstack/services/ses/provider.py
@@ -49,32 +49,30 @@ class SesProvider(SesApi, ABC):
     # Helpers
     #
 
-    def log_email_to_data_dir(self, id: str, email: dict):
-        """Save a copy of the sent email to filesystem for debugging purposes."""
+    def save_email_to_data_dir(self, id: str, region: str, **kwargs: Dict[str, Any]):
+        """Save a copy of a sent email to the filesystem for debugging purposes.
+
+        kwargs should consist of following keys related to the email:
+        - Body
+        - Subject
+        - Body
+        - RawData
+        - Source
+        - Template
+        - TemplateData
+        - Destinations
+        """
         ses_dir = os.path.join(config.dirs.data or config.dirs.tmp, "ses")
 
         mkdir(ses_dir)
         path = os.path.join(ses_dir, id + ".json")
 
+        email = {"Id": id, "Region": region, **kwargs}
+
         with open(path, "w") as f:
             f.write(json.dumps(email))
 
         LOGGER.debug(f"Email saved at: {path}")
-
-    def get_email_log_object(
-        self, source, region, destinations, subject=None, body=None, raw_data=None
-    ) -> Dict[str, Any]:
-        """Build a structured object representing an email."""
-        email = {"Source": source, "Region": region, "Destinations": destinations}
-
-        if subject is not None:
-            email["Subject"] = subject
-        if body is not None:
-            email["Body"] = body
-        if raw_data is not None:
-            email["RawData"] = raw_data
-
-        return email
 
     def get_source_from_raw(self, raw_data: str) -> Optional[str]:
         """Given a raw representation of email, return the source/from field."""
@@ -136,10 +134,14 @@ class SesProvider(SesApi, ABC):
     ) -> SendEmailResponse:
         response = call_moto(context)
 
-        log_obj = self.get_email_log_object(
-            source, context.region, destination, subject=message["Subject"], body=message["Body"]
+        self.save_email_to_data_dir(
+            response["MessageId"],
+            context.region,
+            Source=source,
+            Destinations=destination,
+            Subject=message["Subject"],
+            Body=message["Body"],
         )
-        self.log_email_to_data_dir(response["MessageId"], log_obj)
 
         return response
 
@@ -163,15 +165,13 @@ class SesProvider(SesApi, ABC):
             source, [template], template_data, destination, context.region
         )
 
-        self.log_email_to_data_dir(
+        self.save_email_to_data_dir(
             message.id,
-            {
-                "Source": source,
-                "Template": template,
-                "TemplateData": template_data,
-                "Destinations": destination,
-                "Region": context.region,
-            },
+            context.region,
+            Source=source,
+            Template=template,
+            TemplateData=template_data,
+            Destinations=destination,
         )
 
         return SendTemplatedEmailResponse(MessageId=message.id)
@@ -201,7 +201,8 @@ class SesProvider(SesApi, ABC):
 
         message = ses_backend.send_raw_email(source, destinations, raw_data, context.region)
 
-        log_obj = self.get_email_log_object(source, context.region, destinations, raw_data=raw_data)
-        self.log_email_to_data_dir(message.id, log_obj)
+        self.save_email_to_data_dir(
+            message.id, context.region, Source=source, Destinations=destinations, RawData=raw_data
+        )
 
         return SendRawEmailResponse(MessageId=message.id)
