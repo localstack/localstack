6762a1f4f765219b16106d367d48955201a5e5af
Thomas Rausch <thomas@thrau.at>
2022-04-30 04:12:00 +0200

    migrate cloudformation to ASF

--- localstack/services/cloudformation/service_models.py
+++ localstack/services/cloudformation/service_models.py
@@ -187,7 +187,7 @@ class GenericBaseModel:
     @classmethod
     def resolve_refs_recursively(cls, stack_name, value, resources):
         # TODO: restructure code to avoid circular import here
-        from localstack.services.cloudformation.cloudformation_api import find_stack
+        from localstack.services.cloudformation.provider import find_stack
         from localstack.utils.cloudformation.template_deployer import resolve_refs_recursively
 
         stack = find_stack(stack_name)
