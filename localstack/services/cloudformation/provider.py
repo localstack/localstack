482713b9f729b186094bae84aab94745d0255d4e
Dominik Schubert <dominik.schubert91@gmail.com>
2022-05-02 15:16:19 +0200

    fix missing sender fault for ValidationError

--- localstack/services/cloudformation/provider.py
+++ localstack/services/cloudformation/provider.py
@@ -529,7 +529,7 @@ class ValidationError(CommonServiceException):
     """General validation error type (defined in the AWS docs, but not part of the botocore spec)"""
 
     def __init__(self, message=None):
-        super().__init__("ValidationError", message=message)
+        super().__init__("ValidationError", message=message, sender_fault=True)
 
 
 class ResourceNotFoundException(CommonServiceException):
