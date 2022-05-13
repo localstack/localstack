06788ae0cc21f5abbfb05bf8412951bc7ed793a3
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-05-13 20:10:51 +0200

    move requests file to proper test data folder

--- tests/integration/cloudformation/test_cloudformation_cdk.py
+++ tests/integration/cloudformation/test_cloudformation_cdk.py
@@ -6,8 +6,8 @@ import requests
 
 from localstack import config
 from localstack.utils.aws import aws_stack
+from localstack.utils.files import load_file
 from localstack.utils.sync import wait_until
-from tests.integration.cloudformation.utils import load_template_raw
 
 
 class TestCdkInit:
@@ -35,7 +35,9 @@ class TestCdkInit:
     ):
         """Test that simulates a sequence of commands executed by CDK when running 'cdk bootstrap' twice"""
 
-        operations = json.loads(load_template_raw("cdk_bootstrap_sequence.json"))
+        base_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+        requests_file = os.path.join(base_folder, "files", "cdk-bootstrap-requests.json")
+        operations = json.loads(load_file(requests_file))
 
         change_set_name = "cdk-deploy-change-set-a4b98b18"
         stack_name = "CDKToolkit-a4b98b18"
