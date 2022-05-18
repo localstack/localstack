ad7c937f609a0480348db8149a9d4d8fff859547
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-05-18 09:31:47 +0200

    fix ARN rewriting for incoming requests, fix lambda arn creation

--- tests/unit/test_generic_proxy.py
+++ tests/unit/test_generic_proxy.py
@@ -3,7 +3,9 @@ import json
 import pytest
 from requests.models import Response
 
+from localstack.constants import INTERNAL_AWS_ACCESS_KEY_ID
 from localstack.services.generic_proxy import ArnPartitionRewriteListener
+from localstack.utils.aws.aws_stack import mock_aws_request_headers
 from localstack.utils.common import to_bytes, to_str
 
 # Define the callables used to convert the payload to the appropriate encoding for the tests
@@ -28,9 +30,10 @@ def test_no_arn_partition_rewriting_in_request(encoding):
     assert result.headers == {"some-header-without-arn": "nothing to see here"}
 
 
+@pytest.mark.parametrize("internal_call", [True, False])
 @pytest.mark.parametrize("encoding", [byte_encoding, string_encoding])
 @pytest.mark.parametrize("origin_partition", ["aws", "aws-us-gov"])
-def test_arn_partition_rewriting_in_request(encoding, origin_partition):
+def test_arn_partition_rewriting_in_request(internal_call, encoding, origin_partition):
     listener = ArnPartitionRewriteListener()
     data = encoding(
         json.dumps(
@@ -39,9 +42,20 @@ def test_arn_partition_rewriting_in_request(encoding, origin_partition):
             }
         )
     )
-    headers = {
-        "some-header-with-arn": f"arn:{origin_partition}:apigateway:us-gov-west-1::/restapis/arn-in-header/*"
-    }
+
+    # if this test is parameterized to be an internal call, set the internal auth
+    # incoming requests should be rewritten for both, internal and external requests (in contrast to the responses!)
+    if internal_call:
+        headers = mock_aws_request_headers(
+            region_name=origin_partition, access_key=INTERNAL_AWS_ACCESS_KEY_ID
+        )
+    else:
+        headers = {}
+
+    headers[
+        "some-header-with-arn"
+    ] = f"arn:{origin_partition}:apigateway:us-gov-west-1::/restapis/arn-in-header/*"
+
     result = listener.forward_request(
         method="POST",
         path=f"/?arn=arn%3A{origin_partition}%3Aapigateway%3Aus-gov-west-1%3A%3A%2Frestapis%2Farn-in-path%2F%2A&"
@@ -60,9 +74,10 @@ def test_arn_partition_rewriting_in_request(encoding, origin_partition):
             {"some-data-with-arn": "arn:aws:apigateway:us-gov-west-1::/restapis/arn-in-body/*"}
         )
     )
-    assert result.headers == {
-        "some-header-with-arn": "arn:aws:apigateway:us-gov-west-1::/restapis/arn-in-header/*"
-    }
+    assert (
+        result.headers["some-header-with-arn"]
+        == "arn:aws:apigateway:us-gov-west-1::/restapis/arn-in-header/*"
+    )
 
 
 @pytest.mark.parametrize("encoding", [byte_encoding, string_encoding])
@@ -122,6 +137,32 @@ def test_arn_partition_rewriting_in_response(encoding):
             }
         )
     )
+
+
+def test_no_arn_partition_rewriting_in_internal_response():
+    """Partitions should not be rewritten for _responses_ of _internal_ requests."""
+    listener = ArnPartitionRewriteListener()
+    response = Response()
+    body_content = json.dumps(
+        {"some-data-with-arn": "arn:aws:apigateway:us-gov-west-1::/restapis/arn-in-body/*"}
+    )
+    response._content = body_content
+    response._status_code = 200
+    response_header_content = {
+        "some-header-with-arn": "arn:aws:apigateway:us-gov-west-1::/restapis/arn-in-header/*"
+    }
+    response.headers = response_header_content
+
+    # mimic an internal request
+    request_headers = mock_aws_request_headers(
+        region_name="us-gov-west-1", access_key=INTERNAL_AWS_ACCESS_KEY_ID
+    )
+
+    result = listener.return_response(
+        method="POST", path="/", data="ignored", headers=request_headers, response=response
+    )
+
+    assert result is None
 
 
 @pytest.mark.parametrize("encoding", [byte_encoding, string_encoding])
