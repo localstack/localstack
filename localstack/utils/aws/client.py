26256d5aea056fc9cf9c03851396694ca8ad454d
Thomas Rausch <thomas@thrau.at>
2022-05-18 20:50:38 +0200

    add ASF prototype for server-side parameter validation

--- localstack/utils/aws/client.py
+++ localstack/utils/aws/client.py
@@ -17,13 +17,28 @@ class SigningHttpClient:
         &X-Amz-Expires=3600
         &X-Amz-SignedHeaders=host
         &X-Amz-Signature=ae39eb839d0501d731d5dccffd1e6e86fab53749f956caabbb8211b6593f5f9d
+
+    You can also create a client with an endpoint_url set, where you can then make requests with the hostname part.
+    For example, to create a raw GetQueueUrl request, run:
+
+       client = SigningHttpClient(signer, endpoint_url="https://sqs.us-east-1.amazonaws.com")
+       client.post("/", params={"Action", "GetQueueUrl", "QueueName": "my-queue"})
     """
 
-    def __init__(self, signer: BaseSigner, session: requests.Session = None):
+    def __init__(
+        self, signer: BaseSigner, session: requests.Session = None, endpoint_url: str = None
+    ):
         self.signer = signer
         self.session = session or requests.Session()
+        self.endpoint_url = endpoint_url
 
     def request(self, method, url, **kwargs) -> requests.Response:
+        if url.startswith("/"):
+            if not self.endpoint_url:
+                raise ValueError("no hostname provided in url and no endpoint_url set")
+
+            url = self.endpoint_url.rstrip("/") + url
+
         request = AWSRequest(
             method=method,
             url=url,
