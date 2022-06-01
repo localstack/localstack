8075321ea639c6899983e2dfd564c29ea0965589
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-06-01 12:15:08 +0200

    fix API gateway ASF invocation without remote address

--- localstack/services/apigateway/provider_asf.py
+++ localstack/services/apigateway/provider_asf.py
@@ -47,10 +47,11 @@ def to_invocation_context(
     if url_params is None:
         url_params = {}
 
-    if x_forwarded_for := headers.get("X-Forwarded-For"):
-        headers["X-Forwarded-For"] = x_forwarded_for + ", " + request.remote_addr
-    else:
-        headers["X-Forwarded-For"] = request.remote_addr + ", " + request.host
+    if request.remote_addr:
+        if x_forwarded_for := headers.get("X-Forwarded-For"):
+            headers["X-Forwarded-For"] = x_forwarded_for + ", " + request.remote_addr
+        else:
+            headers["X-Forwarded-For"] = request.remote_addr + ", " + request.host
 
     # this is for compatibility with the lower layers of apigw and lambda that make assumptions about header casing
     headers = CaseInsensitiveDict(
