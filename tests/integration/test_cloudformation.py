bf002bde45418511a880979beba631b51752f2f5
Giovanni Grano <me@giograno.com>
2022-06-01 10:12:41 -0400

    Semplifying the test

--- tests/integration/test_cloudformation.py
+++ tests/integration/test_cloudformation.py
@@ -998,17 +998,16 @@ class TestCloudFormation:
         assert rule["Name"] not in [r["Name"] for r in rs["Rules"]]
 
     @pytest.mark.parametrize(
-        "create_bucket_first, patch_region",
-        [(True, "eu-west-1"), (False, "us-east-1")],
-        indirect=["patch_region"],
+        "create_bucket_first, region", [(True, "eu-west-1"), (False, "us-east-1")]
     )
     def test_cfn_handle_s3_notification_configuration(
         self,
-        patch_region,
-        s3_client,
+        region,
+        create_boto_client,
         deploy_cfn_template,
         create_bucket_first,
     ):
+        s3_client = create_boto_client("s3", region_name=region)
         bucket_name = f"target-{short_uid()}"
         queue_name = f"queue-{short_uid()}"
         queue_arn = aws_stack.sqs_queue_arn(queue_name, region_name=s3_client.meta.region_name)
