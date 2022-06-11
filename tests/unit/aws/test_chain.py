57a36323996e49ae8f363d00527e5d9bcb292456
Thomas Rausch <thomas@thrau.at>
2022-06-11 15:07:43 +0200

    add pluggable response handler for asf handler chain (#6251)

--- tests/unit/aws/test_chain.py
+++ tests/unit/aws/test_chain.py
@@ -32,6 +32,58 @@ class TestCompositeHandler:
         inner2.assert_not_called()
         response1.assert_called_once()
 
+    def test_composite_handler_terminates_handler_chain(self):
+        def inner1(_chain: HandlerChain, request: RequestContext, response: Response):
+            _chain.terminate()
+
+        inner2 = mock.MagicMock()
+        outer1 = mock.MagicMock()
+        outer2 = mock.MagicMock()
+        response1 = mock.MagicMock()
+
+        chain = HandlerChain()
+
+        composite = CompositeHandler()
+        composite.handlers.append(inner1)
+        composite.handlers.append(inner2)
+
+        chain.request_handlers.append(outer1)
+        chain.request_handlers.append(composite)
+        chain.request_handlers.append(outer2)
+        chain.response_handlers.append(response1)
+
+        chain.handle(RequestContext(), Response())
+        outer1.assert_called_once()
+        outer2.assert_not_called()
+        inner2.assert_not_called()
+        response1.assert_not_called()
+
+    def test_composite_handler_with_not_return_on_stop(self):
+        def inner1(_chain: HandlerChain, request: RequestContext, response: Response):
+            _chain.stop()
+
+        inner2 = mock.MagicMock()
+        outer1 = mock.MagicMock()
+        outer2 = mock.MagicMock()
+        response1 = mock.MagicMock()
+
+        chain = HandlerChain()
+
+        composite = CompositeHandler(return_on_stop=False)
+        composite.handlers.append(inner1)
+        composite.handlers.append(inner2)
+
+        chain.request_handlers.append(outer1)
+        chain.request_handlers.append(composite)
+        chain.request_handlers.append(outer2)
+        chain.response_handlers.append(response1)
+
+        chain.handle(RequestContext(), Response())
+        outer1.assert_called_once()
+        outer2.assert_not_called()
+        inner2.assert_called_once()
+        response1.assert_called_once()
+
     def test_composite_handler_continues_handler_chain(self):
         inner1 = mock.MagicMock()
         inner2 = mock.MagicMock()
