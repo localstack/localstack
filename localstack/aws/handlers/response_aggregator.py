b9937bcc432547918491e50fd73888bbf9de31f4
sam <samuelpwatson@gmail.com>
2022-06-15 11:04:36 -0600

    dont send http response agg if analytics collection is disabled. refactor to separate handler and aggregator logic.

--- localstack/aws/handlers/response_aggregator.py
+++ localstack/aws/handlers/response_aggregator.py
@@ -1,56 +1,17 @@
-import atexit
-import datetime
-import logging
-import threading
-from collections import Counter, namedtuple
-from typing import Any, Dict
-
+from localstack import config
 from localstack.aws.api import RequestContext
 from localstack.aws.chain import HandlerChain
 from localstack.http import Response
-from localstack.utils import analytics
-from localstack.utils.scheduler import Scheduler
-
-LOG = logging.getLogger(__name__)
-FLUSH_INTERVAL_SECS = 10
+from localstack.utils.analytics.response_aggregator import ResponseAggregator
 
 
-ResponseInfo = namedtuple("ResponseInfo", "service, operation, status_code")
-
-
-class ResponseAggregator:
+class ResponseAggregatorHandler:
     def __init__(self):
-        self.response_counter = Counter()
-        self.period_start_time = datetime.datetime.utcnow()
-        self.flush_scheduler = Scheduler()
-        self.scheduler_thread = threading.Thread(target=self.flush_scheduler.run)
-        self.scheduler_thread.start()
-        self.flush_scheduler.schedule(func=self.flush, period=FLUSH_INTERVAL_SECS, fixed_rate=True)
-        atexit.register(self.flush)
+        self.aggregator = ResponseAggregator()
 
     def __call__(self, chain: HandlerChain, context: RequestContext, response: Response):
-        if response is None:
+        if response is None or config.DISABLE_EVENTS:
             return
-
-        response_info = ResponseInfo(
-            service=context.service.service_name,
-            operation=context.operation.name,
-            status_code=response.status_code,
+        self.aggregator.add_response(
+            context.service.service_name, context.operation.name, response.status_code
         )
-        self.response_counter[response_info] += 1
-
-    def _get_analytics_payload(self) -> Dict[str, Any]:
-        return {
-            "period_start_time": self.period_start_time.isoformat() + "Z",
-            "period_end_time": datetime.datetime.utcnow().isoformat() + "Z",
-            "http_response_aggregations": [
-                {**resp._asdict(), "count": count} for resp, count in self.response_counter.items()
-            ],
-        }
-
-    def flush(self):
-        if len(self.response_counter) > 0:
-            analytics_payload = self._get_analytics_payload()
-            analytics.log.event("http_response_agg", analytics_payload)
-            self.response_counter.clear()
-        self.period_start_time = datetime.datetime.utcnow()
---
+++ localstack/utils/analytics/response_aggregator.py
@@ -0,0 +1,65 @@
+import atexit
+import datetime
+import logging
+import threading
+from collections import Counter, namedtuple
+from typing import Any, Dict
+
+from localstack import config
+from localstack.utils import analytics
+from localstack.utils.scheduler import Scheduler
+
+LOG = logging.getLogger(__name__)
+FLUSH_INTERVAL_SECS = 10
+
+
+ResponseInfo = namedtuple("ResponseInfo", "service, operation, status_code")
+
+
+class ResponseAggregator:
+    """
+    Collects HTTP response data, aggregates it into small batches, and periodically emits (flushes) it as an analytics event
+    """
+
+    def __init__(self):
+        self.response_counter = Counter()
+        self.period_start_time = datetime.datetime.utcnow()
+        self.flush_scheduler = Scheduler()
+        self.scheduler_thread = threading.Thread(target=self.flush_scheduler.run)
+        self.scheduler_thread.start()
+        self.flush_scheduler.schedule(func=self._flush, period=FLUSH_INTERVAL_SECS, fixed_rate=True)
+        atexit.register(self._flush)
+
+    def add_response(self, service_name: str, operation_name: str, response_code: int):
+        """
+        Add an HTTP response for aggregation and collection
+        """
+        if config.DISABLE_EVENTS:
+            return
+
+        response_info = ResponseInfo(
+            service=service_name,
+            operation=operation_name,
+            status_code=response_code,
+        )
+        self.response_counter[response_info] += 1
+
+    def _get_analytics_payload(self) -> Dict[str, Any]:
+        return {
+            "period_start_time": self.period_start_time.isoformat() + "Z",
+            "period_end_time": datetime.datetime.utcnow().isoformat() + "Z",
+            "http_response_aggregations": [
+                {**resp._asdict(), "count": count} for resp, count in self.response_counter.items()
+            ],
+        }
+
+    def _flush(self):
+        """
+        Flushes the current batch of HTTP response data as an analytics event.
+        This happens automatically in the background.
+        """
+        if len(self.response_counter) > 0:
+            analytics_payload = self._get_analytics_payload()
+            analytics.log.event("http_response_agg", analytics_payload)
+            self.response_counter.clear()
+        self.period_start_time = datetime.datetime.utcnow()
