4a8dddbd415e9e6f3727ea1bf4d8e75e824f1894
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-06-21 13:01:23 +0200

    add simple hashable dict util class

--- tests/unit/utils/test_collections.py
+++ tests/unit/utils/test_collections.py
@@ -1,6 +1,6 @@
 from typing import Optional, TypedDict
 
-from localstack.utils.collections import select_from_typed_dict
+from localstack.utils.collections import HashableJsonDict, HashableList, select_from_typed_dict
 
 
 class MyTypeDict(TypedDict):
@@ -18,3 +18,29 @@ def test_select_from_typed_dict():
     del d["key_one"]
     result = select_from_typed_dict(typed_dict=MyTypeDict, obj=d)
     assert result == {"key_optional": "key_optional"}
+
+
+def test_hashable_dict():
+    d1 = HashableJsonDict({"a": ["b"], "c": 1})
+    d2 = HashableJsonDict({"a": "b"})
+    d3 = HashableJsonDict({"c": 1, "a": ["b"]})
+    d4 = HashableJsonDict({})
+
+    assert len({d1, d2, d3}) == 2
+    assert {d1, d2, d3} == {d1, d2} == {d2, d3}
+    assert {d1, d3} == {d3, d1}
+    assert {d1, d1} == {d3, d3}
+    assert {d1, d2, d3} != {d1}
+    assert {d1, d2, d3} != {d2}
+    assert {d1, d2, d3} != {d1, d3}
+    assert {d4, d4} == {d4}
+
+
+def test_hashable_list():
+    l1 = HashableList([1, 2])
+    l2 = HashableList([1, 2])
+    l3 = HashableList([1, 2, 3])
+
+    assert {l1, l2} == {l1} == {l2, l2}
+    assert {l1, l3} == {l2, l3}
+    assert {l1, l2} != {l3}
