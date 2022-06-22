02399456b16c8373497ee9e20d07958a26cb3bc2
Thomas Rausch <thomas@thrau.at>
2022-06-22 15:32:17 +0200

    add rds docdb and neptune to the list of known API conflicts (#6319)

--- tests/unit/aws/test_service_router.py
+++ tests/unit/aws/test_service_router.py
@@ -65,8 +65,8 @@ def _collect_operations() -> Tuple[ServiceModel, OperationModel]:
                 )
             # Exclude services / operations which have ambiguities and where the service routing needs to resolve those
             elif (
-                service.service_name in ["rds", "docdb", "neptune"]
-                or service.service_name in "timestream-write"
+                service.service_name in ["docdb", "neptune"]  # maps to rds
+                or service.service_name in "timestream-write"  # maps to timestream-query
                 or (
                     service.service_name == "sesv2"
                     and operation_name == "PutEmailIdentityDkimSigningAttributes"
