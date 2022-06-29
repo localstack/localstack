8f27bc6fff9e230b3ef8f194b583b524a5157fe8
Dominik Schubert <dominik.schubert91@gmail.com>
2022-06-29 16:15:46 +0200

    add test for nodejs 14 and 16 runtimes and ES6 export syntax

---
+++ tests/integration/awslambda/functions/lambda_handler_es6.mjs
@@ -0,0 +1,8 @@
+
+export const handler = async (event) => {
+   console.log(JSON.stringify(event))
+   return {
+     statusCode: 200,
+     body: JSON.stringify(`response from localstack lambda: ${JSON.stringify(event)}`)
+   }
+}
