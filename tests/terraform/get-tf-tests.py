dc19bf3d2fa7ee30ce87fbb4b8c9a7732f54e9f0
Macwan Nevil <macnev2013@gmail.com>
2022-07-05 13:23:15 +0530

    minor fix

--- tests/terraform/get-tf-tests.py
+++ tests/terraform/get-tf-tests.py
@@ -7,6 +7,8 @@ def print_test_names(service):
     with open("tests/terraform/terraform-tests.yaml") as f:
         dct = yaml.load(f, Loader=yaml.FullLoader)
         tests = dct.get(service)
+        if not tests:
+            sys.exit(1)
         if len(tests) == 1:
             print(tests[0])
         else:
@@ -15,6 +17,6 @@ def print_test_names(service):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("")
+        sys.exit(1)
     else:
         print_test_names(service=sys.argv[1])
