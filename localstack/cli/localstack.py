8fbc9eafbc86d91601f92d1a00a0a7950c73bb57
HarshCasper <erbeusgriffincasper@gmail.com>
2022-07-18 20:45:21 +0530

    fix: print conserved docker logs if container is not running

--- localstack/cli/localstack.py
+++ localstack/cli/localstack.py
@@ -180,12 +180,17 @@ def cmd_stop():
 )
 @publish_invocation
 def cmd_logs(follow: bool):
+    from localstack.utils.bootstrap import LocalstackContainer
     from localstack.utils.docker_utils import DOCKER_CLIENT
 
     container_name = config.MAIN_CONTAINER_NAME
+    logfile = LocalstackContainer(container_name).logfile
 
     if not DOCKER_CLIENT.is_container_running(container_name):
         console.print("localstack container not running")
+        with open(logfile) as fd:
+            for line in fd:
+                console.print(line.rstrip("\n\r"))
         sys.exit(1)
 
     if follow:
