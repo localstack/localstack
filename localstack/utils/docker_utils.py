186c9ddb13f084dcbe3c37c0a98025869fcbb63d
Daniel Fangl <daniel.fangl@localstack.cloud>
2022-07-18 18:36:13 +0200

    fix volume dir path replacement for windows paths, use docker cmd client if not in docker (#6474)

--- localstack/utils/docker_utils.py
+++ localstack/utils/docker_utils.py
@@ -22,7 +22,8 @@ def is_docker_sdk_installed() -> bool:
 
 
 def create_docker_client() -> ContainerClient:
-    if config.LEGACY_DOCKER_CLIENT or not is_docker_sdk_installed():
+    # never use the sdk client if it is not installed or not in docker - too risky for wrong version
+    if config.LEGACY_DOCKER_CLIENT or not is_docker_sdk_installed() or not config.is_in_docker:
         from localstack.utils.container_utils.docker_cmd_client import CmdDockerClient
 
         LOG.debug(
