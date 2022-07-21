1953409954b60b82a76cd4b89cdd0b52b870b77f
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-07-21 15:37:56 -0500

    test for s3 notifications with presign requests

--- tests/integration/s3/test_s3_notifications_lambda.py
+++ tests/integration/s3/test_s3_notifications_lambda.py
@@ -1,10 +1,9 @@
 import os
-import time
 
-from localstack.services.awslambda.lambda_utils import LAMBDA_RUNTIME_PYTHON36
-from localstack.utils import testutil
 from localstack.utils.aws import aws_stack
-from localstack.utils.common import retry, short_uid
+from localstack.utils.common import retry
+from localstack.utils.common import safe_requests as requests
+from localstack.utils.common import short_uid
 
 THIS_FOLDER = os.path.dirname(os.path.realpath(__file__))
 TEST_LAMBDA_PYTHON_TRIGGERED_S3 = os.path.join(
@@ -14,58 +13,111 @@ TEST_LAMBDA_PYTHON_TRIGGERED_S3 = os.path.join(
 
 class TestS3NotificationsToLambda:
     def test_create_object_put_via_dynamodb(
-        self, s3_client, lambda_client, dynamodb_client, s3_create_bucket
+        self,
+        s3_client,
+        dynamodb_client,
+        s3_create_bucket,
+        create_lambda_function,
+        dynamodb_create_table,
+        dynamodb_resource,
     ):
         # TODO: inline lambda function
         bucket_name = s3_create_bucket()
         function_name = "func-%s" % short_uid()
         table_name = "table-%s" % short_uid()
 
-        testutil.create_lambda_function(
+        create_lambda_function(
             handler_file=TEST_LAMBDA_PYTHON_TRIGGERED_S3,
             func_name=function_name,
-            runtime=LAMBDA_RUNTIME_PYTHON36,
-            client=lambda_client,
         )
 
         # this test uses dynamodb as an intermediary to get the notifications from the lambda back to the test
-        aws_stack.create_dynamodb_table(
-            table_name=table_name, partition_key="uuid", client=dynamodb_client
+        dynamodb_create_table(table_name=table_name, partition_key="uuid", client=dynamodb_client)
+
+        s3_client.put_bucket_notification_configuration(
+            Bucket=bucket_name,
+            NotificationConfiguration={
+                "LambdaFunctionConfigurations": [
+                    {
+                        "LambdaFunctionArn": aws_stack.lambda_function_arn(function_name),
+                        "Events": ["s3:ObjectCreated:*"],
+                    }
+                ]
+            },
         )
 
-        try:
-            s3_client.put_bucket_notification_configuration(
-                Bucket=bucket_name,
-                NotificationConfiguration={
-                    "LambdaFunctionConfigurations": [
-                        {
-                            "LambdaFunctionArn": aws_stack.lambda_function_arn(function_name),
-                            "Events": ["s3:ObjectCreated:*"],
-                        }
-                    ]
-                },
-            )
+        # put an object
+        obj = s3_client.put_object(Bucket=bucket_name, Key=table_name, Body="something..")
+        etag = obj["ETag"]
 
-            # put an object
-            obj = s3_client.put_object(Bucket=bucket_name, Key=table_name, Body="something..")
-            etag = obj["ETag"]
-            time.sleep(2)
+        table = dynamodb_resource.Table(table_name)
 
-            table = aws_stack.connect_to_resource("dynamodb").Table(table_name)
-
-            def check_table():
-                rs = table.scan()
-                assert len(rs["Items"]) == 1
-                return rs
-
-            rs = retry(check_table, retries=4, sleep=3)
-
+        def check_table():
+            rs = table.scan()
+            assert len(rs["Items"]) == 1
             event = rs["Items"][0]["data"]
+
             assert event["eventSource"] == "aws:s3"
             assert event["eventName"] == "ObjectCreated:Put"
             assert event["s3"]["bucket"]["name"] == bucket_name
             assert event["s3"]["object"]["eTag"] == etag
-        finally:
-            # clean up
-            lambda_client.delete_function(FunctionName=function_name)
-            dynamodb_client.delete_table(TableName=table_name)
+
+        retry(check_table, retries=5, sleep=1)
+
+    def test_create_object_by_presigned_request_via_dynamodb(
+        self,
+        s3_client,
+        dynamodb_client,
+        s3_create_bucket,
+        create_lambda_function,
+        dynamodb_create_table,
+        dynamodb_resource,
+    ):
+
+        bucket_name = s3_create_bucket()
+        function_name = "func-%s" % short_uid()
+        table_name = "table-%s" % short_uid()
+
+        create_lambda_function(
+            handler_file=TEST_LAMBDA_PYTHON_TRIGGERED_S3,
+            func_name=function_name,
+        )
+
+        dynamodb_create_table(table_name=table_name, partition_key="uuid", client=dynamodb_client)
+
+        s3_client.put_bucket_notification_configuration(
+            Bucket=bucket_name,
+            NotificationConfiguration={
+                "LambdaFunctionConfigurations": [
+                    {
+                        "LambdaFunctionArn": aws_stack.lambda_function_arn(function_name),
+                        "Events": ["s3:ObjectCreated:*"],
+                    }
+                ]
+            },
+        )
+
+        put_url = s3_client.generate_presigned_url(
+            ClientMethod="put_object", Params={"Bucket": bucket_name, "Key": table_name}
+        )
+        requests.put(put_url, data="by_presigned_put")
+
+        presigned_post = s3_client.generate_presigned_post(Bucket=bucket_name, Key=table_name)
+        # method 1
+        requests.post(
+            presigned_post["url"],
+            data=presigned_post["fields"],
+            files={"file": b"by post method 1"},
+        )
+
+        # method 2
+        presigned_post["fields"]["file"] = b"by post method 2"
+        requests.post(presigned_post["url"], data=presigned_post["fields"])
+
+        table = dynamodb_resource.Table(table_name)
+
+        def check_table():
+            rs = table.scan()
+            assert len(rs["Items"]) == 3
+
+        retry(check_table, retries=5, sleep=1)
