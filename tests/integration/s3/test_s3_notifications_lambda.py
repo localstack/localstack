45665ff04c1d72260dbfd0e8fd235de4984488a5
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-07-22 12:54:46 -0500

    aws validation

--- tests/integration/s3/test_s3_notifications_lambda.py
+++ tests/integration/s3/test_s3_notifications_lambda.py
@@ -1,4 +1,7 @@
+import json
 import os
+
+import pytest
 
 from localstack.utils.aws import aws_stack
 from localstack.utils.common import retry
@@ -12,23 +15,52 @@ TEST_LAMBDA_PYTHON_TRIGGERED_S3 = os.path.join(
 
 
 class TestS3NotificationsToLambda:
+    @pytest.mark.aws_validated
     def test_create_object_put_via_dynamodb(
         self,
         s3_client,
         dynamodb_client,
         s3_create_bucket,
+        lambda_client,
         create_lambda_function,
+        iam_client,
+        create_role,
         dynamodb_create_table,
         dynamodb_resource,
     ):
-        # TODO: inline lambda function
         bucket_name = s3_create_bucket()
         function_name = "func-%s" % short_uid()
         table_name = "table-%s" % short_uid()
+        role_name = f"test-role-{short_uid()}"
+        trust_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "lambda.amazonaws.com"},
+                    "Action": "sts:AssumeRole",
+                },
+            ],
+        }
 
-        create_lambda_function(
-            handler_file=TEST_LAMBDA_PYTHON_TRIGGERED_S3,
-            func_name=function_name,
+        role = create_role(RoleName=role_name, AssumeRolePolicyDocument=json.dumps(trust_policy))
+        iam_client.attach_role_policy(
+            RoleName=role_name, PolicyArn="arn:aws:iam::aws:policy/AWSLambdaExecute"
+        )
+        iam_client.attach_role_policy(
+            RoleName=role_name, PolicyArn="arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+        )
+        lambda_role = role["Role"]["Arn"]
+
+        function = create_lambda_function(
+            handler_file=TEST_LAMBDA_PYTHON_TRIGGERED_S3, func_name=function_name, role=lambda_role
+        )["CreateFunctionResponse"]
+
+        lambda_client.add_permission(
+            StatementId="1",
+            FunctionName=function_name,
+            Action="lambda:InvokeFunction",
+            Principal="s3.amazonaws.com",
         )
 
         # this test uses dynamodb as an intermediary to get the notifications from the lambda back to the test
@@ -39,7 +71,7 @@ class TestS3NotificationsToLambda:
             NotificationConfiguration={
                 "LambdaFunctionConfigurations": [
                     {
-                        "LambdaFunctionArn": aws_stack.lambda_function_arn(function_name),
+                        "LambdaFunctionArn": function["FunctionArn"],
                         "Events": ["s3:ObjectCreated:*"],
                     }
                 ]
@@ -60,27 +92,56 @@ class TestS3NotificationsToLambda:
             assert event["eventSource"] == "aws:s3"
             assert event["eventName"] == "ObjectCreated:Put"
             assert event["s3"]["bucket"]["name"] == bucket_name
-            assert event["s3"]["object"]["eTag"] == etag
+            assert event["s3"]["object"]["eTag"] == etag.strip('"')
 
         retry(check_table, retries=5, sleep=1)
 
+    @pytest.mark.aws_validated
     def test_create_object_by_presigned_request_via_dynamodb(
         self,
         s3_client,
         dynamodb_client,
         s3_create_bucket,
         create_lambda_function,
+        lambda_client,
         dynamodb_create_table,
         dynamodb_resource,
+        iam_client,
+        create_role,
     ):
 
         bucket_name = s3_create_bucket()
         function_name = "func-%s" % short_uid()
         table_name = "table-%s" % short_uid()
+        role_name = f"test-role-{short_uid()}"
+        trust_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "lambda.amazonaws.com"},
+                    "Action": "sts:AssumeRole",
+                },
+            ],
+        }
+        role = create_role(RoleName=role_name, AssumeRolePolicyDocument=json.dumps(trust_policy))
+        iam_client.attach_role_policy(
+            RoleName=role_name, PolicyArn="arn:aws:iam::aws:policy/AWSLambdaExecute"
+        )
+        iam_client.attach_role_policy(
+            RoleName=role_name, PolicyArn="arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+        )
+        lambda_role = role["Role"]["Arn"]
 
-        create_lambda_function(
-            handler_file=TEST_LAMBDA_PYTHON_TRIGGERED_S3,
-            func_name=function_name,
+        function = create_lambda_function(
+            handler_file=TEST_LAMBDA_PYTHON_TRIGGERED_S3, func_name=function_name, role=lambda_role
+        )["CreateFunctionResponse"]
+
+        lambda_client.add_permission(
+            StatementId="1",
+            FunctionName=function_name,
+            Action="lambda:InvokeFunction",
+            Principal="s3.amazonaws.com",
         )
 
         dynamodb_create_table(table_name=table_name, partition_key="uuid", client=dynamodb_client)
@@ -90,7 +151,7 @@ class TestS3NotificationsToLambda:
             NotificationConfiguration={
                 "LambdaFunctionConfigurations": [
                     {
-                        "LambdaFunctionArn": aws_stack.lambda_function_arn(function_name),
+                        "LambdaFunctionArn": function["FunctionArn"],
                         "Events": ["s3:ObjectCreated:*"],
                     }
                 ]
@@ -110,7 +171,7 @@ class TestS3NotificationsToLambda:
             files={"file": b"by post method 1"},
         )
 
-        # method 2
+        # this method should not trigger the s3 notification
         presigned_post["fields"]["file"] = b"by post method 2"
         requests.post(presigned_post["url"], data=presigned_post["fields"])
 
@@ -118,6 +179,6 @@ class TestS3NotificationsToLambda:
 
         def check_table():
             rs = table.scan()
-            assert len(rs["Items"]) == 3
+            assert len(rs["Items"]) == 2
 
-        retry(check_table, retries=5, sleep=1)
+        retry(check_table, retries=10, sleep=1)
