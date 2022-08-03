4f42e3247b9925e247cc1387f7c5b7c56490a2c4
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-08-03 12:59:00 +0200

    wait until stream becomes available

--- tests/integration/cloudformation/test_cloudformation_firehose.py
+++ tests/integration/cloudformation/test_cloudformation_firehose.py
@@ -3,6 +3,7 @@ import os.path
 import pytest
 
 from localstack.utils.strings import short_uid
+from localstack.utils.sync import retry
 
 
 @pytest.mark.aws_validated
@@ -27,6 +28,13 @@ def test_firehose_stack_with_kinesis_as_source(deploy_cfn_template, firehose_cli
         max_wait=150,
     )
     snapshot.match("outputs", stack.outputs)
+
+    def _assert_stream_available():
+        status = firehose_client.describe_delivery_stream(DeliveryStreamName=delivery_stream_name)
+        assert status["DeliveryStreamDescription"]["DeliveryStreamStatus"] == "ACTIVE"
+
+    # wait until stream becomes available
+    retry(_assert_stream_available, sleep=1, retries=15)
 
     response = firehose_client.describe_delivery_stream(DeliveryStreamName=delivery_stream_name)
     assert delivery_stream_name == response["DeliveryStreamDescription"]["DeliveryStreamName"]
