e8c0092dfdccd99944d73a738691de121140ebc8
Viren Nadkarni <viren.nadkarni@localstack.cloud>
2022-08-25 18:37:43 +0530

    Update response code
    Caused by https://github.com/spulec/moto/pull/5397

--- tests/integration/test_apigateway.py
+++ tests/integration/test_apigateway.py
@@ -860,7 +860,7 @@ class TestAPIGateway:
         apigw_client = aws_stack.create_external_boto_client("apigateway")
 
         rs = apigw_client.create_domain_name(domainName=domain_name)
-        assert 200 == rs["ResponseMetadata"]["HTTPStatusCode"]
+        assert 201 == rs["ResponseMetadata"]["HTTPStatusCode"]
 
         rs = apigw_client.get_domain_name(domainName=domain_name)
         assert 200 == rs["ResponseMetadata"]["HTTPStatusCode"]
