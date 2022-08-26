f7ee6bafcb21e07e769631c419c65735243de0ad
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2022-08-26 10:49:46 +0100

    fine-grained service locks

--- localstack/utils/threads.py
+++ localstack/utils/threads.py
@@ -5,7 +5,7 @@ import threading
 import traceback
 from concurrent.futures import Future
 from multiprocessing.dummy import Pool
-from typing import Callable, List
+from typing import Callable, List, MutableMapping
 
 LOG = logging.getLogger(__name__)
 
@@ -133,3 +133,50 @@ def parallelize(func: Callable, arr: List, size: int = None):
 
     with Pool(size) as pool:
         return pool.map(func, arr)
+
+
+class SynchronizedDict(MutableMapping):
+    def __init__(self, *args, **kwargs):
+        self._map = {}
+        self.update(dict(*args, **kwargs))
+        self._lock = threading.RLock()
+
+    @classmethod
+    def fromkeys(cls, iterable, value=None):
+        sync_dict = SynchronizedDict()
+
+        sync_dict._map = dict.fromkeys(iterable, value)
+
+        return sync_dict
+
+    def __getitem__(self, key):
+        with self._lock:
+            item = self._map[key]
+
+        return item
+
+    def __setitem__(self, key, value):
+        with self._lock:
+            self._map[key] = value
+
+    def __delitem__(self, key):
+        with self._lock:
+            del self._map[key]
+
+    def __iter__(self):
+        with self._lock:
+            it = iter(self._map)
+
+        return it
+
+    def __len__(self):
+        with self._lock:
+            length = len(self._map)
+
+        return length
+
+    def __str__(self):
+        with self._lock:
+            s = str(self._map)
+
+        return s
