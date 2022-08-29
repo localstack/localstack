ad08c9db912133632df851d59f48bf8d24a10af7
Benjamin Simon <benjh.simon@gmail.com>
2022-08-29 11:58:50 +0200

    migrate test_s3_get_response_header_overrides

--- tests/integration/s3/test_s3.py
+++ tests/integration/s3/test_s3.py
@@ -1495,6 +1495,40 @@ class TestS3PresignedUrl:
         assert "Access-Control-Allow-Methods" not in response.headers
         assert "Access-Control-Allow-Origin" not in response.headers
 
+    @pytest.mark.aws_validated
+    def test_s3_get_response_header_overrides(self, s3_client, s3_bucket):
+        # Signed requests may include certain header overrides in the querystring
+        # https://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectGET.html
+        object_key = "key-header-overrides"
+        s3_client.put_object(Bucket=s3_bucket, Key=object_key, Body="something")
+
+        # get object and assert headers
+        expiry_date = "Wed, 21 Oct 2015 07:28:00 GMT"
+        url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={
+                "Bucket": s3_bucket,
+                "Key": object_key,
+                "ResponseCacheControl": "max-age=74",
+                "ResponseContentDisposition": 'attachment; filename="foo.jpg"',
+                "ResponseContentEncoding": "identity",
+                "ResponseContentLanguage": "de-DE",
+                "ResponseContentType": "image/jpeg",
+                "ResponseExpires": expiry_date,
+            },
+        )
+        response = requests.get(url, verify=False)
+        headers = response.headers
+        assert headers["cache-control"] == "max-age=74"
+        assert headers["content-disposition"] == 'attachment; filename="foo.jpg"'
+        assert headers["content-encoding"] == "identity"
+        assert headers["content-language"] == "de-DE"
+        assert headers["content-type"] == "image/jpeg"
+
+        # Note: looks like depending on the environment/libraries, we can get different date formats...
+        possible_date_formats = ["2015-10-21T07:28:00Z", expiry_date]
+        assert headers["expires"] in possible_date_formats
+
 
 class TestS3Cors:
     @patch.object(config, "DISABLE_CUSTOM_CORS_S3", False)
--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -110,46 +110,6 @@ class TestS3(unittest.TestCase):
     def s3_client(self):
         return TestS3.OVERWRITTEN_CLIENT or self._s3_client
 
-    def test_s3_get_response_header_overrides(self):
-        # Signed requests may include certain header overrides in the querystring
-        # https://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectGET.html
-
-        bucket_name = "test-bucket-%s" % short_uid()
-        client = self._get_test_client()
-        client.create_bucket(Bucket=bucket_name)
-
-        # put object
-        object_key = "key-by-hostname"
-        client.put_object(Bucket=bucket_name, Key=object_key, Body="something")
-
-        # get object and assert headers
-        expiry_date = "Wed, 21 Oct 2015 07:28:00 GMT"
-        url = client.generate_presigned_url(
-            "get_object",
-            Params={
-                "Bucket": bucket_name,
-                "Key": object_key,
-                "ResponseCacheControl": "max-age=74",
-                "ResponseContentDisposition": 'attachment; filename="foo.jpg"',
-                "ResponseContentEncoding": "identity",
-                "ResponseContentLanguage": "de-DE",
-                "ResponseContentType": "image/jpeg",
-                "ResponseExpires": expiry_date,
-            },
-        )
-        response = requests.get(url, verify=False)
-
-        self.assertEqual("max-age=74", response.headers["cache-control"])
-        self.assertEqual('attachment; filename="foo.jpg"', response.headers["content-disposition"])
-        self.assertEqual("identity", response.headers["content-encoding"])
-        self.assertEqual("de-DE", response.headers["content-language"])
-        self.assertEqual("image/jpeg", response.headers["content-type"])
-        # Note: looks like depending on the environment/libraries, we can get different date formats...
-        possible_date_formats = ["2015-10-21T07:28:00Z", expiry_date]
-        self.assertIn(response.headers["expires"], possible_date_formats)
-        # clean up
-        self._delete_bucket(bucket_name, [object_key])
-
     def test_s3_copy_md5(self):
         bucket_name = "test-bucket-%s" % short_uid()
         client = self._get_test_client()
