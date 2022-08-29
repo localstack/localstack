d917cf6379e9e02ea66b9e12e658a7e251ab3b7b
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-08-29 14:13:34 -0500

    restore extended test

--- tests/integration/cloudformation/test_cloudformation_kinesis.py
+++ tests/integration/cloudformation/test_cloudformation_kinesis.py
@@ -1,11 +1,24 @@
 import json
+import time
 
 import pytest
 
+from localstack.testing.aws.util import is_aws_cloud
 
+
+# TODO fix service so it returns the stream mode
 @pytest.mark.aws_validated
+@pytest.mark.skip_snapshot_verify(paths=["$..StreamDescription.StreamModeDetails"])
 def test_stream_creation(kinesis_client, deploy_cfn_template, snapshot):
     snapshot.add_transformer(snapshot.transform.resource_name())
+    snapshot.add_transformers_list(
+        [
+            snapshot.transform.key_value("StreamName", "stream-name"),
+            snapshot.transform.key_value("ShardId", "shard-id", reference_replacement=False),
+            snapshot.transform.key_value("EndingHashKey", "ending-hash-key"),
+            snapshot.transform.key_value("StartingSequenceNumber", "sequence-number"),
+        ]
+    )
 
     template = json.dumps(
         {
@@ -24,3 +37,10 @@ def test_stream_creation(kinesis_client, deploy_cfn_template, snapshot):
 
     outputs = deploy_cfn_template(template=template).outputs
     snapshot.match("stack_output", outputs)
+
+    if not is_aws_cloud():
+        time.sleep(1)
+
+    stream_name = outputs.get("StreamNameFromRef")
+    description = kinesis_client.describe_stream(StreamName=stream_name)
+    snapshot.match("stream_description", description)
