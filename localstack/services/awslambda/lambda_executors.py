08aed23c17f44a85b70107d2eb391b12352c9376
Daniel Fangl <daniel.fangl@localstack.cloud>
2022-08-31 11:55:04 +0200

    cleanup get_host_path_for_path_in_docker

--- localstack/services/awslambda/lambda_executors.py
+++ localstack/services/awslambda/lambda_executors.py
@@ -18,7 +18,7 @@ from multiprocessing import Process, Queue
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 from localstack import config
-from localstack.constants import DEFAULT_LAMBDA_CONTAINER_REGISTRY, DEFAULT_VOLUME_DIR
+from localstack.constants import DEFAULT_LAMBDA_CONTAINER_REGISTRY
 from localstack.runtime.hooks import hook_spec
 from localstack.services.awslambda.lambda_utils import (
     API_PATH_ROOT,
@@ -68,11 +68,7 @@ from localstack.utils.container_utils.container_client import (
     DockerContainerStatus,
     PortMappings,
 )
-from localstack.utils.docker_utils import (
-    DOCKER_CLIENT,
-    get_default_volume_dir_mount,
-    get_host_path_for_path_in_docker,
-)
+from localstack.utils.docker_utils import DOCKER_CLIENT, get_host_path_for_path_in_docker
 from localstack.utils.run import FuncThread
 from localstack.utils.time import timestamp_millis
 
@@ -1607,46 +1603,6 @@ class Util:
                 cls.debug_java_port = m.groups()[1]
 
         return opts
-
-    @classmethod
-    def get_host_path_for_path_in_docker(cls, path):
-        """
-        Returns the calculated host location for a given subpath of DEFAULT_VOLUME_DIR inside the localstack container.
-        The path **has** to be a subdirectory of DEFAULT_VOLUME_DIR (the dir itself *will not* work).
-
-        :param path: Path to be replaced (subpath of DEFAULT_VOLUME_DIR)
-        :return: Path on the host
-        """
-        if config.LEGACY_DIRECTORIES:
-            return re.sub(r"^%s/(.*)$" % config.dirs.tmp, r"%s/\1" % config.dirs.functions, path)
-
-        if config.is_in_docker:
-            volume = get_default_volume_dir_mount()
-
-            if volume:
-                if volume.type != "bind":
-                    raise ValueError(
-                        f"Mount to {DEFAULT_VOLUME_DIR} needs to be a bind mount for mounting to work"
-                    )
-
-                if not path.startswith(f"{DEFAULT_VOLUME_DIR}/") and path != DEFAULT_VOLUME_DIR:
-                    # We should be able to replace something here.
-                    # if this warning is printed, the usage of this function is probably wrong.
-                    # Please check if the target path is indeed prefixed by /var/lib/localstack
-                    # if this happens, mounts may fail
-                    LOG.warning(
-                        "Error while performing automatic host path replacement for path '%s' to source '%s'",
-                        path,
-                        volume.source,
-                    )
-                else:
-                    relative_path = path.removeprefix(DEFAULT_VOLUME_DIR)
-                    result = volume.source + relative_path
-                    return result
-            else:
-                raise ValueError(f"No volume mounted to {DEFAULT_VOLUME_DIR}")
-
-        return path
 
     @classmethod
     def format_windows_path(cls, path):
