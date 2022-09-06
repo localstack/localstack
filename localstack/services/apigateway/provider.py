b646cbf26da73212790801d841251b83f5237e79
Viren Nadkarni <viren.nadkarni@localstack.cloud>
2022-09-06 16:32:01 +0530

    Use stores in API Gateway

--- localstack/services/apigateway/provider.py
+++ localstack/services/apigateway/provider.py
@@ -47,10 +47,10 @@ from localstack.services.apigateway.context import ApiInvocationContext
 from localstack.services.apigateway.helpers import (
     PATH_REGEX_TEST_INVOKE_API,
     PATH_REGEX_USER_REQUEST,
-    APIGatewayRegion,
     OpenApiExporter,
     apply_json_patch_safe,
     find_api_subentity_by_id,
+    get_apigateway_store,
 )
 from localstack.services.apigateway.invocations import invoke_rest_api_from_request
 from localstack.services.apigateway.patches import apply_patches
@@ -135,7 +135,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def create_authorizer(
         self, context: RequestContext, request: CreateAuthorizerRequest
     ) -> Authorizer:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         api_id = request["restApiId"]
         authorizer_id = short_uid()[:6]  # length 6 to make TF tests pass
@@ -156,7 +156,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         limit: NullableInteger = None,
     ) -> Authorizers:
         # TODO add paging
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.authorizers.get(rest_api_id) or []
 
@@ -174,7 +174,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def delete_authorizer(
         self, context: RequestContext, rest_api_id: String, authorizer_id: String
     ) -> None:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.authorizers.get(rest_api_id, [])
         for i in range(len(auth_list)):
@@ -189,7 +189,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         authorizer_id: String,
         patch_operations: ListOfPatchOperation = None,
     ) -> Authorizer:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         authorizer = find_api_subentity_by_id(rest_api_id, authorizer_id, "authorizers")
         if authorizer is None:
@@ -212,14 +212,14 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         self,
         context: RequestContext,
     ) -> Account:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         result = to_account_response_json(region_details.account)
         return Account(**result)
 
     def update_account(
         self, context: RequestContext, patch_operations: ListOfPatchOperation = None
     ) -> Account:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         apply_json_patch_safe(region_details.account, patch_operations, in_place=True)
         result = to_account_response_json(region_details.account)
         return Account(**result)
@@ -229,7 +229,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def get_documentation_parts(
         self, context: RequestContext, request: GetDocumentationPartsRequest
     ) -> DocumentationParts:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         # This function returns either a list or a single entity (depending on the path)
         api_id = request["restApiId"]
@@ -254,7 +254,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         location: DocumentationPartLocation,
         properties: String,
     ) -> DocumentationPart:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         entity_id = short_uid()[:6]  # length 6 for AWS parity / Terraform compatibility
         entry = {
@@ -276,7 +276,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         documentation_part_id: String,
         patch_operations: ListOfPatchOperation = None,
     ) -> DocumentationPart:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         entity = find_api_subentity_by_id(rest_api_id, documentation_part_id, "documentation_parts")
         if entity is None:
@@ -295,7 +295,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def delete_documentation_part(
         self, context: RequestContext, rest_api_id: String, documentation_part_id: String
     ) -> None:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.documentation_parts[rest_api_id]
         for i in range(len(auth_list)):
@@ -312,7 +312,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         position: String = None,
         limit: NullableInteger = None,
     ) -> BasePathMappings:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         mappings_list = region_details.base_path_mappings.get(domain_name) or []
 
@@ -324,7 +324,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def get_base_path_mapping(
         self, context: RequestContext, domain_name: String, base_path: String
     ) -> BasePathMapping:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         mappings_list = region_details.base_path_mappings.get(domain_name) or []
         mapping = ([m for m in mappings_list if m["basePath"] == base_path] or [None])[0]
@@ -342,7 +342,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         base_path: String = None,
         stage: String = None,
     ) -> BasePathMapping:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         # Note: "(none)" is a special value in API GW:
         # https://docs.aws.amazon.com/apigateway/api-reference/link-relation/basepathmapping-by-base-path
@@ -366,7 +366,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         base_path: String,
         patch_operations: ListOfPatchOperation = None,
     ) -> BasePathMapping:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         mappings_list = region_details.base_path_mappings.get(domain_name) or []
 
@@ -393,7 +393,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def delete_base_path_mapping(
         self, context: RequestContext, domain_name: String, base_path: String
     ) -> None:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         mappings_list = region_details.base_path_mappings.get(domain_name) or []
         for i in range(len(mappings_list)):
@@ -408,7 +408,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def get_client_certificate(
         self, context: RequestContext, client_certificate_id: String
     ) -> ClientCertificate:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         result = region_details.client_certificates.get(client_certificate_id)
         if result is None:
             raise NotFoundException(f"Client certificate ID {client_certificate_id} not found")
@@ -417,14 +417,14 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def get_client_certificates(
         self, context: RequestContext, position: String = None, limit: NullableInteger = None
     ) -> ClientCertificates:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         result = list(region_details.client_certificates.values())
         return ClientCertificates(items=result)
 
     def generate_client_certificate(
         self, context: RequestContext, description: String = None, tags: MapOfStringToString = None
     ) -> ClientCertificate:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         cert_id = short_uid()
         creation_time = now_utc()
         entry = {
@@ -445,7 +445,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         client_certificate_id: String,
         patch_operations: ListOfPatchOperation = None,
     ) -> ClientCertificate:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         entity = region_details.client_certificates.get(client_certificate_id)
         if entity is None:
             raise NotFoundException(f'Client certificate ID "{client_certificate_id}" not found')
@@ -456,7 +456,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def delete_client_certificate(
         self, context: RequestContext, client_certificate_id: String
     ) -> None:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         entity = region_details.client_certificates.pop(client_certificate_id, None)
         if entity is None:
             raise NotFoundException(f'VPC link ID "{client_certificate_id}" not found for deletion')
@@ -471,7 +471,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         description: String = None,
         tags: MapOfStringToString = None,
     ) -> VpcLink:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         link_id = short_uid()
         entry = {"id": link_id, "status": "AVAILABLE"}
         region_details.vpc_links[link_id] = entry
@@ -481,14 +481,14 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def get_vpc_links(
         self, context: RequestContext, position: String = None, limit: NullableInteger = None
     ) -> VpcLinks:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         result = region_details.vpc_links.values()
         result = [to_vpc_link_response_json(r) for r in result]
         result = {"items": result}
         return result
 
     def get_vpc_link(self, context: RequestContext, vpc_link_id: String) -> VpcLink:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         vpc_link = region_details.vpc_links.get(vpc_link_id)
         if vpc_link is None:
             raise NotFoundException(f'VPC link ID "{vpc_link_id}" not found')
@@ -501,7 +501,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         vpc_link_id: String,
         patch_operations: ListOfPatchOperation = None,
     ) -> VpcLink:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         vpc_link = region_details.vpc_links.get(vpc_link_id)
         if vpc_link is None:
             raise NotFoundException(f'VPC link ID "{vpc_link_id}" not found')
@@ -510,7 +510,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         return VpcLink(**result)
 
     def delete_vpc_link(self, context: RequestContext, vpc_link_id: String) -> None:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
         vpc_link = region_details.vpc_links.pop(vpc_link_id, None)
         if vpc_link is None:
             raise NotFoundException(f'VPC link ID "{vpc_link_id}" not found for deletion')
@@ -524,7 +524,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         position: String = None,
         limit: NullableInteger = None,
     ) -> RequestValidators:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.validators.get(rest_api_id) or []
 
@@ -534,7 +534,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def get_request_validator(
         self, context: RequestContext, rest_api_id: String, request_validator_id: String
     ) -> RequestValidator:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.validators.get(rest_api_id) or []
         validator = ([a for a in auth_list if a["id"] == request_validator_id] or [None])[0]
@@ -554,7 +554,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         validate_request_body: Boolean = None,
         validate_request_parameters: Boolean = None,
     ) -> RequestValidator:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         # length 6 for AWS parity and TF compatibility
         validator_id = short_uid()[:6]
@@ -577,7 +577,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         request_validator_id: String,
         patch_operations: ListOfPatchOperation = None,
     ) -> RequestValidator:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.validators.get(rest_api_id) or []
         validator = ([a for a in auth_list if a["id"] == request_validator_id] or [None])[0]
@@ -600,7 +600,7 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
     def delete_request_validator(
         self, context: RequestContext, rest_api_id: String, request_validator_id: String
     ) -> None:
-        region_details = APIGatewayRegion.get()
+        region_details = get_apigateway_store()
 
         auth_list = region_details.validators.get(rest_api_id, [])
         for i in range(len(auth_list)):
@@ -621,19 +621,19 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
         position: String = None,
         limit: NullableInteger = None,
     ) -> Tags:
-        result = APIGatewayRegion.TAGS.get(resource_arn, {})
+        result = get_apigateway_store().TAGS.get(resource_arn, {})
         return Tags(tags=result)
 
     def tag_resource(
         self, context: RequestContext, resource_arn: String, tags: MapOfStringToString
     ) -> None:
-        resource_tags = APIGatewayRegion.TAGS.setdefault(resource_arn, {})
+        resource_tags = get_apigateway_store().TAGS.setdefault(resource_arn, {})
         resource_tags.update(tags)
 
     def untag_resource(
         self, context: RequestContext, resource_arn: String, tag_keys: ListOfString
     ) -> None:
-        resource_tags = APIGatewayRegion.TAGS.setdefault(resource_arn, {})
+        resource_tags = get_apigateway_store().TAGS.setdefault(resource_arn, {})
         for key in tag_keys:
             resource_tags.pop(key, None)
 
