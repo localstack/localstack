12d812d34d6a52cbbcf3ecf4b991ec0579a2615c
Benjamin Simon <benjh.simon@gmail.com>
2022-09-09 22:34:45 +0200

    start implementing actions + reduce test failure

--- localstack/services/s3/models.py
+++ localstack/services/s3/models.py
@@ -1,5 +1,12 @@
-from typing import Dict, List
+from typing import Dict, Set  # , List
 
+from localstack.aws.api.s3 import (
+    BucketName,
+    CORSConfiguration,
+    LifecycleConfiguration,
+    NotificationConfiguration,
+    ReplicationConfiguration,
+)
 from localstack.services.stores import (
     AccountRegionBundle,
     BaseStore,
@@ -11,20 +18,24 @@ from localstack.services.stores import (
 class S3Store(BaseStore):
     # store bucket names
     # They must be unique across all AWS accounts in all the AWS Regions within a partition
-    bucket_names: Dict[str, str] = CrossRegionAttribute(default=dict)
+    # this might not be needed, Moto handles it
+    bucket_names: Set[BucketName] = CrossRegionAttribute(default=set)
 
     # maps bucket name to bucket's list of notification configurations
-    # TODO: check type
-    bucket_notification_configs: List[Dict] = LocalAttribute(default=list)
+    bucket_notification_configs: Dict[BucketName, NotificationConfiguration] = LocalAttribute(
+        default=dict
+    )
 
     # maps bucket name to bucket's CORS settings
-    bucket_cors: Dict[str, List[Dict]] = LocalAttribute(default=dict)
+    bucket_cors: Dict[BucketName, CORSConfiguration] = LocalAttribute(default=dict)
 
     # maps bucket name to bucket's lifecycle settings
-    bucket_lifecycle: Dict[str, List[Dict]] = LocalAttribute(default=dict)
+    bucket_lifecycle: Dict[BucketName, LifecycleConfiguration] = LocalAttribute(default=dict)
 
     # maps bucket name to bucket's replication settings
-    bucket_replication: Dict[str, List[Dict]] = LocalAttribute(default=dict)
+    bucket_replication: Dict[BucketName, ReplicationConfiguration] = LocalAttribute(default=dict)
+
+    # bucket_objects? store full config in one key?
 
 
 s3_stores = AccountRegionBundle("s3", S3Store)
