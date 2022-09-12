bfacaea0c17663e7515a25f805a0d64edb673b53
Benjamin Simon <benjh.simon@gmail.com>
2022-09-12 14:25:17 +0200

    remove S3 store attributes for now, will see later if needed

--- localstack/services/s3/models.py
+++ localstack/services/s3/models.py
@@ -1,26 +1,15 @@
-from typing import Dict, Set  # , List
+from typing import Dict
 
 from localstack.aws.api.s3 import (
     BucketName,
     CORSConfiguration,
-    LifecycleConfiguration,
     NotificationConfiguration,
     ReplicationConfiguration,
 )
-from localstack.services.stores import (
-    AccountRegionBundle,
-    BaseStore,
-    CrossRegionAttribute,
-    LocalAttribute,
-)
+from localstack.services.stores import AccountRegionBundle, BaseStore, LocalAttribute
 
 
 class S3Store(BaseStore):
-    # store bucket names
-    # They must be unique across all AWS accounts in all the AWS Regions within a partition
-    # this might not be needed, Moto handles it
-    bucket_names: Set[BucketName] = CrossRegionAttribute(default=set)
-
     # maps bucket name to bucket's list of notification configurations
     bucket_notification_configs: Dict[BucketName, NotificationConfiguration] = LocalAttribute(
         default=dict
@@ -29,13 +18,8 @@ class S3Store(BaseStore):
     # maps bucket name to bucket's CORS settings
     bucket_cors: Dict[BucketName, CORSConfiguration] = LocalAttribute(default=dict)
 
-    # maps bucket name to bucket's lifecycle settings
-    bucket_lifecycle: Dict[BucketName, LifecycleConfiguration] = LocalAttribute(default=dict)
-
     # maps bucket name to bucket's replication settings
     bucket_replication: Dict[BucketName, ReplicationConfiguration] = LocalAttribute(default=dict)
-
-    # bucket_objects? store full config in one key?
 
 
 s3_stores = AccountRegionBundle("s3", S3Store)
