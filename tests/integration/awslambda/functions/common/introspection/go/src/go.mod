d6419cfe047c06c81f009a905ca668bac222834e
Daniel Fangl <daniel.fangl@localstack.cloud>
2022-09-13 20:30:00 +0200

    add snapshot introspection tests

---
+++ tests/integration/awslambda/functions/common/introspection/go/src/go.mod
@@ -0,0 +1,5 @@
+module localstack.cloud/introspection
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.34.1
--- tests/integration/awslambda/test_lambda_common.py
+++ tests/integration/awslambda/test_lambda_common.py
@@ -3,9 +3,14 @@ import logging
 
 import pytest
 
-from localstack.utils.strings import to_bytes
+from localstack.utils.strings import to_bytes, to_str
 
 LOG = logging.getLogger(__name__)
+
+
+@pytest.fixture(autouse=True)
+def snapshot_transformers(snapshot):
+    snapshot.add_transformer(snapshot.transform.lambda_api())
 
 
 class TestLambdaRuntimesCommon:
@@ -54,3 +59,28 @@ class TestLambdaRuntimesCommon:
 
         assert invoke_result["StatusCode"] == 200
         assert json.loads(invoke_result["Payload"].read()) == payload
+
+    @pytest.mark.multiruntime(
+        scenario="introspection",
+        runtimes=["python", "nodejs", "ruby", "java", "go", "dotnet", "custom"],
+    )
+    def test_introspection_invoke(self, lambda_client, multiruntime_lambda, snapshot):
+        # provided lambdas take a little longer for large payloads, hence timeout to 5s
+        create_function_result = multiruntime_lambda.create_function(
+            MemorySize=1024, Environment={"Variables": {"TEST_KEY": "TEST_VAL"}}
+        )
+        snapshot.match("create_function_result", create_function_result)
+
+        # simple payload
+        invoke_result = lambda_client.invoke(
+            FunctionName=create_function_result["FunctionName"],
+            Payload=b'{"simple": "payload"}',
+        )
+
+        assert invoke_result["StatusCode"] == 200
+        invocation_result_payload = to_str(invoke_result["Payload"].read())
+        invocation_result_payload = json.loads(invocation_result_payload)
+        assert "environment" in invocation_result_payload
+        assert "ctx" in invocation_result_payload
+        assert "packages" in invocation_result_payload
+        snapshot.match("invocation_result_payload", invocation_result_payload)
