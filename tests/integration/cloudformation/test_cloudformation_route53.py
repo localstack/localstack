254883fb9d06ce39de635c6c3f6e38457a9238cd
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-09-22 15:08:28 -0500

    route 53

--- tests/integration/cloudformation/test_cloudformation_route53.py
+++ tests/integration/cloudformation/test_cloudformation_route53.py
@@ -5,24 +5,24 @@ def test_create_record_set_via_id(route53_hosted_zone, deploy_cfn_template):
     create_zone_response = route53_hosted_zone()
     hosted_zone_id = create_zone_response["HostedZone"]["Id"]
     route53_name = create_zone_response["HostedZone"]["Name"]
-    template_mapping = {"HostedZoneId": hosted_zone_id, "Name": route53_name}
+    parameters = {"HostedZoneId": hosted_zone_id, "Name": route53_name}
     deploy_cfn_template(
         template_path=os.path.join(
             os.path.dirname(__file__), "../templates/route53_hostedzoneid_template.yaml"
         ),
-        template_mapping=template_mapping,
+        parameters=parameters,
     )
 
 
 def test_create_record_set_via_name(deploy_cfn_template, route53_hosted_zone):
     create_zone_response = route53_hosted_zone()
     route53_name = create_zone_response["HostedZone"]["Name"]
-    template_mapping = {"HostedZoneName": route53_name, "Name": route53_name}
+    parameters = {"HostedZoneName": route53_name, "Name": route53_name}
     deploy_cfn_template(
         template_path=os.path.join(
             os.path.dirname(__file__), "../templates/route53_hostedzonename_template.yaml"
         ),
-        template_mapping=template_mapping,
+        parameters=parameters,
     )
 
 
@@ -30,11 +30,11 @@ def test_create_record_set_without_resource_record(deploy_cfn_template, route53_
     create_zone_response = route53_hosted_zone()
     hosted_zone_id = create_zone_response["HostedZone"]["Id"]
     route53_name = create_zone_response["HostedZone"]["Name"]
-    template_mapping = {"HostedZoneId": hosted_zone_id, "Name": route53_name}
+    parameters = {"HostedZoneId": hosted_zone_id, "Name": route53_name}
     deploy_cfn_template(
         template_path=os.path.join(
             os.path.dirname(__file__),
             "../templates/route53_recordset_without_resource_records.yaml",
         ),
-        template_mapping=template_mapping,
+        parameters=parameters,
     )
