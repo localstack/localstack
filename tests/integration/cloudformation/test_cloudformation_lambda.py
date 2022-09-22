107379e2a1af726c07a3fae014c18b6b2f5f9dab
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-09-22 13:51:56 -0500

    lambda tests

--- tests/integration/cloudformation/test_cloudformation_lambda.py
+++ tests/integration/cloudformation/test_cloudformation_lambda.py
@@ -1,12 +1,9 @@
 import json
 import os
 
-import jinja2
 import pytest
 
-from localstack.testing.aws.cloudformation_utils import load_template_raw
 from localstack.utils.common import short_uid, to_str
-from localstack.utils.generic.wait_utils import wait_until
 from localstack.utils.http import safe_requests
 from localstack.utils.sync import retry
 from localstack.utils.testutil import get_lambda_log_events
@@ -49,49 +46,36 @@ def test_update_lambda_inline_code(cfn_client, lambda_client, return_value, depl
 
 @pytest.mark.aws_validated
 def test_lambda_w_dynamodb_event_filter(
-    cfn_client, dynamodb_client, is_stack_created, cleanup_stacks, logs_client
+    cfn_client, dynamodb_client, logs_client, deploy_cfn_template
 ):
-    stack_name = f"stack-{short_uid()}"
     function_name = f"test-fn-{short_uid()}"
     table_name = f"ddb-tbl-{short_uid()}"
     item_to_put = {"id": {"S": "test123"}, "id2": {"S": "test42"}}
     item_to_put2 = {"id": {"S": "test123"}, "id2": {"S": "test67"}}
 
-    try:
-        template_1 = jinja2.Template(load_template_raw("lambda_dynamodb_filtering.yaml")).render(
-            event_filter='{"eventName": ["MODIFY"]}',
-            table_name=table_name,
-            function_name=function_name,
-        )
+    deploy_cfn_template(
+        template_path=os.path.join(
+            os.path.dirname(__file__), "../templates/lambda_dynamodb_filtering.yaml"
+        ),
+        parameters={
+            "FunctionName": function_name,
+            "TableName": table_name,
+            "Filter": '{"eventName": ["MODIFY"]}',
+        },
+    )
 
-        response = cfn_client.create_stack(
-            StackName=stack_name,
-            TemplateBody=template_1,
-            Capabilities=["CAPABILITY_IAM", "CAPABILITY_AUTO_EXPAND"],
-        )
-        stack_id = response["StackId"]
-        assert stack_id
-        wait_until(is_stack_created(stack_id))
+    dynamodb_client.put_item(TableName=table_name, Item=item_to_put)
+    dynamodb_client.put_item(TableName=table_name, Item=item_to_put2)
 
-        # put item the first time: INSERT
-        dynamodb_client.put_item(TableName=table_name, Item=item_to_put)
+    def _assert_single_lambda_call():
+        events = get_lambda_log_events(function_name, logs_client=logs_client)
+        assert len(events) == 1
+        msg = events[0]
+        if not isinstance(msg, str):
+            msg = json.dumps(msg)
+        assert "MODIFY" in msg and "INSERT" not in msg
 
-        def _assert_single_lambda_call():
-            events = get_lambda_log_events(function_name, logs_client=logs_client)
-            assert len(events) == 1
-            msg = events[0]
-            # msg is either string or a result of json.loads
-            if not isinstance(msg, str):
-                msg = json.dumps(msg)
-            assert "MODIFY" in msg and "INSERT" not in msg
-
-        # put item the second time: MODIFY and lambda should be called
-        dynamodb_client.put_item(TableName=table_name, Item=item_to_put2)
-        retry(_assert_single_lambda_call, retries=30)
-
-    finally:
-        # cleanup
-        cleanup_stacks([stack_name])
+    retry(_assert_single_lambda_call, retries=30)
 
 
 @pytest.mark.aws_validated
