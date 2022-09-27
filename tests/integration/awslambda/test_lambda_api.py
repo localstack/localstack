04d54e7767d247bb337c580f61c6388a13a8cbbd
Dominik Schubert <dominik.schubert91@gmail.com>
2022-09-27 08:15:52 +0200

    add more comprehensive tests for event invoke config API

--- tests/integration/awslambda/test_lambda_api.py
+++ tests/integration/awslambda/test_lambda_api.py
@@ -25,6 +25,7 @@ from localstack.testing.aws.lambda_utils import (
     is_old_provider,
     update_done,
 )
+from localstack.testing.pytest.snapshot import is_aws
 from localstack.testing.snapshots.transformer import SortingTransformer
 from localstack.utils import testutil
 from localstack.utils.aws import aws_stack
@@ -933,63 +934,238 @@ pytestmark = pytest.mark.skip_snapshot_verify(
 
 
 class TestLambdaEventInvokeConfig:
-    @pytest.mark.skip_snapshot_verify(condition=is_old_provider, paths=["$..FunctionArn"])
-    @pytest.mark.aws_validated
-    def test_lambda_asynchronous_invocations(
-        self,
-        lambda_client,
-        create_lambda_function,
-        sqs_queue,
-        sqs_queue_arn,
-        lambda_su_role,
-        snapshot,
-        cleanups,
+    def test_lambda_eventinvokeconfig_exceptions(
+        self, lambda_client, create_lambda_function, snapshot, lambda_su_role
     ):
-        """Testing API actions of function event config"""
-
-        function_name = f"lambda_func-{short_uid()}"
-
+        function_name = f"fn-eventinvoke-{short_uid()}"
+        function_name_2 = f"fn-eventinvoke-2-{short_uid()}"
         create_lambda_function(
             handler_file=TEST_LAMBDA_PYTHON_ECHO,
             func_name=function_name,
             runtime=Runtime.python3_9,
             role=lambda_su_role,
         )
-        queue_arn = sqs_queue_arn(sqs_queue)
-        destination_config = {
-            "OnSuccess": {"Destination": queue_arn},
-            "OnFailure": {"Destination": queue_arn},
-        }
+        get_fn_result = lambda_client.get_function(FunctionName=function_name)
+        fn_arn = get_fn_result["Configuration"]["FunctionArn"]
 
-        # adding event invoke config
+        create_lambda_function(
+            handler_file=TEST_LAMBDA_PYTHON_ECHO,
+            func_name=function_name_2,
+            runtime=Runtime.python3_9,
+            role=lambda_su_role,
+        )
+        get_fn_result_2 = lambda_client.get_function(FunctionName=function_name_2)
+        fn_arn_2 = get_fn_result_2["Configuration"]["FunctionArn"]
+
+        # one version and one alias
+
+        fn_version_result = lambda_client.publish_version(FunctionName=function_name)
+        snapshot.match("fn_version_result", fn_version_result)
+        fn_version = fn_version_result["Version"]
+
+        fn_alias_result = lambda_client.create_alias(
+            FunctionName=function_name, Name="eventinvokealias", FunctionVersion=fn_version
+        )
+        snapshot.match("fn_alias_result", fn_alias_result)
+        fn_alias = fn_alias_result["Name"]
+
+        fake_arn = "arn:aws:lambda:eu-west-1:000000001111:function:doesnotexist"
+
+        with pytest.raises(lambda_client.exceptions.ResourceNotFoundException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName="doesnotexist", MaximumRetryAttempts=1
+            )
+        snapshot.match("put_functionname_name_notfound", e.value.response)
+
+        with pytest.raises(lambda_client.exceptions.ResourceNotFoundException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName=fake_arn, MaximumRetryAttempts=1
+            )
+        snapshot.match("put_functionname_arn_notfound", e.value.response)
+
+        with pytest.raises(lambda_client.exceptions.InvalidParameterValueException) as e:
+            lambda_client.put_function_event_invoke_config(FunctionName="doesnotexist")
+        snapshot.match("put_functionname_nootherargs", e.value.response)
+
+        with pytest.raises(lambda_client.exceptions.InvalidParameterValueException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName=function_name,
+                DestinationConfig={"OnSuccess": {"Destination": fake_arn}},
+            )
+        snapshot.match("put_destination_lambda_doesntexist", e.value.response)
+
+        with pytest.raises(lambda_client.exceptions.InvalidParameterValueException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName=function_name, DestinationConfig={"OnSuccess": {"Destination": fn_arn}}
+            )
+        snapshot.match("put_destination_recursive", e.value.response)
+
         response = lambda_client.put_function_event_invoke_config(
-            FunctionName=function_name,
-            MaximumRetryAttempts=2,
-            MaximumEventAgeInSeconds=123,
-            DestinationConfig=destination_config,
+            FunctionName=function_name, DestinationConfig={"OnSuccess": {"Destination": fn_arn_2}}
         )
-        cleanups.append(
-            lambda: lambda_client.delete_function_event_invoke_config(FunctionName=function_name)
-        )
-        snapshot.match("put_function_event_invoke_config", response)
+        snapshot.match("put_destination_other_lambda", response)
 
-        # over writing event invoke config
+        # add to $LATEST
         response = lambda_client.put_function_event_invoke_config(
-            FunctionName=function_name,
-            MaximumRetryAttempts=2,
-            DestinationConfig=destination_config,
+            FunctionName=function_name, MaximumRetryAttempts=1
         )
-        snapshot.match("put_function_event_invoke_config_overwritemaxeventage", response)
+        snapshot.match("put_destination_latest", response)
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, Qualifier="$LATEST", MaximumRetryAttempts=1
+        )
+        snapshot.match("put_destination_latest_explicit_qualifier", response)
 
-        # updating event invoke config
-        response = lambda_client.update_function_event_invoke_config(
-            FunctionName=function_name,
-            MaximumRetryAttempts=1,
+        # add to version TODO: ARN as function_name here as well
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, Qualifier=fn_version, MaximumRetryAttempts=1
         )
-        snapshot.match("put_function_event_invoke_config_maxattempt1", response)
+        snapshot.match("put_destination_version", response)
+
+        # add to alias
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, Qualifier=fn_alias, MaximumRetryAttempts=1
+        )
+        snapshot.match("put_alias_functionname_qualifier", response)
+        with pytest.raises(lambda_client.exceptions.ResourceNotFoundException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName=function_name,
+                Qualifier=f"{fn_alias}doesnotexist",
+                MaximumRetryAttempts=1,
+            )
+        snapshot.match("put_alias_doesnotexist", e.value.response)
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=fn_alias_result["AliasArn"], MaximumRetryAttempts=1
+        )
+        snapshot.match("put_alias_qualifiedarn", response)
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=fn_alias_result["AliasArn"], Qualifier=fn_alias, MaximumRetryAttempts=1
+        )
+        snapshot.match("put_alias_qualifiedarn_qualifier", response)
+        with pytest.raises(lambda_client.exceptions.InvalidParameterValueException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName=fn_alias_result["AliasArn"],
+                Qualifier=f"{fn_alias}doesnotexist",
+                MaximumRetryAttempts=1,
+            )
+        snapshot.match("put_alias_qualifiedarn_qualifierconflict", e.value.response)
+
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=f"{function_name}:{fn_alias}", MaximumRetryAttempts=1
+        )
+        snapshot.match("put_alias_shorthand", response)
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=f"{function_name}:{fn_alias}", Qualifier=fn_alias, MaximumRetryAttempts=1
+        )
+        snapshot.match("put_alias_shorthand_qualifier", response)
+
+        with pytest.raises(lambda_client.exceptions.InvalidParameterValueException) as e:
+            lambda_client.put_function_event_invoke_config(
+                FunctionName=f"{function_name}:{fn_alias}",
+                Qualifier=f"{fn_alias}doesnotexist",
+                MaximumRetryAttempts=1,
+            )
+        snapshot.match("put_alias_shorthand_qualifierconflict", e.value.response)
+
+        # apparently this also works with function numbers (not in the docs!)
+        response = lambda_client.put_function_event_invoke_config(
+            FunctionName=f"{function_name}:{fn_version}", MaximumRetryAttempts=1
+        )
+        snapshot.match("put_version_shorthand", response)
+
+        # overwrite existing (=> no conflict)
+        # first create a config with both values set, then overwrite it with only one value set
+        first_overwrite_response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, MaximumRetryAttempts=2, MaximumEventAgeInSeconds=60
+        )
+        snapshot.match("put_pre_overwrite", first_overwrite_response)
+        second_overwrite_response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, MaximumRetryAttempts=0
+        )
+        snapshot.match("put_post_overwrite", second_overwrite_response)
+        second_overwrite_existing_response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, MaximumRetryAttempts=0
+        )
+        snapshot.match("second_overwrite_existing_response", second_overwrite_existing_response)
+        get_postoverwrite_response = lambda_client.get_function_event_invoke_config(
+            FunctionName=function_name
+        )
+        snapshot.match("get_post_overwrite", get_postoverwrite_response)
+        assert get_postoverwrite_response["MaximumRetryAttempts"] == 0
+        assert "MaximumEventAgeInSeconds" not in get_postoverwrite_response
+
+        pre_update_response = lambda_client.put_function_event_invoke_config(
+            FunctionName=function_name, MaximumRetryAttempts=2, MaximumEventAgeInSeconds=60
+        )
+        snapshot.match("pre_update_response", pre_update_response)
+        update_response = lambda_client.update_function_event_invoke_config(
+            FunctionName=function_name, MaximumRetryAttempts=0
+        )
+        snapshot.match("update_response", update_response)
+
+        update_response_existing = lambda_client.update_function_event_invoke_config(
+            FunctionName=function_name, MaximumRetryAttempts=0
+        )
+        snapshot.match("update_response_existing", update_response_existing)
+
+        get_postupdate_response = lambda_client.get_function_event_invoke_config(
+            FunctionName=function_name
+        )
+        assert get_postupdate_response["MaximumRetryAttempts"] == 0
+        assert get_postupdate_response["MaximumEventAgeInSeconds"] == 60
+
+        list_response = lambda_client.list_function_event_invoke_configs(FunctionName=function_name)
+        snapshot.match("list_configs", list_response)
+
+        paged_response = lambda_client.list_function_event_invoke_configs(
+            FunctionName=function_name, MaxItems=2
+        )  # 2 out of 3
+        snapshot.match("list_paged", paged_response)
+
+        delete_latest = lambda_client.delete_function_event_invoke_config(
+            FunctionName=function_name, Qualifier="$LATEST"
+        )
+        snapshot.match("delete_latest", delete_latest)
+        delete_version = lambda_client.delete_function_event_invoke_config(
+            FunctionName=function_name, Qualifier=fn_version
+        )
+        snapshot.match("delete_version", delete_version)
+        delete_alias = lambda_client.delete_function_event_invoke_config(
+            FunctionName=function_name, Qualifier=fn_alias
+        )
+        snapshot.match("delete_alias", delete_alias)
+
+        list_response_postdelete = lambda_client.list_function_event_invoke_configs(
+            FunctionName=function_name
+        )
+        snapshot.match("list_configs_postdelete", list_response_postdelete)
+
+        # TODO: think about testing FunctionName/Qualifier combinations with other methods than put_
 
 
 class TestLambdaReservedConcurrency:
+    @pytest.mark.aws_validated
+    @pytest.mark.skip_snapshot_verify(condition=is_old_provider)
+    def test_function_concurrency(self, lambda_client, create_lambda_function, snapshot):
+        """Testing the api of the put function concurrency action"""
+
+        function_name = f"lambda_func-{short_uid()}"
+        create_lambda_function(
+            handler_file=TEST_LAMBDA_PYTHON_ECHO,
+            func_name=function_name,
+            runtime=Runtime.python3_9,
+        )
+        # TODO botocore.errorfactory.InvalidParameterValueException:
+        #  An error occurred (InvalidParameterValueException) when calling the PutFunctionConcurrency operation: Specified ReservedConcurrentExecutions for function decreases account's UnreservedConcurrentExecution below its minimum value of [50].
+        response = lambda_client.put_function_concurrency(
+            FunctionName=function_name, ReservedConcurrentExecutions=1
+        )
+        snapshot.match("put_function_concurrency", response)
+        response = lambda_client.get_function_concurrency(FunctionName=function_name)
+        snapshot.match("get_function_concurrency", response)
+        lambda_client.delete_function_concurrency(FunctionName=function_name)
+
+
+class TestLambdaProvisionedConcurrency:
 
     # TODO: make this more robust & add snapshot
     @pytest.mark.skip(reason="very slow (only execute when needed)")
@@ -1050,30 +1226,8 @@ class TestLambdaReservedConcurrency:
         # TODO: why is this flaky?
         # assert lambda_client.get_function(FunctionName=func_name, Qualifier='$LATEST')['Configuration']['RevisionId'] == lambda_client.get_function(FunctionName=func_name, Qualifier=first_ver['Version'])['Configuration']['RevisionId']
 
-    @pytest.mark.aws_validated
-    @pytest.mark.skip_snapshot_verify(condition=is_old_provider)
-    def test_function_concurrency(self, lambda_client, create_lambda_function, snapshot):
-        """Testing the api of the put function concurrency action"""
-
-        function_name = f"lambda_func-{short_uid()}"
-        create_lambda_function(
-            handler_file=TEST_LAMBDA_PYTHON_ECHO,
-            func_name=function_name,
-            runtime=Runtime.python3_9,
-        )
-        # TODO botocore.errorfactory.InvalidParameterValueException:
-        #  An error occurred (InvalidParameterValueException) when calling the PutFunctionConcurrency operation: Specified ReservedConcurrentExecutions for function decreases account's UnreservedConcurrentExecution below its minimum value of [50].
-        response = lambda_client.put_function_concurrency(
-            FunctionName=function_name, ReservedConcurrentExecutions=1
-        )
-        snapshot.match("put_function_concurrency", response)
-        response = lambda_client.get_function_concurrency(FunctionName=function_name)
-        snapshot.match("get_function_concurrency", response)
-        lambda_client.delete_function_concurrency(FunctionName=function_name)
-
-
-class TestLambdaProvisionedConcurrency:
-    @pytest.mark.skip(reason="very slow (only execute when needed)")
+    @pytest.mark.skipif(condition=is_aws(), reason="very slow (only execute when needed)")
+    @pytest.mark.skipif(condition=is_old_provider(), reason="not supported")
     @pytest.mark.aws_validated
     def test_lambda_provisioned_concurrency_moves_with_alias(
         self, lambda_client, logs_client, create_lambda_function, snapshot
