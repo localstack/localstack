85331c182751060fab1085ef1314dab6965809eb
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-09-27 10:57:39 -0500

    cfn-opensearch test added

--- tests/integration/cloudformation/test_cloudformation_opensearch.py
+++ tests/integration/cloudformation/test_cloudformation_opensearch.py
@@ -5,23 +5,16 @@ import pytest
 from localstack.utils.strings import short_uid
 
 
-@pytest.mark.aws_validated
-def test_domain(deploy_cfn_template, opensearch_client, cfn_client, snapshot):
-    # snapshot.add_transformer(snapshot.transform.resource_name())
-    # snapshot.add_transformer(snapshot.transform.key_value("Name", "trigger-name"))
-    # snapshot.add_transformer(snapshot.transform.cloudformation_api())
-
+# Domain deployment in AWS takes too much time
+@pytest.mark.only_localstack
+def test_domain(deploy_cfn_template, opensearch_client, cfn_client):
     name = f"domain-{short_uid()}"
 
     template_path = os.path.join(os.path.dirname(__file__), "../templates/opensearch_domain.yml")
-    stack = deploy_cfn_template(
+    deploy_cfn_template(
         template_path=template_path,
         parameters={"domainName": name},
     )
-    snapshot.match("stack_outputs", stack.outputs)
-
-    description = cfn_client.describe_stack_resources(StackName=stack.stack_name)
-    snapshot.match("stack_resource_description", description["StackResources"])
 
     domain = opensearch_client.describe_domain(DomainName=name)
-    snapshot.match("domain", domain["DomainStatus"])
+    assert domain["DomainStatus"]
