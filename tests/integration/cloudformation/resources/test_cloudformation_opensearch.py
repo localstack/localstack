a8be9c958d861d871ef956966942919b3f28e057
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-10-07 11:16:32 -0500

    more fix errors

--- tests/integration/cloudformation/resources/test_cloudformation_opensearch.py
+++ tests/integration/cloudformation/resources/test_cloudformation_opensearch.py
@@ -6,7 +6,7 @@ from localstack.utils.strings import short_uid
 def test_domain(deploy_cfn_template, opensearch_client, cfn_client):
     name = f"domain-{short_uid()}"
 
-    template_path = os.path.join(os.path.dirname(__file__), "../templates/opensearch_domain.yml")
+    template_path = os.path.join(os.path.dirname(__file__), "../../templates/opensearch_domain.yml")
     deploy_cfn_template(
         template_path=template_path,
         parameters={"domainName": name},
