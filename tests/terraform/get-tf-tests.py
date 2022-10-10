162e8d705a629025a8edf7b53c58c51dd9f62ac3
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-10-10 16:24:58 +0200

    implement test partitioning for TF tests

--- tests/terraform/get-tf-tests.py
+++ tests/terraform/get-tf-tests.py
@@ -3,13 +3,25 @@ import sys
 import yaml
 
 
-def print_test_names(service):
+def print_test_names(service_name, partition_name):
     with open("tests/terraform/terraform-tests.yaml") as f:
         dct = yaml.load(f, Loader=yaml.FullLoader)
-        tests = dct.get(service)
-        # exits if no tests are specified under service in yaml file
+
+        if not partition_name:
+            # If not partition is given, the tests are directly in the service section
+            tests = dct.get(service_name)
+        else:
+            # Otherwise, we select the tests in the specific partition
+            partitions = dct.get(service_name)
+            if not partitions:
+                # Exit if there are no partitions
+                sys.exit(1)
+            tests = partitions.get(partition_name)
+
+        # exits if no tests were found
         if not tests:
             sys.exit(1)
+
         if len(tests) == 1:
             print(tests[0])
         else:
@@ -17,8 +29,13 @@ def print_test_names(service):
 
 
 if __name__ == "__main__":
-    # not tests should run if no arguments are provided
-    if len(sys.argv) != 2:
-        sys.exit(1)
+    if len(sys.argv) == 2:
+        service_name = sys.argv[1]
+        partition_name = None
+    elif len(sys.argv) == 3:
+        service_name = sys.argv[1]
+        partition_name = sys.argv[2]
     else:
-        print_test_names(service=sys.argv[1])
+        sys.exit(1)
+
+    print_test_names(service_name=service_name, partition_name=partition_name)
