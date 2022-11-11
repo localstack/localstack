56437c8dc6da985eb4989f675fd9578fa398fe7a
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2022-11-11 13:31:09 -0500

    test to update and prevent deletion

--- tests/integration/cloudformation/api/test_stack_policies.py
+++ tests/integration/cloudformation/api/test_stack_policies.py
@@ -3,6 +3,7 @@ import os
 
 import botocore.exceptions
 import pytest
+import yaml
 
 from localstack.utils.files import load_file
 from localstack.utils.strings import short_uid
@@ -135,49 +136,94 @@ def test_prevent_update(resource_type, cfn_client, deploy_cfn_template):
         cfn_client.delete_stack(StackName=stack.stack_name)
 
 
-# @pytest.mark.parametrize("resource_type, resource_id", ["AWS::S3::Bucket", "AWS::SNS::Topic"], ["bucket123", "topic123"])
-# def test_delete_by_type(resource_type, resource_id, cfn_client, deploy_cfn_template):
-#     template = load_file(
-#         os.path.join(os.path.dirname(__file__), "../../templates/stack_policy_test.yaml")
-#     )
-#     stack = deploy_cfn_template(
-#         template=template,
-#         parameters={"TopicName": f"topic-{short_uid()}", "BucketName": f"bucket-{short_uid()}"},
-#     )
-#     policy = {
-#         "Statement": [
-#             {
-#                 "Effect": "Deny",
-#                 "Action": "Update:*",
-#                 "Principal": "*",
-#                 "Resource": "*",
-#                 "Condition": {"StringEquals": {"ResourceType": [resource_type]}},
-#             }
-#         ]
-#     }
-#     cfn_client.set_stack_policy(StackName=stack.stack_name, StackPolicyBody=json.dumps(policy))
-#
-#     cfn_client.update_stack(
-#         StackName=stack.stack_name,
-#         TemplateBody=template,
-#     )
-#
-#
-#     def _assert_failing_update_state():
-#         events = cfn_client.describe_stack_events(StackName=stack.stack_name)["StackEvents"]
-#         failed_event_update = [
-#             event for event in events if event["ResourceStatus"] == "UPDATE_FAILED"
-#         ]
-#         assert failed_event_update
-#         assert "Action denied by stack policy" in failed_event_update[0]["ResourceStatusReason"]
-#
-#     try:
-#         retry(_assert_failing_update_state, retries=5, sleep=2, sleep_before=2)
-#     finally:
-#         progress_is_finished = False
-#         while not progress_is_finished:
-#             status = cfn_client.describe_stacks(StackName=stack.stack_name)["Stacks"][0][
-#                 "StackStatus"
-#             ]
-#             progress_is_finished = "PROGRESS" not in status
-#         cfn_client.delete_stack(StackName=stack.stack_name)
+@pytest.mark.aws_validated
+@pytest.mark.skip(reason="Not implemented")
+@pytest.mark.parametrize(
+    "resource",
+    [{"id": "bucket123", "type": "AWS::S3::Bucket"}, {"id": "topic123", "type": "AWS::SNS::Topic"}],
+)
+def test_prevent_deletion(resource, cfn_client, deploy_cfn_template):
+    template = load_file(
+        os.path.join(os.path.dirname(__file__), "../../templates/stack_policy_test.yaml")
+    )
+    stack = deploy_cfn_template(
+        template=template,
+        parameters={"TopicName": f"topic-{short_uid()}", "BucketName": f"bucket-{short_uid()}"},
+    )
+    policy = {
+        "Statement": [
+            {
+                "Effect": "Deny",
+                "Action": "Update:Delete",
+                "Principal": "*",
+                "Resource": "*",
+                "Condition": {"StringEquals": {"ResourceType": [resource["type"]]}},
+            }
+        ]
+    }
+    cfn_client.set_stack_policy(StackName=stack.stack_name, StackPolicyBody=json.dumps(policy))
+
+    template_dict = yaml.load(template)
+    del template_dict["Resources"][resource["id"]]
+    template = yaml.dump(template_dict)
+
+    cfn_client.update_stack(
+        StackName=stack.stack_name,
+        TemplateBody=template,
+        Parameters=[
+            {"ParameterKey": "TopicName", "ParameterValue": f"new-topic-{short_uid()}"},
+            {"ParameterKey": "BucketName", "ParameterValue": f"new-bucket-{short_uid()}"},
+        ],
+    )
+
+    def _assert_failing_update_state():
+        events = cfn_client.describe_stack_events(StackName=stack.stack_name)["StackEvents"]
+        failed_event_update = [
+            event
+            for event in events
+            if "ResourceStatusReason" in event
+            and "Action denied by stack policy" in event["ResourceStatusReason"]
+        ]
+        assert failed_event_update
+
+    try:
+        retry(_assert_failing_update_state, retries=6, sleep=2, sleep_before=2)
+    finally:
+        progress_is_finished = False
+        while not progress_is_finished:
+            status = cfn_client.describe_stacks(StackName=stack.stack_name)["Stacks"][0][
+                "StackStatus"
+            ]
+            progress_is_finished = "PROGRESS" not in status
+        cfn_client.delete_stack(StackName=stack.stack_name)
+
+
+@pytest.mark.aws_validated
+@pytest.mark.skip(reason="Not implemented")
+def test_update_with_policy(deploy_cfn_template, cfn_client):
+    template = load_file(
+        os.path.join(os.path.dirname(__file__), "../../templates/stack_policy_test.yaml")
+    )
+    stack = deploy_cfn_template(
+        template=template,
+        parameters={"TopicName": f"topic-{short_uid()}", "BucketName": f"bucket-{short_uid()}"},
+    )
+    policy = {
+        "Statement": [
+            {
+                "Effect": "Deny",
+                "Action": "Update:*",
+                "Principal": "*",
+                "Resource": "*",
+                "Condition": {"StringEquals": {"ResourceType": ["AWS::EC2::Subnet"]}},
+            },
+            {"Effect": "Allow", "Action": "Update:*", "Principal": "*", "Resource": "*"},
+        ]
+    }
+    cfn_client.set_stack_policy(StackName=stack.stack_name, StackPolicyBody=json.dumps(policy))
+    deploy_cfn_template(
+        is_update=True,
+        stack_name=stack.stack_name,
+        template=template,
+        parameters={"TopicName": f"topic-{short_uid()}", "BucketName": f"bucket-{short_uid()}"},
+    )
