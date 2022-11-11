d41e88a9d691d07e1f1c4ad44af0b501c8338fef
Dominik Schubert <dominik.schubert91@gmail.com>
2022-11-11 11:53:44 +0100

    test ASF lambda provider in default CI

--- tests/integration/awslambda/test_lambda_common.py
+++ tests/integration/awslambda/test_lambda_common.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import platform
 
 import pytest
 
@@ -37,6 +38,9 @@ def snapshot_transformers(snapshot):
 @pytest.mark.skipif(
     condition=is_old_provider(),
     reason="Local executor does not support the majority of the runtimes",
+)
+@pytest.mark.skipif(
+    condition=platform.machine() != "x86_64", reason="build process doesn't support arm64 right now"
 )
 class TestLambdaRuntimesCommon:
     """
