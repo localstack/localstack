191efd6e1dcbf9a831a560b2d56ad5dbc9ccc062
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-11-16 14:02:49 +0100

    remove legacy sqs provider

--- localstack/services/sqs/legacy/plugins.py
+++
@@ -1,8 +0,0 @@
-from localstack.packages import package
-
-
-@package(name="elasticmq")
-def elasticmq_package():
-    from localstack.services.sqs.legacy.packages import elasticmq_package
-
-    return elasticmq_package
