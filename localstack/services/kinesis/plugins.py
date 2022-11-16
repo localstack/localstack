35fa294a2be91132a2aa0a72a26a0e901445b355
Alexander Rashed <alexander.rashed@localstack.cloud>
2022-11-16 13:06:08 +0100

    remove legacy kinesis provider and kinesalite support

--- localstack/services/kinesis/plugins.py
+++ localstack/services/kinesis/plugins.py
@@ -1,13 +1,6 @@
 from localstack.packages import Package, package
 
 
-@package(name="kinesalite")
-def kinesalite_package() -> Package:
-    from localstack.services.kinesis.packages import kinesalite_package
-
-    return kinesalite_package
-
-
 @package(name="kinesis-mock")
 def kinesismock_package() -> Package:
     from localstack.services.kinesis.packages import kinesismock_package
