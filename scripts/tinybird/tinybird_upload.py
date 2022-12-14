97de4a8cf20f5d529d97477bfe30369408f260f6
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2022-12-14 10:38:28 +0100

    adapted schema

--- scripts/tinybird/tinybird_upload.py
+++ scripts/tinybird/tinybird_upload.py
@@ -3,7 +3,7 @@ import datetime
 import json
 import os
 
-# import requests
+import requests
 
 DATA_TO_KEEP: list[str] = [
     "service",
@@ -24,31 +24,90 @@ def convert_to_bool(input):
     return True if input.lower() == "true" else False
 
 
-def send_data_to_tinybird(data: list[str], data_name: str, token: str):
-    pass
-    # data_to_send = "\n".join(data)
-    # r = requests.post(
-    #     "https://api.tinybird.co/v0/events",
-    #     params={
-    #         "name": data_name,
-    #         "token": token,
-    #     },
-    #     data=data_to_send,
-    # )
-    # print(f"sent data to tinybird, status code: {r.status_code}:\n{r.text}\n")
+def send_data_to_tinybird(data: list[str], data_name: str):
+    token = os.environ.get("TINYBIRD_PARITY_ANALYTICS_TOKEN", "")
+    if not token:
+        print("missing ENV 'TINYBIRD_PARITY_ANALYTICS_TOKEN', no token defined. cannot send data")
+        return
+
+    data_to_send = "\n".join(data)
+    r = requests.post(
+        "https://api.tinybird.co/v0/events",
+        params={
+            "name": data_name,
+            "token": token,
+        },
+        data=data_to_send,
+    )
+    print(f"sent data to tinybird, status code: {r.status_code}:\n{r.text}\n")
 
 
-def send_metric_report(path: str, timestamp: str, token: str):
+def send_metadata_for_build(build_id: str, timestamp: str):
+    """
+    sends the metadata for the build to tinybird
+
+    SCHEMA >
+    `build_id` String `json:$.build_id`,
+    `timestamp` DateTime `json:$.timestamp`,
+    `branch` String `json:$.branch`,
+    `build_url` String `json:$.build_url`,
+    `pull_requests` String `json:$.pull_requests`,
+    `build_num` String `json:$.build_num`,
+    `workflow_id` String `json:$.workflow_id`
+
+    CircleCI env examples:
+    CIRCLE_PULL_REQUESTS=https://github.com/localstack/localstack/pull/7324
+    CIRCLE_BRANCH=coverage-tinybird
+    CIRCLE_BUILD_NUM=78206
+    CIRCLE_BUILD_URL=https://circleci.com/gh/localstack/localstack/78206
+    CIRCLE_WORKFLOW_ID=b86a4bc4-bcd1-4170-94d6-4af66846c1c1
+    """
+    data = {
+        "build_id": build_id,
+        "timestamp": timestamp,
+        "branch": os.environ.get("CIRCLE_BRANCH", ""),
+        "build_url": os.environ.get("CIRCLE_BUILD_URL", ""),
+        "pull_requests": os.environ.get("CIRCLE_PULL_REQUESTS", ""),
+        "build_num": os.environ.get("CIRCLE_BUILD_NUM", ""),
+        "workflow_id": os.environ.get("CIRCLE_WORKFLOW_ID", ""),
+    }
+    data_to_send = [json.dumps(data)]
+    send_data_to_tinybird(data_to_send, data_name="tests_raw_builds")
+
+
+def send_metric_report(path: str, timestamp: str):
+    """
+
+    SCHEMA >
+    `timestamp` DateTime `json:$.timestamp`,
+    `ls_source` String `json:$.ls_source`,
+    `test_node_id` String `json:$.test_node_id`,
+    `operation` String `json:$.operation`,
+    `origin` String `json:$.origin`,
+    `parameters` String `json:$.parameters`,
+    `response_code` String `json:$.response_code`,
+    `service` String `json:$.service`,
+    `snapshot` UInt8 `json:$.snapshot`,
+    `snapshot_skipped_paths` String `json:$.snapshot_skipped_paths`,
+    `aws_validated` UInt8 `json:$.aws_validated`,
+    `xfail` UInt8 `json:$.xfail`,
+    `build_id` String `json:$.build_id`
+    """
     tmp: list[str] = []
     count: int = 0
-    data_name = "events_example"  # TODO
+    data_name = "tests_raw"
+    build_id = os.environ.get("CIRCLE_WORKFLOW_ID", "")
+    send_metadata_for_build(build_id, timestamp)
     with open(path, "r") as csv_obj:
         reader_obj = csv.DictReader(csv_obj)
         data_to_remove = [field for field in reader_obj.fieldnames if field not in DATA_TO_KEEP]
         for row in reader_obj:
             count = count + 1
-            # add timestamp
+
+            # add timestamp, build_id, ls_source
             row["timestamp"] = timestamp
+            row["build_id"] = build_id
+            row["ls_source"] = "community"
 
             # remove data we are currently not interested in
             for field in data_to_remove:
@@ -61,20 +120,31 @@ def send_metric_report(path: str, timestamp: str, token: str):
             tmp.append(json.dumps(row))
             if len(tmp) == 500:
                 # send data in batches
-                send_data_to_tinybird(tmp, data_name=data_name, token=token)
+                send_data_to_tinybird(tmp, data_name=data_name)
                 tmp.clear()
 
         if tmp:
             # send last batch
-            send_data_to_tinybird(tmp, data_name=data_name, token=token)
+            send_data_to_tinybird(tmp, data_name=data_name)
 
     print(f"---> processed {count} rows from community test coverage {path}")
 
 
-def send_implemented_coverage(file: str, timestamp: str, type: str, token: str):
+def send_implemented_coverage(file: str, timestamp: str, type: str):
+    """
+    SCHEMA >
+                `build_id` String `json:$.build_id`,
+        `timestamp` DateTime `json:$.timestamp`,
+                `ls_source` String `json:$.ls_source`,
+        `operation` String `json:$.operation`,
+        `service` String `json:$.service`,
+        `status_code` Int32 `json:$.status_code`,
+        `error_code` String `json:$.error_code`,
+        `is_implemented` UInt8 `json:$.is_implemented`
+    """
     tmp: list[str] = []
     count: int = 0
-    data_name = "steffy_hackt"  # TODO
+    data_name = "implementation_coverage"
     with open(file, "r") as csv_obj:
         reader_obj = csv.DictReader(csv_obj)
         for row in reader_obj:
@@ -88,16 +158,18 @@ def send_implemented_coverage(file: str, timestamp: str, type: str, token: str):
 
             # add timestamp and source
             row["timestamp"] = timestamp
-            row["source"] = type
+            row["ls_source"] = type
+            row["build_id"] = os.environ.get("CIRCLE_WORKFLOW_ID", "")
+
             tmp.append(json.dumps(row))
             if len(tmp) == 500:
                 # send data in batches
-                send_data_to_tinybird(tmp, data_name=data_name, token=token)
+                send_data_to_tinybird(tmp, data_name=data_name)
                 tmp.clear()
 
         if tmp:
             # send last batch
-            send_data_to_tinybird(tmp, data_name=data_name, token=token)
+            send_data_to_tinybird(tmp, data_name=data_name)
     print(f"---> processed {count} rows from {file} ({type})")
 
 
@@ -129,11 +201,9 @@ def main():
     # create one timestamp that will be used for all the data sent
     timestamp: str = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
-    send_metric_report(metric_report, timestamp, token)
-    send_implemented_coverage(
-        community_impl_coverage, timestamp=timestamp, type="community", token=token
-    )
-    send_implemented_coverage(pro_impl_coverage, timestamp=timestamp, type="pro", token=token)
+    send_metric_report(metric_report, timestamp)
+    send_implemented_coverage(community_impl_coverage, timestamp=timestamp, type="community")
+    send_implemented_coverage(pro_impl_coverage, timestamp=timestamp, type="pro")
 
 
 if __name__ == "__main__":
