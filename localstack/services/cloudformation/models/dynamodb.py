11b2d66baa44b27803c869aab9849e792ce3d2ea
Giovanni Grano <me@giograno.com>
2022-12-15 10:02:56 +0100

    Fix CFN deploy of DynamoDB with ContributorInsightsSpecification (#7322)

--- localstack/services/cloudformation/models/dynamodb.py
+++ localstack/services/cloudformation/models/dynamodb.py
@@ -22,27 +22,26 @@ def get_ddb_provisioned_throughput(params, **kwargs):
     return args
 
 
-def get_ddb_global_sec_indexes(params, **kwargs):
-    args = params.get("GlobalSecondaryIndexes")
+def get_ddb_global_sec_indexes(params: dict, **kwargs) -> list | None:
+    args: list = params.get("GlobalSecondaryIndexes")
     is_ondemand = params.get("BillingMode") == "PAY_PER_REQUEST"
-    if args:
-        for index in args:
-            provisioned_throughput = index.get("ProvisionedThroughput")
-            if is_ondemand and provisioned_throughput is None:
-                pass  # optional for API calls
-            elif provisioned_throughput is not None:
-                # convert types
-                if isinstance(provisioned_throughput["ReadCapacityUnits"], str):
-                    provisioned_throughput["ReadCapacityUnits"] = int(
-                        provisioned_throughput["ReadCapacityUnits"]
-                    )
-                if isinstance(provisioned_throughput["WriteCapacityUnits"], str):
-                    provisioned_throughput["WriteCapacityUnits"] = int(
-                        provisioned_throughput["WriteCapacityUnits"]
-                    )
-            else:
-                raise Exception("Can't specify ProvisionedThroughput with PAY_PER_REQUEST")
+    if not args:
+        return
 
+    for index in args:
+        # we ignore ContributorInsightsSpecification as not supported yet in DynamoDB and CloudWatch
+        index.pop("ContributorInsightsSpecification", None)
+        provisioned_throughput = index.get("ProvisionedThroughput")
+        if is_ondemand and provisioned_throughput is None:
+            pass  # optional for API calls
+        elif provisioned_throughput is not None:
+            # convert types
+            if isinstance((read_units := provisioned_throughput["ReadCapacityUnits"]), str):
+                provisioned_throughput["ReadCapacityUnits"] = int(read_units)
+            if isinstance((write_units := provisioned_throughput["WriteCapacityUnits"]), str):
+                provisioned_throughput["WriteCapacityUnits"] = int(write_units)
+        else:
+            raise Exception("Can't specify ProvisionedThroughput with PAY_PER_REQUEST")
     return args
 
 
