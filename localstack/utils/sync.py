ba7c1741b58f13754e4c946a061d4106302a1bfb
Dominik Schubert <dominik.schubert91@gmail.com>
2022-12-16 15:16:05 +0100

    fix CI issues

--- localstack/utils/sync.py
+++ localstack/utils/sync.py
@@ -4,12 +4,17 @@ import sys
 import threading
 import time
 from collections import defaultdict
-from typing import Callable, ParamSpec, TypeVar
+from typing import Callable, TypeVar
 
 if sys.version_info >= (3, 8):
     from typing import Literal
 else:
     from typing_extensions import Literal
+
+if sys.version_info >= (3, 10):
+    from typing import ParamSpec
+else:
+    from typing_extensions import ParamSpec
 
 
 class ShortCircuitWaitException(Exception):
