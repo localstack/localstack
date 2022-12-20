c8e58721960217771728a55c06f658d873d1fed0
Dominik Schubert <dominik.schubert91@gmail.com>
2022-12-20 12:19:44 +0100

    prepare move of awslambda cfn resources

--- localstack/services/cloudformation/models/awslambda.py
+++ localstack/services/cloudformation/models/awslambda.py
@@ -22,6 +22,7 @@ from localstack.utils.common import (
     select_attributes,
     to_bytes,
 )
+from localstack.utils.strings import short_uid
 from localstack.utils.testutil import create_zip_file
 
 
@@ -464,4 +465,77 @@ class LambdaCodeSigningConfig(GenericBaseModel):
                     "CodeSigningConfigArn": _arn,
                 },
             },
+        }
+
+
+# TODO: test
+class LambdaLayerVersion(GenericBaseModel):
+    @staticmethod
+    def cloudformation_type():
+        return "AWS::Lambda::LayerVersion"
+
+    def get_physical_resource_id(self, attribute=None, **kwargs):
+        return self.state.get("LayerVersionArn")
+
+    def fetch_state(self, stack_name, resources):
+        layer_name = self.resolve_refs_recursively(
+            stack_name, self.props.get("LayerName"), resources
+        )
+        # TODO extract region name if layer_name is an ARN
+        client = aws_stack.connect_to_service("lambda")
+        layers = client.list_layer_versions(LayerName=layer_name).get("LayerVersions", [])
+        return layers[-1] if layers else None
+
+    @staticmethod
+    def add_defaults(resource, stack_name: str):
+        resource["Properties"]["LayerName"] = (
+            resource["Properties"].get("LayerName") or f"layer-{short_uid()}"
+        )
+
+    @staticmethod
+    def get_deploy_templates():
+        return {"create": {"function": "publish_layer_version"}}
+
+
+# TODO: test
+# TODO: remove inheritance
+class LambdaLayerVersionPermission(LambdaPermission):
+    @staticmethod
+    def cloudformation_type():
+        return "AWS::Lambda::LayerVersionPermission"
+
+    def fetch_state(self, stack_name, resources):
+        props = self.props
+        props["LayerVersionArn"] = self.resolve_refs_recursively(
+            stack_name, props["LayerVersionArn"], resources
+        )
+        layer_name, version_number = self.layer_name_and_version(props)
+        layer_arn = arns.lambda_layer_arn(layer_name)
+        layer_arn_qualified = "%s:%s" % (layer_arn, version_number)
+        result = self.do_fetch_state(layer_name, layer_arn_qualified)
+        return result
+
+    @staticmethod
+    def layer_name_and_version(params):
+        layer_arn = params.get("LayerVersionArn", "")
+        parts = layer_arn.split(":")
+        layer_name = parts[6] if ":" in layer_arn else layer_arn
+        version_number = int(parts[7] if len(parts) > 7 else 1)  # TODO fetch latest version number
+        return layer_name, version_number
+
+    @classmethod
+    def get_deploy_templates(cls):
+        def layer_permission_params(params, **kwargs):
+            layer_name, version_number = cls.layer_name_and_version(params)
+            result = select_attributes(params, ["Action", "Principal"])
+            result["StatementId"] = short_uid()
+            result["LayerName"] = layer_name
+            result["VersionNumber"] = version_number
+            return result
+
+        return {
+            "create": {
+                "function": "add_layer_version_permission",
+                "parameters": layer_permission_params,
+            }
         }
