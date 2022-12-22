833d74df9074c5a0985a6f0738ac0e9fcf8e1be4
Benjamin Simon <benjh.simon@gmail.com>
2022-12-22 20:48:36 +0100

    try dirty fix for unit tests - better

--- localstack/aws/protocol/op_router.py
+++ localstack/aws/protocol/op_router.py
@@ -31,11 +31,11 @@ class _HttpOperation(NamedTuple):
 
     @staticmethod
     def from_operation(op: OperationModel) -> "_HttpOperation":
-        uri = (
-            op.http.get("requestUri")
-            if op.service_model.service_name != "s3"
-            else op.http.get("authPath")
-        )
+        if "authPath" in op.http:
+            uri = op.http["authPath"].rstrip("/")
+        else:
+            uri = op.http.get("requestUri")
+
         method = op.http.get("method")
         deprecated = op.deprecated
 
