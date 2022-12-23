e85e4acb6f0e4b5965039f74a8ef1df9fbc4c7e7
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2022-12-23 14:49:02 +0100

    fix NIT - require account_id + region_name for get_moto_logs_backend

--- localstack/services/logs/models.py
+++ localstack/services/logs/models.py
@@ -3,15 +3,10 @@ from typing import Dict
 from moto.logs.models import LogsBackend as MotoLogsBackend
 from moto.logs.models import logs_backends as moto_logs_backend
 
-from localstack.constants import DEFAULT_AWS_ACCOUNT_ID
 from localstack.services.stores import AccountRegionBundle, BaseStore, CrossRegionAttribute
-from localstack.utils.aws import aws_stack
 
 
-def get_moto_logs_backend(account_id: str = None, region_name: str = None) -> MotoLogsBackend:
-    account_id = account_id or DEFAULT_AWS_ACCOUNT_ID
-    region_name = region_name or aws_stack.get_region()
-
+def get_moto_logs_backend(account_id: str, region_name: str) -> MotoLogsBackend:
     return moto_logs_backend[account_id][region_name]
 
 
