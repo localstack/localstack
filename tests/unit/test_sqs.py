e5bfe39be0b899ff40b360c5644be2cf87973e2a
Thomas Rausch <thomas@thrau.at>
2022-12-30 15:13:06 +0100

    add sqs developer endpoints at /_aws/sqs (#7393)

--- tests/unit/test_sqs.py
+++ tests/unit/test_sqs.py
@@ -5,7 +5,7 @@ import localstack.services.sqs.models
 from localstack.aws.api.sqs import Message
 from localstack.services.sqs import provider
 from localstack.services.sqs.constants import DEFAULT_MAXIMUM_MESSAGE_SIZE
-from localstack.services.sqs.utils import get_message_attributes_md5
+from localstack.services.sqs.utils import get_message_attributes_md5, parse_queue_url
 from localstack.utils.common import convert_to_printable_chars
 
 
@@ -65,3 +65,81 @@ def test_check_message_size():
     message_body = "a"
     message_attributes = {"k": {"DataType": "String", "StringValue": "x"}}
     provider.check_message_size(message_body, message_attributes, DEFAULT_MAXIMUM_MESSAGE_SIZE)
+
+
+def test_parse_queue_url_valid():
+    assert parse_queue_url("http://localhost:4566/queue/eu-central-2/000000000001/my-queue") == (
+        "eu-central-2",
+        "000000000001",
+        "my-queue",
+    )
+    assert parse_queue_url("http://localhost:4566/000000000001/my-queue") == (
+        None,
+        "000000000001",
+        "my-queue",
+    )
+    assert parse_queue_url("http://localhost/000000000001/my-queue") == (
+        None,
+        "000000000001",
+        "my-queue",
+    )
+
+    assert parse_queue_url("http://localhost/queue/eu-central-2/000000000001/my-queue") == (
+        "eu-central-2",
+        "000000000001",
+        "my-queue",
+    )
+
+    assert parse_queue_url(
+        "http://queue.localhost.localstack.cloud:4566/000000000001/my-queue"
+    ) == (
+        "us-east-1",
+        "000000000001",
+        "my-queue",
+    )
+
+    assert parse_queue_url(
+        "http://eu-central-2.queue.localhost.localstack.cloud:4566/000000000001/my-queue"
+    ) == (
+        "eu-central-2",
+        "000000000001",
+        "my-queue",
+    )
+
+    # in this case, eu-central-2.foobar... is treated as a regular hostname
+    assert parse_queue_url(
+        "http://eu-central-2.foobar.localhost.localstack.cloud:4566/000000000001/my-queue"
+    ) == (
+        None,
+        "000000000001",
+        "my-queue",
+    )
+
+
+def test_parse_queue_url_invalid():
+    with pytest.raises(ValueError):
+        parse_queue_url("http://localhost:4566/my-queue")
+
+    with pytest.raises(ValueError):
+        parse_queue_url("http://localhost:4566/eu-central-1/000000000001/my-queue")
+
+    with pytest.raises(ValueError):
+        parse_queue_url("http://localhost:4566/foobar/eu-central-1/000000000001/my-queue")
+
+    with pytest.raises(ValueError):
+        parse_queue_url(
+            "http://eu-central-2.queue.localhost.localstack.cloud:4566/000000000001/my-queue/foobar"
+        )
+
+    with pytest.raises(ValueError):
+        parse_queue_url(
+            "http://queue.localhost.localstack.cloud:4566/us-east-1/000000000001/my-queue"
+        )
+
+    with pytest.raises(ValueError):
+        assert parse_queue_url("queue.localhost.localstack.cloud:4566/000000000001/my-queue")
+
+    with pytest.raises(ValueError):
+        assert parse_queue_url(
+            "http://foo.bar.queue.localhost.localstack.cloud:4566/000000000001/my-queue"
+        )
