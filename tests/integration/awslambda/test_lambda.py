3b7a568f0483954711b88a1d47370aef02fd32c4
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-01-10 09:12:45 +0100

    Bump nodejs lambda runtime

--- tests/integration/awslambda/test_lambda.py
+++ tests/integration/awslambda/test_lambda.py
@@ -691,7 +691,7 @@ class TestLambdaPermissions:
         create_lambda_function(
             func_name=function_name,
             zip_file=testutil.create_zip_file(TEST_LAMBDA_URL, get_content=True),
-            runtime=Runtime.nodejs14_x,
+            runtime=Runtime.nodejs18_x,
             handler="lambda_url.handler",
         )
         url_config = lambda_client.create_function_url_config(
