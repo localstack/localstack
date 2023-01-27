1a2a55862a07c64a7f81abb305c1c1253b633ee5
Dominik Schubert <dominik.schubert91@gmail.com>
2023-01-27 09:47:28 +0100

    fix java non-transparent endpoint usage

--- tests/integration/awslambda/functions/common/endpointinjection/java/src/src/main/java/echo/Handler.java
+++ tests/integration/awslambda/functions/common/endpointinjection/java/src/src/main/java/echo/Handler.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.ListQueuesRequest;
@@ -21,11 +22,13 @@ public class Handler implements RequestHandler<Map<String, String>, String> {
     private AmazonSQS getSqsClient() {
         if (Objects.equals(System.getenv("CONFIGURE_CLIENT"), "1")) {
             String endpointUrl = "http://" + System.getenv("LOCALSTACK_HOSTNAME") + ":" + System.getenv("EDGE_PORT");
-            URI uri = URI.create(endpointUrl);
-            return AmazonSQSClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("test", "test"))).withRegion(Regions.US_EAST_1).build();
+            return AmazonSQSClientBuilder.standard()
+                                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpointUrl, "us-east-1"))
+                                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("test", "test")))
+                                .build();
         } else {
             return AmazonSQSClientBuilder.standard()
-                               .withCredentials(new EnvironmentVariableCredentialsProvider())
+                               .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("test", "test")))
                                .withRegion(Regions.US_EAST_1)
                                .build();
         }
