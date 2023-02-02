71c8f64c8ca0fc89cacef1ccb401ca9988698c17
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-02-01 19:00:00 -0500

    wip test for stack events

--- tests/integration/cloudformation/api/test_stacks.py
+++ tests/integration/cloudformation/api/test_stacks.py
@@ -528,3 +528,13 @@ def test_update_termination_protection(deploy_cfn_template, cfn_client, sns_clie
     )
     res = cfn_client.describe_stacks(StackName=stack.stack_name)
     snapshot.match("describe-stack-2", res)
+
+
+@pytest.mark.aws_validated
+def test_events_types(deploy_cfn_template, cfn_client, snapshot):
+    template_path = os.path.join(os.path.dirname(__file__), "../../templates/sfn_apigateway.yaml")
+    stack = deploy_cfn_template(template_path=template_path)
+    events = cfn_client.describe_stack_events(StackName=stack.stack_name)["StackEvents"]
+
+    resource_types = set([event["ResourceType"] for event in events])
+    assert resource_types
