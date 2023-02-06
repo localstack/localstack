9a4cf8045b28d5f20c4aa7733b4912e828476958
Thomas Rausch <thomas@thrau.at>
2023-02-06 21:06:12 +0100

    add preserve-host functionality for Proxy

--- localstack/http/proxy.py
+++ localstack/http/proxy.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from typing import Mapping, Union
+from urllib.parse import urlparse
 
 from werkzeug import Request, Response
 from werkzeug.datastructures import Headers
@@ -24,6 +25,9 @@ def forward(
 
 
 class Proxy(HttpClient):
+
+    preserve_host: bool
+
     def __init__(self, forward_base_url: str, client: HttpClient = None):
         """
         Creates a new HTTP Proxy which can be used to forward incoming requests according to the configuration.
@@ -33,12 +37,14 @@ class Proxy(HttpClient):
         """
         self.forward_base_url = forward_base_url
         self.client = client or SimpleRequestsClient()
+        self.preserve_host = True
 
-    def request(self, request: Request) -> Response:
+    def request(self, request: Request, server: str | None = None) -> Response:
         """
         Compatibility with HttpClient interface. A call is equivalent to ``Proxy.forward(request, None, None)``.
 
         :param request: the request to proxy
+        :param server: ignored for a proxy, since the server is already set by `forward_base_url`.
         :return: the proxied response
         """
         return self.forward(request)
@@ -72,7 +78,12 @@ class Proxy(HttpClient):
             forward_path = "/" + forward_path.lstrip("/")
 
         proxy_request = _copy_request(request, self.forward_base_url, forward_path, headers)
-        return self.client.request(proxy_request)
+
+        if self.preserve_host:
+            proxy_request.headers["Host"] = request.host
+
+        target = urlparse(self.forward_base_url)
+        return self.client.request(proxy_request, server=f"{target.scheme}://{target.netloc}")
 
     def close(self):
         self.client.close()
@@ -152,5 +163,8 @@ def _copy_request(
     # explicitly set the path in the environment and in the newly created request
     if path is not None:
         new_request.environ["RAW_URI"] = path or "/"
+
+    # copy headers s.t. they are no longer immutable (by default, EnvironHeaders are used)
+    new_request.headers = Headers(new_request.headers)
 
     return new_request
