a8f1e66889f8c7f02e24da96824ddaed1ac86c08
Alexander Rashed <alexander.rashed@localstack.cloud>
2023-02-06 17:23:09 +0100

    move deprecated to presence env vars

--- localstack/runtime/analytics.py
+++ localstack/runtime/analytics.py
@@ -34,14 +34,16 @@ PRESENCE_ENV_VAR = ["LAMBDA_FALLBACK_URL", "LAMBDA_FORWARD_URL"]
 def _publish_config_as_analytics_event():
     from localstack.deprecations import DEPRECATIONS
 
-    deprecated_env_vars = [dep.env_var for dep in DEPRECATIONS]
-    env_vars = list(set(TRACKED_ENV_VAR + deprecated_env_vars))
+    env_vars = list(TRACKED_ENV_VAR)
 
     for key, value in os.environ.items():
         if key.startswith("PROVIDER_OVERRIDE_"):
             env_vars.append(key)
 
     env_vars = {key: os.getenv(key) for key in env_vars}
-    present_env_vars = {env_var: 1 for env_var in PRESENCE_ENV_VAR if os.getenv(env_var)}
+
+    deprecated_env_vars = [dep.env_var for dep in DEPRECATIONS if dep.env_var not in env_vars]
+    presence_env_vars = list(set(PRESENCE_ENV_VAR + deprecated_env_vars))
+    present_env_vars = {env_var: 1 for env_var in presence_env_vars if os.getenv(env_var)}
 
     log.event("config", env_vars=env_vars, set_vars=present_env_vars)
