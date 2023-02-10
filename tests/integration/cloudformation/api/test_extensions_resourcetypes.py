b171aee7a7ff260ef4e510f1735737698e12dead
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-02-10 16:09:31 -0500

    test for extension resource deployment

--- tests/integration/cloudformation/api/test_extensions_resourcetypes.py
+++ tests/integration/cloudformation/api/test_extensions_resourcetypes.py
@@ -1,9 +1,12 @@
 import os
 
+import pytest
+
 from localstack.utils.strings import short_uid
 
 
 class TestExtensionResourceTypes:
+    @pytest.mark.skip(reason="feature not supported")
     def test_crud_resource_type(
         self, deploy_cfn_template, cfn_client, s3_client, snapshot, cleanups
     ):
@@ -81,16 +84,18 @@ class TestExtensionResourceTypes:
         )
         snapshot.match("deregister_response", deregister_response)
 
+    @pytest.mark.skip(reason="test not completed")
     def test_deploy_resource_type(
         self, deploy_cfn_template, cfn_client, s3_client, snapshot, cleanups
     ):
-        role_stack = "localstack-testing-testresource-role-stack"
+        role_stack = "localstack-testing-deployableresource-role-stack"
 
         execution_role_arn = deploy_cfn_template(
             stack_name=role_stack,
             template_path=os.path.join(
                 os.path.dirname(__file__), "../../templates/registry/resource-role.yml"
             ),
+            parameters={"ResourceType": "resource/LocalStack-Testing-DeployableResource"},
         ).outputs["ExecutionRoleArn"]
 
         infra_stack = "CloudFormationManagedUploadInfrastructure"
@@ -99,7 +104,7 @@ class TestExtensionResourceTypes:
             template_path=os.path.join(
                 os.path.dirname(__file__), "../../templates/registry/upload-infra.yml"
             ),
-            max_wait=500,
+            max_wait=300,
         )
 
         bucket_name = stack.outputs["CloudFormationManagedUploadBucketName"]
@@ -107,17 +112,17 @@ class TestExtensionResourceTypes:
 
         artifact_path = os.path.join(
             os.path.dirname(__file__),
-            "../artifacts/extensions/resourcetypes/localstack-testing-testresource.zip",
+            "../artifacts/extensions/resourcetypes/localstack-testing-deployableresource.zip",
         )
-        key_name = "localstack-testing-testresource-2023-02-07T19-16-19.zip"
+        key_name = "localstack-testing-deployableresource-2023-02-07T19-16-19.zip"
         s3_client.upload_file(artifact_path, bucket_name, key_name)
         register_response = cfn_client.register_type(
             Type="RESOURCE",
-            TypeName="LocalStack::Testing::TestResource",
+            TypeName="LocalStack::Testing::DeployableResource",
             SchemaHandlerPackage=f"s3://{bucket_name}/{key_name}",
             LoggingConfig={
                 "LogRoleArn": logs_role_arn,
-                "LogGroupName": "ls-testing-testresource-logs",
+                "LogGroupName": "ls-testing-deployable-logs",
             },
             ExecutionRoleArn=execution_role_arn,
             ClientRequestToken=f"token-{short_uid()}",
@@ -136,7 +141,7 @@ class TestExtensionResourceTypes:
         )
 
         stack = deploy_cfn_template(
-            template_path=template_path, parameters={"Name": f"name-{short_uid()}"}
+            template_path=template_path, parameters={"Name": f"name-{short_uid()}"}, max_wait=300
         )
         resources = cfn_client.describe_stack_resources(StackName=stack.stack_name)[
             "StackResources"
@@ -144,3 +149,8 @@ class TestExtensionResourceTypes:
 
         snapshot.match("outputs", stack.outputs)
         snapshot.match("resource_description", resources[0])
+
+        # TODO Add to cleanups
+        cfn_client.deregister_type(
+            TypeName="LocalStack::Testing::DeployableResource", Type="RESOURCE"
+        )
