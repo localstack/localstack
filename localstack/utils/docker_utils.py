4ceeb185ed1caf30f43b479c6da882006ec75667
Daniel Fangl <daniel.fangl@localstack.cloud>
2023-02-20 14:20:23 +0100

    use special portrange subclass, add test

--- localstack/utils/docker_utils.py
+++ localstack/utils/docker_utils.py
@@ -23,7 +23,6 @@ PORT_START = 0
 PORT_END = 65536
 RANDOM_PORT_START = 1024
 RANDOM_PORT_END = 65536
-reserved_docker_ports = PortRange(PORT_START, PORT_END)
 
 
 def is_docker_sdk_installed() -> bool:
@@ -135,11 +134,6 @@ def get_host_path_for_path_in_docker(path):
     return path
 
 
-def is_port_available_for_containers(port: int) -> bool:
-    """Check whether the given port can be bound by containers and is not currently reserved"""
-    return not is_container_port_reserved(port) and container_port_can_be_bound(port)
-
-
 def container_port_can_be_bound(port: int) -> bool:
     """Determine whether a port can be bound by Docker containers"""
     ports = PortMappings()
@@ -163,6 +157,31 @@ def container_port_can_be_bound(port: int) -> bool:
             "Unexpected output when attempting to determine container port status: %s", result[0]
         )
     return True
+
+
+class _DockerPortRange(PortRange):
+    """
+    PortRange which checks whether the port can be bound on the host instead of inside the container.
+    """
+
+    def _try_reserve_port(self, port: int, duration: int) -> int:
+        """Checks if the given port is currently not reserved."""
+        if not self.is_port_reserved(port) and container_port_can_be_bound(port):
+            # reserve the port for a short period of time
+            self._ports_cache[port] = "__reserved__"
+            if duration:
+                self._ports_cache.set_expiry(port, duration)
+            return port
+        else:
+            raise PortNotAvailableException(f"The given port ({port}) is already reserved.")
+
+
+reserved_docker_ports = _DockerPortRange(PORT_START, PORT_END)
+
+
+def is_port_available_for_containers(port: int) -> bool:
+    """Check whether the given port can be bound by containers and is not currently reserved"""
+    return not is_container_port_reserved(port) and container_port_can_be_bound(port)
 
 
 def reserve_container_port(port: int, duration: int = None):
@@ -192,9 +211,11 @@ def reserve_available_container_port(
     retries = 10
     for i in range(retries):
         port = _random_port()
-        if container_port_can_be_bound(port):
+        try:
             reserve_container_port(port, duration=duration)
             return port
+        except PortNotAvailableException as e:
+            LOG.debug("Could not bind port %s, trying the next one: %s", port, e)
 
     raise PortNotAvailableException(
         f"Unable to determine available Docker container port after {retries} retries"
