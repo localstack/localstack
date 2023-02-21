18520d5ea5d7f7d7851daf15317b5ec9978ae71b
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2023-02-21 09:44:01 +0000

    add rest api fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -180,21 +180,6 @@ def localstack_runtime():
 
 
 @pytest.fixture
-def create_rest_apigw(apigateway_client):
-    rest_api_ids = []
-
-    def _create_apigateway_function(*args, **kwargs):
-        api_id, name, root_id = create_rest_api(apigateway_client, **kwargs)
-        rest_api_ids.append(api_id)
-        return api_id, name, root_id
-
-    yield _create_apigateway_function
-
-    for rest_api_id in rest_api_ids:
-        delete_rest_api(apigateway_client, restApiId=rest_api_id)
-
-
-@pytest.fixture
 def import_apigw(apigateway_client):
     rest_api_ids = []
 
