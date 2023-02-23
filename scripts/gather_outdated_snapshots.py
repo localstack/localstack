1c27ced11f93cb34897979774909168b6a93dad3
Bernhard Matyas <bernhard.matyas@localstack.cloud>
2023-02-23 14:15:04 +0100

    jsonify data

--- scripts/gather_outdated_snapshots.py
+++ scripts/gather_outdated_snapshots.py
@@ -1,38 +1,41 @@
 import datetime
 import json
 import os
+import sys
 
-integration_test_path = "../tests/integration/"
 
-
-def get_outdated_snapshots(date_limit: str) -> list[str]:
+def get_outdated_snapshots_for_directory(path: str, date_limit: str) -> dict:
     """
-    Fetches all snapshots that were recorded before the given date_limit.
+    Fetches all snapshots that were recorded before the given date_limit
+    :param path: The directory where to look for snapshot files.
     :param date_limit: All snapshots whose recorded-date is older than date-limit are considered outdated.
             Format of the date-string must be "DD-MM-YYYY".
     :return: List of test names whose snapshots (if any) are outdated.
     """
-    # FIXME: this might have side effects
-    os.chdir(integration_test_path)
-    date_limit = datetime.datetime.strptime(date_limit, "%d-%m-%Y")
-    outdated_snapshots = []
 
-    for file in os.listdir():
+    date_limit = datetime.datetime.strptime(date_limit, "%d-%m-%Y")
+    result = {"date": date_limit}
+    outdated_snapshots = []
+    if not path.endswith("/"):
+        path = f"{path}/"
+    for file in os.listdir(path):
         if not file.endswith(".snapshot.json"):
             continue
-        with open(file) as f:
+        with open(f"{path}{file}") as f:
             json_content: dict = json.load(f)
             for name, snapshot in json_content.items():
                 date = snapshot.get("recorded-date")
                 date = datetime.datetime.strptime(date, "%d-%m-%Y, %H:%M:%S")
                 if date < date_limit:
                     outdated_snapshots.append(name)
-    return outdated_snapshots
+    result["count"] = len(outdated_snapshots)
+    result["outdated_snapshots"] = outdated_snapshots
+    return result
 
 
 if __name__ == "__main__":
-    snapshots = get_outdated_snapshots("14-09-2022")
-    print(f"Number of outdated snapshots: {len(snapshots)}\n")
-    print(f"Names of outdated snapshots: {snapshots}")
-    join = " ".join(snapshots)
-    pass
+    snapshots = get_outdated_snapshots_for_directory(sys.argv[1], sys.argv[2])
+    # turn the list of snapshots into a whitespace separated string usable by pytest
+    join = " ".join(snapshots["outdated_snapshots"])
+    snapshots["pytest_executable_list"] = join
+    print(json.dumps(snapshots, default=str))
