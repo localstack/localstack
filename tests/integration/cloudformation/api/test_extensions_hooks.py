b9dd0ddf7a3b25a904047945bc6f0572051b6739
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-03-02 16:27:49 -0500

    wip hook test

--- tests/integration/cloudformation/api/test_extensions_hooks.py
+++ tests/integration/cloudformation/api/test_extensions_hooks.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import botocore
@@ -9,12 +10,12 @@ from localstack.utils.strings import short_uid
 
 class TestExtensionsHooks:
     @pytest.mark.skip(reason="test not finished")
-    def test_hook_deployment(self, register_extension, cfn_client, snapshot):
+    def test_hook_deployment(self, register_extension, cfn_client, snapshot, cleanups):
         artifact_path = os.path.join(
             os.path.dirname(__file__),
             "../artifacts/extensions/hooks/localstack-testing-deployablehook.zip",
         )
-        register_extension(
+        extension = register_extension(
             extension_type="HOOK",
             extension_name="LocalStack::Testing::DeployableHook",
             artifact_path=artifact_path,
@@ -27,14 +28,31 @@ class TestExtensionsHooks:
             )
         )
 
+        extension_configuration = json.dumps(
+            {
+                "CloudFormationConfiguration": {
+                    "HookConfiguration": {"TargetStacks": "ALL", "FailureMode": "FAIL"}
+                }
+            }
+        )
+
+        cfn_client.set_type_configuration(
+            TypeArn=extension["TypeArn"], Configuration=extension_configuration
+        )
+
         stack_name = f"stack-{short_uid()}"
         cfn_client.create_stack(
             StackName=stack_name,
             TemplateBody=template,
             Parameters=[{"ParameterKey": "Name", "ParameterValue": f"bucket-{short_uid()}"}],
         )
+        cleanups.append(lambda: cfn_client.delete_stack(StackName=stack_name))
 
-        with pytest.raises(botocore.exceptions.WaiterError) as e:
+        with pytest.raises(botocore.exceptions.WaiterError):
             cfn_client.get_waiter("stack_create_complete").wait(StackName=stack_name)
 
-        snapshot.match("error", e.value)
+        events = cfn_client.describe_stack_events(StackName=stack_name)["StackEvents"]
+
+        failed_events = [e for e in events if "HookStatusReason" in e]
+        snapshot.add_transformer(snapshot.transform.cloudformation_api())
+        snapshot.match("event_error", failed_events[0])
