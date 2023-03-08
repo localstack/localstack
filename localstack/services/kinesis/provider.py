9dd06d6db6700f4f0021c4fb2975c0a8d2889c64
Thomas Rausch <thomas@thrau.at>
2023-03-09 00:55:41 +0100

    refactor kinesis state lifecycle hook

--- localstack/services/kinesis/provider.py
+++ localstack/services/kinesis/provider.py
@@ -3,7 +3,6 @@ import os
 import time
 from random import random
 
-import localstack.services.kinesis.kinesis_starter as starter
 from localstack import config
 from localstack.aws.accounts import get_aws_account_id
 from localstack.aws.api import RequestContext
@@ -27,6 +26,7 @@ from localstack.aws.api.kinesis import (
     SubscribeToShardOutput,
 )
 from localstack.constants import LOCALHOST
+from localstack.services.kinesis.kinesis_mock_server import KinesisServerManager
 from localstack.services.kinesis.models import KinesisStore, kinesis_stores
 from localstack.services.plugins import ServiceLifecycleHook
 from localstack.state import AssetDirectory, StateVisitor
@@ -35,6 +35,7 @@ from localstack.utils.time import now_utc
 
 LOG = logging.getLogger(__name__)
 MAX_SUBSCRIPTION_SECONDS = 300
+SERVER_STARTUP_TIMEOUT = 120
 
 
 def find_stream_for_consumer(consumer_arn):
@@ -48,19 +49,35 @@ def find_stream_for_consumer(consumer_arn):
 
 
 class KinesisProvider(KinesisApi, ServiceLifecycleHook):
+
+    server_manager: KinesisServerManager
+
+    def __init__(self):
+        self.server_manager = KinesisServerManager()
+
     def accept_state_visitor(self, visitor: StateVisitor):
         visitor.visit(kinesis_stores)
         visitor.visit(AssetDirectory(os.path.join(config.dirs.data, "kinesis")))
 
-    @staticmethod
-    def get_store(account_id: str, region_name: str) -> KinesisStore:
-        return kinesis_stores[account_id][region_name]
+    def on_before_state_load(self):
+        # no need to restart servers, since that happens lazily in `server_manager.get_server_for_account`.
+        self.server_manager.shutdown_all()
+
+    def on_before_state_reset(self):
+        self.server_manager.shutdown_all()
+
+    def on_before_stop(self):
+        self.server_manager.shutdown_all()
 
     def get_forward_url(self):
         """Return the URL of the backend Kinesis server to forward requests to"""
         account_id = get_aws_account_id()
-        starter.start_kinesis(account_id=account_id)
-        return f"http://{LOCALHOST}:{starter.get_server(account_id).port}"
+        server = self.server_manager.get_server_for_account(account_id)
+        return f"http://{LOCALHOST}:{server.port}"
+
+    @staticmethod
+    def get_store(account_id: str, region_name: str) -> KinesisStore:
+        return kinesis_stores[account_id][region_name]
 
     def subscribe_to_shard(
         self,
