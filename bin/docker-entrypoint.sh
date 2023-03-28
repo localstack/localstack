d800794587a69982e810fd1e7ac308865d0e1a8b
Alexander Rashed <alexander.rashed@localstack.cloud>
2023-03-28 14:49:12 +0200

    remove ligh / full image warnings

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -20,17 +20,6 @@ if [[ $LOCALSTACK_API_KEY ]] && ! compgen -G /usr/lib/localstack/.*pro-version >
     echo "  See: https://github.com/localstack/localstack/issues/7882"
     echo "============================================================================"
     echo ""
-elif [[ -f /usr/lib/localstack/.light-version ]] || [[ -f /usr/lib/localstack/.full-version ]]; then
-    echo "WARNING"
-    echo "============================================================================"
-    echo "  It seems you are using a deprecated image (localstack/localstack-light"
-    echo "  or localstack/localstack-full)."
-    echo "  These images are deprecated and will be removed in the future."
-    echo "  To fix this warning, use localstack/localstack instead."
-    echo ""
-    echo "  See: https://github.com/localstack/localstack/issues/7257"
-    echo "============================================================================"
-    echo ""
 fi
 
 # Strip `LOCALSTACK_` prefix in environment variables name (except
@@ -56,9 +45,11 @@ fi
 LOG_DIR=/var/lib/localstack/logs
 test -d ${LOG_DIR} || mkdir -p ${LOG_DIR}
 
-# run runtime init hooks BOOT stage before starting localstack
-test -d /etc/localstack/init/boot.d && /opt/code/localstack/.venv/bin/python -m localstack.runtime.init BOOT
+# activate the virtual environment
+source /opt/code/localstack/.venv/bin/activate
 
-source .venv/bin/activate
+# run runtime init hooks BOOT stage before starting localstack
+test -d /etc/localstack/init/boot.d && python3 -m localstack.runtime.init BOOT
+
 # run the localstack supervisor. it's important to run with `exec` and don't use pipes so signals are handled correctly
 exec localstack-supervisor.py
