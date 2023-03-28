e9c144a7be16536600606695e1ecd750b9463b79
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-03-28 19:28:05 +0200

    Add lambda config validations

--- localstack/services/awslambda/plugins.py
+++ localstack/services/awslambda/plugins.py
@@ -1,4 +1,10 @@
+import logging
+
+from localstack.config import LAMBDA_DOCKER_NETWORK
 from localstack.packages import Package, package
+from localstack.runtime import hooks
+
+LOG = logging.getLogger(__name__)
 
 
 @package(name="awslambda-go-runtime")
@@ -20,3 +26,11 @@ def lambda_java_libs() -> Package:
     from localstack.services.awslambda.packages import lambda_java_libs_package
 
     return lambda_java_libs_package
+
+
+@hooks.on_infra_start()
+def validate_configuration() -> None:
+    if LAMBDA_DOCKER_NETWORK == "host":
+        LOG.warning(
+            "The configuration LAMBDA_DOCKER_NETWORK=host is currently not supported with the new lambda provider."
+        )
