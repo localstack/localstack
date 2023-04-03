d937b444293001fa3cd395fb0159fe1ad601919d
Giovanni Grano <me@giograno.com>
2023-04-03 18:51:57 -0400

    Implements #8053
            If a message is provided to NotImplementedError, that gets
            printed in the logs instead of the default message

--- localstack/aws/skeleton.py
+++ localstack/aws/skeleton.py
@@ -153,8 +153,8 @@ class Skeleton:
             return self.dispatch_request(context, instance)
         except ServiceException as e:
             return self.on_service_exception(context, e)
-        except NotImplementedError:
-            return self.on_not_implemented_error(context)
+        except NotImplementedError as e:
+            return self.on_not_implemented_error(context, e)
 
     def dispatch_request(self, context: RequestContext, instance: ServiceRequest) -> HttpResponse:
         operation = context.operation
@@ -189,11 +189,14 @@ class Skeleton:
             exception, context.operation, context.request.headers
         )
 
-    def on_not_implemented_error(self, context: RequestContext) -> HttpResponse:
+    def on_not_implemented_error(
+        self, context: RequestContext, exception: NotImplementedError
+    ) -> HttpResponse:
         """
         Called by invoke if either the dispatch table did not contain an entry for the operation, or the service
         provider raised a NotImplementedError
         :param context: the request context
+        :param exception: the NotImplementedError that was raised
         :return: an HttpResponse object
         """
         operation = context.operation
@@ -201,7 +204,8 @@ class Skeleton:
 
         action_name = operation.name
         service_name = operation.service_model.service_name
-        message = (
+        exception_message: str | None = exception.args[0] if exception.args else None
+        message = exception_message or (
             f"API action '{action_name}' for service '{service_name}' not yet implemented or pro feature"
             f" - check https://docs.localstack.cloud/user-guide/aws/feature-coverage for further information"
         )
