24344051ba982aa4101eeaf90d61046671524620
lukqw <lukqw@users.noreply.github.com>
2023-04-05 11:23:18 +0200

    resolve circular dep

--- localstack/utils/coverage_docs.py
+++ localstack/utils/coverage_docs.py
@@ -1,17 +1,21 @@
-from localstack.services.plugins import SERVICE_PLUGINS
-
 COVERAGE_LINK_BASE = "https://docs.localstack.cloud/references/coverage/"
 MESSAGE_TEMPLATE = (
-    f"API '%s'for service '%s' not yet implemented or pro feature"
+    f"API %sfor service '%s' not yet implemented or pro feature"
     f" - please check {COVERAGE_LINK_BASE}%s for further information"
 )
 
 
 def get_coverage_link_for_service(service_name: str, action_name: str) -> str:
+    from localstack.services.plugins import SERVICE_PLUGINS
+
     available_services = SERVICE_PLUGINS.list_available()
 
     if service_name not in available_services:
-        return MESSAGE_TEMPLATE % (f"action {action_name} ", service_name, "")
+        return MESSAGE_TEMPLATE % ("", service_name, "")
 
     else:
-        return MESSAGE_TEMPLATE % (action_name, service_name, f"coverage_{service_name}/")
+        return MESSAGE_TEMPLATE % (
+            f"action '{action_name}' ",
+            service_name,
+            f"coverage_{service_name}/",
+        )
