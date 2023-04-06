bb91d6fc2b41f1edeb502988fac772c18470a735
jcanabarro <joao@querylayer.com>
2023-04-06 09:28:41 -0300

    feat: Adding plaintext max size bytes constant
    Adding plaintext max size bytes constant

--- localstack/services/kms/provider.py
+++ localstack/services/kms/provider.py
@@ -131,6 +131,8 @@ VALID_OPERATIONS = [
     "GenerateDataKeyPair",
     "GenerateDataKeyPairWithoutPlaintext",
 ]
+# maximum size allowed by AWS
+PLAINTEXT_MAX_SIZE_BYTES = 4096
 
 
 class ValidationError(CommonServiceException):
@@ -1026,28 +1028,28 @@ class KmsProvider(KmsApi, ServiceLifecycleHook):
     ):
         # max size values extracted from AWS boto3 documentation
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms/client/encrypt.html
-        max_bytes_size = 0
+        max_size_bytes = PLAINTEXT_MAX_SIZE_BYTES
         if key.metadata["KeySpec"] == "RSA_2048" and encryption_algorithm == "RSAES_OAEP_SHA_1":
-            max_bytes_size = 214
+            max_size_bytes = 214
         elif key.metadata["KeySpec"] == "RSA_2048" and encryption_algorithm == "RSAES_OAEP_SHA_256":
-            max_bytes_size = 190
+            max_size_bytes = 190
         elif key.metadata["KeySpec"] == "RSA_3072" and encryption_algorithm == "RSAES_OAEP_SHA_1":
-            max_bytes_size = 342
+            max_size_bytes = 342
         elif key.metadata["KeySpec"] == "RSA_3072" and encryption_algorithm == "RSAES_OAEP_SHA_256":
-            max_bytes_size = 318
+            max_size_bytes = 318
         elif key.metadata["KeySpec"] == "RSA_4096" and encryption_algorithm == "RSAES_OAEP_SHA_1":
-            max_bytes_size = 470
+            max_size_bytes = 470
         elif key.metadata["KeySpec"] == "RSA_4096" and encryption_algorithm == "RSAES_OAEP_SHA_256":
-            max_bytes_size = 446
+            max_size_bytes = 446
 
-        if len(plaintext) > max_bytes_size:
+        if len(plaintext) > max_size_bytes:
             raise ValidationException(
-                f"Algorithm {encryption_algorithm} and key spec {key.metadata['KeySpec']} cannot encrypt data larger than {max_bytes_size} bytes."
+                f"Algorithm {encryption_algorithm} and key spec {key.metadata['KeySpec']} cannot encrypt data larger than {max_size_bytes} bytes."
             )
 
     def _validate_plaintext_max_size(self, plaintext: PlaintextType):
-        # Check if all plaintext are smaller than 4096 bytes independent of they key type.
-        if len(plaintext) > 4096:
+        # Check if any plaintext are smaller than 4096 bytes independent of they key type.
+        if len(plaintext) > PLAINTEXT_MAX_SIZE_BYTES:
             raise ValidationException(
                 "1 validation error detected: Value at 'plaintext' failed to satisfy constraint:"
                 "Member must have length less than or equal to 4096."
