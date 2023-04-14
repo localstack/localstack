67e98d0ab08c2c5c035dfe4d9eb3394778b3a0de
Daniel Fangl <daniel.fangl@localstack.cloud>
2023-04-14 12:00:21 +0200

    add patch to moto which reduces the effort by rewriting requests a lot

--- localstack/services/iam/provider.py
+++ localstack/services/iam/provider.py
@@ -1,9 +1,9 @@
 import json
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Optional
 from urllib.parse import quote
 
-from moto.iam.models import AWSManagedPolicy, IAMBackend, InlinePolicy, Policy
+from moto.iam.models import AccessKey, AWSManagedPolicy, IAMBackend, InlinePolicy, Policy
 from moto.iam.models import Role as MotoRole
 from moto.iam.models import filter_items_with_path_prefix, iam_backends
 from moto.iam.policy_validation import VALID_STATEMENT_ELEMENTS
@@ -15,7 +15,6 @@ from localstack.aws.api.iam import (
     ActionNameType,
     AttachedPermissionsBoundary,
     ContextEntryListType,
-    CreateAccessKeyResponse,
     CreateRoleRequest,
     CreateRoleResponse,
     CreateServiceLinkedRoleResponse,
@@ -374,16 +373,6 @@ class IamProvider(IamApi):
             )
         return response
 
-    def create_access_key(
-        self, context: RequestContext, user_name: existingUserNameType = None
-    ) -> CreateAccessKeyResponse:
-        response = call_moto(context=context)
-        if not config.PARITY_AWS_ACCESS_KEY_ID and (
-            access_key_id := response.get("AccessKey", {}).get("AccessKeyId")
-        ):
-            response["AccessKey"]["AccessKeyId"] = "L" + access_key_id[1:]
-        return response
-
     def get_user(
         self, context: RequestContext, user_name: existingUserNameType = None
     ) -> GetUserResponse:
@@ -508,3 +497,17 @@ def apply_patches():
         except Exception:
             # Actually role can be deleted before policy being deleted in cloudformation
             pass
+
+    @patch(AccessKey.__init__)
+    def access_key__init__(
+        fn,
+        self,
+        user_name: Optional[str],
+        prefix: str,
+        account_id: str,
+        status: str = "Active",
+        **kwargs,
+    ):
+        if not config.PARITY_AWS_ACCESS_KEY_ID:
+            prefix = "L" + prefix[1:]
+        fn(self, user_name, prefix, account_id, status, **kwargs)
--- localstack/services/sts/provider.py
+++ localstack/services/sts/provider.py
@@ -1,13 +1,7 @@
 import logging
 
-from localstack import config
-from localstack.aws.api import RequestContext, handler
-from localstack.aws.api.sts import (
-    AssumeRoleRequest,
-    AssumeRoleResponse,
-    GetCallerIdentityResponse,
-    StsApi,
-)
+from localstack.aws.api import RequestContext
+from localstack.aws.api.sts import GetCallerIdentityResponse, StsApi
 from localstack.services.moto import call_moto
 from localstack.services.plugins import ServiceLifecycleHook
 
@@ -19,15 +13,4 @@ class StsProvider(StsApi, ServiceLifecycleHook):
         response = call_moto(context)
         if "user/moto" in response["Arn"] and "sts" in response["Arn"]:
             response["Arn"] = f"arn:aws:iam::{response['Account']}:root"
-        return response
-
-    @handler("AssumeRole", expand=False)
-    def assume_role(
-        self, context: RequestContext, request: AssumeRoleRequest
-    ) -> AssumeRoleResponse:
-        response = call_moto(context)
-        if not config.PARITY_AWS_ACCESS_KEY_ID and (
-            access_key_id := response.get("Credentials", {}).get("AccessKeyId")
-        ):
-            response["Credentials"]["AccessKeyId"] = "L" + access_key_id[1:]
         return response
