3fc9a7e5d97c4e9561e885aa5f2288997905126d
matt-mercer <449892+matt-mercer@users.noreply.github.com>
2023-05-02 14:31:54 +0100

    add support for python3.10 lambda runtime (#8184)

--- localstack/services/awslambda/invocation/lambda_models.py
+++ localstack/services/awslambda/invocation/lambda_models.py
@@ -66,6 +66,7 @@ IMAGE_MAPPING = {
     "python3.7": "python:3.7",
     "python3.8": "python:3.8",
     "python3.9": "python:3.9",
+    "python3.10": "python:3.10",
     "nodejs12.x": "nodejs:12",
     "nodejs14.x": "nodejs:14",
     "nodejs16.x": "nodejs:16",
