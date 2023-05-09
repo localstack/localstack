41a180d30acd734d217380f9b3f61c81bcb75dd2
Benjamin Simon <benjh.simon@gmail.com>
2023-05-09 13:34:03 +0300

    add special char integration test

--- tests/integration/s3/test_s3.py
+++ tests/integration/s3/test_s3.py
@@ -627,6 +627,20 @@ class TestS3:
         snapshot.match("get_object", obj)
 
     @pytest.mark.aws_validated
+    @pytest.mark.skip_snapshot_verify(paths=["$..ServerSideEncryption"])
+    def test_put_get_object_special_character(self, s3_bucket, aws_client, snapshot):
+        snapshot.add_transformer(snapshot.transform.s3_api())
+        key = "test@key/"
+        resp = aws_client.s3.put_object(Bucket=s3_bucket, Key=key, Body=b"test")
+        snapshot.match("put-object-special-char", resp)
+        resp = aws_client.s3.list_objects_v2(Bucket=s3_bucket)
+        snapshot.match("list-object-special-char", resp)
+        resp = aws_client.s3.get_object(Bucket=s3_bucket, Key=key)
+        snapshot.match("get-object-special-char", resp)
+        aws_client.s3.delete_object(Bucket=s3_bucket, Key=key)
+        snapshot.match("del-object-special-char", resp)
+
+    @pytest.mark.aws_validated
     @pytest.mark.parametrize("delimiter", ["/", "%2F"])
     def test_list_objects_with_prefix(self, s3_create_bucket, delimiter, snapshot, aws_client):
         snapshot.add_transformer(snapshot.transform.s3_api())
