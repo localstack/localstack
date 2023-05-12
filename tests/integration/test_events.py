0e8a7be2998590a283edfd501119585f971251a9
Ben Simon Hartung <42031100+bentsku@users.noreply.github.com>
2023-05-12 23:43:49 +0300

    fix PutEvents non-existent custom EventBus (#8264)

--- tests/integration/test_events.py
+++ tests/integration/test_events.py
@@ -1737,3 +1737,84 @@ class TestEvents:
             )
 
         retry(check_invocation, sleep=5, retries=15)
+
+    @pytest.mark.aws_validated
+    def test_put_events_nonexistent_event_bus(
+        self,
+        aws_client,
+        sqs_create_queue,
+        sqs_queue_arn,
+        events_put_rule,
+        events_allow_event_rule_to_sqs_queue,
+        snapshot,
+    ):
+        default_bus_rule_name = f"rule-{short_uid()}"
+        default_bus_target_id = f"test-target-default-b-{short_uid()}"
+        nonexistent_event_bus = f"event-bus-{short_uid()}"
+
+        snapshot.add_transformer(
+            [
+                snapshot.transform.key_value("MD5OfBody"),  # the event contains a timestamp
+                snapshot.transform.key_value("ReceiptHandle"),
+                snapshot.transform.regex(nonexistent_event_bus, "<custom-event-bus>"),
+            ]
+        )
+        # create SQS queue + add rules & targets so that we can check the default event bus received the message
+        # even if one entry was wrong
+
+        queue_url = sqs_create_queue()
+        queue_arn = sqs_queue_arn(queue_url)
+
+        rule_on_default_bus = events_put_rule(
+            Name=default_bus_rule_name,
+            EventPattern=json.dumps({"detail-type": ["CustomType"], "source": ["MySource"]}),
+            State="ENABLED",
+        )
+
+        events_allow_event_rule_to_sqs_queue(
+            event_rule_arn=rule_on_default_bus["RuleArn"],
+            sqs_queue_arn=queue_arn,
+            sqs_queue_url=queue_url,
+        )
+
+        aws_client.events.put_targets(
+            Rule=default_bus_rule_name,
+            Targets=[{"Id": default_bus_target_id, "Arn": queue_arn}],
+        )
+
+        # create two entries, one with no EventBus specified (so it will target the default one)
+        # and one with a nonexistent EventBusName, which should be ignored
+        entries = [
+            {
+                "Source": "MySource",
+                "DetailType": "CustomType",
+                "Detail": json.dumps({"message": "for the default event bus"}),
+            },
+            {
+                "EventBusName": nonexistent_event_bus,
+                "Source": "MySource",
+                "DetailType": "CustomType",
+                "Detail": json.dumps({"message": "for the custom event bus"}),
+            },
+        ]
+        response = aws_client.events.put_events(Entries=entries)
+        snapshot.match("put-events", response)
+
+        def _get_sqs_messages():
+            resp = aws_client.sqs.receive_message(
+                QueueUrl=queue_url, VisibilityTimeout=0, WaitTimeSeconds=1
+            )
+            msgs = resp.get("Messages")
+            assert len(msgs) == 1
+            aws_client.sqs.delete_message(
+                QueueUrl=queue_url, ReceiptHandle=msgs[0]["ReceiptHandle"]
+            )
+            return msgs
+
+        messages = retry(_get_sqs_messages, retries=5, sleep=0.1)
+        snapshot.match("get-events", messages)
+
+        # try to get the custom EventBus we passed the Event to
+        with pytest.raises(ClientError) as e:
+            aws_client.events.describe_event_bus(Name=nonexistent_event_bus)
+        snapshot.match("non-existent-bus", e.value.response)
