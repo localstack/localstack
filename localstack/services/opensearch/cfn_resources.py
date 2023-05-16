0678a9e7bd37d40e4e11a67ec65929ea43700e94
Simon Walker <simon.walker@localstack.cloud>
2023-05-16 16:53:41 +0100

    Implement opensearch provider

---
+++ localstack/services/opensearch/cfn_resources.py
@@ -0,0 +1,222 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from localstack.services.cloudformation.resource_provider import (
+    OperationStatus,
+    ProgressEvent,
+    ResourceProvider,
+    ResourceRequest,
+    register_resource_provider,
+)
+
+
+@dataclass
+class ZoneAwarenessConfig:
+    AvailabilityZoneCount: Optional[int] = None
+
+
+@dataclass
+class ClusterConfig:
+    InstanceCount: Optional[int] = None
+    WarmEnabled: Optional[bool] = None
+    WarmCount: Optional[int] = None
+    DedicatedMasterEnabled: Optional[bool] = None
+    ZoneAwarenessConfig: Optional[ZoneAwarenessConfig] = None
+    DedicatedMasterCount: Optional[int] = None
+    InstanceType: Optional[str] = None
+    WarmType: Optional[str] = None
+    ZoneAwarenessEnabled: Optional[bool] = None
+    DedicatedMasterType: Optional[str] = None
+
+
+@dataclass
+class SnapshotOptions:
+    AutomatedSnapshotStartHour: Optional[int] = None
+
+
+@dataclass
+class VPCOptions:
+    SecurityGroupIds: Optional[list] = None
+    SubnetIds: Optional[list] = None
+
+
+@dataclass
+class NodeToNodeEncryptionOptions:
+    Enabled: Optional[bool] = None
+
+
+@dataclass
+class DomainEndpointOptions:
+    CustomEndpointCertificateArn: Optional[str] = None
+    CustomEndpointEnabled: Optional[bool] = None
+    EnforceHTTPS: Optional[bool] = None
+    CustomEndpoint: Optional[str] = None
+    TLSSecurityPolicy: Optional[str] = None
+
+
+@dataclass
+class CognitoOptions:
+    Enabled: Optional[bool] = None
+    IdentityPoolId: Optional[str] = None
+    UserPoolId: Optional[str] = None
+    RoleArn: Optional[str] = None
+
+
+@dataclass
+class MasterUserOptions:
+    MasterUserPassword: Optional[str] = None
+    MasterUserName: Optional[str] = None
+    MasterUserARN: Optional[str] = None
+
+
+@dataclass
+class Idp:
+    MetadataContent: Optional[str] = None
+    EntityId: Optional[str] = None
+
+
+@dataclass
+class SAMLOptions:
+    Enabled: Optional[bool] = None
+    Idp: Optional[Idp] = None
+    MasterUserName: Optional[str] = None
+    MasterBackendRole: Optional[str] = None
+    SubjectKey: Optional[str] = None
+    RolesKey: Optional[str] = None
+    SessionTimeoutMinutes: Optional[int] = None
+
+
+@dataclass
+class AdvancedSecurityOptions:
+    Enabled: Optional[bool] = None
+    MasterUserOptions: Optional[MasterUserOptions] = None
+    InternalUserDatabaseEnabled: Optional[bool] = None
+    AnonymousAuthEnabled: Optional[bool] = None
+    SAMLOptions: Optional[SAMLOptions] = None
+    AnonymousAuthDisableDate: Optional[str] = None
+
+
+@dataclass
+class EBSOptions:
+    EBSEnabled: Optional[bool] = None
+    VolumeType: Optional[str] = None
+    Iops: Optional[int] = None
+    VolumeSize: Optional[int] = None
+    Throughput: Optional[int] = None
+
+
+@dataclass
+class EncryptionAtRestOptions:
+    KmsKeyId: Optional[str] = None
+    Enabled: Optional[bool] = None
+
+
+@dataclass
+class ServiceSoftwareOptions:
+    CurrentVersion: Optional[str] = None
+    NewVersion: Optional[str] = None
+    UpdateAvailable: Optional[bool] = None
+    Cancellable: Optional[bool] = None
+    UpdateStatus: Optional[str] = None
+    Description: Optional[str] = None
+    AutomatedUpdateDate: Optional[str] = None
+    OptionalDeployment: Optional[bool] = None
+
+
+@dataclass
+class WindowStartTime:
+    Hours: Optional[int] = None
+    Minutes: Optional[int] = None
+
+
+@dataclass
+class OffPeakWindow:
+    WindowStartTime: Optional[WindowStartTime] = None
+
+
+@dataclass
+class OffPeakWindowOptions:
+    Enabled: Optional[bool] = None
+    OffPeakWindow: Optional[OffPeakWindow] = None
+
+
+@dataclass
+class SoftwareUpdateOptions:
+    AutoSoftwareUpdateEnabled: Optional[bool] = None
+
+
+@dataclass
+class OpenSearchServiceDomainProperties:
+    ClusterConfig: Optional[ClusterConfig] = None
+    DomainName: Optional[str] = None
+    AccessPolicies: Optional[dict] = None
+    EngineVersion: Optional[str] = None
+    AdvancedOptions: Optional[dict] = None
+    LogPublishingOptions: Optional[dict] = None
+    SnapshotOptions: Optional[SnapshotOptions] = None
+    VPCOptions: Optional[VPCOptions] = None
+    NodeToNodeEncryptionOptions: Optional[NodeToNodeEncryptionOptions] = None
+    DomainEndpointOptions: Optional[DomainEndpointOptions] = None
+    CognitoOptions: Optional[CognitoOptions] = None
+    AdvancedSecurityOptions: Optional[AdvancedSecurityOptions] = None
+    DomainEndpoint: Optional[str] = None
+    DomainEndpoints: Optional[dict] = None
+    EBSOptions: Optional[EBSOptions] = None
+    Id: Optional[str] = None
+    Arn: Optional[str] = None
+    DomainArn: Optional[str] = None
+    EncryptionAtRestOptions: Optional[EncryptionAtRestOptions] = None
+    Tags: Optional[list] = None
+    ServiceSoftwareOptions: Optional[ServiceSoftwareOptions] = None
+    OffPeakWindowOptions: Optional[OffPeakWindowOptions] = None
+    SoftwareUpdateOptions: Optional[SoftwareUpdateOptions] = None
+
+
+class OpenSearchServiceDomainAllProperties(OpenSearchServiceDomainProperties):
+    id: Optional[str] = None
+    physical_resource_id: Optional[str] = None
+
+
+@register_resource_provider
+class OpenSearchServiceDomainProvider(ResourceProvider[OpenSearchServiceDomainAllProperties]):
+    TYPE = "AWS::OpenSearchService::Domain"
+
+    def create(
+        self,
+        request: ResourceRequest[OpenSearchServiceDomainAllProperties],
+    ) -> ProgressEvent[OpenSearchServiceDomainAllProperties]:
+        model = request.desired_state
+
+        # Validations
+        assert model.DomainName
+
+        if model.physical_resource_id is None:
+            # resource is not ready
+
+            # Idempotency
+            try:
+                request.aws_client_factory.opensearch.describe_domain(DomainName=model.DomainName)
+            except request.aws_client_factory.opensearch.exceptions.ResourceNotFoundException:
+                pass
+
+            # Defaults
+
+            # Create resource
+            res = request.aws_client_factory.opensearch.create_domain(DomainName=model.DomainName)
+            model.physical_resource_id = res["DomainStatus"]["ARN"]
+            return ProgressEvent(status=OperationStatus.IN_PROGRESS, resource_model=model)
+
+        # check on the status of the domain to see if it has been created yet or not
+        res = request.aws_client_factory.opensearch.describe_domain(DomainName=model.DomainName)
+        if res["DomainStatus"]["Processing"] is False:
+            return ProgressEvent(status=OperationStatus.SUCCESS, resource_model=model)
+        else:
+            return ProgressEvent(status=OperationStatus.IN_PROGRESS, resource_model=model)
+
+    def delete(
+        self, request: ResourceRequest[OpenSearchServiceDomainAllProperties]
+    ) -> ProgressEvent[OpenSearchServiceDomainAllProperties]:
+        # TODO: re-entrant check that the cluster has been deleted
+        name = request.desired_state.DomainName
+        request.aws_client_factory.opensearch.delete_domain(DomainName=name)
+        return ProgressEvent(status=OperationStatus.SUCCESS, resource_model=request.desired_state)
--- tests/integration/cloudformation/nextgen/test_deploy.py
+++ tests/integration/cloudformation/nextgen/test_deploy.py
@@ -1,3 +1,5 @@
+import logging
+import time
 import uuid
 
 import pytest
@@ -7,12 +9,15 @@ from localstack.services.cloudformation.resource_provider import (
     ResourceProviderExecutor,
     ResourceProviderPayload,
 )
+from localstack.services.opensearch.cfn_resources import OpenSearchServiceDomainAllProperties
 from localstack.services.ssm.cfn_resources import SSMParameterAllProperties
 from localstack.utils.strings import short_uid
 
+LOG = logging.getLogger(__name__)
 
-@pytest.mark.skip
-def test_manual_deploy(aws_client, aws_client_factory):
+
+@pytest.mark.skip(reason="This is an example")
+def test_ssm_deploy(aws_client, aws_client_factory):
     stack_name = f"stack-{short_uid()}"
     stack_id = f"arn:aws:::cloudformation/{stack_name}"
 
@@ -60,6 +65,87 @@ def test_manual_deploy(aws_client, aws_client_factory):
 
     res = aws_client.ssm.get_parameter(Name=props.Name)
     assert res["Parameter"]["Value"] == props.Value
+
+    # create the delete event
+    resource_provider_payload: ResourceProviderPayload = {
+        "awsAccountId": "000000000000",
+        "callbackContext": {},
+        "stackId": stack_name,
+        "resourceType": resource_type,
+        "resourceTypeVersion": "000000",
+        # TODO: not actually a UUID
+        "bearerToken": str(uuid.uuid4()),
+        "region": "us-east-1",
+        "action": "Remove",
+        "requestData": {
+            "logicalResourceId": "MyParameter",
+            "resourceProperties": props,
+            "previousResourceProperties": None,
+            "callerCredentials": creds,
+            "providerCredentials": creds,
+            "systemTags": {},
+            "previousSystemTags": {},
+            "stackTags": {},
+            "previousStackTags": {},
+        },
+    }
+    delete_event = executor.execute_action(resource_provider_payload)
+    assert delete_event.status == OperationStatus.SUCCESS
+
+
+@pytest.mark.skip(reason="This is an example")
+def test_opensearch_deploy(aws_client, aws_client_factory):
+    stack_name = f"stack-{short_uid()}"
+    stack_id = f"arn:aws:::cloudformation/{stack_name}"
+
+    resource_type = "AWS::OpenSearchService::Domain"
+
+    executor = ResourceProviderExecutor(
+        resource_type=resource_type, stack_name=stack_name, stack_id=stack_id
+    )
+
+    # definition from the template
+    props = OpenSearchServiceDomainAllProperties(DomainName=f"domain-{short_uid()}")
+
+    change = {"ResourceChange": {"Action": "Add"}}
+
+    creds = {
+        "accessKeyId": "test",
+        "secretAccessKey": "test",
+        "sessionToken": "",
+    }
+    resource_provider_payload: ResourceProviderPayload = {
+        "awsAccountId": "000000000000",
+        "callbackContext": {},
+        "stackId": stack_name,
+        "resourceType": resource_type,
+        "resourceTypeVersion": "000000",
+        # TODO: not actually a UUID
+        "bearerToken": str(uuid.uuid4()),
+        "region": "us-east-1",
+        "action": change["ResourceChange"]["Action"],
+        "requestData": {
+            "logicalResourceId": "MyDomain",
+            "resourceProperties": props,
+            "previousResourceProperties": None,
+            "callerCredentials": creds,
+            "providerCredentials": creds,
+            "systemTags": {},
+            "previousSystemTags": {},
+            "stackTags": {},
+            "previousStackTags": {},
+        },
+    }
+
+    for i in range(30):
+        event = executor.execute_action(resource_provider_payload)
+        if event.status == OperationStatus.SUCCESS:
+            LOG.debug(f"took {i + 1} loop iterations to deploy")
+            break
+        time.sleep(5)
+    else:
+        # we did not break the loop, so consider a timeout error here
+        raise RuntimeError("Could not deploy resource")
 
     # create the delete event
     resource_provider_payload: ResourceProviderPayload = {
