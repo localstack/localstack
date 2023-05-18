37417a021a750a26ded8a2a7d87a0096ec4dbf7b
Simon Walker <simon.walker@localstack.cloud>
2023-05-18 22:13:26 +0100

    Extract test fixture to perform roundtrip

--- tests/integration/cloudformation/nextgen/test_deploy.py
+++ tests/integration/cloudformation/nextgen/test_deploy.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import Literal, TypeVar
 
 import pytest
 
@@ -10,146 +11,86 @@ from localstack.services.opensearch.cfn_resources import OpenSearchServiceDomain
 from localstack.services.ssm.cfn_resources import SSMParameterAllProperties
 from localstack.utils.strings import short_uid
 
+Properties = TypeVar("Properties")
 
-@pytest.mark.skip(reason="This is an example")
-def test_ssm_deploy(aws_client, aws_client_factory):
+
+@pytest.fixture
+def perform_cfn_operation(aws_client, aws_client_factory):
+    """
+    Deploys a resource, then deletes it
+    """
     stack_name = f"stack-{short_uid()}"
     stack_id = f"arn:aws:::cloudformation/{stack_name}"
 
-    resource_type = "AWS::SSM::Parameter"
+    executor = ResourceProviderExecutor(stack_name=stack_name, stack_id=stack_id)
 
-    executor = ResourceProviderExecutor(
-        resource_type=resource_type, stack_name=stack_name, stack_id=stack_id
+    def run(
+        logical_resource_id: str,
+        resource_type: str,
+        action: Literal["Add", "Remove"],
+        resource_props: Properties,
+    ):
+        creds = {
+            "accessKeyId": "test",
+            "secretAccessKey": "test",
+            "sessionToken": "",
+        }
+        resource_provider_payload: ResourceProviderPayload = {
+            "awsAccountId": "000000000000",
+            "callbackContext": {},
+            "stackId": stack_name,
+            "resourceType": resource_type,
+            "resourceTypeVersion": "000000",
+            # TODO: not actually a UUID
+            "bearerToken": str(uuid.uuid4()),
+            "region": "us-east-1",
+            "action": action,
+            "requestData": {
+                "logicalResourceId": logical_resource_id,
+                "resourceProperties": resource_props,
+                "previousResourceProperties": None,
+                "callerCredentials": creds,
+                "providerCredentials": creds,
+                "systemTags": {},
+                "previousSystemTags": {},
+                "stackTags": {},
+                "previousStackTags": {},
+            },
+        }
+
+        return executor.deploy_loop(resource_provider_payload)
+
+    return run
+
+
+@pytest.mark.parametrize(
+    "type_name,props",
+    [
+        (
+            "AWS::OpenSearchService::Domain",
+            OpenSearchServiceDomainAllProperties(DomainName=f"domain-{short_uid()}"),
+        ),
+        (
+            "AWS::SSM::Parameter",
+            SSMParameterAllProperties(Type="String", Value=f"value-{short_uid()}"),
+        ),
+    ],
+    ids=["opensearch-domain", "ssm-parameter"],
+)
+@pytest.mark.skip(reason="Example")
+def test_roundtrip(type_name, props, perform_cfn_operation, aws_client):
+    # deploy
+    event = perform_cfn_operation(
+        logical_resource_id="MyResource",
+        resource_type=type_name,
+        action="Add",
+        resource_props=props,
     )
 
-    # definition from the template
-    props = SSMParameterAllProperties(Type="String", Value=f"value-{short_uid()}")
-
-    creds = {
-        "accessKeyId": "test",
-        "secretAccessKey": "test",
-        "sessionToken": "",
-    }
-    resource_provider_payload: ResourceProviderPayload = {
-        "awsAccountId": "000000000000",
-        "callbackContext": {},
-        "stackId": stack_name,
-        "resourceType": resource_type,
-        "resourceTypeVersion": "000000",
-        # TODO: not actually a UUID
-        "bearerToken": str(uuid.uuid4()),
-        "region": "us-east-1",
-        "action": "Add",
-        "requestData": {
-            "logicalResourceId": "MyParameter",
-            "resourceProperties": props,
-            "previousResourceProperties": None,
-            "callerCredentials": creds,
-            "providerCredentials": creds,
-            "systemTags": {},
-            "previousSystemTags": {},
-            "stackTags": {},
-            "previousStackTags": {},
-        },
-    }
-
-    deployed_resource = executor.deploy_loop(resource_provider_payload).resource_model
-
-    res = aws_client.ssm.get_parameter(Name=deployed_resource.Name)
-    assert res["Parameter"]["Value"] == deployed_resource.Value
-
-    # create the delete event
-    resource_provider_payload: ResourceProviderPayload = {
-        "awsAccountId": "000000000000",
-        "callbackContext": {},
-        "stackId": stack_name,
-        "resourceType": resource_type,
-        "resourceTypeVersion": "000000",
-        # TODO: not actually a UUID
-        "bearerToken": str(uuid.uuid4()),
-        "region": "us-east-1",
-        "action": "Remove",
-        "requestData": {
-            "logicalResourceId": "MyParameter",
-            "resourceProperties": deployed_resource,
-            "previousResourceProperties": None,
-            "callerCredentials": creds,
-            "providerCredentials": creds,
-            "systemTags": {},
-            "previousSystemTags": {},
-            "stackTags": {},
-            "previousStackTags": {},
-        },
-    }
-    executor.deploy_loop(resource_provider_payload)
-
-
-@pytest.mark.skip(reason="This is an example")
-def test_opensearch_deploy(aws_client, aws_client_factory):
-    stack_name = f"stack-{short_uid()}"
-    stack_id = f"arn:aws:::cloudformation/{stack_name}"
-
-    resource_type = "AWS::OpenSearchService::Domain"
-
-    executor = ResourceProviderExecutor(
-        resource_type=resource_type, stack_name=stack_name, stack_id=stack_id
+    # delete
+    perform_cfn_operation(
+        logical_resource_id="MyResource",
+        resource_type=type_name,
+        action="Remove",
+        resource_props=event.resource_model,
     )
-
-    # definition from the template
-    props = OpenSearchServiceDomainAllProperties(DomainName=f"domain-{short_uid()}")
-
-    creds = {
-        "accessKeyId": "test",
-        "secretAccessKey": "test",
-        "sessionToken": "",
-    }
-    resource_provider_payload: ResourceProviderPayload = {
-        "awsAccountId": "000000000000",
-        "callbackContext": {},
-        "stackId": stack_name,
-        "resourceType": resource_type,
-        "resourceTypeVersion": "000000",
-        # TODO: not actually a UUID
-        "bearerToken": str(uuid.uuid4()),
-        "region": "us-east-1",
-        "action": "Add",
-        "requestData": {
-            "logicalResourceId": "MyDomain",
-            "resourceProperties": props,
-            "previousResourceProperties": None,
-            "callerCredentials": creds,
-            "providerCredentials": creds,
-            "systemTags": {},
-            "previousSystemTags": {},
-            "stackTags": {},
-            "previousStackTags": {},
-        },
-    }
-
-    executor.deploy_loop(resource_provider_payload)
-
-    # create the delete event
-    resource_provider_payload: ResourceProviderPayload = {
-        "awsAccountId": "000000000000",
-        "callbackContext": {},
-        "stackId": stack_name,
-        "resourceType": resource_type,
-        "resourceTypeVersion": "000000",
-        # TODO: not actually a UUID
-        "bearerToken": str(uuid.uuid4()),
-        "region": "us-east-1",
-        "action": "Remove",
-        "requestData": {
-            "logicalResourceId": "MyParameter",
-            "resourceProperties": props,
-            "previousResourceProperties": None,
-            "callerCredentials": creds,
-            "providerCredentials": creds,
-            "systemTags": {},
-            "previousSystemTags": {},
-            "stackTags": {},
-            "previousStackTags": {},
-        },
-    }
-
-    executor.deploy_loop(resource_provider_payload)
