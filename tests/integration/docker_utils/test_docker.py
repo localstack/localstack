3aa8eaf854ac750ec4605fd81bc62db76ea2b137
Waldemar Hummer <waldemar.hummer@gmail.com>
2023-05-18 13:36:07 +0200

    fix extraction of container Image when using podman

--- tests/integration/docker_utils/test_docker.py
+++ tests/integration/docker_utils/test_docker.py
@@ -7,6 +7,7 @@ import time
 from typing import NamedTuple
 
 import pytest
+from docker.models.containers import Container
 
 from localstack import config
 from localstack.config import in_docker
@@ -709,6 +710,40 @@ class TestDockerClient:
         )
         assert 1 == len(container_list)
         assert c1.container_name == container_list[0]["name"]
+
+    def test_list_containers_with_podman_image_ref_format(
+        self, docker_client: ContainerClient, create_container, cleanups, monkeypatch
+    ):
+        # create custom image tag
+        image_name = f"alpine:tag-{short_uid()}"
+        docker_client.tag_image("alpine", image_name)
+        cleanups.append(lambda: docker_client.remove_image(image_name))
+
+        def container_init(self, attrs=None, *args, **kwargs):
+            # Simulate podman API response, Docker returns "sha:..." for Image, podman returns "<image-name>:<tag>".
+            #  See https://github.com/containers/podman/issues/8329
+            attrs["Image"] = image_name
+            container_init_orig(self, attrs=attrs, *args, **kwargs)
+
+        # apply patch to simulate podman behavior
+        container_init_orig = Container.__init__
+        monkeypatch.setattr(Container, "__init__", container_init)
+
+        # start a container from the custom image tag
+        c1 = create_container(image_name, command=["sleep", "3"])
+        docker_client.start_container(c1.container_id, attach=False)
+
+        # list containers, assert that container is contained in the list
+        container_list = docker_client.list_containers()
+        running_containers = [cnt for cnt in container_list if cnt["status"] == "running"]
+        assert running_containers
+        container_names = [info["name"] for info in container_list]
+        assert c1.container_name in container_names
+
+        # assert that get_running_container_names(..) call is successful as well
+        container_names = docker_client.get_running_container_names()
+        assert len(running_containers) == len(container_names)
+        assert c1.container_name in container_names
 
     def test_get_container_entrypoint(self, docker_client: ContainerClient):
         entrypoint = docker_client.get_image_entrypoint("alpine")
