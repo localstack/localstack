6e0f30f4cb22d04d817207a9e8d23a49350f56a0
Simon Walker <simon.walker@localstack.cloud>
2023-05-23 17:31:06 +0100

    Update other tests and make template navigatable

--- tests/integration/cloudformation/resource_providers/opensearch/test_domain.py
+++ tests/integration/cloudformation/resource_providers/opensearch/test_domain.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from localstack.aws.connect import ServiceLevelClientFactory
@@ -17,7 +19,8 @@ RESOURCE_GETATT_TARGETS = [
 class TestAttributeAccess:
     @pytest.mark.parametrize("attribute", RESOURCE_GETATT_TARGETS)
     @pytest.mark.xfail(
-        reason="Some tests are expected to fail, since they try to access invalid CFn attributes"
+        reason="Some tests are expected to fail, since they try to access invalid CFn attributes",
+        raises=StackDeployError,
     )
     @pytest.mark.skipif(condition=not is_aws_cloud(), reason="Exploratory test only")
     def test_getattr(
@@ -25,27 +28,23 @@ class TestAttributeAccess:
         aws_client: ServiceLevelClientFactory,
         deploy_cfn_template,
         attribute,
-        template_root,
         snapshot,
     ):
         """
         Capture the behaviour of getting all available attributes of the model
         """
 
-        try:
-            stack = deploy_cfn_template(
-                template_path=template_root.joinpath(
-                    "resource_providers", "opensearch", "domain.yaml"
-                ),
-                parameters={"AttributeName": attribute},
-            )
-        except StackDeployError:
-            pass
-        else:
-            snapshot.match("stack_outputs", stack.outputs)
+        stack = deploy_cfn_template(
+            template_path=os.path.join(
+                os.path.dirname(__file__),
+                "../../../templates/resource_providers/opensearch/domain.yaml",
+            ),
+            parameters={"AttributeName": attribute},
+        )
+        snapshot.match("stack_outputs", stack.outputs)
 
-            # check physical resource id
-            res = aws_client.cloudformation.describe_stack_resource(
-                StackName=stack.stack_name, LogicalResourceId="MyResource"
-            )["StackResourceDetail"]
-            snapshot.match("physical_resource_id", res.get("PhysicalResourceId"))
+        # check physical resource id
+        res = aws_client.cloudformation.describe_stack_resource(
+            StackName=stack.stack_name, LogicalResourceId="MyResource"
+        )["StackResourceDetail"]
+        snapshot.match("physical_resource_id", res.get("PhysicalResourceId"))
