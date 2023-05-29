3fedcd1d2ba8d05282cab7f853606fe112ff9258
Thomas Rausch <thomas@thrau.at>
2023-05-30 01:36:15 +0200

    add AWS::SQS::Queue cfn resource

--- tests/integration/cloudformation/nextgen/test_deploy.py
+++ tests/integration/cloudformation/nextgen/test_deploy.py
@@ -3,6 +3,7 @@ from typing import TypeVar
 import pytest
 
 from localstack.services.opensearch.resource_providers.domain import OpenSearchDomainAllProperties
+from localstack.services.sqs.resource_providers.queue import SQSQueueAllProperties
 from localstack.services.ssm.resource_providers.parameter import SSMParameterAllProperties
 from localstack.utils.strings import short_uid
 
@@ -20,9 +21,25 @@ Properties = TypeVar("Properties")
             "AWS::SSM::Parameter",
             SSMParameterAllProperties(Type="String", Value=f"value-{short_uid()}"),
         ),
+        (
+            "AWS::SQS::Queue",
+            SQSQueueAllProperties(
+                QueueName=f"queue-{short_uid()}.fifo",
+                FifoQueue=True,
+                VisibilityTimeout=10,
+                RedrivePolicy={
+                    "deadLetterTargetArn": "arn:aws:sqs:us-east-1:123456789012:my-dead-letter-queue",
+                    "maxReceiveCount": 5,
+                },
+            ),
+        ),
         # TODO: Add your resource definitions here!
     ],
-    ids=["opensearch-domain", "ssm-parameter"],
+    ids=[
+        "opensearch-domain",
+        "ssm-parameter",
+        "sqs-queue",
+    ],
 )
 @pytest.mark.skip(reason="Example")
 def test_roundtrip(type_name, props, perform_cfn_operation):
