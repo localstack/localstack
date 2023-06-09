94280444fabc1e6851b004cc64a5e559b62a2fcf
Benjamin Simon <benjh.simon@gmail.com>
2023-06-09 17:32:51 +0300

    add test and clean up key name to be consistent with moto

--- tests/integration/s3/test_s3.py
+++ tests/integration/s3/test_s3.py
@@ -628,13 +628,15 @@ class TestS3:
 
     @pytest.mark.aws_validated
     @pytest.mark.skip_snapshot_verify(paths=["$..ServerSideEncryption"])
-    def test_put_get_object_special_character(self, s3_bucket, aws_client, snapshot):
+    @pytest.mark.parametrize("key", ["file%2Fname", "test@key/"])
+    def test_put_get_object_special_character(self, s3_bucket, aws_client, snapshot, key):
         snapshot.add_transformer(snapshot.transform.s3_api())
-        key = "test@key/"
         resp = aws_client.s3.put_object(Bucket=s3_bucket, Key=key, Body=b"test")
         snapshot.match("put-object-special-char", resp)
         resp = aws_client.s3.list_objects_v2(Bucket=s3_bucket)
-        snapshot.match("list-object-special-char", resp)
+        # FIXME: Moto will by default clean up key name, but they will return the cleaned up key name in ListObject...
+        if "%" not in key or is_aws_cloud():
+            snapshot.match("list-object-special-char", resp)
         resp = aws_client.s3.get_object(Bucket=s3_bucket, Key=key)
         snapshot.match("get-object-special-char", resp)
         resp = aws_client.s3.delete_object(Bucket=s3_bucket, Key=key)
