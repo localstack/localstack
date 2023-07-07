6545cd3bf44208d84ce6e7f1f236915173d79211
sannya-singal <sannyasingal@gmail.com>
2023-07-07 10:34:01 +0530

    add check for no togs

--- localstack/services/ses/provider.py
+++ localstack/services/ses/provider.py
@@ -345,35 +345,36 @@ class SesProvider(SesApi, ServiceLifecycleHook):
         emitter = SNSEmitter(context)
         recipients = recipients_from_destination(destination)
 
-        for tag in tags:
-            tag_name = tag.get("Name", "")
-            tag_value = tag.get("Value", "")
-            if tag_name == "":
-                raise CommonServiceException(
-                    "InvalidParameterValue", "The tag name must be specified."
-                )
-            if tag_value == "":
-                raise CommonServiceException(
-                    "InvalidParameterValue", "The tag value must be specified."
-                )
-            if len(tag_name) > 255:
-                raise CommonServiceException(
-                    "InvalidParameterValue", "Tag name cannot exceed 255 characters."
-                )
-            if not re.match(ALLOWED_TAG_CHARS, tag_name):
-                raise CommonServiceException(
-                    "InvalidParameterValue",
-                    f"Invalid tag name <{tag_name}>: only alphanumeric ASCII characters, '_', and '-' are allowed.",
-                )
-            if len(tag_value) > 255:
-                raise CommonServiceException(
-                    "InvalidParameterValue", "Tag value cannot exceed 255 characters."
-                )
-            if not re.match(ALLOWED_TAG_CHARS, tag_value):
-                raise CommonServiceException(
-                    "InvalidParameterValue",
-                    f"Invalid tag value <{tag_value}>: only alphanumeric ASCII characters, '_', and '-' are allowed.",
-                )
+        if tags:
+            for tag in tags:
+                tag_name = tag.get("Name", "")
+                tag_value = tag.get("Value", "")
+                if tag_name == "":
+                    raise CommonServiceException(
+                        "InvalidParameterValue", "The tag name must be specified."
+                    )
+                if tag_value == "":
+                    raise CommonServiceException(
+                        "InvalidParameterValue", "The tag value must be specified."
+                    )
+                if len(tag_name) > 255:
+                    raise CommonServiceException(
+                        "InvalidParameterValue", "Tag name cannot exceed 255 characters."
+                    )
+                if not re.match(ALLOWED_TAG_CHARS, tag_name):
+                    raise CommonServiceException(
+                        "InvalidParameterValue",
+                        f"Invalid tag name <{tag_name}>: only alphanumeric ASCII characters, '_', and '-' are allowed.",
+                    )
+                if len(tag_value) > 255:
+                    raise CommonServiceException(
+                        "InvalidParameterValue", "Tag value cannot exceed 255 characters."
+                    )
+                if not re.match(ALLOWED_TAG_CHARS, tag_value):
+                    raise CommonServiceException(
+                        "InvalidParameterValue",
+                        f"Invalid tag value <{tag_value}>: only alphanumeric ASCII characters, '_', and '-' are allowed.",
+                    )
 
         for event_destination in backend.config_set_event_destination.values():
             if not event_destination["Enabled"]:
