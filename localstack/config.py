1dfdc57fe197a3fc23d874c83c1b44713aa26e78
Benjamin Simon <benjh.simon@gmail.com>
2023-07-07 10:21:07 +0200

    remove stream provider override

--- localstack/config.py
+++ localstack/config.py
@@ -424,10 +424,7 @@ LEGACY_EDGE_PROXY = is_env_true("LEGACY_EDGE_PROXY")
 # whether legacy s3 is enabled
 LEGACY_S3_PROVIDER = os.environ.get("PROVIDER_OVERRIDE_S3", "") == "legacy"
 
-# FIXME: remove, this is for testing this PR with the full test suite (also in -ext thanks to the PR there)
-if not os.environ.get("PROVIDER_OVERRIDE_S3", ""):
-    os.environ["PROVIDER_OVERRIDE_S3"] = "stream"
-
+# whether the S3 streaming provider is enabled (beware, it breaks persistence for now)
 STREAM_S3_PROVIDER = os.environ.get("PROVIDER_OVERRIDE_S3", "") == "stream"
 
 # Whether to report internal failures as 500 or 501 errors.
