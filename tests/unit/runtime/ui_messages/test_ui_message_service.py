655e6de5754c5a86996046b7a617347d435f5b34
Simon Wallner <me@simonwallner.at>
2023-07-25 16:08:34 +0200

    fixing and adding tests for valid before/after

--- tests/unit/runtime/ui_messages/test_ui_message_service.py
+++ tests/unit/runtime/ui_messages/test_ui_message_service.py
@@ -1,16 +1,17 @@
+import sys
+import time
 from typing import Final
 import pytest
 
 from localstack.runtime import ui_messages
-from localstack.runtime.ui_messages.news_client import dispatch_news_client
-from localstack.runtime.ui_messages.ui_message_service import UIMessageService, Message, MessageContent
+from localstack.runtime.ui_messages.ui_message_service import UIMessageService, Message
 
-TEST_MESSAGE_LICENSING_A: Final[Message] = Message(priority=100, content=MessageContent(title="licensing message A title", body="licensing message A body"))
-TEST_MESSAGE_NEWS_A: Final[Message] = Message(priority=100, content=MessageContent(title="new message A title", body="news message A body"))
-TEST_MESSAGE_NEWS_B: Final[Message] = Message(priority=100, content=MessageContent(title="new message B title", body="news message B body"))
+TEST_MESSAGE_LICENSING_A: Final[Message] = Message(priority=100, validAfterTimestampUTC=-1, validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="licensing message A title", body="licensing message A body")
+TEST_MESSAGE_NEWS_A: Final[Message] = Message(priority=100, validAfterTimestampUTC=-1, validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="messageA title", body="news message A body")
+TEST_MESSAGE_NEWS_B: Final[Message] = Message(priority=100, validAfterTimestampUTC=-1, validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="messageB title", body="news message B body")
 
 def is_message_in_output(message: Message, output: str):
-    return message['content']['title'] in output and message['content']['body'] in output
+    return message['title'] in output and message['body'] in output
 
 @pytest.fixture
 def message_service():
@@ -45,18 +46,18 @@ class TestUiMessageService:
         assert is_message_in_output(TEST_MESSAGE_NEWS_B, captured.out)
 
     def test_display_messages_ordered_by_priority(self, message_service: UIMessageService, capsys: pytest.CaptureFixture[str]):
-        prio_100 = Message(priority=100, content=MessageContent(title="-- prio 100 --", body="body"))
-        prio_200 = Message(priority=200, content=MessageContent(title="-- prio 200 --", body="body"))
-        prio_300 = Message(priority=300, content=MessageContent(title="-- prio 300 --", body="body"))
+        prio_100 = Message(priority=100, validAfterTimestampUTC=-1, validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="-- prio 100 --", body="news message A body")
+        prio_200 = Message(priority=200, validAfterTimestampUTC=-1, validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="-- prio 200 --", body="news message A body")
+        prio_300 = Message(priority=300, validAfterTimestampUTC=-1, validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="-- prio 300 --", body="news message A body")
 
         # add messages in jumbled order
         message_service.set_messages_for_topic("news", [prio_200, prio_100, prio_300])
         message_service.print_cached_messages()
 
         output: str = capsys.readouterr().out
-        position_100 = output.find(prio_100['content']['title'])
-        position_200 = output.find(prio_200['content']['title'])
-        position_300 = output.find(prio_300['content']['title'])
+        position_100 = output.find(prio_100['title'])
+        position_200 = output.find(prio_200['title'])
+        position_300 = output.find(prio_300['title'])
 
         assert position_100 > 0
         assert position_200 > 0
@@ -74,3 +75,16 @@ class TestUiMessageService:
 
         output: str = capsys.readouterr().out
         assert output == ""
+
+    def test_invalid_messages_not_shown(self, message_service: UIMessageService, capsys: pytest.CaptureFixture[str]):
+        now = int(time.time())
+        not_valid_yet = Message(priority=100, validAfterTimestampUTC=(now + 60), validBeforeTimestampUTC=sys.maxsize, presentationStyle="default", title="title", body="body")
+        not_valid_anymore = Message(priority=200, validAfterTimestampUTC=-1, validBeforeTimestampUTC=(now - 60), presentationStyle="default", title="title", body="body")
+
+        message_service.set_messages_for_topic("news", [not_valid_yet, not_valid_anymore])
+
+        message_service.print_cached_messages()
+        captured = capsys.readouterr()
+
+        assert not is_message_in_output(not_valid_yet, captured.out)
+        assert not is_message_in_output(not_valid_anymore, captured.out)
