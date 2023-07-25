834f6677be4e9a4a4f1f533f88d3e4228a51e66a
Simon Walker <simon.walker@localstack.cloud>
2023-07-25 16:29:18 +0100

    Implement network connectivity test

--- tests/bootstrap/test_localstack_container.py
+++ tests/bootstrap/test_localstack_container.py
@@ -54,11 +54,13 @@ class TestLocalstackContainerServer:
 
 @pytest.mark.skipif(condition=in_docker(), reason="cannot run bootstrap tests in docker")
 class TestInterNetworkConnectivity:
-    @pytest.mark.skip(reason="WIP")
+    # @pytest.mark.skip(reason="WIP")
     def test_user_defined_network_connectivity(
         self,
-        localstack_container: LocalstackContainerServer,
+        docker_client: ContainerClient,
+        # order is important: create_network must come before localstack_container
         create_network,
+        localstack_container: LocalstackContainerServer,
     ):
         network_name = f"net-{short_uid()}"
         create_network(network_name)
@@ -70,7 +72,17 @@ class TestInterNetworkConnectivity:
         localstack_container.start()
         assert localstack_container.wait_is_up(60)
 
-        self.run_connectivity_test_from_external_container(localstack_container.container)
+        self.run_connectivity_test_from_external_container(
+            docker_client, network_name, localstack_container.container
+        )
 
-    def run_connectivity_test_from_external_container(self, container: LocalstackContainer):
-        raise NotImplementedError
+    def run_connectivity_test_from_external_container(
+        self, docker_client: ContainerClient, network_name: str, container: LocalstackContainer
+    ):
+        docker_client.run_container(
+            image_name="localstack/localstack",
+            entrypoint="bash",
+            network=network_name,
+            command=["-c", f"curl http://{container.name}:4566/_localstack/health"],
+        )
+        # no exception means success
