ee52f6af36b9f387b58fc21e49cc3d551d7cc9af
Simon Walker <simon.walker@localstack.cloud>
2023-07-25 13:43:23 +0100

    wip: extract start fixture

--- tests/bootstrap/test_localstack_container.py
+++ tests/bootstrap/test_localstack_container.py
@@ -6,22 +6,25 @@ from localstack.config import in_docker
 from localstack.utils.bootstrap import LocalstackContainerServer
 
 
+@pytest.fixture
+def localstack_server():
+    server = LocalstackContainerServer()
+    assert not server.is_up()
+
+    yield server
+
+    server.shutdown()
+    server.join(30)
+    assert not server.is_up()
+
+
 @pytest.mark.skipif(condition=in_docker(), reason="cannot run bootstrap tests in docker")
 class TestLocalstackContainerServer:
-    def test_lifecycle(self):
+    def test_lifecycle(self, localstack_server):
+        localstack_server.container.ports.add(config.EDGE_PORT)
+        localstack_server.start()
+        assert localstack_server.wait_is_up(60)
 
-        server = LocalstackContainerServer()
-        server.container.ports.add(config.EDGE_PORT)
+        response = requests.get("http://localhost:4566/_localstack/health")
 
-        assert not server.is_up()
-        try:
-            server.start()
-            assert server.wait_is_up(60)
-
-            response = requests.get("http://localhost:4566/_localstack/health")
-            assert response.ok, "expected health check to return OK: %s" % response.text
-        finally:
-            server.shutdown()
-
-        server.join(30)
-        assert not server.is_up()
+        assert response.ok, "expected health check to return OK: %s" % response.text
