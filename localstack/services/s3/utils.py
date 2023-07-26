599770a8698f909a5cadd2be9379810b72a3be3e
Benjamin Simon <benjh.simon@gmail.com>
2023-07-26 15:16:12 +0200

    fix matching rule when ExpiredObjectDeleteMarker is set

--- localstack/services/s3/utils.py
+++ localstack/services/s3/utils.py
@@ -399,7 +399,7 @@ def get_lifecycle_rule_from_object(
     lifecycle_conf_rules: LifecycleRules, moto_object: FakeKey, object_tags: dict[str, str]
 ) -> LifecycleRule:
     for rule in lifecycle_conf_rules:
-        if "Expiration" not in rule:
+        if not (expiration := rule.get("Expiration")) or "ExpiredObjectDeleteMarker" in expiration:
             continue
 
         if not (rule_filter := rule.get("Filter")):
