ebae875c24872b6bb60a7ebc1d5b324eb5d5bb5c
Alexander Rashed <alexander.rashed@localstack.cloud>
2023-08-01 10:18:33 +0200

    upgrade kinesis-mock to scalajs release, add node installer

--- localstack/services/kinesis/kinesis_mock_server.py
+++ localstack/services/kinesis/kinesis_mock_server.py
@@ -5,13 +5,7 @@ from typing import Dict, List, Optional, Tuple
 
 from localstack import config
 from localstack.services.kinesis.packages import kinesismock_package
-from localstack.utils.common import (
-    TMP_THREADS,
-    ShellCommandThread,
-    chmod_r,
-    get_free_tcp_port,
-    mkdir,
-)
+from localstack.utils.common import TMP_THREADS, ShellCommandThread, get_free_tcp_port, mkdir
 from localstack.utils.run import FuncThread
 from localstack.utils.serving import Server
 
@@ -26,7 +20,7 @@ class KinesisMockServer(Server):
     def __init__(
         self,
         port: int,
-        bin_path: str,
+        js_path: str,
         latency: str,
         account_id: str,
         host: str = "localhost",
@@ -39,7 +33,7 @@ class KinesisMockServer(Server):
         self._initialize_streams = initialize_streams
         self._data_dir = data_dir
         self._data_filename = f"{self._account_id}.json"
-        self._bin_path = bin_path
+        self._js_path = js_path
         self._log_level = log_level
         super().__init__(port, host)
 
@@ -63,6 +57,8 @@ class KinesisMockServer(Server):
         Helper method for creating kinesis mock invocation command
         :return: returns a tuple containing the command list and a dictionary with the environment variables
         """
+        # TODO handle KINESIS_MOCK_CERT_PATH
+
         env_vars = {
             "KINESIS_MOCK_PLAIN_PORT": self.port,
             # Each kinesis-mock instance listens to two ports - secure and insecure.
@@ -95,14 +91,7 @@ class KinesisMockServer(Server):
             env_vars["PERSIST_INTERVAL"] = config.KINESIS_MOCK_PERSIST_INTERVAL
 
         env_vars["LOG_LEVEL"] = self._log_level
-        if self._initialize_streams:
-            env_vars["INITIALIZE_STREAMS"] = self._initialize_streams
-
-        if self._bin_path.endswith(".jar"):
-            cmd = ["java", "-XX:+UseG1GC", "-jar", self._bin_path]
-        else:
-            chmod_r(self._bin_path, 0o777)
-            cmd = [self._bin_path, "--gc=G1"]
+        cmd = ["node", self._js_path]
         return cmd, env_vars
 
     def _log_listener(self, line, **_kwargs):
@@ -149,12 +138,11 @@ class KinesisServerManager:
         """
         port = get_free_tcp_port()
         kinesismock_package.install()
-        kinesis_mock_bin_path = kinesismock_package.get_installer().get_executable_path()
+        kinesis_mock_js_path = kinesismock_package.get_installer().get_executable_path()
 
         # kinesis-mock stores state in json files <account_id>.json, so we can dump everything into `kinesis/`
         persist_path = os.path.join(config.dirs.data, "kinesis")
         mkdir(persist_path)
-
         if config.LS_LOG:
             if config.LS_LOG == "warning":
                 log_level = "WARN"
@@ -162,7 +150,6 @@ class KinesisServerManager:
                 log_level = config.LS_LOG.upper()
         else:
             log_level = "INFO"
-
         latency = config.KINESIS_LATENCY + "ms"
         initialize_streams = (
             config.KINESIS_INITIALIZE_STREAMS if config.KINESIS_INITIALIZE_STREAMS else None
@@ -170,7 +157,7 @@ class KinesisServerManager:
 
         server = KinesisMockServer(
             port=port,
-            bin_path=kinesis_mock_bin_path,
+            js_path=kinesis_mock_js_path,
             log_level=log_level,
             latency=latency,
             initialize_streams=initialize_streams,
