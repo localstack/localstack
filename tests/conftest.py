fc5525a3a581b5b12792dfc22403df345f1fe6b7
Viren Nadkarni <viren.nadkarni@gmail.com>
2023-08-02 17:02:43 +0530

    Fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,10 @@ import pytest
 from _pytest.config import PytestPluginManager
 from _pytest.config.argparsing import Parser
 
-from localstack.testing.aws.util import (
-    base_aws_client_factory,
-    base_aws_session,
-    primary_testing_aws_client,
-    secondary_testing_aws_client,
-)
-
 os.environ["LOCALSTACK_INTERNAL_TEST_RUN"] = "1"
 
 pytest_plugins = [
+    "tests.integration.fixtures",
     "localstack.testing.pytest.cloudtrail_tracking",
     "localstack.testing.pytest.fixtures",
     "localstack.testing.pytest.snapshot",
@@ -58,46 +52,3 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if "skip_offline" in item.keywords:
             item.add_marker(skip_offline)
-
-
-#
-# Following fixtures are defined in this file so that they are available to all test suites
-#
-
-
-@pytest.fixture(scope="session")
-def aws_session():
-    """
-    This fixture returns the Boto Session instance for testing.
-    """
-    return base_aws_session()
-
-
-@pytest.fixture(scope="session")
-def aws_client_factory(aws_session):
-    """
-    This fixture returns a client factory for testing.
-
-    Use this fixture if you need to use custom endpoint or Boto config.
-    """
-    return base_aws_client_factory(aws_session)
-
-
-@pytest.fixture(scope="session")
-def aws_client(aws_client_factory):
-    """
-    This fixture can be used to obtain Boto clients for testing.
-
-    The clients are configured with the primary testing credentials.
-    """
-    return primary_testing_aws_client(aws_client_factory)
-
-
-@pytest.fixture(scope="session")
-def secondary_aws_client(aws_client_factory):
-    """
-    This fixture can be used to obtain Boto clients for testing.
-
-    The clients are configured with the secondary testing credentials.
-    """
-    return secondary_testing_aws_client(aws_client_factory)
