580b65a1d78d2db42cfd2342497e3f142d58cb7a
baermat <bernhard.matyas@gmx.at>
2023-08-17 11:04:30 +0200

    move files to new folder (with usage search)

---
+++ tests/aws/services/apigateway/conftest.py
@@ -0,0 +1,176 @@
+import pytest
+
+from localstack.constants import APPLICATION_JSON
+from localstack.utils.strings import short_uid
+from tests.aws.services.apigateway.apigateway_fixtures import (
+    create_rest_api_deployment,
+    create_rest_api_integration,
+    create_rest_api_integration_response,
+    create_rest_api_method_response,
+    create_rest_api_stage,
+    create_rest_resource,
+    create_rest_resource_method,
+    delete_rest_api,
+    import_rest_api,
+)
+
+# default name used for created REST API stages
+DEFAULT_STAGE_NAME = "dev"
+
+STEPFUNCTIONS_ASSUME_ROLE_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {"Service": "states.amazonaws.com"},
+            "Action": "sts:AssumeRole",
+        }
+    ],
+}
+
+APIGATEWAY_STEPFUNCTIONS_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [{"Effect": "Allow", "Action": "states:*", "Resource": "*"}],
+}
+
+APIGATEWAY_KINESIS_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [{"Effect": "Allow", "Action": "kinesis:*", "Resource": "*"}],
+}
+
+APIGATEWAY_LAMBDA_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [{"Effect": "Allow", "Action": "lambda:*", "Resource": "*"}],
+}
+
+APIGATEWAY_DYNAMODB_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [{"Effect": "Allow", "Action": "dynamodb:*", "Resource": "*"}],
+}
+
+APIGATEWAY_ASSUME_ROLE_POLICY = {
+    "Statement": {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {"Service": "apigateway.amazonaws.com"},
+        "Action": "sts:AssumeRole",
+    }
+}
+
+
+@pytest.fixture
+def create_rest_api_with_integration(
+    create_rest_apigw, wait_for_stream_ready, create_iam_role_with_policy, aws_client
+):
+    def _factory(
+        integration_uri,
+        req_templates=None,
+        res_templates=None,
+        integration_type=None,
+        stage=DEFAULT_STAGE_NAME,
+    ):
+        name = f"test-apigw-{short_uid()}"
+        api_id, name, root_id = create_rest_apigw(
+            name=name, endpointConfiguration={"types": ["REGIONAL"]}
+        )
+
+        resource_id, _ = create_rest_resource(
+            aws_client.apigateway, restApiId=api_id, parentId=root_id, pathPart="test"
+        )
+
+        method, _ = create_rest_resource_method(
+            aws_client.apigateway,
+            restApiId=api_id,
+            resourceId=resource_id,
+            httpMethod="POST",
+            authorizationType="NONE",
+            apiKeyRequired=False,
+        )
+
+        # set AWS policy to give API GW access to backend resources
+        if ":dynamodb:" in integration_uri:
+            policy = APIGATEWAY_DYNAMODB_POLICY
+        elif ":kinesis:" in integration_uri:
+            policy = APIGATEWAY_KINESIS_POLICY
+        else:
+            raise Exception(f"Unexpected integration URI: {integration_uri}")
+        assume_role_arn = create_iam_role_with_policy(
+            RoleName=f"role-apigw-{short_uid()}",
+            PolicyName=f"policy-apigw-{short_uid()}",
+            RoleDefinition=APIGATEWAY_ASSUME_ROLE_POLICY,
+            PolicyDefinition=policy,
+        )
+
+        create_rest_api_integration(
+            aws_client.apigateway,
+            restApiId=api_id,
+            resourceId=resource_id,
+            httpMethod=method,
+            integrationHttpMethod="POST",
+            type=integration_type or "AWS",
+            credentials=assume_role_arn,
+            uri=integration_uri,
+            requestTemplates=req_templates or {},
+        )
+
+        create_rest_api_method_response(
+            aws_client.apigateway,
+            restApiId=api_id,
+            resourceId=resource_id,
+            httpMethod="POST",
+            statusCode="200",
+        )
+
+        res_templates = res_templates or {APPLICATION_JSON: "$input.json('$')"}
+        create_rest_api_integration_response(
+            aws_client.apigateway,
+            restApiId=api_id,
+            resourceId=resource_id,
+            httpMethod="POST",
+            statusCode="200",
+            responseTemplates=res_templates,
+        )
+
+        deployment_id, _ = create_rest_api_deployment(aws_client.apigateway, restApiId=api_id)
+        create_rest_api_stage(
+            aws_client.apigateway, restApiId=api_id, stageName=stage, deploymentId=deployment_id
+        )
+
+        return api_id
+
+    yield _factory
+
+
+@pytest.fixture
+def apigw_redeploy_api(aws_client):
+    def _factory(rest_api_id: str, stage_name: str):
+        deployment_id = aws_client.apigateway.create_deployment(restApiId=rest_api_id)["id"]
+
+        aws_client.apigateway.update_stage(
+            restApiId=rest_api_id,
+            stageName=stage_name,
+            patchOperations=[{"op": "replace", "path": "/deploymentId", "value": deployment_id}],
+        )
+
+    return _factory
+
+
+@pytest.fixture
+def import_apigw(aws_client):
+    rest_api_ids = []
+
+    def _import_apigateway_function(*args, **kwargs):
+        response, root_id = import_rest_api(aws_client.apigateway, **kwargs)
+        rest_api_ids.append(response.get("id"))
+        return response, root_id
+
+    yield _import_apigateway_function
+
+    for rest_api_id in rest_api_ids:
+        delete_rest_api(aws_client.apigateway, restApiId=rest_api_id)
+
+
+@pytest.fixture
+def apigw_add_transformers(snapshot):
+    snapshot.add_transformer(snapshot.transform.jsonpath("$..items..id", "id"))
+    snapshot.add_transformer(snapshot.transform.key_value("deploymentId"))
---
+++ tests/aws/services/stepfunctions/v2/base/test_base.py
@@ -0,0 +1,111 @@
+import json
+
+import pytest
+
+from localstack.testing.aws.util import is_aws_cloud
+from localstack.testing.pytest import markers
+from tests.aws.services.stepfunctions.templates.base.base_templates import BaseTemplate
+from tests.aws.services.stepfunctions.utils import (
+    create_and_record_events,
+    create_and_record_execution,
+    is_old_provider,
+)
+
+pytestmark = pytest.mark.skipif(
+    condition=is_old_provider(), reason="Test suite for v2 provider only."
+)
+
+
+@markers.snapshot.skip_snapshot_verify(
+    paths=["$..loggingConfiguration", "$..tracingConfiguration", "$..previousEventId"]
+)
+class TestSnfApi:
+    @markers.aws.unknown
+    def test_state_fail(
+        self,
+        aws_client,
+        create_iam_role_for_sfn,
+        create_state_machine,
+        sfn_snapshot,
+    ):
+        template = BaseTemplate.load_sfn_template(BaseTemplate.BASE_RAISE_FAILURE)
+        definition = json.dumps(template)
+
+        exec_input = json.dumps({})
+        create_and_record_execution(
+            aws_client.stepfunctions,
+            create_iam_role_for_sfn,
+            create_state_machine,
+            sfn_snapshot,
+            definition,
+            exec_input,
+        )
+
+    @markers.aws.validated
+    def test_state_fail_empty(
+        self,
+        aws_client,
+        create_iam_role_for_sfn,
+        create_state_machine,
+        sfn_snapshot,
+    ):
+        template = BaseTemplate.load_sfn_template(BaseTemplate.RAISE_EMPTY_FAILURE)
+        definition = json.dumps(template)
+
+        exec_input = json.dumps({})
+        create_and_record_execution(
+            aws_client.stepfunctions,
+            create_iam_role_for_sfn,
+            create_state_machine,
+            sfn_snapshot,
+            definition,
+            exec_input,
+        )
+
+    @markers.aws.validated
+    def test_event_bridge_events_base(
+        self,
+        create_iam_role_for_sfn,
+        create_state_machine,
+        sfn_events_to_sqs_queue,
+        aws_client,
+        sfn_snapshot,
+    ):
+        template = BaseTemplate.load_sfn_template(BaseTemplate.BASE_WAIT_1_MIN)
+        template["States"]["State_1"]["Seconds"] = 60 if is_aws_cloud() else 1
+        definition = json.dumps(template)
+        execution_input = json.dumps(dict())
+        create_and_record_events(
+            create_iam_role_for_sfn,
+            create_state_machine,
+            sfn_events_to_sqs_queue,
+            aws_client,
+            sfn_snapshot,
+            definition,
+            execution_input,
+        )
+
+    @pytest.mark.skip(reason="flaky")  # FIXME
+    @markers.aws.unknown
+    def test_event_bridge_events_failure(
+        self,
+        create_iam_role_for_sfn,
+        create_state_machine,
+        sfn_events_to_sqs_queue,
+        aws_client,
+        sfn_snapshot,
+    ):
+        template = BaseTemplate.load_sfn_template(BaseTemplate.WAIT_AND_FAIL)
+        template["States"]["State_1"]["Seconds"] = 60 if is_aws_cloud() else 1
+        definition = json.dumps(template)
+
+        exec_input = json.dumps({})
+        create_and_record_events(
+            create_iam_role_for_sfn,
+            create_state_machine,
+            sfn_events_to_sqs_queue,
+            aws_client,
+            sfn_snapshot,
+            definition,
+            exec_input,
+        )
