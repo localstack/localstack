fc5570fd55939b18b90069fdd6f7a7707f77ac95
steffyP <steffyP@users.noreply.github.com>
2023-08-24 14:04:51 +0200

    Update dynamodb snapshots (#8972)

--- tests/aws/services/dynamodb/test_dynamodb.py
+++ tests/aws/services/dynamodb/test_dynamodb.py
@@ -7,12 +7,10 @@ from typing import Dict
 import pytest
 from boto3.dynamodb.types import STRING
 
-from localstack.aws.api.dynamodb import (
-    ContinuousBackupsUnavailableException,
-    PointInTimeRecoverySpecification,
-)
+from localstack.aws.api.dynamodb import PointInTimeRecoverySpecification
 from localstack.services.dynamodbstreams.dynamodbstreams_api import get_kinesis_stream_name
 from localstack.testing.aws.lambda_utils import _await_dynamodb_table_active
+from localstack.testing.aws.util import is_aws_cloud
 from localstack.testing.pytest import markers
 from localstack.testing.snapshots.transformer import SortingTransformer
 from localstack.utils import testutil
@@ -1391,13 +1389,6 @@ class TestDynamoDB:
         assert result["TableDescription"]["SSEDescription"]["KMSMasterKeyArn"] == kms_master_key_arn
 
     @markers.aws.validated
-    @markers.snapshot.skip_snapshot_verify(
-        paths=[
-            "$..KeyMetadata..KeyUsage",
-            "$..KeyMetadata..MultiRegion",
-            "$..KeyMetadata..SigningAlgorithms",
-        ]
-    )
     def test_dynamodb_create_table_with_partial_sse_specification(
         self, dynamodb_create_table_with_parameters, snapshot, aws_client
     ):
@@ -1593,10 +1584,7 @@ class TestDynamoDB:
         table = client.describe_table(TableName=table_name)
         assert table.get("Table")
 
-    @markers.aws.only_localstack(reason="wait_for_stream_ready of kinesis stream")
-    @markers.snapshot.skip_snapshot_verify(
-        paths=["$..eventID", "$..SequenceNumber", "$..SizeBytes"]
-    )
+    @markers.aws.validated
     def test_data_encoding_consistency(
         self, dynamodb_create_table_with_parameters, wait_for_stream_ready, snapshot, aws_client
     ):
@@ -1611,8 +1599,10 @@ class TestDynamoDB:
                 "StreamViewType": "NEW_AND_OLD_IMAGES",
             },
         )
-        stream_name = get_kinesis_stream_name(table_name)
-        wait_for_stream_ready(stream_name)
+        if not is_aws_cloud():
+            # required for LS because the stream is using kinesis, which needs to be ready
+            stream_name = get_kinesis_stream_name(table_name)
+            wait_for_stream_ready(stream_name)
 
         # put item
         aws_client.dynamodb.put_item(
@@ -1689,10 +1679,14 @@ class TestDynamoDB:
                     ),
                 )
                 return True
-            except ContinuousBackupsUnavailableException:
+            except Exception:  # noqa
                 return False
 
-        assert poll_condition(wait_for_continuous_backend, timeout=10)
+        assert poll_condition(
+            wait_for_continuous_backend,
+            timeout=50 if is_aws_cloud() else 10,
+            interval=1 if is_aws_cloud() else 0.5,
+        )
 
         response = aws_client.dynamodb.update_continuous_backups(
             TableName=table_name,
