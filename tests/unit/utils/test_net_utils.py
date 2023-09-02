a67851fd8c50f8b3b4cea74bb8ebd8d1d6ba83a9
Thomas Rausch <thomas@thrau.at>
2023-09-02 23:54:31 +0200

    add utility to find contiguous port range

--- tests/unit/utils/test_net_utils.py
+++ tests/unit/utils/test_net_utils.py
@@ -8,7 +8,12 @@ from localstack.testing.pytest import markers
 from localstack.utils.common import short_uid
 from localstack.utils.net import (
     Port,
+    PortNotAvailableException,
+    PortRange,
+    dynamic_port_range,
     get_addressable_container_host,
+    get_free_tcp_port,
+    get_free_tcp_port_range,
     get_free_udp_port,
     is_ip_address,
     port_can_be_bound,
@@ -47,6 +52,59 @@ def test_port_open(protocol):
 def test_get_free_udp_port():
     port = get_free_udp_port()
     assert port_can_be_bound(Port(port, "udp"))
+
+
+def test_free_tcp_port_blocklist_raises_exception():
+    blocklist = range(0, 70000)  # blocklist all existing ports
+    with pytest.raises(Exception) as ctx:
+        get_free_tcp_port(blocklist)
+
+    assert "Unable to determine free TCP" in str(ctx.value)
+
+
+def test_port_can_be_bound():
+    port = get_free_tcp_port()
+    assert port_can_be_bound(port)
+
+
+def test_port_can_be_bound_illegal_port():
+    assert not port_can_be_bound(9999999999)
+
+
+def test_port_can_be_bound_already_bound():
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        tcp.bind(("", 0))
+        addr, port = tcp.getsockname()
+        assert not port_can_be_bound(port)
+    finally:
+        tcp.close()
+
+    assert port_can_be_bound(port)
+
+
+def test_get_free_tcp_port_range():
+    port_range = get_free_tcp_port_range(20)
+
+    assert len(port_range) == 20
+
+    for port in port_range:
+        assert dynamic_port_range.is_port_reserved(port)
+
+    for port in port_range:
+        assert port_can_be_bound(port)
+
+
+def test_get_free_tcp_port_range_fails_if_reserved(monkeypatch):
+    monkeypatch.setattr(dynamic_port_range, "is_port_reserved", lambda *args, **kwargs: True)
+
+    with pytest.raises(PortNotAvailableException):
+        get_free_tcp_port_range(20)
+
+
+def test_port_range_iter():
+    ports = PortRange(10, 13)
+    assert list(ports) == [10, 11, 12, 13]
 
 
 def test_get_addressable_container_host(monkeypatch):
