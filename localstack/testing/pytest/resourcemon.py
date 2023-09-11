8071e4f71c602d6326a2f0fe4d21c4464d514ab5
Simon Walker <simon.walker@localstack.cloud>
2023-09-11 13:33:45 +0100

    Update plugin to track usage

--- localstack/testing/pytest/resourcemon.py
+++ localstack/testing/pytest/resourcemon.py
@@ -8,7 +8,7 @@ know when it is out of memory.
 import json
 import sys
 import time
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 from dataclasses import asdict, dataclass
 from typing import Optional, Tuple
 
@@ -23,13 +23,22 @@ class Serializable:
 
 
 class FromPsutil(ABC):
-    @abstractclassmethod
+    """
+    Represents a piece of data that can be extracted from psutil
+    """
+
+    @classmethod
+    @abstractmethod
     def from_psutil(cls, result):
         pass
 
 
 @dataclass
 class Memory(Serializable, FromPsutil):
+    """
+    Represents the state captured about memory usage.
+    """
+
     total: int
     available: int
     percent: float
@@ -84,8 +93,12 @@ class Disk(Serializable, FromPsutil):
 
 
 class ResourceMonitorPlugin:
+    def __init__(self):
+        self._per_test = []
+
     @pytest.hookimpl()
-    def pytest_collectstart(self, collector: pytest.Collector):
+    def pytest_collection_finish(self, session: pytest.Session):
+        # Initialise after collection
         self.capture("start")
 
     @pytest.hookimpl()
@@ -97,8 +110,15 @@ class ResourceMonitorPlugin:
             print(f"Error capturing statistics: {e}", file=sys.stderr)
 
     @pytest.hookimpl()
-    def pytest_terminal_summary(self, *args, **kwargs):
-        pass
+    def pytest_terminal_summary(
+        self, terminalreporter: "TerminalReporter", exitstatus: int, config: "Config"
+    ):
+        memory_usage_per_test = self._compute_memory_usage_per_test()
+        memory_usage_per_test.sort(reverse=True)
+
+        terminalreporter.section("Memory usage")
+        for name, percent in memory_usage_per_test[:10]:
+            terminalreporter.write_line(f"{name}: {percent:.1f} %")
 
     def capture(
         self,
@@ -127,7 +147,25 @@ class ResourceMonitorPlugin:
             "disk": disk.to_dict(),
         }
 
+        self.record_row(row)
         self.print_row(row)
 
     def print_row(self, row: dict):
-        print(f"*** RESOURCE USAGE:{json.dumps(row)}")
+        print(f"RESOURCE USAGE:{json.dumps(row)}")
+
+    def record_row(self, row: dict):
+        self._per_test.append(row)
+
+    def _print_top_memory_users(self):
+        print()
+        print("Top memory usages")
+        print()
+
+    def _compute_memory_usage_per_test(self) -> list[tuple[str, float]]:
+        out = []
+        for item1, item2 in zip(self._per_test[:1], self._per_test[1:]):
+            diff = item2["memory"]["percent"] - item1["memory"]["percent"]
+            test_path = item2["node_id"]
+            out.append((test_path, diff))
+
+        return out
