aa2c8857e0c35bf6520c0df210f6b34d0d6f573b
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-09-12 11:33:52 -0500

    aws validated test

---
+++ tests/aws/services/cloudformation/resource_providers/scheduler/test_scheduler.py
@@ -0,0 +1,24 @@
+import os
+
+from localstack.testing.pytest import markers
+
+
+@markers.aws.validated
+def test_schedule_and_group(deploy_cfn_template, aws_client, snapshot):
+    stack = deploy_cfn_template(
+        template_path=os.path.join(os.path.dirname(__file__), "templates/schedule.yml")
+    )
+
+    snapshot.add_transformer(
+        snapshot.transform.key_value("PhysicalResourceId", "physical_resource_id")
+    )
+    snapshot.add_transformer(snapshot.transform.cloudformation_api())
+    schedule = aws_client.cloudformation.describe_stack_resource(
+        StackName=stack.stack_name, LogicalResourceId="MySchedule"
+    )["StackResourceDetail"]
+    snapshot.match("Schedule", schedule)
+
+    group = aws_client.cloudformation.describe_stack_resource(
+        StackName=stack.stack_name, LogicalResourceId="MyScheduleGroup"
+    )["StackResourceDetail"]
+    snapshot.match("Group", group)
