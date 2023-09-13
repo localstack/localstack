45644da9f32d4916cdebf6f6a484b2c0d30fba90
Benjamin Simon <benjh.simon@gmail.com>
2023-09-13 16:51:45 +0200

    validate existing test + add snapshot

--- tests/aws/services/resource_groups/test_resource_groups.py
+++ tests/aws/services/resource_groups/test_resource_groups.py
@@ -1,15 +1,53 @@
+import contextlib
 import json
+
+import pytest
+from botocore.exceptions import ClientError
 
 from localstack.testing.pytest import markers
 from localstack.utils.common import short_uid
 
 
+@pytest.fixture
+def resourcegroups_create_group(aws_client):
+    groups = []
+
+    def _create_group(**kwargs):
+        response = aws_client.resource_groups.create_group(**kwargs)
+        groups.append(response["Group"]["Name"])
+        return response
+
+    yield _create_group
+
+    for group_name in groups:
+        with contextlib.suppress(
+            ClientError, KeyError
+        ):  # adding KeyError to the list because Moto has a bug
+            aws_client.resource_groups.delete_group(GroupName=group_name)
+
+
+@pytest.fixture(autouse=True)
+def resource_groups_snapshot_transformers(snapshot):
+    snapshot.add_transformers_list(
+        [
+            snapshot.transform.key_value("Name"),
+            snapshot.transform.key_value("NextToken"),
+        ]
+    )
+
+
+@markers.snapshot.skip_snapshot_verify(
+    paths=[
+        "$..GroupArn",  # Moto is always returning the region as `us-west-1`, seems to be hard-coded
+        "$..GroupConfiguration",
+        "$..NextToken",
+    ]
+)
 class TestResourceGroups:
-    @markers.aws.unknown
-    def test_create_group(self, aws_client):
-        resource_group_client = aws_client.resource_groups
-        name = "resource_group-{}".format(short_uid())
-        response = resource_group_client.create_group(
+    @markers.aws.validated
+    def test_create_group(self, aws_client, resourcegroups_create_group, snapshot):
+        name = f"resource_group-{short_uid()}"
+        response = resourcegroups_create_group(
             Name=name,
             Description="description",
             ResourceQuery={
@@ -28,20 +66,25 @@ class TestResourceGroups:
             },
             Tags={"resource_group_tag_key": "resource_group_tag_value"},
         )
+        snapshot.match("create-group", response)
         assert name == response["Group"]["Name"]
         assert "TAG_FILTERS_1_0" == response["ResourceQuery"]["Type"]
         assert "resource_group_tag_value" == response["Tags"]["resource_group_tag_key"]
 
-        response = resource_group_client.get_group(GroupName=name)
+        response = aws_client.resource_groups.get_group(GroupName=name)
+        snapshot.match("get-group", response)
         assert "description" == response["Group"]["Description"]
 
-        response = resource_group_client.list_groups()
+        response = aws_client.resource_groups.list_groups()
+        snapshot.match("list-groups", response)
         assert 1 == len(response["GroupIdentifiers"])
         assert 1 == len(response["Groups"])
 
-        response = resource_group_client.delete_group(GroupName=name)
+        response = aws_client.resource_groups.delete_group(GroupName=name)
+        snapshot.match("delete-group", response)
         assert name == response["Group"]["Name"]
 
-        response = resource_group_client.list_groups()
+        response = aws_client.resource_groups.list_groups()
+        snapshot.match("list-groups-after-delete", response)
         assert 0 == len(response["GroupIdentifiers"])
         assert 0 == len(response["Groups"])
