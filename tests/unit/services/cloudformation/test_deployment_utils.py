422e7575b933af31fd52e3e8a850cd9748841400
Julian Coy <julian@coy.cloud>
2023-09-15 11:02:29 -0400

    Added one more test to cover the top-level parameters

--- tests/unit/services/cloudformation/test_deployment_utils.py
+++ tests/unit/services/cloudformation/test_deployment_utils.py
@@ -15,3 +15,13 @@ class TestFixBotoParametersBasedOnReport:
         value = fixed_params["LaunchTemplate"]["Version"]
         assert value == "1"
         assert type(value) == str
+
+    @markers.aws.only_localstack
+    def test_top_level_parameters_are_converted(self):
+        params = {"Version": 1}
+        message = "Invalid type for parameter Version, value: 1, type: <class 'int'>, valid types: <class 'str'>"
+
+        fixed_params = fix_boto_parameters_based_on_report(params, message)
+        value = fixed_params["Version"]
+        assert value == "1"
+        assert type(value) == str
