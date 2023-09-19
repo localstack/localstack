0d2dcd53618c710a6472b477e641de50a3983cff
Viren Nadkarni <viren.nadkarni@gmail.com>
2023-09-19 14:42:57 +0530

    Minor fixes

--- localstack/services/ram/provider.py
+++ localstack/services/ram/provider.py
@@ -1,10 +1,9 @@
 import logging
 
 from localstack.aws.api.ram import RamApi
-from localstack.services.plugins import ServiceLifecycleHook
 
 LOG = logging.getLogger(__name__)
 
 
-class RamProvider(RamApi, ServiceLifecycleHook):
+class RamProvider(RamApi):
     pass
