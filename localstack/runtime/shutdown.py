c97fa429a95451cfdbb7a5e0f268855cd29243cb
Thomas Rausch <thomas@thrau.at>
2023-09-19 11:30:29 +0200

    fix gateway shutdown procedure (#9129)

--- localstack/runtime/shutdown.py
+++ localstack/runtime/shutdown.py
@@ -1,7 +1,13 @@
+import logging
 from typing import Any, Callable
 
 from localstack.runtime import hooks
 from localstack.utils.functions import call_safe
+
+LOG = logging.getLogger(__name__)
+
+SERVICE_SHUTDOWN_PRIORITY = -10
+"""Shutdown hook priority for shutting down service plugins."""
 
 
 class ShutdownHandlers:
@@ -42,8 +48,26 @@ class ShutdownHandlers:
 
 
 SHUTDOWN_HANDLERS = ShutdownHandlers()
+"""Shutdown handlers run with default priority in an on_infra_shutdown hook."""
+
+ON_AFTER_SERVICE_SHUTDOWN_HANDLERS = ShutdownHandlers()
+"""Shutdown handlers that are executed after all services have been shut down."""
 
 
 @hooks.on_infra_shutdown()
 def run_shutdown_handlers():
     SHUTDOWN_HANDLERS.run()
+
+
+@hooks.on_infra_shutdown(priority=SERVICE_SHUTDOWN_PRIORITY)
+def shutdown_services():
+    # TODO: this belongs into the shutdown procedure of a `Platform` or `RuntimeContainer` class.
+    from localstack.services.plugins import SERVICE_PLUGINS
+
+    LOG.info("[shutdown] Stopping all services")
+    SERVICE_PLUGINS.stop_all_services()
+
+
+@hooks.on_infra_shutdown(priority=SERVICE_SHUTDOWN_PRIORITY - 10)
+def run_on_after_service_shutdown_handlers():
+    ON_AFTER_SERVICE_SHUTDOWN_HANDLERS.run()
