d67c80b00b2489f06e89158789d94acfecdcfff7
Julian Coy <julian@coy.cloud>
2023-09-21 09:20:49 -0400

    Addressed PR feedback
    - remove unnecessary test markers
    - do not interpolate debug strings

--- tests/unit/services/cloudformation/test_deployment_utils.py
+++ tests/unit/services/cloudformation/test_deployment_utils.py
@@ -1,9 +1,7 @@
 from localstack.services.cloudformation.deployment_utils import fix_boto_parameters_based_on_report
-from localstack.testing.pytest import markers
 
 
 class TestFixBotoParametersBasedOnReport:
-    @markers.aws.only_localstack
     def test_nested_parameters_are_fixed(self):
         params = {"LaunchTemplate": {"Version": 1}}
         message = (
@@ -16,7 +14,6 @@ class TestFixBotoParametersBasedOnReport:
         assert value == "1"
         assert type(value) == str
 
-    @markers.aws.only_localstack
     def test_top_level_parameters_are_converted(self):
         params = {"Version": 1}
         message = "Invalid type for parameter Version, value: 1, type: <class 'int'>, valid types: <class 'str'>"
