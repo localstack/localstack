6416ab818b90e23248af50889781d820b0de4cee
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2023-09-29 10:43:08 +0200

    Implementation of CloudFormation support for AWS::KinesisFirehose::DeliveryStream

--- localstack/services/cloudformation/resource_provider.py
+++ localstack/services/cloudformation/resource_provider.py
@@ -59,7 +59,8 @@ PROVIDER_DEFAULTS = {
     "AWS::Scheduler::ScheduleGroup": "ResourceProvider",
     "AWS::Route53::HealthCheck": "ResourceProvider",
     "AWS::Route53::RecordSet": "ResourceProvider",
-    "AWS::SNS::Topic": "ResourceProvider"
+    "AWS::SNS::Topic": "ResourceProvider",
+    "AWS::KinesisFirehose::DeliveryStream": "ResourceProvider"
     # "AWS::SSM::Parameter": "GenericBaseModel",
     # "AWS::OpenSearchService::Domain": "GenericBaseModel",
 }
