9971de1ac6c5525d5f25a85963ec07bb2fb2f6d1
Simon Walker <simon.walker@localstack.cloud>
2023-09-29 16:13:23 +0100

    Update sqs queue

--- localstack/services/sqs/resource_providers/aws_sqs_queue.py
+++ localstack/services/sqs/resource_providers/aws_sqs_queue.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
-from typing import Optional, Type, TypedDict
+from typing import Optional, TypedDict
 
 import localstack.services.cloudformation.provider_utils as util
 from localstack.services.cloudformation.resource_provider import (
-    CloudFormationResourceProviderPlugin,
     OperationStatus,
     ProgressEvent,
     ResourceProvider,
@@ -226,13 +225,3 @@ class SQSQueueProvider(ResourceProvider[SQSQueueProperties]):
             result[k] = v
 
         return result
-
-
-class SQSQueueProviderPlugin(CloudFormationResourceProviderPlugin):
-    name = "AWS::SQS::Queue"
-
-    def __init__(self):
-        self.factory: Optional[Type[ResourceProvider]] = None
-
-    def load(self):
-        self.factory = SQSQueueProvider
