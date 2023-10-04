ecee66767bf7d95ea2a3df5b060195350c868b29
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2023-10-04 13:05:04 +0200

    Implemented Subnet resource provider

--- localstack/services/cloudformation/resource_provider.py
+++ localstack/services/cloudformation/resource_provider.py
@@ -69,6 +69,7 @@ PROVIDER_DEFAULTS = {
     "AWS::EC2::SubnetRouteTableAssociation": "ResourceProvider",
     "AWS::EC2::VPCGatewayAttachment": "ResourceProvider",
     "AWS::EC2::SecurityGroup": "ResourceProvider",
+    "AWS::EC2::Subnet": "ResourceProvider",
     # "AWS::SSM::Parameter": "GenericBaseModel",
     # "AWS::OpenSearchService::Domain": "GenericBaseModel",
 }
