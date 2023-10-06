f9905947cc0f8148d55733e94a0b2b8146bbfc17
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2023-10-06 09:12:57 +0200

    Implementation of AWS::CloudWatch::* resource providers for CloudFormation (#9296)
    * Implemented Cloudwatch alarm resource
    * Implemented CloudWatch CompositeAlarm resource provider

--- localstack/services/cloudformation/resource_provider.py
+++ localstack/services/cloudformation/resource_provider.py
@@ -64,6 +64,8 @@ PROVIDER_DEFAULTS = {
     "AWS::Kinesis::StreamConsumer": "ResourceProvider",
     "AWS::KinesisFirehose::DeliveryStream": "ResourceProvider",
     "AWS::DynamoDB::Table": "ResourceProvider",
+    "AWS::CloudWatch::Alarm": "ResourceProvider",
+    "AWS::CloudWatch::CompositeAlarm": "ResourceProvider",
 }
 
 
