7c856637aea33cfc401aa83167578e4cc159a255
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2023-10-09 11:13:20 +0200

    Add support for AWS::CertificateManager::Certificate resource
    This commit introduces support for the AWS::CertificateManager::Certificate resource in LocalStack's CloudFormation service.

--- localstack/services/cloudformation/resource_provider.py
+++ localstack/services/cloudformation/resource_provider.py
@@ -67,6 +67,7 @@ PROVIDER_DEFAULTS = {
     "AWS::CloudWatch::Alarm": "ResourceProvider",
     "AWS::CloudWatch::CompositeAlarm": "ResourceProvider",
     "AWS::ECR::Repository": "ResourceProvider",
+    "AWS::CertificateManager::Certificate": "ResourceProvider",
 }
 
 
