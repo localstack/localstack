db7dd4d4db32c3ee01102e8740fe91537fed6125
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-10-13 17:20:10 -0500

    starting with simple files

---
+++ localstack/services/cloudwatch/provider_v2.py
@@ -0,0 +1,85 @@
+import logging
+
+from localstack.aws.api import RequestContext
+from localstack.aws.api.cloudwatch import AlarmNames, CloudwatchApi
+from localstack.deprecations import deprecated_endpoint
+from localstack.services.cloudwatch.alarm_scheduler import AlarmScheduler
+from localstack.services.edge import ROUTER
+from localstack.services.plugins import SERVICE_PLUGINS, ServiceLifecycleHook
+from localstack.utils.sync import poll_condition
+from localstack.utils.tagging import TaggingService
+from localstack.utils.threads import start_worker_thread
+
+PATH_GET_RAW_METRICS = "/_aws/cloudwatch/metrics/raw"
+DEPRECATED_PATH_GET_RAW_METRICS = "/cloudwatch/metrics/raw"
+MOTO_INITIAL_UNCHECKED_REASON = "Unchecked: Initial alarm creation"
+
+LOG = logging.getLogger(__name__)
+
+
+class CloudwatchProvider(CloudwatchApi, ServiceLifecycleHook):
+    """
+    Cloudwatch provider.
+
+    LIMITATIONS:
+        - no alarm rule evaluation
+    """
+
+    def __init__(self):
+        self.tags = TaggingService()
+        self.alarm_scheduler: AlarmScheduler = None
+        self.store = None
+
+    def on_after_init(self):
+        ROUTER.add(PATH_GET_RAW_METRICS, self.get_raw_metrics)
+        # TODO remove with 2.0
+        ROUTER.add(
+            DEPRECATED_PATH_GET_RAW_METRICS,
+            deprecated_endpoint(
+                self.get_raw_metrics,
+                previous_path=DEPRECATED_PATH_GET_RAW_METRICS,
+                deprecation_version="1.3.0",
+                new_path=PATH_GET_RAW_METRICS,
+            ),
+        )
+        self.start_alarm_scheduler()
+
+    def on_before_state_reset(self):
+        self.shutdown_alarm_scheduler()
+
+    def on_after_state_reset(self):
+        self.start_alarm_scheduler()
+
+    def on_before_state_load(self):
+        self.shutdown_alarm_scheduler()
+
+    def on_after_state_load(self):
+        self.start_alarm_scheduler()
+
+        def restart_alarms(*args):
+            poll_condition(lambda: SERVICE_PLUGINS.is_running("cloudwatch"))
+            self.alarm_scheduler.restart_existing_alarms()
+
+        start_worker_thread(restart_alarms)
+
+    def on_before_stop(self):
+        self.shutdown_alarm_scheduler()
+
+    def start_alarm_scheduler(self):
+        if not self.alarm_scheduler:
+            LOG.debug("starting cloudwatch scheduler")
+            self.alarm_scheduler = AlarmScheduler()
+
+    def shutdown_alarm_scheduler(self):
+        LOG.debug("stopping cloudwatch scheduler")
+        self.alarm_scheduler.shutdown_scheduler()
+        self.alarm_scheduler = None
+
+    def delete_alarms(self, context: RequestContext, alarm_names: AlarmNames) -> None:
+        """
+        Delete alarms.
+        """
+
+        for alarm_name in alarm_names.alarm_names:
+            alarm_arn = ""  # obtain alarm ARN from alarm name
+            self.alarm_scheduler.delete_alarm(alarm_arn)
