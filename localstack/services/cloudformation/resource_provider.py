aac20ba86a6757e676f7502e8ae551192b370ae1
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2023-10-23 17:49:45 +0200

    Add AWS::Logs::LogGroup support to CloudFormation

--- localstack/services/cloudformation/resource_provider.py
+++ localstack/services/cloudformation/resource_provider.py
@@ -76,6 +76,7 @@ PROVIDER_DEFAULTS = {
     "AWS::CertificateManager::Certificate": "ResourceProvider",
     "AWS::EKS::Nodegroup": "ResourceProvider",
     "AWS::Redshift::Cluster": "ResourceProvider",
+    "AWS::Logs::LogGroup": "ResourceProvider",
 }
 
 
