57680d56b37fe9cb6554d508f222a3f35bfe7727
Viren Nadkarni <viren.nadkarni@gmail.com>
2023-11-02 16:21:00 +0530

    KMS: Remove legacy 'local-kms' based provider

--- localstack/services/kms/plugins.py
+++
@@ -1,8 +0,0 @@
-from localstack.packages import Package, package
-
-
-@package(name="local-kms")
-def local_kms_package() -> Package:
-    from localstack.services.kms.packages import kms_local_package
-
-    return kms_local_package
