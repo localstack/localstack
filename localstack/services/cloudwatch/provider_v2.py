1d262aace4dade2cb5f66e556e568ac87e5d2122
Bernhard Matyas <90144234+baermat@users.noreply.github.com>
2023-11-03 16:56:26 +0100

    put_metric_alarm and describe_alarms for v2 cloudwatch

--- localstack/services/cloudwatch/provider_v2.py
+++ localstack/services/cloudwatch/provider_v2.py
@@ -1,11 +1,21 @@
 import logging
 
-from localstack.aws.api import RequestContext
+from localstack.aws.api import CommonServiceException, RequestContext, handler
 from localstack.aws.api.cloudwatch import (
+    ActionPrefix,
+    AlarmName,
+    AlarmNamePrefix,
     AlarmNames,
+    AlarmTypes,
     AmazonResourceName,
     CloudwatchApi,
+    DescribeAlarmsOutput,
+    InvalidParameterValueException,
     ListTagsForResourceOutput,
+    MaxRecords,
+    NextToken,
+    PutMetricAlarmInput,
+    StateValue,
     TagKeyList,
     TagList,
     TagResourceOutput,
@@ -13,8 +23,14 @@ from localstack.aws.api.cloudwatch import (
 )
 from localstack.http import Request
 from localstack.services.cloudwatch.alarm_scheduler import AlarmScheduler
+from localstack.services.cloudwatch.models import (
+    CloudWatchStore,
+    LocalStackMetricAlarm,
+    cloudwatch_stores,
+)
 from localstack.services.edge import ROUTER
 from localstack.services.plugins import SERVICE_PLUGINS, ServiceLifecycleHook
+from localstack.utils.aws import arns
 from localstack.utils.sync import poll_condition
 from localstack.utils.tagging import TaggingService
 from localstack.utils.threads import start_worker_thread
@@ -24,6 +40,12 @@ DEPRECATED_PATH_GET_RAW_METRICS = "/cloudwatch/metrics/raw"
 MOTO_INITIAL_UNCHECKED_REASON = "Unchecked: Initial alarm creation"
 
 LOG = logging.getLogger(__name__)
+
+
+class ValidationError(CommonServiceException):
+    # TODO: check this error against AWS (doesn't exist in the API)
+    def __init__(self, message: str):
+        super().__init__("ValidationError", message, 400, True)
 
 
 class CloudwatchProvider(CloudwatchApi, ServiceLifecycleHook):
@@ -38,6 +60,10 @@ class CloudwatchProvider(CloudwatchApi, ServiceLifecycleHook):
         self.tags = TaggingService()
         self.alarm_scheduler: AlarmScheduler = None
         self.store = None
+
+    @staticmethod
+    def get_store(account_id: str, region: str) -> CloudWatchStore:
+        return cloudwatch_stores[account_id][region]
 
     def on_after_init(self):
         ROUTER.add(PATH_GET_RAW_METRICS, self.get_raw_metrics)
@@ -79,14 +105,97 @@ class CloudwatchProvider(CloudwatchApi, ServiceLifecycleHook):
         Delete alarms.
         """
 
-        for alarm_name in alarm_names.alarm_names:
-            alarm_arn = ""  # obtain alarm ARN from alarm name
-            self.alarm_scheduler.delete_alarm(alarm_arn)
+        for alarm_name in alarm_names:
+            alarm_arn = arns.cloudwatch_alarm_arn(alarm_name)  # obtain alarm ARN from alarm name
+            self.alarm_scheduler.delete_scheduler_for_alarm(alarm_arn)
 
     def get_raw_metrics(self, request: Request):
         # TODO this needs to be read from the database
         # FIXME this is just a placeholder for now
         return {"metrics": []}
+
+    @handler("PutMetricAlarm", expand=False)
+    def put_metric_alarm(self, context: RequestContext, request: PutMetricAlarmInput) -> None:
+        # missing will be the default, when not set (but it will not explicitly be set)
+        if request.get("TreatMissingData", "missing") not in [
+            "breaching",
+            "notBreaching",
+            "ignore",
+            "missing",
+        ]:
+            raise ValidationError(
+                f"The value {request['TreatMissingData']} is not supported for TreatMissingData parameter. Supported values are [breaching, notBreaching, ignore, missing]."
+            )
+            # do some sanity checks:
+        if request.get("Period"):
+            # Valid values are 10, 30, and any multiple of 60.
+            value = request.get("Period")
+            if value not in (10, 30):
+                if value % 60 != 0:
+                    raise ValidationError("Period must be 10, 30 or a multiple of 60")
+        if request.get("Statistic"):
+            if request.get("Statistic") not in [
+                "SampleCount",
+                "Average",
+                "Sum",
+                "Minimum",
+                "Maximum",
+            ]:
+                raise ValidationError(
+                    f"Value '{request.get('Statistic')}' at 'statistic' failed to satisfy constraint: Member must satisfy enum value set: [Maximum, SampleCount, Sum, Minimum, Average]"
+                )
+
+        extended_statistic = request.get("ExtendedStatistic")
+        if extended_statistic and not extended_statistic.startswith("p"):
+            raise InvalidParameterValueException(
+                f"The value {extended_statistic} for parameter ExtendedStatistic is not supported."
+            )
+        evaluate_low_sample_count_percentile = request.get("EvaluateLowSampleCountPercentile")
+        if evaluate_low_sample_count_percentile and evaluate_low_sample_count_percentile not in (
+            "evaluate",
+            "ignore",
+        ):
+            raise ValidationError(
+                f"Option {evaluate_low_sample_count_percentile} is not supported. "
+                "Supported options for parameter EvaluateLowSampleCountPercentile are evaluate and ignore."
+            )
+
+        store = self.get_store(context.account_id, context.region)
+        metric_alarm = LocalStackMetricAlarm(context.account_id, context.region, {**request})
+        alarm_arn = metric_alarm.alarm["AlarmArn"]
+        store.Alarms[alarm_arn] = metric_alarm
+        self.alarm_scheduler.schedule_metric_alarm(alarm_arn)
+
+    def describe_alarms(
+        self,
+        context: RequestContext,
+        alarm_names: AlarmNames = None,
+        alarm_name_prefix: AlarmNamePrefix = None,
+        alarm_types: AlarmTypes = None,
+        children_of_alarm_name: AlarmName = None,
+        parents_of_alarm_name: AlarmName = None,
+        state_value: StateValue = None,
+        action_prefix: ActionPrefix = None,
+        max_records: MaxRecords = None,
+        next_token: NextToken = None,
+    ) -> DescribeAlarmsOutput:
+        store = self.get_store(context.account_id, context.region)
+        alarms = list(store.Alarms.values())
+        if action_prefix:
+            alarms = [a.alarm for a in alarms if a.alarm["AlarmAction"].startswith(action_prefix)]
+        elif alarm_name_prefix:
+            alarms = [a.alarm for a in alarms if a.alarm["AlarmName"].startswith(alarm_name_prefix)]
+        elif alarm_names:
+            alarms = [a.alarm for a in alarms if a.alarm["AlarmName"] in alarm_names]
+        elif state_value:
+            alarms = [a.alarm for a in alarms if a.alarm["StateValue"] == state_value]
+        else:
+            alarms = [a.alarm for a in list(store.Alarms.values())]
+
+        # TODO: Pagination
+        metric_alarms = [a for a in alarms if a.get("AlarmRule") is None]
+        composite_alarms = [a for a in alarms if a.get("AlarmRule") is not None]
+        return DescribeAlarmsOutput(CompositeAlarms=composite_alarms, MetricAlarms=metric_alarms)
 
     def list_tags_for_resource(
         self, context: RequestContext, resource_arn: AmazonResourceName
@@ -105,4 +214,3 @@ class CloudwatchProvider(CloudwatchApi, ServiceLifecycleHook):
     ) -> TagResourceOutput:
         self.tags.tag_resource(resource_arn, tags)
         return TagResourceOutput()
-
