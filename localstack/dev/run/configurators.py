5da79e98273474f0918553d154bd458d385794a6
Waldemar Hummer <waldemar.hummer@gmail.com>
2023-11-08 21:26:31 +0100

    skip mounting binary pip dependencies in dev container

--- localstack/dev/run/configurators.py
+++ localstack/dev/run/configurators.py
@@ -274,6 +274,9 @@ class DependencyMountConfigurator:
 
     dependency_glob = "/opt/code/localstack/.venv/lib/python3.*/site-packages/*"
 
+    # skip mounting dependencies with incompatible binaries (e.g., on MacOS)
+    skipped_dependencies = ["cryptography", "psutil", "rpds"]
+
     def __init__(
         self,
         *,
@@ -303,6 +306,9 @@ class DependencyMountConfigurator:
             if dep_path.name.endswith(".dist-info"):
                 continue
             if dep_path.name == "__pycache__":
+                continue
+
+            if dep_path.name in self.skipped_dependencies:
                 continue
 
             if dep_path.name in container_path_index:
