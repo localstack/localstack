1633ba8caa9952fa8e33d38835645797513d9343
Simon Walker <simon.walker@localstack.cloud>
2023-11-09 16:10:39 +0000

    Remove all usages of service_port

--- localstack/utils/kinesis/kinesis_connector.py
+++ localstack/utils/kinesis/kinesis_connector.py
@@ -23,6 +23,7 @@ from localstack.utils.strings import short_uid, to_str
 from localstack.utils.sync import retry
 from localstack.utils.threads import TMP_THREADS
 from localstack.utils.time import now
+from localstack.utils.urls import localstack_host
 
 EVENTS_FILE_PATTERN = os.path.join(tempfile.gettempdir(), "kclipy.*.fifo")
 LOG_FILE_PATTERN = os.path.join(tempfile.gettempdir(), "kclipy.*.log")
@@ -305,7 +306,7 @@ def get_stream_info(
     if aws_stack.is_local_env(env):
         stream_info["conn_kwargs"] = {
             "host": LOCALHOST,
-            "port": config.service_port("kinesis"),
+            "port": localstack_host().port,
             "is_secure": bool(config.USE_SSL),
         }
     if endpoint_url:
