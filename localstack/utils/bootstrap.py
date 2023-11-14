ccb95521b867a35576e9ad3d61e620fbd761f015
Ben Simon Hartung <42031100+bentsku@users.noreply.github.com>
2023-11-14 18:42:36 +0100

    add services to API_DEPENDENCIES (#9621)

--- localstack/utils/bootstrap.py
+++ localstack/utils/bootstrap.py
@@ -46,9 +46,11 @@ API_DEPENDENCIES = {
     "dynamodb": ["dynamodbstreams"],
     "dynamodbstreams": ["kinesis"],
     "es": ["opensearch"],
+    "cloudformation": ["s3", "sts"],
     "lambda": ["s3", "sqs", "sts"],
     "firehose": ["kinesis"],
     "sqs": ["sqs-query"],
+    "transcribe": ["s3"],
 }
 # composites define an abstract name like "serverless" that maps to a set of services
 API_COMPOSITES = {
