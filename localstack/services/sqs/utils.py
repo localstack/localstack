6c16619ff5d297d1911f1ba184362a444a34b85b
Viren Nadkarni <viren.nadkarni@gmail.com>
2023-11-15 13:02:27 +0530

    SQS: Rework is_sqs_queue_url() utility

--- localstack/services/sqs/utils.py
+++ localstack/services/sqs/utils.py
@@ -8,18 +8,28 @@ from urllib.parse import urlparse
 from moto.sqs.exceptions import MessageAttributesInvalid
 from moto.sqs.models import TRANSPORT_TYPE_ENCODINGS, Message
 
-from localstack.aws.accounts import get_aws_account_id
 from localstack.aws.api.sqs import QueueAttributeName, ReceiptHandleIsInvalid
+from localstack.services.sqs.constants import (
+    DOMAIN_STRATEGY_URL_REGEX,
+    LEGACY_STRATEGY_URL_REGEX,
+    PATH_STRATEGY_URL_REGEX,
+    STANDARD_STRATEGY_URL_REGEX,
+)
 from localstack.utils.aws.arns import parse_arn
 from localstack.utils.common import clone
 from localstack.utils.objects import singleton_factory
 from localstack.utils.strings import long_uid
-from localstack.utils.urls import path_from_url
 
 
-def is_sqs_queue_url(url):
-    path = path_from_url(url).partition("?")[0]
-    return re.match(r"^/(queue|%s)/[a-zA-Z0-9_-]+(.fifo)?$" % get_aws_account_id(), path)
+def is_sqs_queue_url(url: str) -> bool:
+    return any(
+        [
+            re.search(STANDARD_STRATEGY_URL_REGEX, url),
+            re.search(DOMAIN_STRATEGY_URL_REGEX, url),
+            re.search(PATH_STRATEGY_URL_REGEX, url),
+            re.search(LEGACY_STRATEGY_URL_REGEX, url),
+        ]
+    )
 
 
 def is_message_deduplication_id_required(queue):
