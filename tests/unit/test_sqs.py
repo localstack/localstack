b1542b0e1d1ce2c91e8fe8396616a747b16dcadc
Viren Nadkarni <viren.nadkarni@gmail.com>
2023-11-15 15:20:33 +0530

    Consider special case of domain strategy

--- tests/unit/test_sqs.py
+++ tests/unit/test_sqs.py
@@ -147,10 +147,12 @@ def test_parse_queue_url_invalid():
 
 
 def test_is_sqs_queue_url():
+    # General cases
     assert is_sqs_queue_url("http://localstack.cloud") is False
     assert is_sqs_queue_url("https://localstack.cloud:4566") is False
     assert is_sqs_queue_url("local.localstack.cloud:4566") is False
 
+    # Without proto prefix
     assert (
         is_sqs_queue_url("sqs.us-east-1.localhost.localstack.cloud:4566/111111111111/foo") is True
     )
@@ -160,18 +162,23 @@ def test_is_sqs_queue_url():
     assert is_sqs_queue_url("localhost:4566/queue/ap-south-1/222222222222/bar") is True
     assert is_sqs_queue_url("localhost:4566/111111111111/bar") is True
 
+    # With proto prefix
     assert (
-        is_sqs_queue_url("http://sqs.us-east-1.localhost.localstack.cloud:4566/111111111111/foo")
+        is_sqs_queue_url(
+            "http://sqs.us-east-1.localhost.localstack.cloud:4566/111111111111/foo.fifo"
+        )
         is True
     )
     assert (
-        is_sqs_queue_url("http://us-east-1.queue.localhost.localstack.cloud:4566/111111111111/foo")
+        is_sqs_queue_url("http://us-east-1.queue.localhost.localstack.cloud:4566/111111111111/foo1")
         is True
     )
-    assert is_sqs_queue_url("http://localhost:4566/queue/ap-south-1/222222222222/bar") is True
+    assert is_sqs_queue_url("http://localhost:4566/queue/ap-south-1/222222222222/my-queue") is True
     assert is_sqs_queue_url("http://localhost:4566/111111111111/bar") is True
 
     # Path strategy uses `localhost`
     assert (
         is_sqs_queue_url("local.localstack.cloud:4566/queue/ap-south-1/222222222222/bar") is False
     )
+    # Domain strategy may omit region
+    assert is_sqs_queue_url("http://queue.localhost.localstack.cloud:4566/111111111111/foo") is True
