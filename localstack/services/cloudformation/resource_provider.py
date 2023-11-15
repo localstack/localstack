82bf075502b46f064d671009bd01e1bd00fa4059
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2023-11-15 16:47:05 +0100

    Added new AWS::ApiGatewayV2::Api resource to CloudFormation

--- localstack/services/cloudformation/resource_provider.py
+++ localstack/services/cloudformation/resource_provider.py
@@ -72,6 +72,7 @@ PROVIDER_DEFAULTS = {
     "AWS::ApiGatewayV2::VpcLink": "ResourceProvider",
     "AWS::ApiGatewayV2::DomainName": "ResourceProvider",
     "AWS::ApiGatewayV2::Authorizer": "ResourceProvider",
+    "AWS::ApiGatewayV2::Api": "ResourceProvider",
     "AWS::CertificateManager::Certificate": "ResourceProvider",
     "AWS::CloudWatch::Alarm": "ResourceProvider",
     "AWS::CloudWatch::CompositeAlarm": "ResourceProvider",
