2899525ced13a2c4c3dd4aabfde48a988b42ac73
MEPalma <marcoepalma@tutanota.com>
2023-11-28 10:20:45 +0100

    fix filtering empty strings after splitting

--- tests/aws/services/stepfunctions/v2/intrinsic_functions/test_string_operations.py
+++ tests/aws/services/stepfunctions/v2/intrinsic_functions/test_string_operations.py
@@ -14,6 +14,10 @@ class TestStringOperations:
         self, create_iam_role_for_sfn, create_state_machine, sfn_snapshot, aws_client
     ):
         input_values = [
+            {"fst": " ", "snd": ","},
+            {"fst": " , ", "snd": ","},
+            {"fst": ", , ,", "snd": ","},
+            {"fst": ",,,,", "snd": ","},
             {"fst": "1,2,3,4,5", "snd": ","},
             {"fst": "This.is+a,test=string", "snd": ".+,="},
         ]
@@ -23,5 +27,28 @@ class TestStringOperations:
             create_state_machine,
             sfn_snapshot,
             IFT.STRING_SPLIT,
+            input_values,
+        )
+
+    @markers.aws.validated
+    def test_string_split_context_object(
+        self, create_iam_role_for_sfn, create_state_machine, sfn_snapshot, aws_client
+    ):
+        input_values = [
+            (
+                "Value1,Value2,Value3\n"
+                "Value4,Value5,Value6\n"
+                ",,,\n"
+                "true,1,'HelloWorld'\n"
+                "Null,None,\n"
+                "   \n"
+            )
+        ]
+        create_and_test_on_inputs(
+            aws_client.stepfunctions,
+            create_iam_role_for_sfn,
+            create_state_machine,
+            sfn_snapshot,
+            IFT.STRING_SPLIT_CONTEXT_OBJECT,
             input_values,
         )
