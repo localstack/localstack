238484fd674b709f23265900734bc9e7e1b0ea20
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-12-04 14:43:07 +0100

    Fix incompatible f-strings for Python 3.7

--- tests/aws/services/lambda_/functions/common/endpointinjection/python/src/handler.py
+++ tests/aws/services/lambda_/functions/common/endpointinjection/python/src/handler.py
@@ -7,5 +7,5 @@ sqs_client = boto3.client("sqs", endpoint_url=os.environ.get("AWS_ENDPOINT_URL")
 
 def handler(event, context):
     queues = sqs_client.list_queues()
-    print(f"{queues=}")
+    print("queues=" + str(queues))
     return "ok"
