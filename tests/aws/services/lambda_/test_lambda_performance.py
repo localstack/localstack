5f4e6242697ab0fa3222b0c9b5bb720f43c49fb3
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-12-18 17:58:33 +0100

    Increase connection pool for AWS client in test

--- tests/aws/services/lambda_/test_lambda_performance.py
+++ tests/aws/services/lambda_/test_lambda_performance.py
@@ -16,6 +16,7 @@ import timeit
 from datetime import datetime, timedelta
 
 import pytest
+from botocore.config import Config
 
 from localstack import config
 from localstack.aws.api.lambda_ import InvocationType, Runtime
@@ -97,7 +98,7 @@ class ThreadSafeCounter:
 
 
 @markers.aws.unknown
-def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client):
+def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client, aws_client_factory):
     function_name = f"echo-func-{short_uid()}"
     create_lambda_function(
         handler_file=TEST_LAMBDA_PYTHON_S3_INTEGRATION,
@@ -114,7 +115,7 @@ def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client):
     lock = threading.Lock()
     request_ids = []
     error_counter = ThreadSafeCounter()
-    num_invocations = 200
+    num_invocations = 150
     invoke_barrier = threading.Barrier(num_invocations)
 
     def invoke(runner: int):
@@ -124,7 +125,11 @@ def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client):
         try:
             payload = {"file_size_bytes": 1}
             invoke_barrier.wait()
-            result = aws_client.lambda_.invoke(
+            pool_config = Config(
+                max_pool_connections=num_invocations,
+            )
+            lambda_client = aws_client_factory(config=pool_config).lambda_
+            result = lambda_client.invoke(
                 FunctionName=function_name,
                 InvocationType=InvocationType.Event,
                 Payload=to_bytes(json.dumps(payload)),
@@ -151,7 +156,7 @@ def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client):
     print(f"N={num_invocations} took {diff.total_seconds()} seconds")
     assert error_counter.counter == 0
 
-    sleep_seconds = 120
+    sleep_seconds = 200
     print(f"Sleeping for {sleep_seconds} ...")
     time.sleep(sleep_seconds)
 
@@ -167,7 +172,9 @@ def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client):
             "Statistics": ["Sum"],
         }
         response = aws_client.cloudwatch.get_metric_statistics(**metric_query_params)
-        num_invocations_metric = response["Datapoints"][0]["Sum"]
+        num_invocations_metric = 0
+        for datapoint in response["Datapoints"]:
+            num_invocations_metric += datapoint["Sum"]
         # assert num_invocations_metric == num_invocations
         return num_invocations_metric
 
@@ -176,9 +183,12 @@ def test_lambda_event_invoke(create_lambda_function, s3_bucket, aws_client):
 
     # Validate CloudWatch invocation logs
     def assert_log_events():
-        log_events = aws_client.logs.filter_log_events(
+        # the default and maximum limit is 10k events
+        response = aws_client.logs.filter_log_events(
             logGroupName=f"/aws/lambda/{function_name}",
-        )["events"]
+        )
+        assert "nextToken" not in response  # guard against pagination
+        log_events = response["events"]
         invocation_count = len(
             [event["message"] for event in log_events if event["message"].startswith("REPORT")]
         )
