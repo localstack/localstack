ca735ca5b8ebb7c7a460cb256c872df580fdd89d
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-12-21 16:56:08 +0100

    Switch lambda poller from long to short polling

--- localstack/services/lambda_/invocation/event_manager.py
+++ localstack/services/lambda_/invocation/event_manager.py
@@ -3,6 +3,7 @@ import dataclasses
 import json
 import logging
 import threading
+import time
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from math import ceil
@@ -106,6 +107,7 @@ def has_enough_time_for_retry(
 
 # TODO: optimize this client configuration. Do we need to consider client caching here?
 CLIENT_CONFIG = Config(
+    # We shut down the LocalStack gateway last, hence this does not delay LocalStack cleanup anymore
     connect_timeout=60,
     read_timeout=60,
     retries={"max_attempts": 2},
@@ -113,7 +115,7 @@ CLIENT_CONFIG = Config(
     # might push beyond. We need to track how these connections and/or clients are shared.
     # An insufficient pool can lead to connection timeout error with the warning:
     # urllib3.connectionpool: Connection pool is full, discarding connection: localhost. Connection pool size: 1
-    max_pool_connections=2500,
+    # max_pool_connections=10,
 )
 
 
@@ -140,17 +142,16 @@ class Poller:
             )
             function_timeout = self.version_manager.function_version.config.timeout
             while not self._shutdown_event.is_set():
-                LOG.debug("[%s] Polling event queue", self.event_queue_url)
                 response = sqs_client.receive_message(
                     QueueUrl=self.event_queue_url,
-                    WaitTimeSeconds=2,
+                    # TODO: consider replacing with short polling instead of long polling to prevent keeping connections open
+                    # WaitTimeSeconds=2,
                     # Related: SQS event source mapping batches up to 10 messages:
                     # https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html
                     MaxNumberOfMessages=10,
                     VisibilityTimeout=function_timeout + 60,
                 )
                 if not response.get("Messages"):
-                    LOG.debug("[%s] No message in queue", self.event_queue_url)
                     continue
                 LOG.debug("[%s] Got %d messages", self.event_queue_url, len(response["Messages"]))
                 # Guard against shutdown event arriving while polling SQS for messages
@@ -159,11 +160,14 @@ class Poller:
                         # NOTE: queueing within the thread pool executor could lead to double executions
                         #  due to the visibility timeout
                         self.invoker_pool.submit(self.handle_message, message)
+
+                # Try short polling instead of long polling to reduce the number of open connections
+                time.sleep(2)
         except Exception as e:
-            # TODO gateway shuts down before shutdown event even is set, so this log message might be sent regardless
+            # TODO: if the gateway shuts down before the shutdown event even is set,
+            #  this log message might be sent regardless
             if isinstance(e, ConnectionRefusedError) and self._shutdown_event.is_set():
                 return
-            # TODO: investigate what causes ReadTimeoutError (fixed with increasing read timeout?)
             LOG.error(
                 "Error while polling lambda events for function %s: %s",
                 self.version_manager.function_version.qualified_arn,
