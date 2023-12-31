da6f547755eabf2d228f1b5dd7df6f3490a4a519
Thomas Rausch <thomas@thrau.at>
2023-12-31 17:45:00 +0100

    add subrange operation for ports

--- tests/unit/utils/test_net_utils.py
+++ tests/unit/utils/test_net_utils.py
@@ -96,6 +96,27 @@ def test_get_free_tcp_port_range():
         assert port_can_be_bound(port)
 
 
+def test_subrange():
+    r = PortRange(50000, 60000)
+    r.mark_reserved(50000)
+    r.mark_reserved(50001)
+    r.mark_reserved(50002)
+    r.mark_reserved(50003)
+
+    sr = r.subrange(end=50005)
+    assert sr.as_range() == range(50000, 50006)
+
+    assert sr.is_port_reserved(50000)
+    assert sr.is_port_reserved(50001)
+    assert sr.is_port_reserved(50002)
+    assert sr.is_port_reserved(50003)
+    assert not sr.is_port_reserved(50004)
+    assert not sr.is_port_reserved(50005)
+
+    sr.mark_reserved(50005)
+    assert r.is_port_reserved(50005)
+
+
 def test_get_free_tcp_port_range_fails_if_reserved(monkeypatch):
     mock = MagicMock()
     mock.return_value = True
