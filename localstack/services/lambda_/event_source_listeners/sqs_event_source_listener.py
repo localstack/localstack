3e68b1a53e91ceb90ecdf1df1f960dd8b74bea19
Daniel Fangl <daniel.fangl@localstack.cloud>
2024-01-11 19:00:09 +0100

    fix type, add sec suffix

--- localstack/services/lambda_/event_source_listeners/sqs_event_source_listener.py
+++ localstack/services/lambda_/event_source_listeners/sqs_event_source_listener.py
@@ -26,7 +26,7 @@ LOG = logging.getLogger(__name__)
 class SQSEventSourceListener(EventSourceListener):
     # SQS listener thread settings
     SQS_LISTENER_THREAD: Dict = {}
-    SQS_POLL_INTERVAL_SEC: float = config.LAMBDA_SQS_EVENT_SOURCE_MAPPING_INTERVAL
+    SQS_POLL_INTERVAL_SEC: float = config.LAMBDA_SQS_EVENT_SOURCE_MAPPING_INTERVAL_SEC
 
     _invoke_adapter: EventSourceAdapter
 
