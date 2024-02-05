e231c6ae05bf7be8f9845c51041388cfcfdf68f9
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-02-05 18:44:19 +0100

    feat: add resharding split test

--- tests/aws/services/firehose/conftest.py
+++ tests/aws/services/firehose/conftest.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from typing import List, Literal, Union
 
@@ -94,3 +95,45 @@ def read_s3_data(aws_client):
         return bucket_data
 
     return _read_s3_data
+
+
+@pytest.fixture
+def get_shards_starting_hash_keys(aws_client):
+    def _get_shards_starting_hash_keys(stream_name: str) -> dict[str, str]:
+        starting_hash_keys = {}
+        response = aws_client.kinesis.describe_stream(StreamName=stream_name)
+        shards = response["StreamDescription"]["Shards"]
+        for shard in shards:
+            shard_id = shard["ShardId"][-2:]
+            starting_hash_keys[shard_id] = shard["HashKeyRange"]["StartingHashKey"]
+        return starting_hash_keys
+
+    return _get_shards_starting_hash_keys
+
+
+@pytest.fixture
+def get_all_messages_from_s3(read_s3_data):
+    def _get_all_messages_from_s3(
+        bucket_name: str,
+        timeout: int = 300,
+        sleep: int = 5,
+        retries: int = 3,
+        assert_message_count: int = 4,
+    ) -> list[str]:
+        # poll file from s3 buckets
+        def get_all_messages():
+            s3_data_bucket = read_s3_data(bucket_name, timeout=timeout)
+            messages = []
+            for input_string in s3_data_bucket.values():
+                json_array_string = "[" + input_string.replace("}{", "},{") + "]"
+                message = json.loads(json_array_string)
+                messages.extend(message)
+            if len(messages) != assert_message_count:
+                raise Exception(f"Failed to receive all sent messages: {messages}")
+            else:
+                return messages
+
+        messages = retry(get_all_messages, sleep=sleep, retries=retries)
+        return messages
+
+    yield _get_all_messages_from_s3
