80b9c24155d59b6f62ef0942682dc9e8e4af59d5
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2024-02-06 16:24:25 -0500

    revert

--- localstack/services/cloudformation/engine/transformers.py
+++ localstack/services/cloudformation/engine/transformers.py
@@ -232,7 +232,7 @@ def execute_macro(
     if result.get("status") != "success":
         error_message = result.get("errorMessage")
         message = (
-            f"Transform {transformation_id} failed with: {error_message}.. Rollback requested by user."
+            f"Transform {transformation_id} failed with: {error_message}. Rollback requested by user."
             if error_message
             else f"Transform {transformation_id} failed without an error message.. Rollback requested by user."
         )
