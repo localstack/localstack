cac370ac3427b206b9c90a433bd383453b1b2a56
Daniel Fangl <daniel.fangl@localstack.cloud>
2024-02-09 12:48:43 +0100

    change AWS_EXECUTION_ENV variable setting for the init, allow copy of init without setting debug

--- localstack/services/lambda_/invocation/execution_environment.py
+++ localstack/services/lambda_/invocation/execution_environment.py
@@ -156,7 +156,7 @@ class ExecutionEnvironment:
             env_vars["_HANDLER"] = self.function_version.config.handler
         # Not defined for custom runtimes (e.g., provided, provided.al2)
         if self.function_version.config.runtime:
-            env_vars["AWS_EXECUTION_ENV"] = f"Aws_Lambda_{self.function_version.config.runtime}"
+            env_vars["AWS_EXECUTION_ENV"] = "AWS_Lambda_rapid"
         if self.function_version.config.environment:
             env_vars.update(self.function_version.config.environment)
         if config.LAMBDA_INIT_DEBUG:
