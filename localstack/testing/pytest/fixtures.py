d812bbb6481bb57eb6adef40390a026c7d876a99
Viren Nadkarni <viren.nadkarni@gmail.com>
2024-02-12 15:39:06 +0530

    Fix secondary_region_name fixture

--- localstack/testing/pytest/fixtures.py
+++ localstack/testing/pytest/fixtures.py
@@ -1819,11 +1819,8 @@ def secondary_account_id(secondary_aws_client):
 
 
 @pytest.fixture(scope="session")
-def secondary_region_name(secondary_aws_client):
-    if is_aws_cloud() or is_api_enabled("sts"):
-        return secondary_aws_client.sts.meta.region_name
-    else:
-        return constants.SECONDARY_TEST_AWS_REGION_NAME
+def secondary_region_name():
+    return constants.SECONDARY_TEST_AWS_REGION_NAME
 
 
 @pytest.hookimpl
