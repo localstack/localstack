0bbe1d8b5bd2e72177d34b8de672f4b6f36b67bd
MEPalma <marcoepalma@tutanota.com>
2024-02-26 00:28:49 +0100

    runTask with response and sync positive cases

--- localstack/services/stepfunctions/asl/component/state/state_execution/state_task/service/state_task_service_ecs.py
+++ localstack/services/stepfunctions/asl/component/state/state_execution/state_task/service/state_task_service_ecs.py
@@ -1,6 +1,7 @@
 from typing import Final, Optional
 
 from localstack.services.stepfunctions.asl.component.state.state_execution.state_task.service.resource import (
+    ResourceCondition,
     ResourceRuntimePart,
 )
 from localstack.services.stepfunctions.asl.component.state.state_execution.state_task.service.state_task_service_callback import (
@@ -26,6 +27,9 @@ class StateTaskServiceEcs(StateTaskServiceCallback):
             "EnableExecuteCommand",
         }
     }
+
+    _STARTED_BY_PARAMETER_RAW_KEY: Final[str] = "StartedBy"
+    _STARTED_BY_PARAMETER_VALUE: Final[str] = "AWS Step Functions"
 
     def _get_supported_parameters(self) -> Optional[set[str]]:
         return self._SUPPORTED_API_PARAM_BINDINGS.get(self.resource.api_action.lower())
@@ -62,26 +66,14 @@ class StateTaskServiceEcs(StateTaskServiceCallback):
     #         )
     #     return super()._from_error(env=env, ex=ex)
 
-    # def _normalise_parameters(
-    #         self,
-    #         parameters: dict,
-    #         boto_service_name: Optional[str] = None,
-    #         service_action_name: Optional[str] = None,
-    # ) -> None:
-    #     if service_action_name is None:
-    #         if self._get_boto_service_action() == "start_execution":
-    #             optional_input = parameters.get("Input")
-    #             if not isinstance(optional_input, str):
-    #                 # AWS Sfn's documentation states:
-    #                 # If you don't include any JSON input data, you still must include the two braces.
-    #                 if optional_input is None:
-    #                     optional_input = {}
-    #                 parameters["Input"] = to_json_str(optional_input, separators=(",", ":"))
-    #     super()._normalise_parameters(
-    #         parameters=parameters,
-    #         boto_service_name=boto_service_name,
-    #         service_action_name=service_action_name,
-    #     )
+    def _before_eval_execution(
+        self, env: Environment, resource_runtime_part: ResourceRuntimePart, raw_parameters: dict
+    ) -> None:
+        if self.resource.condition == ResourceCondition.Sync:
+            raw_parameters[self._STARTED_BY_PARAMETER_RAW_KEY] = self._STARTED_BY_PARAMETER_VALUE
+        super()._before_eval_execution(
+            env=env, resource_runtime_part=resource_runtime_part, raw_parameters=raw_parameters
+        )
 
     def _eval_service_task(
         self,
@@ -91,13 +83,30 @@ class StateTaskServiceEcs(StateTaskServiceCallback):
     ):
         service_name = self._get_boto_service_name()
         api_action = self._get_boto_service_action()
-        client = boto_client_for(
+        ecs_client = boto_client_for(
             region=resource_runtime_part.region,
             account=resource_runtime_part.account,
             service=service_name,
         )
-        response = getattr(client, api_action)(**normalised_parameters)
+        response = getattr(ecs_client, api_action)(**normalised_parameters)
         response.pop("ResponseMetadata", None)
+
+        # AWS outputs the description of the task, not the output of run_task.
+        match self._get_boto_service_action():
+            case "run_task":
+                self._normalise_response(response=response, service_action_name="run_task")
+                cluster_arn: str = response["Tasks"][0]["ClusterArn"]
+                task_arn: str = response["Tasks"][0]["TaskArn"]
+                describe_tasks_output = ecs_client.describe_tasks(
+                    cluster=cluster_arn, tasks=[task_arn]
+                )
+                describe_tasks_output.pop("ResponseMetadata", None)
+                self._normalise_response(
+                    response=describe_tasks_output, service_action_name="describe_tasks"
+                )
+                env.stack.append(describe_tasks_output)
+                return
+
         env.stack.append(response)
 
     def _sync_to_run_task(
@@ -106,52 +115,43 @@ class StateTaskServiceEcs(StateTaskServiceCallback):
         resource_runtime_part: ResourceRuntimePart,
     ) -> None:
         ...
-        # ecs_client = boto_client_for(
-        #     region=resource_runtime_part.region,
-        #     account=resource_runtime_part.account,
-        #     service="ecs",
-        # )
-
-        # submission_output: dict = env.stack.pop()
-        # TODO
-        # execution_arn: str = submission_output["ExecutionArn"]
+        ecs_client = boto_client_for(
+            region=resource_runtime_part.region,
+            account=resource_runtime_part.account,
+            service="ecs",
+        )
+        submission_output: dict = env.stack.pop()
+        task_arn: str = submission_output["Tasks"][0]["TaskArn"]
+        cluster_arn: str = submission_output["Tasks"][0]["ClusterArn"]
 
         def _has_terminated() -> Optional[dict]:
-            return {}
-            # connect_to().ecs.describe_tasks(cluster="", tasks=[])['tasks']
-            # connect_to().ecs.run_task()['tasks'][0]['']
-            # describe_execution_output = sfn_client.describe_execution(executionArn=execution_arn)
-            # describe_execution_output: DescribeExecutionOutput = select_from_typed_dict(
-            #     DescribeExecutionOutput, describe_execution_output
-            # )
-            # execution_status: ExecutionStatus = describe_execution_output["status"]
-            #
-            # if execution_status != ExecutionStatus.RUNNING:
-            #     if sync2_response:
-            #         self._sync2_api_output_of(
-            #             typ=DescribeExecutionOutput, value=describe_execution_output
-            #         )
-            #     self._normalise_response(
-            #         response=describe_execution_output, service_action_name="describe_execution"
-            #     )
-            #     if execution_status == ExecutionStatus.SUCCEEDED:
-            #         return describe_execution_output
-            #     else:
-            #         raise FailureEventException(
-            #             FailureEvent(
-            #                 error_name=StatesErrorName(typ=StatesErrorNameType.StatesTaskFailed),
-            #                 event_type=HistoryEventType.TaskFailed,
-            #                 event_details=EventDetails(
-            #                     taskFailedEventDetails=TaskFailedEventDetails(
-            #                         resource=self._get_sfn_resource(),
-            #                         resourceType=self._get_sfn_resource_type(),
-            #                         error=StatesErrorNameType.StatesTaskFailed.to_name(),
-            #                         cause=to_json_str(describe_execution_output),
-            #                     )
-            #                 ),
-            #             )
-            #         )
-            # return None
+            describe_tasks_output = ecs_client.describe_tasks(cluster=cluster_arn, tasks=[task_arn])
+            last_status: str = describe_tasks_output["tasks"][0]["lastStatus"]
+
+            if last_status == "STOPPED":
+                self._normalise_response(
+                    response=describe_tasks_output, service_action_name="describe_tasks"
+                )
+                return describe_tasks_output["Tasks"][0]  # noqa
+
+                # if execution_status == ExecutionStatus.SUCCEEDED:
+                #     return describe_execution_output
+                # else:
+                #     raise FailureEventException(
+                #         FailureEvent(
+                #             error_name=StatesErrorName(typ=StatesErrorNameType.StatesTaskFailed),
+                #             event_type=HistoryEventType.TaskFailed,
+                #             event_details=EventDetails(
+                #                 taskFailedEventDetails=TaskFailedEventDetails(
+                #                     resource=self._get_sfn_resource(),
+                #                     resourceType=self._get_sfn_resource_type(),
+                #                     error=StatesErrorNameType.StatesTaskFailed.to_name(),
+                #                     cause=to_json_str(describe_execution_output),
+                #                 )
+                #             ),
+                #         )
+                #     )
+            return None
 
         termination_output: Optional[dict] = None
         while env.is_running() and termination_output is None:
--- tests/aws/services/stepfunctions/v2/services/test_ecs_task_service.py
+++ tests/aws/services/stepfunctions/v2/services/test_ecs_task_service.py
@@ -1,12 +1,14 @@
+import json
+
 import aws_cdk as cdk
 import aws_cdk.aws_ecs as ecs
 import aws_cdk.aws_stepfunctions as sfn
 import aws_cdk.aws_stepfunctions_tasks as tasks
 import pytest
+from localstack_snapshot.snapshots.transformer import RegexTransformer
 
 from localstack.testing.pytest import markers
-from localstack.utils.analytics.metadata import is_license_activated
-from tests.aws.services.stepfunctions.utils import await_execution_terminated
+from tests.aws.services.stepfunctions.utils import launch_and_record_execution
 
 
 # TODO: figure out a better way, maybe via marker? e.g. @markers.localstack.ext
@@ -14,8 +16,8 @@ from tests.aws.services.stepfunctions.utils import await_execution_terminated
 class TestTaskServiceECS:
     STACK_NAME = "StepFunctionsEcsTaskStack"
 
-    @pytest.fixture(scope="class", autouse=True)
-    def infrastructure(self, aws_client, infrastructure_setup):
+    @pytest.fixture(scope="class", autouse=False)
+    def infrastructure_test_run_task(self, aws_client, infrastructure_setup):
         infra = infrastructure_setup(namespace="StepFunctionsEcsTask", force_synth=True)
         stack = cdk.Stack(infra.cdk_app, self.STACK_NAME)
 
@@ -39,7 +41,54 @@ class TestTaskServiceECS:
             stack,
             "ecstask",
             cluster=cluster,
-            launch_target=launch_target,
+            launch_target=launch_target,  # noqa
+            task_definition=task_def,
+            integration_pattern=sfn.IntegrationPattern.REQUEST_RESPONSE,
+        )
+        definition_body = sfn.DefinitionBody.from_chainable(run_task)
+        statemachine = sfn.StateMachine(stack, "statemachine", definition_body=definition_body)
+
+        # stack outputs
+        cdk.CfnOutput(stack, "TaskDefinitionArn", value=task_def.task_definition_arn)
+        cdk.CfnOutput(stack, "ClusterArn", value=cluster.cluster_arn)
+        cdk.CfnOutput(stack, "StateMachineArn", value=statemachine.state_machine_arn)
+        cdk.CfnOutput(stack, "ClusterName", value=cluster.cluster_name)
+        cdk.CfnOutput(stack, "StateMachineRoleArn", value=statemachine.role.role_arn)
+        cdk.CfnOutput(stack, "TaskDefinitionFamily", value=task_def.family)
+        cdk.CfnOutput(
+            stack, "TaskDefinitionContainerName", value=task_def.default_container.container_name
+        )
+
+        # provisioning
+        with infra.provisioner(skip_deployment=False, skip_teardown=False) as prov:
+            yield prov
+
+    @pytest.fixture(scope="class", autouse=False)
+    def infrastructure_test_run_task_sync(self, aws_client, infrastructure_setup):
+        infra = infrastructure_setup(namespace="StepFunctionsEcsTask", force_synth=True)
+        stack = cdk.Stack(infra.cdk_app, self.STACK_NAME)
+
+        # cluster setup
+        cluster = ecs.Cluster(stack, "cluster")
+
+        # task setup
+        launch_target = tasks.EcsFargateLaunchTarget(
+            platform_version=ecs.FargatePlatformVersion.VERSION1_4
+        )
+        task_def = ecs.FargateTaskDefinition(stack, "taskdef", cpu=256, memory_limit_mib=512)
+        task_def.add_container(
+            "maincontainer",
+            image=ecs.ContainerImage.from_registry("busybox"),
+            entry_point=["echo", "hello"],
+            essential=True,
+        )
+
+        # state machine setup
+        run_task = tasks.EcsRunTask(
+            stack,
+            "ecstask",
+            cluster=cluster,
+            launch_target=launch_target,  # noqa
             task_definition=task_def,
             integration_pattern=sfn.IntegrationPattern.RUN_JOB,
         )
@@ -47,37 +96,171 @@ class TestTaskServiceECS:
         statemachine = sfn.StateMachine(stack, "statemachine", definition_body=definition_body)
 
         # stack outputs
+        cdk.CfnOutput(stack, "TaskDefinitionArn", value=task_def.task_definition_arn)
+        cdk.CfnOutput(stack, "ClusterArn", value=cluster.cluster_arn)
         cdk.CfnOutput(stack, "StateMachineArn", value=statemachine.state_machine_arn)
         cdk.CfnOutput(stack, "ClusterName", value=cluster.cluster_name)
+        cdk.CfnOutput(stack, "StateMachineRoleArn", value=statemachine.role.role_arn)
+        cdk.CfnOutput(stack, "TaskDefinitionFamily", value=task_def.family)
+        cdk.CfnOutput(
+            stack, "TaskDefinitionContainerName", value=task_def.default_container.container_name
+        )
 
         # provisioning
-        with infra.provisioner(skip_deployment=False, skip_teardown=True) as prov:
+        with infra.provisioner(skip_deployment=False, skip_teardown=False) as prov:
             yield prov
 
     @markers.aws.validated
-    def test_run_machine(self, aws_client, infrastructure, snapshot):
+    @markers.snapshot.skip_snapshot_verify(
+        paths=[
+            "$..Attachments..Details",
+            "$..Attachments..Id",
+            "$..Attachments..Status",
+            "$..Attachments..Type",
+            "$..AvailabilityZone",
+            "$..ClusterArn",
+            "$..Connectivity",
+            "$..ConnectivityAt",
+            "$..Cpu",
+            "$..DesiredStatus",
+            "$..ExecutionStoppedAt",
+            "$..GpuIds",
+            "$..Group",
+            "$..HealthStatus",
+            "$..ImageDigest",
+            "$..InferenceAccelerators",
+            "$..LastStatus",
+            "$..ManagedAgents",
+            "$..Memory",
+            "$..NetworkInterfaces",
+            "$..Overrides.ContainerOverrides",
+            "$..Overrides.InferenceAcceleratorOverrides",
+            "$..PlatformFamily",
+            "$..PullStartedAt",
+            "$..PullStoppedAt",
+            "$..RuntimeId",
+            "$..SdkHttpMetadata",
+            "$..SdkResponseMetadata",
+            "$..StartedAt",
+            "$..StartedBy",
+            "$..StopCode",
+            "$..StoppedAt",
+            "$..StoppedReason",
+            "$..StoppingAt",
+            "$..TaskDefinitionArn",
+            "$..Version",
+            "$..parameters.Cluster",
+        ]
+    )
+    def test_run_task(self, aws_client, infrastructure_test_run_task, sfn_ecs_snapshot):
         # TODO: transformers potentially needed for all the ECS task outputs
         # TODO: generate snapshot
-        outputs = infrastructure.get_stack_outputs(stack_name=self.STACK_NAME)
-        sm_arn = outputs["StateMachineArn"]
-        cluster_name = outputs["ClusterName"]
+        stack_outputs = infrastructure_test_run_task.get_stack_outputs(stack_name=self.STACK_NAME)
+        state_machine_arn = stack_outputs["StateMachineArn"]
 
-        cluster = aws_client.ecs.describe_clusters(clusters=[cluster_name])["clusters"][0]
-        snapshot.match("cluster", cluster)
-        execution_arn = aws_client.stepfunctions.start_execution(stateMachineArn=sm_arn)[
-            "executionArn"
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["TaskDefinitionArn"], "task_definition_arn")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["StateMachineRoleArn"], "state_machine_role_arn")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["ClusterArn"], "cluster_arn")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(
+                stack_outputs["TaskDefinitionContainerName"], "task_definition_container_name"
+            )
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["TaskDefinitionFamily"], "task_definition_family")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["ClusterName"], "cluster_name")
+        )
+        sfn_ecs_snapshot.add_transformer(RegexTransformer(state_machine_arn, "state_machine_arn"))
+
+        launch_and_record_execution(
+            stepfunctions_client=aws_client.stepfunctions,
+            sfn_snapshot=sfn_ecs_snapshot,
+            state_machine_arn=state_machine_arn,
+            execution_input=json.dumps({}),
+        )
+
+    @markers.aws.validated
+    @markers.snapshot.skip_snapshot_verify(
+        paths=[
+            "$..Attachments..Details",
+            "$..Attachments..Id",
+            "$..Attachments..Status",
+            "$..Attachments..Type",
+            "$..AvailabilityZone",
+            "$..ClusterArn",
+            "$..Connectivity",
+            "$..ConnectivityAt",
+            "$..Cpu",
+            "$..DesiredStatus",
+            "$..ExecutionStoppedAt",
+            "$..GpuIds",
+            "$..Group",
+            "$..HealthStatus",
+            "$..ImageDigest",
+            "$..InferenceAccelerators",
+            "$..LastStatus",
+            "$..ManagedAgents",
+            "$..Memory",
+            "$..NetworkInterfaces",
+            "$..Overrides.ContainerOverrides",
+            "$..Overrides.InferenceAcceleratorOverrides",
+            "$..PlatformFamily",
+            "$..PullStartedAt",
+            "$..PullStoppedAt",
+            "$..RuntimeId",
+            "$..SdkHttpMetadata",
+            "$..SdkResponseMetadata",
+            "$..StartedAt",
+            "$..StopCode",
+            "$..StoppedAt",
+            "$..StoppedReason",
+            "$..StoppingAt",
+            "$..TaskDefinitionArn",
+            "$..Version",
+            "$..parameters.Cluster",
         ]
-        await_execution_terminated(aws_client.stepfunctions, execution_arn)
-        assert (
-            aws_client.stepfunctions.describe_execution(executionArn=execution_arn)["status"]
-            == "SUCCEEDED"
+    )
+    def test_run_task_sync(self, aws_client, infrastructure_test_run_task_sync, sfn_ecs_snapshot):
+        # TODO: transformers potentially needed for all the ECS task outputs
+        # TODO: generate snapshot
+        stack_outputs = infrastructure_test_run_task_sync.get_stack_outputs(
+            stack_name=self.STACK_NAME
         )
-        execution = aws_client.stepfunctions.describe_execution(executionArn=execution_arn)
-        snapshot.match("execution", execution)
+        state_machine_arn = stack_outputs["StateMachineArn"]
 
-        execution_history = (
-            aws_client.stepfunctions.get_paginator("get_execution_history")
-            .paginate(executionArn=execution_arn)
-            .build_full_result()
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["TaskDefinitionArn"], "task_definition_arn")
         )
-        snapshot.match("execution_history", execution_history)
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["StateMachineRoleArn"], "state_machine_role_arn")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["ClusterArn"], "cluster_arn")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(
+                stack_outputs["TaskDefinitionContainerName"], "task_definition_container_name"
+            )
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["TaskDefinitionFamily"], "task_definition_family")
+        )
+        sfn_ecs_snapshot.add_transformer(
+            RegexTransformer(stack_outputs["ClusterName"], "cluster_name")
+        )
+        sfn_ecs_snapshot.add_transformer(RegexTransformer(state_machine_arn, "state_machine_arn"))
+
+        launch_and_record_execution(
+            stepfunctions_client=aws_client.stepfunctions,
+            sfn_snapshot=sfn_ecs_snapshot,
+            state_machine_arn=state_machine_arn,
+            execution_input=json.dumps({}),
+        )
