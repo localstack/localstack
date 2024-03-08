622e533040e71f2019f0bea13b5b571c8593b4e8
Dominik Schubert <dominik.schubert91@gmail.com>
2024-03-08 08:04:36 +0100

    make matching rules extensible / optional

--- localstack/testing/testselection/testselection.py
+++ localstack/testing/testselection/testselection.py
@@ -1,19 +1,25 @@
-from localstack.testing.testselection.matching import MATCHING_RULES
+from typing import Iterable, Optional
+
+from localstack.testing.testselection.matching import MATCHING_RULES, MatchingRule
 
 
-def get_affected_tests_from_changes(changed_files: [str]) -> "set[str]":
-    # TODO: reduce based on inclusion (won't hurt but is a bit weird)
-    # e.g. Number of affected test determined: 3
-    # {'tests/aws/services/stepfunctions/',
-    #  'tests/aws/services/stepfunctions/templates/scenarios/scenarios_templates.py',
-    #  'tests/aws/services/stepfunctions/v2/scenarios/test_base_scenarios.py'}
-    # should only really have 'tests/aws/services/stepfunctions'
+def get_affected_tests_from_changes(
+    changed_files: Iterable[str], matching_rules: Optional[list[MatchingRule]] = None
+) -> list[str]:
+    """
+    Generate test selectors based on the changed files and matching rules to apply to.
+
+    :param matching_rules: A list of matching rules which are used to generate test selectors from a given changed file
+    :param changed_files: Iterable of file paths where changes were detected
+    :return: Sorted list of test selectors
+    """
+    if matching_rules is None:
+        matching_rules = MATCHING_RULES
 
     result = set()
-
     for changed_file in changed_files:
         added_test_rules = set()
-        for rule in MATCHING_RULES:
+        for rule in matching_rules:
             added_test_rules.update(rule(changed_file))
 
         # default case where no rule was matching where we default to execute all tests
@@ -22,4 +28,4 @@ def get_affected_tests_from_changes(changed_files: [str]) -> "set[str]":
             added_test_rules.add("SENTINEL_ALL_TESTS")
         result.update(added_test_rules)
 
-    return result
+    return sorted(result)
