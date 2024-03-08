2b4897504adc31438d3ecd8c457a86907ae754aa
Simon Walker <s.r.walker101@googlemail.com>
2024-03-08 18:07:27 +0000

    Take package from WIP feature branch in artifacts repo

--- localstack/packages/functiontrace.py
+++ localstack/packages/functiontrace.py
@@ -1,10 +1,13 @@
 import logging
 from typing import List
 
+from localstack.constants import ARTIFACTS_REPO
 from localstack.packages import InstallTarget, Package, PackageInstaller
 from localstack.packages.core import (
     PermissionDownloadInstaller,
 )
+from localstack.utils.files import mkdir, chmod_r
+from localstack.utils.http import download_github_artifact
 from localstack.utils.run import run
 
 LOG = logging.getLogger(__name__)
@@ -60,7 +63,18 @@ class FunctionTraceServerPackageInstaller(PermissionDownloadInstaller):
     # TODO: migrate this to the upcoming pip installer
 
     def _get_download_url(self) -> str:
-        return "http://172.17.0.1:8000/functiontrace-server"
+        return (
+            ARTIFACTS_REPO
+            + "/raw/79c588fe97515a5124f58c4ae28201938084cc64/functiontrace/amd64/functiontrace-server"
+        )
+
+    def _install(self, target: InstallTarget) -> None:
+        target_directory = self._get_install_dir(target)
+        mkdir(target_directory)
+        download_url = self._get_download_url()
+        target_path = self._get_install_marker_path(target_directory)
+        download_github_artifact(download_url, target_path)
+        chmod_r(self.get_executable_path(), 0o777)
 
 
 functiontrace_package = FunctionTracePackage()
