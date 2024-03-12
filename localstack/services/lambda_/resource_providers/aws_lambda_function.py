7d8f4e516df1921bd6abf244cdb55d8311d7a0bb
Cristopher Pinzón <cristopher.pinzon@gmail.com>
2024-03-12 10:14:44 -0500

    add fix for cfn-response code for nodejs custome resource (#10408)

--- localstack/services/lambda_/resource_providers/aws_lambda_function.py
+++ localstack/services/lambda_/resource_providers/aws_lambda_function.py
@@ -161,7 +161,7 @@ def send(event, context, responseStatus, responseData, physicalResourceId=None, 
 """
 
 # https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/cfn-lambda-function-code-cfnresponsemodule.html
-NODEJS_CFN_RESPONSE_CONTENT = """
+NODEJS_CFN_RESPONSE_CONTENT = r"""
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
@@ -189,7 +189,7 @@ exports.send = function(event, context, responseStatus, responseData, physicalRe
     var parsedUrl = url.parse(event.ResponseURL);
     var options = {
         hostname: parsedUrl.hostname,
-        port: 443,
+        port: parsedUrl.port, // Modified line: LS uses port 4566 for https; hard coded 443 causes error
         path: parsedUrl.path,
         method: "PUT",
         headers: {
