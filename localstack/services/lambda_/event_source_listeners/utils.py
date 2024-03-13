e2b5abe6917c81de5201a20de25f3b2257f15179
sannya-singal <sannyasingal@gmail.com>
2024-03-13 13:46:07 +0530

    change function names

--- localstack/services/lambda_/event_source_listeners/utils.py
+++ localstack/services/lambda_/event_source_listeners/utils.py
@@ -45,28 +45,28 @@ def filter_stream_record(filter_rule: Dict[str, any], record: Dict[str, any]) ->
                 append_record = filter_stream_record(value, record_value)
         else:
             # special case 'exists'
-            def filter_rule_value_list(val):
+            def _filter_rule_value_list(val):
                 if isinstance(val[0], dict):
                     return not val[0].get("exists", True)
                 elif val[0] is None:
                     # support null filter
                     return True
 
-            def filter_rule_value_dict(val):
+            def _filter_rule_value_dict(val):
                 for k, v in val.items():
                     return (
-                        filter_rule_value_list(val[k])
+                        _filter_rule_value_list(val[k])
                         if isinstance(val[k], list)
-                        else filter_rule_value_dict(val[k])
+                        else _filter_rule_value_dict(val[k])
                     )
                 return True
 
             if isinstance(value, list) and len(value) > 0:
-                append_record = filter_rule_value_list(value)
+                append_record = _filter_rule_value_list(value)
             elif isinstance(value, dict):
                 # special case 'exists' for S type, e.g. {"S": [{"exists": false}]}
                 # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Streams.Lambda.Tutorial2.html
-                append_record = filter_rule_value_dict(value)
+                append_record = _filter_rule_value_dict(value)
 
         filter_results.append(append_record)
     return all(filter_results)
