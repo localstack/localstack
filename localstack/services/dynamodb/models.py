dc135ad4f6a0d9e3167f5db312e72f5d68af001a
Benjamin Simon <benjh.simon@gmail.com>
2024-03-15 19:47:14 +0100

    refactor streams

--- localstack/services/dynamodb/models.py
+++ localstack/services/dynamodb/models.py
@@ -1,6 +1,12 @@
+import dataclasses
+from typing import Optional, TypedDict
+
 from localstack.aws.api.dynamodb import (
+    AttributeMap,
+    Key,
     RegionName,
     ReplicaDescription,
+    StreamViewType,
     TableName,
     TimeToLiveSpecification,
 )
@@ -10,6 +16,70 @@ from localstack.services.stores import (
     CrossRegionAttribute,
     LocalAttribute,
 )
+
+
+@dataclasses.dataclass
+class TableStreamType:
+    """
+    When an item in the table is modified, StreamViewType determines what information is written to the stream for this table.
+    - KEYS_ONLY - Only the key attributes of the modified item are written to the stream.
+    - NEW_IMAGE - The entire item, as it appears after it was modified, is written to the stream.
+    - OLD_IMAGE - The entire item, as it appeared before it was modified, is written to the stream.
+    - NEW_AND_OLD_IMAGES - Both the new and the old item images of the item are written to the stream.
+    Special case:
+    is_kinesis: equivalent to NEW_AND_OLD_IMAGES, can be set at the same time as StreamViewType
+    """
+
+    stream_view_type: Optional[StreamViewType]
+    is_kinesis: bool
+
+    @property
+    def needs_old_image(self):
+        return self.is_kinesis or self.stream_view_type in (
+            StreamViewType.OLD_IMAGE,
+            StreamViewType.NEW_AND_OLD_IMAGES,
+        )
+
+    @property
+    def needs_new_image(self):
+        return self.is_kinesis or self.stream_view_type in (
+            StreamViewType.NEW_IMAGE,
+            StreamViewType.NEW_AND_OLD_IMAGES,
+        )
+
+
+class DynamoDbStreamRecord(TypedDict, total=False):
+    ApproximateCreationDateTime: int
+    SizeBytes: int
+    Keys: Key
+    StreamViewType: Optional[StreamViewType]
+    OldImage: Optional[AttributeMap]
+    NewImage: Optional[AttributeMap]
+    SequenceNumber: Optional[int]
+
+
+class StreamRecord(TypedDict, total=False):
+    """
+    Global record type, this can contain both a KinesisRecord and a DynamoDBStreams record?
+    """
+
+    eventName: str
+    eventID: str
+    eventVersion: str
+    dynamodb: DynamoDbStreamRecord
+    awsRegion: str
+    eventSource: str
+
+
+StreamRecords = list[StreamRecord]
+
+
+class TableRecords(TypedDict):
+    table_stream_type: TableStreamType
+    records: StreamRecords
+
+
+RecordsMap = dict[TableName, TableRecords]
 
 
 class DynamoDBStore(BaseStore):
--- localstack/services/dynamodbstreams/dynamodbstreams_api.py
+++ localstack/services/dynamodbstreams/dynamodbstreams_api.py
@@ -1,12 +1,11 @@
 import logging
 import threading
-from collections import defaultdict
 from typing import TYPE_CHECKING, Dict
 
 from bson.json_util import dumps
 
 from localstack import config
-from localstack.aws.api.dynamodbstreams import StreamStatus, StreamViewType
+from localstack.aws.api.dynamodbstreams import StreamStatus, StreamViewType, TableName
 from localstack.aws.connect import connect_to
 from localstack.services.dynamodbstreams.models import DynamoDbStreamsStore, dynamodbstreams_stores
 from localstack.utils.aws import arns, resources
@@ -88,31 +87,61 @@ def get_stream_for_table(account_id: str, region_name: str, table_arn: str) -> d
     return store.ddb_streams.get(table_name)
 
 
-def forward_events(account_id: str, region_name: str, records: dict) -> None:
+def forward_events(account_id: str, region_name: str, records_map: dict[TableName, dict]) -> None:
     kinesis = get_kinesis_client(account_id, region_name)
-    # cache to avoid calling the store if all the records are from the same table (often the case)
-    tables_streams = {}
-    kinesis_records = defaultdict(list)
-    for record in records:
-        table_arn = record.pop("eventSourceARN", "")
-        if not (stream := tables_streams.get(table_arn)):
-            if not (stream := get_stream_for_table(account_id, region_name, table_arn)):
-                continue
-            tables_streams[table_arn] = stream
 
-        table_name = table_name_from_stream_arn(stream["StreamArn"])
+    for table_name, table_records in records_map.items():
+        records = table_records["records"]
+        stream_type = table_records["table_stream_type"]
+        # if the table does not have a DynamoDB Streams enabled, skip publishing anything
+        if not stream_type.stream_view_type:
+            continue
+
+        # in this case, Kinesis forces the record to have both OldImage and NewImage, so we need to filter it
+        # as the settings are different for DDB Streams and Kinesis
+        if (
+            stream_type.is_kinesis
+            and stream_type.stream_view_type != StreamViewType.NEW_AND_OLD_IMAGES
+        ):
+            kinesis_records = []
+            for record in records:
+                # StreamViewType determines what information is written to the stream for the table
+                # When an item in the table is inserted, updated or deleted
+                image_filter = set()
+                if stream_type.stream_view_type == StreamViewType.KEYS_ONLY:
+                    image_filter = {"OldImage", "NewImage"}
+                elif stream_type.stream_view_type == StreamViewType.OLD_IMAGE:
+                    image_filter = {"NewImage"}
+                elif stream_type.stream_view_type == StreamViewType.NEW_IMAGE:
+                    image_filter = {"OldImage"}
+
+                record["dynamodb"] = {
+                    k: v for k, v in record["dynamodb"].items() if k not in image_filter
+                }
+
+                if "SequenceNumber" not in record["dynamodb"]:
+                    record["dynamodb"]["SequenceNumber"] = str(
+                        get_and_increment_sequence_number_counter()
+                    )
+
+                kinesis_records.append({"Data": dumps(record), "PartitionKey": "TODO"})
+
+        else:
+            kinesis_records = []
+            for record in records:
+                if "SequenceNumber" not in record["dynamodb"]:
+                    # we can mutate the record for SequenceNumber, the Kinesis forwarding takes care of filtering it
+                    record["dynamodb"]["SequenceNumber"] = str(
+                        get_and_increment_sequence_number_counter()
+                    )
+
+                # simply pass along the records, they already have the right format
+                kinesis_records.append({"Data": dumps(record), "PartitionKey": "TODO"})
+
         stream_name = get_kinesis_stream_name(table_name)
-        kinesis_records[stream_name].append(
-            {
-                "Data": dumps(record),
-                "PartitionKey": "TODO",
-            }
-        )
-
-    for stream_name, records in kinesis_records.items():
         kinesis.put_records(
             StreamName=stream_name,
-            Records=records,
+            Records=kinesis_records,
         )
 
 
@@ -123,7 +152,8 @@ def delete_streams(account_id: str, region_name: str, table_arn: str) -> None:
         stream_name = get_kinesis_stream_name(table_name)
         # stream_arn = stream["StreamArn"]
 
-        # we're basically asynchronously trying to delete the stream, or should we do this "synchronous" with the table deletion?
+        # we're basically asynchronously trying to delete the stream, or should we do this "synchronous" with the table
+        # deletion?
         def _delete_stream(*args, **kwargs):
             try:
                 kinesis_client = get_kinesis_client(account_id, region_name)
