db97b53fea9684f6638f734a2081eeae0b93921d
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2024-03-22 10:43:26 +0100

    Add guard for wrong usage of dev release

--- bin/release-dev.sh
+++ bin/release-dev.sh
@@ -4,8 +4,7 @@ set -e
 
 # use UTC timestamp as version
 timestamp=$(date -u +%Y%m%d%H%M%S)
-
-echo "$timestamp" >> VERSION
+sed -i -r "s/^([0-9]+\.[0-9]+\.[0-9]+\.dev).*/\1${timestamp}/" VERSION
 
 echo "release $(cat VERSION)? (press CTRL+C to abort)"
 read
