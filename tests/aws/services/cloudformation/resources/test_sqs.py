d8052d151926117fed421bef2149553a82e4abbb
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2024-03-26 16:27:11 -0500

    wip: add update operations for some sns and sqs resources

--- tests/aws/services/cloudformation/resources/test_sqs.py
+++ tests/aws/services/cloudformation/resources/test_sqs.py
@@ -120,3 +120,32 @@ def test_update_queue_no_change(deploy_cfn_template, aws_client, snapshot):
         },
     )
     snapshot.match("outputs-2", updated_stack.outputs)
+
+
+@markers.aws.validated
+def test_update_sqs_queuepolicy(deploy_cfn_template, aws_client, snapshot):
+    stack = deploy_cfn_template(
+        template_path=os.path.join(
+            os.path.dirname(__file__), "../../../templates/sqs_with_queuepolicy.yaml"
+        )
+    )
+
+    policy = aws_client.sqs.get_queue_attributes(
+        QueueUrl=stack.outputs["QueueUrlOutput"], AttributeNames=["Policy"]
+    )
+    snapshot.add_transformer(snapshot.transform.regex(policy["Attributes"]["Policy"], "<policy>"))
+    snapshot.match("policy1", policy["Attributes"]["Policy"])
+
+    # deploy a second time with
+    updated_stack = deploy_cfn_template(
+        template_path=os.path.join(
+            os.path.dirname(__file__), "../../../templates/sqs_with_queuepolicy_updated.yaml"
+        ),
+        is_update=True,
+        stack_name=stack.stack_name,
+    )
+    policy = aws_client.sqs.get_queue_attributes(
+        QueueUrl=updated_stack.outputs["QueueUrlOutput"], AttributeNames=["Policy"]
+    )
+    snapshot.add_transformer(snapshot.transform.regex(policy["Attributes"]["Policy"], "<policy>"))
+    snapshot.match("policy2", policy["Attributes"]["Policy"])
