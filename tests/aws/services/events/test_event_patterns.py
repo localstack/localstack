3a6dcee38ee8696d95451ab4cbddef0d2f8c6475
Joel Scheuner <joel.scheuner.dev@gmail.com>
2024-03-26 17:57:31 +0100

    Externalize event samples

--- tests/aws/services/events/test_event_patterns.py
+++ tests/aws/services/events/test_event_patterns.py
@@ -1,42 +1,40 @@
 import json
+import os
+from pathlib import Path
+from typing import List, Tuple
 
+import json5
 import pytest
 
 from localstack.testing.pytest import markers
 
+THIS_FOLDER: str = os.path.dirname(os.path.realpath(__file__))
+REQUEST_TEMPLATE_DIR = os.path.join(THIS_FOLDER, "event_pattern_templates")
 REFERENCE_DATE: str = (
     "2022-07-13T13:48:01Z"  # v1.0.0 commit timestamp cf26bd9199354a9a55e0b65e312ceee4c407f6c0
 )
 
-sample1 = {
-    "Label": "sample1",
-    "Event": {
-        "id": "1",
-        "source": "order",
-        "detail-type": "Test",
-        "account": "$account",
-        "region": "$region",
-        "time": "$time",
-    },
-    "EventPattern": {
-        "source": ["order"],
-        "detail-type": ["Test"],
-    },
-}
+
+def load_request_templates(directory_path: str) -> List[Tuple[dict, str]]:
+    json5_files = list_files_with_suffix(directory_path, ".json5")
+    return [load_request_template(file_path) for file_path in json5_files]
 
 
-@pytest.mark.parametrize("request_template", [sample1])
-@markers.aws.unknown
-def test_test_event_pattern(aws_client, snapshot, account_id, region_name, request_template):
-    event = apply_event_template(request_template["Event"], account_id, region_name)
-    event_pattern = request_template["EventPattern"]
-    print(event)
-    response = aws_client.events.test_event_pattern(
-        Event=json.dumps(event),
-        EventPattern=json.dumps(event_pattern),
-    )
-    label = request_template["Label"]
-    snapshot.match(label, response)
+def load_request_template(file_path: str) -> Tuple[dict, str]:
+    with open(file_path, "r") as df:
+        template = json5.load(df)
+    return template, Path(file_path).stem
+
+
+def list_files_with_suffix(directory_path: str, suffix: str) -> List[str]:
+    files = []
+    for root, _, filenames in os.walk(directory_path):
+        for filename in filenames:
+            if filename.endswith(suffix):
+                absolute_filepath = os.path.join(root, filename)
+                files.append(absolute_filepath)
+
+    return files
 
 
 def apply_event_template(event_template, account_id, region_name):
@@ -56,3 +54,20 @@ def apply_event_template(event_template, account_id, region_name):
         elif isinstance(value, dict):
             event_template[key] = apply_event_template(value, account_id, region_name)
     return event_template
+
+
+request_template_tuples = load_request_templates(REQUEST_TEMPLATE_DIR)
+
+
+@pytest.mark.parametrize(
+    "request_template,label", request_template_tuples, ids=[t[1] for t in request_template_tuples]
+)
+@markers.aws.unknown
+def test_test_event_pattern(aws_client, snapshot, account_id, region_name, request_template, label):
+    event = apply_event_template(request_template["Event"], account_id, region_name)
+    event_pattern = request_template["EventPattern"]
+    response = aws_client.events.test_event_pattern(
+        Event=json.dumps(event),
+        EventPattern=json.dumps(event_pattern),
+    )
+    snapshot.match(label, response)
