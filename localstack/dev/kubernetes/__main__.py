d04f607baf61cfac19a3a9dd970f24580fe5c37c
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2024-03-27 13:19:17 -0500

    wip: config works

--- localstack/dev/kubernetes/__main__.py
+++ localstack/dev/kubernetes/__main__.py
@@ -8,25 +8,27 @@ def generate_k8s_cluster_config(
     pro: bool = False, mount_moto: bool = False, write_files: bool = False
 ):
     volumes = []
-    root_path = os.path.join(os.path.dirname(__file__), "..", "..")
+    root_path = os.path.join(os.path.dirname(__file__), "..", "..", "..")
+    localstack_code_path = os.path.join(root_path, "localstack")
     volumes.append(
         {
-            "volume": f"{os.path.normpath(root_path)}:/code/localstack",
+            "volume": f"{os.path.normpath(localstack_code_path)}:/code/localstack",
             "nodeFilters": ["server:*", "agent:*"],
         }
     )
 
+    egg_path = os.path.join(root_path, "localstack_core.egg-info")
     if pro:
         ext_path = os.path.join(root_path, "..", "localstack-ext")
+        ext_code_path = os.path.join(ext_path, "localstack_ext")
         egg_path = os.path.join(ext_path, "localstack_ext.egg-info")
+
         volumes.append(
             {
-                "volume": f"{os.path.normpath(ext_path)}:/code/localstack_ext",
+                "volume": f"{os.path.normpath(ext_code_path)}:/code/localstack_ext",
                 "nodeFilters": ["server:*", "agent:*"],
             }
         )
-    else:
-        egg_path = os.path.join(root_path, "localstack.egg-info")
 
     volumes.append(
         {
@@ -36,7 +38,7 @@ def generate_k8s_cluster_config(
     )
 
     if mount_moto:
-        moto_path = os.path.join(root_path, "..", "moto")
+        moto_path = os.path.join(root_path, "..", "moto", "moto")
         volumes.append(
             {"volume": f"{moto_path}:/code/moto", "nodeFilters": ["server:*", "agent:*"]}
         )
@@ -58,9 +60,8 @@ def generate_k8s_cluster_config(
     return config
 
 
-def convert_snake_to_camel_case(snake_str):
-    components = snake_str.split("_")
-    return components[0] + "".join(x.title() for x in components[1:])
+def snake_to_kebab_case(string: str):
+    return string.lower().replace("_", "-")
 
 
 def generate_k8s_cluster_overrides(
@@ -70,7 +71,7 @@ def generate_k8s_cluster_overrides(
     for volume in cluster_config["volumes"]:
         volumes.append(
             {
-                "name": convert_snake_to_camel_case(volume["volume"].split(":")[-1].split("/")[-1]),
+                "name": snake_to_kebab_case(volume["volume"].split(":")[-1].split("/")[-1]),
                 "hostPath": {"path": volume["volume"].split(":")[-1], "type": "Directory"},
             }
         )
@@ -79,12 +80,14 @@ def generate_k8s_cluster_overrides(
     target_path = "/opt/code/localstack/"
     venv_path = os.path.join(target_path, ".venv", "lib", "python3.11", "site-packages")
     for volume in volumes:
-        if volume["name"] == "eggInfo":
+        if volume["name"] == "egg-info":
             volumen_mounts.append(
                 {
                     "name": volume["name"],
+                    "readOnly": True,
                     "mountPath": os.path.join(
-                        target_path, "localstack-ext.egg-info" if pro else "localstack.egg-info"
+                        target_path,
+                        "localstack-ext.egg-info" if pro else "localstack_core.egg-info",
                     ),
                 }
             )
@@ -93,6 +96,7 @@ def generate_k8s_cluster_overrides(
         volumen_mounts.append(
             {
                 "name": volume["name"],
+                "readOnly": True,
                 "mountPath": os.path.join(venv_path, volume["hostPath"]["path"].split("/")[-1]),
             }
         )
@@ -114,11 +118,21 @@ def generate_k8s_cluster_overrides(
         print("=====================================")
 
 
-@click.option("--pro", is_flag=True, help="Mount the localstack-ext code into the cluster.")
-@click.option("--mount-moto", is_flag=True, help="Mount the moto code into the cluster.")
-@click.option("--write-files", is_flag=True, help="Write the configuration and overrides to files.")
+@click.command("run")
+@click.option(
+    "--pro", is_flag=True, default=None, help="Mount the localstack-ext code into the cluster."
+)
+@click.option(
+    "--mount-moto", is_flag=True, default=None, help="Mount the moto code into the cluster."
+)
+@click.option(
+    "--write-files",
+    is_flag=True,
+    default=None,
+    help="Write the configuration and overrides to files.",
+)
 @click.argument("command", nargs=-1, required=False)
-def run(pro: bool = None, mount_moto: bool = False, write_files: bool = False):
+def run(pro: bool = None, mount_moto: bool = False, write_files: bool = False, command: str = None):
     """
     A tool for localstack developers to generated the kubernetes cluster configuration file and the overrides to mount the localstack code into the cluster.
     """
@@ -130,5 +144,9 @@ def run(pro: bool = None, mount_moto: bool = False, write_files: bool = False):
     generate_k8s_cluster_overrides(pro, config, write_files=write_files)
 
 
-if __name__ == "__main__":
+def main():
     run()
+
+
+if __name__ == "__main__":
+    main()
