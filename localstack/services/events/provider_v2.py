cd97846f441818dee0dcb04946d7d93e84a50853
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-04-02 13:53:24 +0200

    feat: add create event bus

--- localstack/services/events/provider_v2.py
+++ localstack/services/events/provider_v2.py
@@ -1,19 +1,23 @@
+from __future__ import annotations
+
 import logging
+from typing import TypedDict
 
 from localstack.aws.api import RequestContext, handler
+from localstack.aws.api.core import ServiceException
 from localstack.aws.api.events import (
+    Arn,
     CreateEventBusResponse,
     EventBusName,
     EventBusNameOrArn,
-    EventPattern,
     EventsApi,
     EventSourceName,
-    PutRuleResponse,
+    InternalException,
+    ResourceNotFoundException,
     RoleArn,
     RuleDescription,
     RuleName,
     RuleState,
-    ScheduleExpression,
     TagList,
 )
 from localstack.services.plugins import ServiceLifecycleHook
@@ -23,8 +27,9 @@ LOG = logging.getLogger(__name__)
 
 class EventsProvider(EventsApi, ServiceLifecycleHook):
     def __init__(self):
-        self._rules = {}
-        self._event_buses = {}
+        self._event_buses: EventBusDict = {}
+        self._events_workers = {}
+        self._add_default_event_bus()
 
     @handler("CreateEventBus")
     def create_event_bus(
@@ -35,41 +40,108 @@ class EventsProvider(EventsApi, ServiceLifecycleHook):
         tags: TagList = None,
         **kwargs,
     ) -> CreateEventBusResponse:
-        event_bus_arn = f"arn:aws:events:{context.region}:{context.account_id}:event-bus/{name}"
-        event_bus = {"Name": name, "Arn": event_bus_arn}
-        self._event_buses[name] = event_bus
+        event_bus_arn = self._get_event_bus_arn(name, context.region, context.account_id)
+        event_bus = EventBus(name, event_bus_arn)
+        event_bus_key = self._get_event_bus_key(name, context.region)
+        self._event_buses[event_bus_key] = event_bus
 
         response = CreateEventBusResponse(
             EventBusArn=event_bus_arn,
         )
         return response
 
-    @handler("PutRule")
-    def put_rule(
-        self,
-        context: RequestContext,
-        name: RuleName,
-        schedule_expression: ScheduleExpression = None,
-        event_pattern: EventPattern = None,
-        state: RuleState = None,
-        description: RuleDescription = None,
-        role_arn: RoleArn = None,
-        tags: TagList = None,
-        event_bus_name: EventBusNameOrArn = None,
-        **kwargs,
-    ) -> PutRuleResponse:
-        rule = {
-            "Name": name,
-            "ScheduleExpression": schedule_expression,
-            "EventPattern": event_pattern,
-            "State": state,
-            "Description": description,
-            "RoleArn": role_arn,
-            "EventBusName": event_bus_name,
-        }
-        self._rules[name] = rule
+    @handler("DeleteEventBus")
+    def delete_event_bus(self, context: RequestContext, name: EventBusName, **kwargs) -> None:
+        if name == "default":
+            raise InternalException("ValidationException", "Cannot delete event bus default.")
+        event_bus_key = self._get_event_bus_key(name, context.region)
+        if event_bus := self._event_buses.pop(event_bus_key):
+            event_bus.delete()
+        else:
+            raise ResourceNotFoundException(
+                f"EventBus {name} for region {context.region} does not exist"
+            )
 
-        response = PutRuleResponse(
-            RuleArn=f"arn:aws:events:{context.region}:{context.account_id}:rule/{name}",
-        )
-        return response
+    def _add_default_event_bus(self) -> None:
+        name = "default"
+        default_account_id = "000000000000"
+        default_region = "us-east-1"
+        arn = self._get_event_bus_arn(name, default_region, default_account_id)
+        self._event_buses["default"] = EventBus(name, arn)
+
+    def _extract_event_bus_name(
+        self, event_bus_name_or_arn: EventBusNameOrArn | None
+    ) -> EventBusName:
+        if not event_bus_name_or_arn:
+            return "default"
+        return event_bus_name_or_arn.split("/")[-1]
+
+    def _get_event_bus(self, name: EventBusName, region: str) -> EventBus:
+        event_bus_key = self._get_event_bus_key(name, region)
+        if event_bus_key not in self._event_buses:
+            raise ResourceNotFoundException(f"EventBus {name} for region {region} does not exist")
+        return self._event_buses[event_bus_key]
+
+    def _get_event_bus_key(self, name: EventBusName, region: str) -> str:
+        return f"{name}-{region}"
+
+    def _get_event_bus_arn(self, name: EventBusName, region: str, account_id: str) -> str:
+        return f"arn:aws:events:{region}:{account_id}:event-bus/{name}"
+
+
+class EventBus:
+    def __init__(self, name: str, arn: Arn):
+        self.name = name
+        self.arn = arn
+        self._rules: RuleDict = {}
+
+    def delete(self):
+        self._rules.clear()
+
+
+EventBusDict = dict[str, EventBus]
+
+
+class Rule:
+    def __init__(
+        self,
+        name: RuleName,
+        state: RuleState = RuleState.ENABLED,
+        description: RuleDescription | None = None,
+        role_arn: RoleArn = None,
+    ):
+        self.name = name
+        self.state = state
+        self.description = description
+        self.role_arn = role_arn
+
+    def enable(self):
+        self.state = RuleState.ENABLED
+
+    def disable(self):
+        self.state = RuleState.DISABLED
+
+
+RuleDict = dict[str, Rule]
+
+
+class Event(TypedDict, total=False):
+    version: str
+    id: str
+    source: str
+    account: str
+    time: str
+    region: str
+    resources: list[str]
+    detail_type: str
+    detail: dict
+    additional_attributes: dict
+
+
+EventList = list[Event]
+
+
+class ValidationException(ServiceException):
+    code: str = "ValidationException"
+    sender_fault: bool = True
+    status_code: int = 400
