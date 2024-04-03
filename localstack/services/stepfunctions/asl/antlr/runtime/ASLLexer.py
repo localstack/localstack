eaae152e255eaa78c81fe0a720668119303d6cbb
MEPalma <marcoepalma@tutanota.com>
2024-04-03 10:49:53 +0200

    conflicts, cleanups

--- localstack/services/stepfunctions/asl/antlr/runtime/ASLLexer.py
+++ localstack/services/stepfunctions/asl/antlr/runtime/ASLLexer.py
@@ -1,4 +1,4 @@
-# Generated from /Users/mep/LocalStack/localstack/localstack/services/stepfunctions/asl/antlr/ASLLexer.g4 by ANTLR 4.13.1
+# Generated from ASLLexer.g4 by ANTLR 4.13.1
 from antlr4 import *
 from io import StringIO
 import sys
