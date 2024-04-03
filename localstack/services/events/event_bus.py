34bed738de51d7f4790bbd1558e36bfc46ee9a45
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-04-03 18:08:35 +0200

    fix: event bus crud

--- localstack/services/events/event_bus.py
+++ localstack/services/events/event_bus.py
@@ -1,23 +1,32 @@
-from localstack.aws.api.events import Arn, EventBusList
+from localstack.aws.api.events import Arn, EventBusList, EventBusName, EventSourceName, TagList
 from localstack.aws.api.events import EventBus as ApiTypeEventBus
 from localstack.services.events.rule import RuleDict
 
 
 class EventBus:
-    def __init__(self, name: str, arn: Arn, policy: str | None = None):
+    def __init__(
+        self,
+        name: EventBusName,
+        arn: Arn,
+        policy: str | None = None,
+        event_source_name: EventSourceName | None = None,
+        tags: TagList | None = None,
+    ):
         self.name = name
         self.arn = arn
         self.policy = policy
+        self.event_source_name = event_source_name
+        self._tags = tags
         self._rules: RuleDict = {}
 
     def delete(self):
         self._rules.clear()
 
 
-EventBusDict = dict[str, EventBus]
+EventBusDict = dict[EventBusName, EventBus]
 
 
-def event_bus_to_api_type_event_bus(event_bus: EventBus) -> ApiTypeEventBus:
+def event_bus_dict_to_api_type_event_bus(event_bus: EventBus) -> ApiTypeEventBus:
     if event_bus.policy:
         event_bus = {
             "Name": event_bus.name,
@@ -34,6 +43,6 @@ def event_bus_to_api_type_event_bus(event_bus: EventBus) -> ApiTypeEventBus:
 
 def event_bust_dict_to_list(event_buses: EventBusDict) -> EventBusList:
     event_bus_list = [
-        event_bus_to_api_type_event_bus(event_bus) for event_bus in event_buses.values()
+        event_bus_dict_to_api_type_event_bus(event_bus) for event_bus in event_buses.values()
     ]
     return event_bus_list
--- localstack/services/events/provider_v2.py
+++ localstack/services/events/provider_v2.py
@@ -2,10 +2,8 @@ from __future__ import annotations
 
 import base64
 import logging
-from typing import TypedDict
 
 from localstack.aws.api import RequestContext, handler
-from localstack.aws.api.core import ServiceException
 from localstack.aws.api.events import (
     CreateEventBusResponse,
     DescribeEventBusResponse,
@@ -18,6 +16,7 @@ from localstack.aws.api.events import (
     ListEventBusesResponse,
     ListRulesResponse,
     NextToken,
+    ResourceAlreadyExistsException,
     ResourceNotFoundException,
     RuleName,
     TagList,
@@ -25,9 +24,10 @@ from localstack.aws.api.events import (
 from localstack.services.events.event_bus import (
     EventBus,
     EventBusDict,
-    event_bus_to_api_type_event_bus,
+    event_bus_dict_to_api_type_event_bus,
     event_bust_dict_to_list,
 )
+from localstack.services.events.models_v2 import EventsStore, events_store
 from localstack.services.plugins import ServiceLifecycleHook
 
 LOG = logging.getLogger(__name__)
@@ -35,9 +35,7 @@ LOG = logging.getLogger(__name__)
 
 class EventsProvider(EventsApi, ServiceLifecycleHook):
     def __init__(self):
-        self._event_buses: EventBusDict = {}
         self._events_workers = {}
-        self._add_default_event_bus()
 
     @handler("CreateEventBus")
     def create_event_bus(
@@ -48,13 +46,16 @@ class EventsProvider(EventsApi, ServiceLifecycleHook):
         tags: TagList = None,
         **kwargs,
     ) -> CreateEventBusResponse:
-        event_bus_arn = self._get_event_bus_arn(name, context.region, context.account_id)
-        event_bus = EventBus(name, event_bus_arn)
-        event_bus_key = self._get_event_bus_key(name, context.region)
-        self._event_buses[event_bus_key] = event_bus
+        account_id = context.account_id
+        region = context.region
+        store = self._get_store(account_id, region)
+        if name in store.event_bus.keys():
+            raise ResourceAlreadyExistsException(f"Event bus {name} already exists.")
+        event_bus = self._create_event_bus(name, account_id, region, event_source_name, tags)
+        store.event_bus[event_bus.name] = event_bus
 
         response = CreateEventBusResponse(
-            EventBusArn=event_bus_arn,
+            EventBusArn=event_bus.arn,
         )
         return response
 
@@ -62,11 +63,13 @@ class EventsProvider(EventsApi, ServiceLifecycleHook):
     def delete_event_bus(self, context: RequestContext, name: EventBusName, **kwargs) -> None:
         if name == "default":
             raise InternalException("ValidationException", "Cannot delete event bus default.")
-        event_bus_key = self._get_event_bus_key(name, context.region)
-        if event_bus := self._event_buses.pop(event_bus_key):
-            event_bus.delete()
-        else:
-            raise ResourceNotFoundException(f"EventBus {name} does not exist")
+        store = self._get_store(context.account_id, context.region)
+        try:
+            if event_bus := self._get_event_bus(name, store):
+                event_bus.delete()
+                del store.event_bus[name]
+        except ResourceNotFoundException as e:
+            return e
 
     @handler("ListEventBuses")
     def list_event_buses(
@@ -77,33 +80,37 @@ class EventsProvider(EventsApi, ServiceLifecycleHook):
         limit: LimitMax100 = None,
         **kwargs,
     ) -> ListEventBusesResponse:
+        store = self._get_store(context.account_id, context.region)
         event_buses = (
-            self._get_filtered_event_buses(name_prefix) if name_prefix else self._event_buses
+            self._get_filtered_event_buses(name_prefix, store.event_bus)
+            if name_prefix
+            else store.event_bus
         )
-        event_buses_length = len(event_buses)
+        event_buses_len = len(event_buses)
         start_index = self._decode_next_token(next_token) if next_token is not None else 0
-        end_index = start_index + limit if limit is not None else event_buses_length
-        limited_event_buses_list = dict(list(event_buses.items())[start_index:end_index])
+        end_index = start_index + limit if limit is not None else event_buses_len
+        limited_event_buses = dict(list(event_buses.items())[start_index:end_index])
 
         next_token = (
             self._encode_next_token(end_index)
             # return a next_token (encoded integer of next starting index) if not all event buses are returned
-            if end_index <= event_buses_length
+            if end_index < event_buses_len
             else None
         )
 
-        return {
-            "EventBuses": event_bust_dict_to_list(limited_event_buses_list),
-            "NextToken": next_token,
-        }
+        response = {"EventBuses": event_bust_dict_to_list(limited_event_buses)}
+        if next_token is not None:
+            response["NextToken"] = next_token
+        return response
 
     @handler("DescribeEventBus")
     def describe_event_bus(
         self, context: RequestContext, name: EventBusNameOrArn = None, **kwargs
     ) -> DescribeEventBusResponse:
-        event_bus_key = self._extract_event_bus_name(name)
-        event_bus = self._get_event_bus(event_bus_key, context.region)
-        return event_bus_to_api_type_event_bus(event_bus)
+        name = self._extract_event_bus_name(name)
+        store = self._get_store(context.account_id, context.region)
+        event_bus = self._get_event_bus(name, store)
+        return event_bus_dict_to_api_type_event_bus(event_bus)
 
     @handler("ListRules")
     def list_rules(
@@ -115,39 +122,45 @@ class EventsProvider(EventsApi, ServiceLifecycleHook):
         limit: LimitMax100 = None,
         **kwargs,
     ) -> ListRulesResponse:
-        return {"Rules": []}
+        return {"Rules": []}  # TODO implement
 
-    def _add_default_event_bus(self) -> None:
+    def _get_store(self, account_id: str, region: str) -> EventsStore:
+        store = events_store[account_id][region]
+        # create default event bus on first call
         name = "default"
-        default_account_id = "000000000000"
-        default_region = "us-east-1"
-        arn = self._get_event_bus_arn(name, default_region, default_account_id)
-        self._event_buses["default"] = EventBus(name, arn)
+        if name not in store.event_bus.keys():
+            event_bus = self._create_event_bus(name, account_id, region)
+            store.event_bus[event_bus.name] = event_bus
 
-    def _extract_event_bus_name(
-        self, event_bus_name_or_arn: EventBusNameOrArn | None
-    ) -> EventBusName:
-        if not event_bus_name_or_arn:
-            return "default"
-        return event_bus_name_or_arn.split("/")[-1]
+        return store
 
-    def _get_event_bus(self, name: EventBusName, region: str) -> EventBus:
-        event_bus_key = self._get_event_bus_key(name, region)
-        if event_bus_key not in self._event_buses:
-            raise ResourceNotFoundException(f"EventBus {name} does not exist")
-        return self._event_buses[event_bus_key]
-
-    def _get_event_bus_key(self, name: EventBusName, region: str) -> str:
-        return f"{name}-{region}"
+    def _create_event_bus(
+        self,
+        name: EventBusName,
+        account_id: str,
+        region: str,
+        event_source_name: EventSourceName = None,
+        tags: TagList = None,
+    ) -> EventBus:
+        event_bus_arn = self._get_event_bus_arn(name, region, account_id)
+        event_bus = EventBus(name, event_bus_arn, event_source_name, tags)
+        return event_bus
 
     def _get_event_bus_arn(self, name: EventBusName, region: str, account_id: str) -> str:
         return f"arn:aws:events:{region}:{account_id}:event-bus/{name}"
 
-    def _get_filtered_event_buses(self, name_prefix: EventBusName) -> EventBusDict:
+    def _get_event_bus(self, name: EventBusName, store: EventsStore) -> EventBus:
+        if name not in store.event_bus.keys():
+            raise ResourceNotFoundException(f"Event bus {name} does not exist.")
+        return store.event_bus[name]
+
+    def _get_filtered_event_buses(
+        self, name_prefix: EventBusName, event_buses: EventBusDict
+    ) -> EventBusDict:
         return {
-            key: event_bus
-            for key, event_bus in self._event_buses.items()
-            if event_bus.name.startswith(name_prefix)
+            name: event_bus
+            for name, event_bus in event_buses.items()
+            if name.startswith(name_prefix)
         }
 
     def _encode_next_token(self, token: int) -> NextToken:
@@ -156,24 +169,9 @@ class EventsProvider(EventsApi, ServiceLifecycleHook):
     def _decode_next_token(self, token: NextToken) -> int:
         return int.from_bytes(base64.b64decode(token), "big")
 
-
-class Event(TypedDict, total=False):
-    version: str
-    id: str
-    source: str
-    account: str
-    time: str
-    region: str
-    resources: list[str]
-    detail_type: str
-    detail: dict
-    additional_attributes: dict
-
-
-EventList = list[Event]
-
-
-class ValidationException(ServiceException):
-    code: str = "ValidationException"
-    sender_fault: bool = True
-    status_code: int = 400
+    def _extract_event_bus_name(
+        self, event_bus_name_or_arn: EventBusNameOrArn | None
+    ) -> EventBusName:
+        if not event_bus_name_or_arn:
+            return "default"
+        return event_bus_name_or_arn.split("/")[-1]
