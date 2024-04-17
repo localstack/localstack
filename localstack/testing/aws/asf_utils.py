b921177e3afb8b79a1fc1ae36052fcbb334feb1b
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-04-17 19:09:21 +0200

    feat: updated warning if __future__ import in provider for api signature checks

--- localstack/testing/aws/asf_utils.py
+++ localstack/testing/aws/asf_utils.py
@@ -125,7 +125,7 @@ def check_provider_signature(sub_class: type, base_class: type, method_name: str
         base_spec = inspect.getfullargspec(base_function)
         assert sub_spec == base_spec, (
             f"{sub_class.__name__}#{method_name} breaks with {base_class.__name__}#{method_name}. "
-            f"This can also be caused by 'from __future__ import annotations'!"
+            f"This can also be caused by 'from __future__ import annotations in a provider file'!"
         )
     except AttributeError:
         # the function is not defined in the superclass
