091d406de84e7e5d582cd72007492225e10fa30f
Joel Scheuner <joel.scheuner.dev@gmail.com>
2024-04-24 11:52:46 +0200

    Use filter param instead of boolean flags

--- localstack/services/lambda_/custom_endpoints.py
+++ localstack/services/lambda_/custom_endpoints.py
@@ -4,7 +4,6 @@ from typing import List, TypedDict
 from rolo import Request, route
 
 from localstack.aws.api.lambda_ import Runtime
-from localstack.constants import TRUE_STRINGS
 from localstack.services.lambda_.runtimes import (
     ALL_RUNTIMES,
     DEPRECATED_RUNTIMES,
@@ -21,11 +20,16 @@ class LambdaCustomEndpoints:
     def runtimes(self, request: Request) -> LambdaRuntimesResponse:
         """This metadata endpoint needs to be loaded before the Lambda provider.
         It can be used by the Webapp to query supported Lambda runtimes of an unknown LocalStack version."""
-        # Query parameter values are all lists. Example: { "all": ["true"] }
         query_params = urllib.parse.parse_qs(request.environ["QUERY_STRING"])
-        if query_params.get("all", ["false"])[0] in TRUE_STRINGS:
-            return LambdaRuntimesResponse(Runtimes=ALL_RUNTIMES)
-        elif query_params.get("deprecated", ["false"])[0] in TRUE_STRINGS:
-            return LambdaRuntimesResponse(Runtimes=DEPRECATED_RUNTIMES)
-        else:
-            return LambdaRuntimesResponse(Runtimes=SUPPORTED_RUNTIMES)
+        # Query parameter values are all lists. Example: { "filter": ["all"] }
+        filter_params = query_params.get("filter", [])
+        runtimes = set()
+        if "all" in filter_params:
+            runtimes.update(ALL_RUNTIMES)
+        if "deprecated" in filter_params:
+            runtimes.update(DEPRECATED_RUNTIMES)
+        # By default (i.e., without any filter param), we return the supported runtimes because that is most useful.
+        if "supported" in filter_params or len(runtimes) == 0:
+            runtimes.update(SUPPORTED_RUNTIMES)
+
+        return LambdaRuntimesResponse(Runtimes=list(runtimes))
