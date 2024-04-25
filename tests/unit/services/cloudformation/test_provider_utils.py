6378518df6b6e8e3e01445402e1440132a6babf7
Cristopher Pinzón <cristopher.pinzon@gmail.com>
2024-04-25 11:49:17 -0500

    Update tests/unit/services/cloudformation/test_provider_utils.py
    Co-authored-by: Simon Walker <simon.walker@localstack.cloud>

--- tests/unit/services/cloudformation/test_provider_utils.py
+++ tests/unit/services/cloudformation/test_provider_utils.py
@@ -6,7 +6,4 @@ class TestDictUtils:
         original = {"Parameter": "1", "SecondParameter": ["2", "2"], "ThirdParameter": "3"}
         transformed = utils.convert_values_to_numbers(original, ["ThirdParameter"])
 
-        assert original["Parameter"] == "1"
-        assert transformed["Parameter"] == 1
-        assert transformed["SecondParameter"][0] == 2
-        assert transformed["ThirdParameter"] == "3"
+        assert transformed == {"Parameter": 1, "SecondParameter": [2, 2], "ThirdParameter": "3"}
