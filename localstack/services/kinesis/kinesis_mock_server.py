2316328064b46c98c3db5da53a157e2a92af4803
Giovanni Grano <me@giograno.com>
2024-05-02 14:27:16 -0400

    remove un-needed relpath

--- localstack/services/kinesis/kinesis_mock_server.py
+++ localstack/services/kinesis/kinesis_mock_server.py
@@ -86,9 +86,7 @@ class KinesisMockServer(Server):
 
         if self._data_dir and config.KINESIS_PERSISTENCE:
             env_vars["SHOULD_PERSIST_DATA"] = "true"
-            # FIXME use relative path to current working directory until
-            #  https://github.com/etspaceman/kinesis-mock/issues/554 is resolved
-            env_vars["PERSIST_PATH"] = os.path.relpath(self._data_dir)
+            env_vars["PERSIST_PATH"] = self._data_dir
             env_vars["PERSIST_FILE_NAME"] = self._data_filename
             env_vars["PERSIST_INTERVAL"] = config.KINESIS_MOCK_PERSIST_INTERVAL
 
