4902c9fc2015050e3029e9c0b2fb6f01105941c0
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-05-14 13:27:58 +0200

    feat: add test invalid schedule expression

--- tests/aws/services/events/test_events_schedule.py
+++ tests/aws/services/events/test_events_schedule.py
@@ -1,3 +1,6 @@
+import pytest
+from botocore.exceptions import ClientError
+
 from localstack.testing.pytest import markers
 from localstack.utils.strings import short_uid
 
@@ -12,5 +15,38 @@ class TestScheduleRate:
         snapshot.match("put-rule", response)
 
         response = aws_client.events.list_rules(NamePrefix=rule_name)
-
         snapshot.match("list-rules", response)
+
+    @markers.aws.validated
+    @pytest.mark.parametrize(
+        "schedule_expression",
+        [
+            "rate(10 seconds)",
+            "rate(10 years)",
+            "rate(1 minutes)",
+            "rate(1 hours)",
+            "rate(1 days)",
+            "rate(10 minute)",
+            "rate(10 hour)",
+            "rate(10 day)",
+            "rate()",
+            "rate(10)",
+            "rate(10 minutess)",
+            "rate(foo minutes)",
+            "rate(0 minutes)",
+            "rate(-10 minutes)",
+            "rate(10 MINUTES)",
+            "rate( 10 minutes )",
+            " rate(10 minutes)",
+        ],
+    )
+    def test_put_rule_with_invalid_schedule(self, schedule_expression, aws_client):
+        with pytest.raises(ClientError) as e:
+            aws_client.events.put_rule(
+                Name=f"rule-{short_uid()}", ScheduleExpression=schedule_expression
+            )
+
+        assert e.value.response["Error"] == {
+            "Code": "ValidationException",
+            "Message": "Parameter ScheduleExpression is not valid.",
+        }
