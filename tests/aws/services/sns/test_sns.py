e7a6ad7b99e9d2c3a3c22520bb0af7543b96081c
Benjamin Simon <benjh.simon@gmail.com>
2024-05-14 21:33:20 +0200

    update test

--- tests/aws/services/sns/test_sns.py
+++ tests/aws/services/sns/test_sns.py
@@ -841,12 +841,13 @@ class TestSNSSubscriptionCrud:
             sns_subscription(TopicArn=topic_arn, Protocol="sms", Endpoint=phone_number)
 
         response = aws_client.sns.list_subscriptions_by_topic(TopicArn=topic_arn)
-        page_1_subs = {sub["SubscriptionArn"] for sub in response["Subscriptions"]}
         # not snapshotting the results, it contains 100 entries
         assert "NextToken" in response
         # seems to be b64 encoded
         assert response["NextToken"].endswith("==")
         assert len(response["Subscriptions"]) == 100
+        # keep the page 1 subscriptions ARNs
+        page_1_subs = {sub["SubscriptionArn"] for sub in response["Subscriptions"]}
 
         response = aws_client.sns.list_subscriptions_by_topic(
             TopicArn=topic_arn, NextToken=response["NextToken"]
@@ -854,6 +855,7 @@ class TestSNSSubscriptionCrud:
         snapshot.match("list-sub-per-topic-page-2", response)
         assert "NextToken" not in response
         assert len(response["Subscriptions"]) == 1
+        # assert that the last Subscription is not present in page 1
         assert response["Subscriptions"][0]["SubscriptionArn"] not in page_1_subs
 
         response = aws_client.sns.list_subscriptions()
