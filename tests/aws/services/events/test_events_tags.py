58306e61f7c17de692030c29ec7bab9be86ee4d7
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-05-15 16:24:00 +0200

    refactor: align test snapshot names

--- tests/aws/services/events/test_events_tags.py
+++ tests/aws/services/events/test_events_tags.py
@@ -10,7 +10,11 @@ from tests.aws.services.events.test_events import TEST_EVENT_PATTERN
 @markers.aws.validated
 @pytest.mark.parametrize("resource_to_tag", ["event_bus", "rule"])
 def tests_tag_untag_resource(
-    resource_to_tag, events_create_event_bus, events_put_rule, aws_client, snapshot
+    resource_to_tag,
+    events_create_event_bus,
+    events_put_rule,
+    aws_client,
+    snapshot,
 ):
     bus_name = f"test_bus-{short_uid()}"
     response = events_create_event_bus(Name=bus_name)
@@ -28,7 +32,6 @@ def tests_tag_untag_resource(
         resource_arn = event_bus_arn
     if resource_to_tag == "rule":
         resource_arn = rule_arn
-
     tag_key_2 = "tag2"
     response_tag_resource = aws_client.events.tag_resource(
         ResourceARN=resource_arn,
@@ -46,7 +49,7 @@ def tests_tag_untag_resource(
     snapshot.match("tag_resource", response_tag_resource)
 
     response = aws_client.events.list_tags_for_resource(ResourceARN=resource_arn)
-    snapshot.match("list_tagged_rule", response)
+    snapshot.match("list_tags_for_resource", response)
 
     response_untag_resource = aws_client.events.untag_resource(
         ResourceARN=resource_arn,
@@ -55,6 +58,7 @@ def tests_tag_untag_resource(
     snapshot.match("untag_resource", response_untag_resource)
 
     response = aws_client.events.list_tags_for_resource(ResourceARN=resource_arn)
+    snapshot.match("list_tags_for_untagged_resource", response)
 
 
 @markers.aws.validated
