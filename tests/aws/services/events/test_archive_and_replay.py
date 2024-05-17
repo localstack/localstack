1eba1d905602a44342e967eab2285bc45d3360f8
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-05-17 18:28:47 +0200

    feat: extend test list archive with name prefix on custom event bus

--- tests/aws/services/events/test_archive_and_replay.py
+++ tests/aws/services/events/test_archive_and_replay.py
@@ -135,11 +135,30 @@ class TestArchive:
         snapshot.match("describe-archive-unknown-archive-error", error)
 
     @markers.aws.validated
-    def test_list_archive_with_name_prefix(self, events_create_archive, aws_client, snapshot):
+    @pytest.mark.parametrize("event_bus_type", ["default", "custom"])
+    def test_list_archive_with_name_prefix(
+        self,
+        event_bus_type,
+        region_name,
+        account_id,
+        events_create_event_bus,
+        events_create_archive,
+        aws_client,
+        snapshot,
+    ):
+        if event_bus_type == "default":
+            event_bus_name = "default"
+            event_source_arn = f"arn:aws:events:{region_name}:{account_id}:event-bus/default"
+        if event_bus_type == "custom":
+            event_bus_name = f"test-bus-{short_uid()}"
+            response = events_create_event_bus(Name=event_bus_name)
+            event_source_arn = response["EventBusArn"]
+
         archive_name_prefix = "test-archive"
         archive_name = f"{archive_name_prefix}-{short_uid()}"
         events_create_archive(
             ArchiveName=archive_name,
+            EventSourceArn=event_source_arn,
             Description="description of the archive",
             EventPattern=json.dumps(TEST_EVENT_PATTERN),
             RetentionDays=1,
@@ -147,6 +166,13 @@ class TestArchive:
 
         response_list_archives_prefix = aws_client.events.list_archives(
             NamePrefix=archive_name_prefix
+        )
+
+        snapshot.add_transformer(
+            [
+                snapshot.transform.regex(event_bus_name, "<event-bus-name>"),
+                snapshot.transform.regex(archive_name, "<archive-name>"),
+            ]
         )
         snapshot.match("list-archives-with-name-prefix", response_list_archives_prefix)
 
