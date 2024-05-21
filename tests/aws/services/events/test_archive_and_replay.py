8b6fb5414eefaf3502359f61fc80d9aeadea923b
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-05-21 10:55:31 +0200

    refactor: separate error tests

--- tests/aws/services/events/test_archive_and_replay.py
+++ tests/aws/services/events/test_archive_and_replay.py
@@ -7,7 +7,6 @@ from localstack.utils.strings import short_uid
 from tests.aws.services.events.test_events import TEST_EVENT_PATTERN, TEST_EVENT_PATTERN_NO_DETAIL
 
 
-# TODO order tests - add section errors
 class TestArchive:
     @markers.aws.validated
     @pytest.mark.parametrize("event_bus_type", ["default", "custom"])
@@ -63,77 +62,6 @@ class TestArchive:
 
         response_delete_archive = aws_client.events.delete_archive(ArchiveName=archive_name)
         snapshot.match("delete-archive", response_delete_archive)
-
-    @markers.aws.validated
-    def test_create_archive_error_unknown_event_bus(self, aws_client, snapshot):
-        not_existing_event_bus_name = f"doesnotexist-{short_uid()}"
-        non_existing_event_bus_arn = (
-            f"arn:aws:events:us-east-1:123456789012:event-bus/{not_existing_event_bus_name}"
-        )
-        with pytest.raises(Exception) as error:
-            aws_client.events.create_archive(
-                ArchiveName="test-archive",
-                EventSourceArn=non_existing_event_bus_arn,
-                Description="description of the archive",
-                EventPattern=json.dumps(TEST_EVENT_PATTERN),
-                RetentionDays=1,
-            )
-
-        snapshot.add_transformer(
-            [snapshot.transform.regex(not_existing_event_bus_name, "<event-bus-name>")]
-        )
-        snapshot.match("create-archive-unknown-event-bus-error", error)
-
-    @markers.aws.validated
-    @pytest.mark.parametrize("event_bus_type", ["default", "custom"])
-    def test_create_archive_error_duplicate(
-        self,
-        event_bus_type,
-        events_create_event_bus,
-        region_name,
-        account_id,
-        events_create_archive,
-        aws_client,
-        snapshot,
-    ):
-        if event_bus_type == "default":
-            event_bus_name = "default"
-            event_source_arn = f"arn:aws:events:{region_name}:{account_id}:event-bus/default"
-        if event_bus_type == "custom":
-            event_bus_name = f"test-bus-{short_uid()}"
-            response = events_create_event_bus(Name=event_bus_name)
-            event_source_arn = response["EventBusArn"]
-
-        archive_name = f"test-archive-{short_uid()}"
-        events_create_archive(
-            ArchiveName=archive_name,
-            EventSourceArn=event_source_arn,
-            Description="description of the archive",
-            EventPattern=json.dumps(TEST_EVENT_PATTERN),
-            RetentionDays=1,
-        )
-        with pytest.raises(Exception) as error:
-            aws_client.events.create_archive(
-                ArchiveName=archive_name,
-                EventSourceArn=event_source_arn,
-                Description="description of the archive",
-                EventPattern=json.dumps(TEST_EVENT_PATTERN),
-                RetentionDays=1,
-            )
-
-        snapshot.add_transformer([snapshot.transform.regex(archive_name, "<archive-name>")])
-        snapshot.match("create-archive-duplicate-error", error)
-
-    @markers.aws.validated
-    def test_describe_archive_error_unknown_archive(self, aws_client, snapshot):
-        not_existing_archive_name = f"doesnotexist-{short_uid()}"
-        with pytest.raises(Exception) as error:
-            aws_client.events.describe_archive(ArchiveName=not_existing_archive_name)
-
-        snapshot.add_transformer(
-            [snapshot.transform.regex(not_existing_archive_name, "<archive-name>")]
-        )
-        snapshot.match("describe-archive-unknown-archive-error", error)
 
     @markers.aws.validated
     @pytest.mark.parametrize("event_bus_type", ["default", "custom"])
@@ -263,6 +191,78 @@ class TestArchive:
             ]
         )
         snapshot.match("list-archives-state-enabled", response_list_archives)
+
+    # Tests Errors
+    @markers.aws.validated
+    @pytest.mark.parametrize("event_bus_type", ["default", "custom"])
+    def test_create_archive_error_duplicate(
+        self,
+        event_bus_type,
+        events_create_event_bus,
+        region_name,
+        account_id,
+        events_create_archive,
+        aws_client,
+        snapshot,
+    ):
+        if event_bus_type == "default":
+            event_bus_name = "default"
+            event_source_arn = f"arn:aws:events:{region_name}:{account_id}:event-bus/default"
+        if event_bus_type == "custom":
+            event_bus_name = f"test-bus-{short_uid()}"
+            response = events_create_event_bus(Name=event_bus_name)
+            event_source_arn = response["EventBusArn"]
+
+        archive_name = f"test-archive-{short_uid()}"
+        events_create_archive(
+            ArchiveName=archive_name,
+            EventSourceArn=event_source_arn,
+            Description="description of the archive",
+            EventPattern=json.dumps(TEST_EVENT_PATTERN),
+            RetentionDays=1,
+        )
+        with pytest.raises(Exception) as error:
+            aws_client.events.create_archive(
+                ArchiveName=archive_name,
+                EventSourceArn=event_source_arn,
+                Description="description of the archive",
+                EventPattern=json.dumps(TEST_EVENT_PATTERN),
+                RetentionDays=1,
+            )
+
+        snapshot.add_transformer([snapshot.transform.regex(archive_name, "<archive-name>")])
+        snapshot.match("create-archive-duplicate-error", error)
+
+    @markers.aws.validated
+    def test_create_archive_error_unknown_event_bus(self, aws_client, snapshot):
+        not_existing_event_bus_name = f"doesnotexist-{short_uid()}"
+        non_existing_event_bus_arn = (
+            f"arn:aws:events:us-east-1:123456789012:event-bus/{not_existing_event_bus_name}"
+        )
+        with pytest.raises(Exception) as error:
+            aws_client.events.create_archive(
+                ArchiveName="test-archive",
+                EventSourceArn=non_existing_event_bus_arn,
+                Description="description of the archive",
+                EventPattern=json.dumps(TEST_EVENT_PATTERN),
+                RetentionDays=1,
+            )
+
+        snapshot.add_transformer(
+            [snapshot.transform.regex(not_existing_event_bus_name, "<event-bus-name>")]
+        )
+        snapshot.match("create-archive-unknown-event-bus-error", error)
+
+    @markers.aws.validated
+    def test_describe_archive_error_unknown_archive(self, aws_client, snapshot):
+        not_existing_archive_name = f"doesnotexist-{short_uid()}"
+        with pytest.raises(Exception) as error:
+            aws_client.events.describe_archive(ArchiveName=not_existing_archive_name)
+
+        snapshot.add_transformer(
+            [snapshot.transform.regex(not_existing_archive_name, "<archive-name>")]
+        )
+        snapshot.match("describe-archive-unknown-archive-error", error)
 
     @markers.aws.validated
     def test_list_archive_error_unknown_source_arn(
