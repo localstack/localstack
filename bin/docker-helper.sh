bf764488cf7761a77ca8e939f151e1215c477350
Alexander Rashed <alexander.rashed@localstack.cloud>
2024-05-23 11:10:36 +0200

    add docker save and load, add default tag env config

--- bin/docker-helper.sh
+++ bin/docker-helper.sh
@@ -7,6 +7,7 @@ shopt -s nullglob
 # global defaults
 VERSION_FILE=${VERSION_FILE-"VERSION"}
 DOCKERFILE=${DOCKERFILE-"Dockerfile"}
+DEFAULT_TAG=${DEFAULT_TAG-"latest"}
 
 # TODO extend help
 function usage() {
@@ -17,13 +18,19 @@ function usage() {
     echo ""
     echo "Commands:"
     echo "  build"
-    echo "      Build the Docker image for the project in the working directory"
+    echo "      Build a platform-specific Docker image for the project in the working directory"
+    echo ""
+    echo "  save"
+    echo "      Save the Docker image to disk (to transfer it to other runners / machines)"
+    echo ""
+    echo "  load"
+    echo "      Load a previously saved Docker image from disk"
     echo ""
     echo "  push"
-    echo "      Push the Docker image for the project (if you're on the main branch)"
+    echo "      Push a platform-specific the Docker image for the project"
     echo ""
     echo "  push-manifests"
-    echo "      Push the multi-arch Docker manifests (if you're on the main branch)"
+    echo "      Create and push the multi-arch Docker manifests for already pushed platform-specific images"
     echo ""
     echo "  help"
     echo "      Show this message"
@@ -50,6 +57,10 @@ function _get_current_branch() {
     git branch --show-current
 }
 
+function _enforce_image_name() {
+    if [ -z "$IMAGE_NAME" ]; then _fail "Mandatory parameter IMAGE_NAME missing."; fi
+}
+
 function _enforce_main_branch() {
     MAIN_BRANCH=${MAIN_BRANCH-"master"}
     CURRENT_BRANCH=$(_get_current_branch)
@@ -65,15 +76,14 @@ function _enforce_no_fork() {
 }
 
 function _enforce_docker_credentials() {
-    if [ -z "$DOCKER_USERNAME" ] || [ -z "$DOCKER_PASSWORD" ]; then
-      _fail "Mandatory Docker credentials are missing."
-    fi
+    if [ -z "$DOCKER_USERNAME" ] || [ -z "$DOCKER_PASSWORD" ]; then _fail "Mandatory Docker credentials are missing."; fi
 }
 
-function _set_defaults() {
-    # assert required variables are defined
-    if [ -z "$IMAGE_NAME" ]; then _fail "Mandatory parameter IMAGE_NAME missing."; fi
+function _enforce_platform() {
+    if [ -z "$PLATFORM" ]; then _fail "Mandatory parameter PLATFORM is missing."; fi
+}
 
+function _set_version_defaults() {
     # determine major/minor/patch versions
     if [ -z "$IMAGE_TAG" ]; then
       IMAGE_TAG=$(_get_current_version)
@@ -91,7 +101,8 @@ function _set_defaults() {
 
 function cmd-build() {
     # start build of a platform-specific image (this target will get called for multiple archs like AMD64/ARM64)
-    _set_defaults
+    _enforce_image_name
+    _set_version_defaults
 
     if [ -z "$DOCKERFILE" ]; then DOCKERFILE=Dockerfile; fi
     # by default we load the result to the docker daemon
@@ -107,47 +118,67 @@ function cmd-build() {
       --build-arg=LOCALSTACK_BUILD_DATE=$(date -u +"%Y-%m-%d") \
       --build-arg=LOCALSTACK_BUILD_VERSION=$IMAGE_TAG \
       --add-host="localhost.localdomain:127.0.0.1" \
-      -t "$IMAGE_NAME" $DOCKER_BUILD_FLAGS . -f $DOCKERFILE
+      -t "$IMAGE_NAME:$DEFAULT_TAG" $DOCKER_BUILD_FLAGS . -f $DOCKERFILE
+}
+
+function cmd-save() {
+    _enforce_image_name
+
+    if [ -z "$IMAGE_FILENAME" ]; then
+      _enforce_platform
+      IMAGE_FILENAME="localstack-docker-image-$PLATFORM.tar.gz";
+    fi
+
+    docker save $IMAGE_NAME:$DEFAULT_TAG | gzip > $IMAGE_FILENAME
+    # set the filename as github output if it's available
+    if [ -n "$GITHUB_OUTPUT" ]; then
+      echo "IMAGE_FILENAME=$IMAGE_FILENAME" >> "$GITHUB_OUTPUT"
+    fi
+}
+
+function cmd-load() {
+    if [ -z "$IMAGE_FILENAME" ]; then
+      _enforce_platform
+      IMAGE_FILENAME="localstack-docker-image-$PLATFORM.tar.gz";
+    fi
+
+    docker load -i $IMAGE_FILENAME
 }
 
 function cmd-push() {
-    ## Push a single platform-specific Docker image to registry, if we are currently on the main branch
-    _set_defaults
+    _enforce_image_name
     _enforce_main_branch
     _enforce_no_fork
     _enforce_docker_credentials
-
-    # enforce PLATFORM
-    if [ -z "$PLATFORM" ]; then
-      _fail "Mandatory parameter PLATFORM is missing."
-    fi
+    _enforce_platform
+    _set_version_defaults
 
     if [ -z "$TARGET_IMAGE_NAME" ]; then TARGET_IMAGE_NAME=$IMAGE_NAME; fi
 
     # login to DockerHub
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 
-    # create the platform specific latest tag
-    docker tag $IMAGE_NAME:latest $TARGET_IMAGE_NAME:latest-$PLATFORM
+    # create the platform specific default tag
+    docker tag $IMAGE_NAME:$DEFAULT_TAG $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM
 
-    # push latest tag
-    docker push $TARGET_IMAGE_NAME:latest-$PLATFORM
+    # push default tag
+    docker push $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM
 
     function _push() {
       # create explicitly set image tag (via $IMAGE_TAG)
-      docker tag $TARGET_IMAGE_NAME:latest-$PLATFORM $TARGET_IMAGE_NAME:$IMAGE_TAG-$PLATFORM
+      docker tag $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM $TARGET_IMAGE_NAME:$IMAGE_TAG-$PLATFORM
 
       # create "stable" tag
-      docker tag $TARGET_IMAGE_NAME:latest-$PLATFORM $TARGET_IMAGE_NAME:stable-$PLATFORM
+      docker tag $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM $TARGET_IMAGE_NAME:stable-$PLATFORM
 
       # create <major> tag (f.e. 4)
-      docker tag $TARGET_IMAGE_NAME:latest-$PLATFORM $TARGET_IMAGE_NAME:$MAJOR_VERSION-$PLATFORM
+      docker tag $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM $TARGET_IMAGE_NAME:$MAJOR_VERSION-$PLATFORM
 
       # create <major>.<minor> (f.e. 4.0)
-      docker tag $TARGET_IMAGE_NAME:latest-$PLATFORM $TARGET_IMAGE_NAME:$MAJOR_VERSION.$MINOR_VERSION-$PLATFORM
+      docker tag $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM $TARGET_IMAGE_NAME:$MAJOR_VERSION.$MINOR_VERSION-$PLATFORM
 
       # create <major>.<minor>.<patch> (f.e. 4.0.0)
-      docker tag $TARGET_IMAGE_NAME:latest-$PLATFORM $TARGET_IMAGE_NAME:$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$PLATFORM
+      docker tag $TARGET_IMAGE_NAME:$DEFAULT_TAG-$PLATFORM $TARGET_IMAGE_NAME:$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$PLATFORM
 
       # push all the created tags
       docker push $TARGET_IMAGE_NAME:stable-$PLATFORM
@@ -171,19 +202,20 @@ function cmd-push() {
 }
 
 function cmd-push-manifests() {
-    _set_defaults
+    _enforce_image_name
     _enforce_main_branch
     _enforce_no_fork
     _enforce_docker_credentials
+    _set_version_defaults
 
     # login to DockerHub
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
     # create the multiarch manifest
-    docker manifest create $IMAGE_NAME:latest --amend $IMAGE_NAME:latest-amd64 --amend $IMAGE_NAME:latest-arm64
+    docker manifest create $IMAGE_NAME:$DEFAULT_TAG --amend $IMAGE_NAME:$DEFAULT_TAG-amd64 --amend $IMAGE_NAME:$DEFAULT_TAG-arm64
 
-    # push latest tag
-    docker manifest push $IMAGE_NAME:latest
+    # push default tag
+    docker manifest push $IMAGE_NAME:$DEFAULT_TAG
 
     function _push() {
       # create explicitly set image tag (via $IMAGE_TAG)
@@ -245,6 +277,8 @@ function main() {
     # invoke command
     case $command_name in
         "build")               cmd-build "$@" ;;
+        "save")                cmd-save "$@" ;;
+        "load")                cmd-load "$@" ;;
         "push")                cmd-push "$@" ;;
         "push-manifests")      cmd-push-manifests "$@" ;;
         "help")                usage && exit 0 ;;
