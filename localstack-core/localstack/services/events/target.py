ed93576a36565a767c63c8b4fc73ecb412bf5f66
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-05-23 12:57:29 +0200

    feat: use default events target for archive

--- localstack-core/localstack/services/events/target.py
+++ localstack-core/localstack/services/events/target.py
@@ -292,11 +292,6 @@ class EventsTargetSender(TargetSender):
             return json.dumps({"original_id": original_id, "original_account": original_account})
 
 
-class EventsArchiveTargetSender(TargetSender):
-    def send_event(self, event):
-        raise NotImplementedError("EventsArchive target is not yet implemented")
-
-
 class FirehoseTargetSender(TargetSender):
     def send_event(self, event):
         delivery_stream_name = firehose_name(self.target["Arn"])
@@ -414,7 +409,6 @@ class TargetSenderFactory:
         "batch": BatchTargetSender,
         "ecs": ContainerTargetSender,
         "events": EventsTargetSender,
-        "events-archive": EventsArchiveTargetSender,
         "firehose": FirehoseTargetSender,
         "kinesis": KinesisTargetSender,
         "lambda": LambdaTargetSender,
