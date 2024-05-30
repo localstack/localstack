67884aa6ee6c5ec29899f2ae60ae3b68278f9eeb
Simon Walker <simon.walker@localstack.cloud>
2024-05-30 11:23:55 +0100

    Mount community entry points

--- localstack-core/localstack/dev/kubernetes/__main__.py
+++ localstack-core/localstack/dev/kubernetes/__main__.py
@@ -18,11 +18,15 @@ def generate_k8s_cluster_config(pro: bool = False, mount_moto: bool = False, por
     )
 
     egg_path = os.path.join(root_path, "localstack_core.egg-info/entry_points.txt")
+    volumes.append(
+        {
+            "volume": f"{os.path.normpath(egg_path)}:/code/entry_points_community",
+            "nodeFilters": ["server:*", "agent:*"],
+        }
+    )
     if pro:
         ext_path = os.path.join(root_path, "..", "localstack-ext")
         ext_code_path = os.path.join(ext_path, "localstack_ext")
-        egg_path = os.path.join(ext_path, "localstack_ext.egg-info/entry_points.txt")
-
         volumes.append(
             {
                 "volume": f"{os.path.normpath(ext_code_path)}:/code/localstack_ext",
@@ -30,12 +34,13 @@ def generate_k8s_cluster_config(pro: bool = False, mount_moto: bool = False, por
             }
         )
 
-    volumes.append(
-        {
-            "volume": f"{os.path.normpath(egg_path)}:/code/entry_points",
-            "nodeFilters": ["server:*", "agent:*"],
-        }
-    )
+        egg_path = os.path.join(ext_path, "localstack_ext.egg-info/entry_points.txt")
+        volumes.append(
+            {
+                "volume": f"{os.path.normpath(egg_path)}:/code/entry_points_ext",
+                "nodeFilters": ["server:*", "agent:*"],
+            }
+        )
 
     if mount_moto:
         moto_path = os.path.join(root_path, "..", "moto", "moto")
