d57a82b92c97805daa5c2a75cbe790fbd441f311
Benjamin Simon <benjh.simon@gmail.com>
2024-06-02 18:30:29 +0200

    add config flag

--- localstack-core/localstack/config.py
+++ localstack-core/localstack/config.py
@@ -1032,6 +1032,9 @@ OPENSEARCH_MULTI_CLUSTER = is_env_not_false("OPENSEARCH_MULTI_CLUSTER")
 # Whether to really publish to GCM while using SNS Platform Application (needs credentials)
 LEGACY_SNS_GCM_PUBLISHING = is_env_true("LEGACY_SNS_GCM_PUBLISHING")
 
+# Whether to enable the Next Gen APIGW invocation logic (handler chain)
+ENABLE_APIGW_NEXT_GEN = is_env_true("ENABLE_APIGW_NEXT_GEN")
+
 # TODO remove fallback to LAMBDA_DOCKER_NETWORK with next minor version
 MAIN_DOCKER_NETWORK = os.environ.get("MAIN_DOCKER_NETWORK", "") or LAMBDA_DOCKER_NETWORK
 
