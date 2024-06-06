5300e70ec7bba794646224bb6e9c555367c995f7
Daniel Fangl <daniel.fangl@localstack.cloud>
2024-06-06 14:43:24 +0200

    Add support for placeholder values in env files

--- tests/integration/docker_utils/test_docker.py
+++ tests/integration/docker_utils/test_docker.py
@@ -1446,7 +1446,7 @@ class TestRunWithAdditionalArgs:
         assert stdout.decode(config.DEFAULT_ENCODING).strip() == "1024"
 
     def test_run_with_additional_arguments_env_files(
-        self, docker_client: ContainerClient, tmp_path
+        self, docker_client: ContainerClient, tmp_path, monkeypatch
     ):
         env_variable = "TEST1=VAL1"
         env_file = tmp_path / "env1"
@@ -1456,6 +1456,7 @@ class TestRunWithAdditionalArgs:
             TEST2=VAL2
             TEST3=${TEST2}
             TEST4=VAL # end comment
+            TEST5="VAL"
             """)
         env_file.write_text(env_vars)
 
@@ -1472,6 +1473,7 @@ class TestRunWithAdditionalArgs:
         assert "TEST2=VAL2" in env_output
         assert "TEST3=${TEST2}" in env_output
         assert "TEST4=VAL # end comment" in env_output
+        assert 'TEST5="VAL"' in env_output
 
         env_vars = textwrap.dedent("""
             # Some comment
@@ -1487,6 +1489,31 @@ class TestRunWithAdditionalArgs:
         )
         env_output = stdout.decode(config.DEFAULT_ENCODING)
         assert "TEST1" not in env_output
+
+        monkeypatch.setenv("TEST1", "VAL1")
+        stdout, _ = docker_client.run_container(
+            "alpine",
+            remove=True,
+            command=["env"],
+            additional_flags=f"--env-file {env_file}",
+        )
+        env_output = stdout.decode(config.DEFAULT_ENCODING)
+        assert "TEST1=VAL1" in env_output
+
+        env_vars = textwrap.dedent("""
+            # Some comment
+            TEST1=
+            """)
+        env_file.write_text(env_vars)
+
+        stdout, _ = docker_client.run_container(
+            "alpine",
+            remove=True,
+            command=["env"],
+            additional_flags=f"--env-file {env_file}",
+        )
+        env_output = stdout.decode(config.DEFAULT_ENCODING)
+        assert "TEST1=" in env_output.splitlines()
 
 
 class TestDockerImages:
