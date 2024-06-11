98794ebc69f97277a95418a464dd0a4f9af72ef1
Benjamin Simon <benjh.simon@gmail.com>
2024-06-12 00:51:11 +0200

    validate test_list_schedules

--- tests/aws/services/scheduler/test_scheduler.py
+++ tests/aws/services/scheduler/test_scheduler.py
@@ -1,7 +1,7 @@
 from localstack.testing.pytest import markers
 
 
-@markers.aws.unknown
+@markers.aws.validated
 def test_list_schedules(aws_client):
     # simple smoke test to assert that the provider is available, without creating any schedules
     result = aws_client.scheduler.list_schedules()
