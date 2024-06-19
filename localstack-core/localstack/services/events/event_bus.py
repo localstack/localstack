46e580056041175a9a64d10587836d79f326b985
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-06-19 13:14:52 +0200

    fix: dont set new create time on put permission

--- localstack-core/localstack/services/events/event_bus.py
+++ localstack-core/localstack/services/events/event_bus.py
@@ -51,7 +51,6 @@ class EventBusService:
     ):
         if policy and any([action, principal, statement_id, condition]):
             raise ValueError("Combination of policy with other arguments is not allowed")
-        self.event_bus.creation_time = datetime.now(timezone.utc)
         self.event_bus.last_modified_time = datetime.now(timezone.utc)
         if policy:  # policy document replaces all existing permissions
             policy = json.loads(policy)
