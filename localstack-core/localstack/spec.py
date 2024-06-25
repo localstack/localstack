5807a1bae4871a91a6d219baaf82046dcf5c7ba0
Viren Nadkarni <viren.nadkarni@gmail.com>
2024-06-25 15:04:47 +0530

    Add /_localstack/health

--- localstack-core/localstack/spec.py
+++ localstack-core/localstack/spec.py
@@ -2,15 +2,17 @@
 API specifications and schema.
 """
 
+from typing import Final
+
 from localstack.version import __version__
 
 # OpenAPI specification for the internal REST API
-OPENAPI = {
+OPENAPI: Final = {
     "openapi": "3.1.0",
     "info": {
         "title": "LocalStack REST API for Community Edition",
         "summary": (
-            "The LocalStack API exposes functionality related to diagnostics, health checks, plugins, "
+            "The LocalStack REST API exposes functionality related to diagnostics, health checks, plugins, "
             "initialisation hooks, service introspection, and more."
         ),
         "termsOfService": "https://www.localstack.cloud/legal/tos",
@@ -30,23 +32,24 @@ OPENAPI = {
         "/_aws/lambda/runtimes": {},
         "/_aws/ses": {
             "get": {
-                "description": "Retrieve all emails sent from SES",
+                "description": "Retrieve sent emails",
                 "responses": {
                     "200": {
+                        "description": "List of sent emails",
                         "content": {
                             "application/json": {
                                 "schema": {
                                     "type": "object",
                                     "properties": {
                                         "messages": {
-                                            "description": "List of SES messages",
+                                            "description": "List of sent SES messages",
                                             "type": "array",
                                             "items": {"type": "object", "properties": {}},
                                         }
                                     },
                                 }
                             }
-                        }
+                        },
                     }
                 },
             },
@@ -59,13 +62,112 @@ OPENAPI = {
         #
         "/_localstack/config": {},
         "/_localstack/diagnose": {},
-        "/_localstack/health": {},
+        "/_localstack/health": {
+            "get": {
+                "parameters": [
+                    {
+                        "name": "reload",
+                        "in": "query",
+                        "allowEmptyValue": True,
+                        "schema": {
+                            "type": "string",
+                        },
+                        "required": False,
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "",  # TODO
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "object",
+                                    "properties": {
+                                        "edition": {
+                                            "type": "string",
+                                            "enum": ["community", "pro", "enterprise"],
+                                        },
+                                        "features": {
+                                            "type": "object",
+                                        },
+                                        "services": {
+                                            "type": "object",
+                                        },
+                                        "version": {
+                                            "type": "string",
+                                        },
+                                    },
+                                }
+                            }
+                        },
+                    }
+                },
+            },
+            "head": {
+                "responses": {
+                    "200": {
+                        "description": "",  # TODO
+                        "content": {"text/plain": {}},
+                    }
+                }
+            },
+            "post": {
+                "responses": {
+                    "200": {
+                        "description": "",  # TODO
+                        "content": {"text/plain": {}},
+                    }
+                },
+                "requestBody": {
+                    "description": "",  # TODO
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "type": "object",
+                                "properties": {
+                                    "action": {"type": "string", "enum": ["restart", "kill"]}
+                                },
+                            }
+                        }
+                    },
+                    "required": True,
+                },
+            },
+            "put": {
+                "responses": {
+                    "200": {
+                        "description": "",  # TODO
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "object",
+                                    "properties": {"status": {"type": "string"}},
+                                }
+                            }
+                        },
+                    }
+                },
+                "requestBody": {
+                    "description": "",  # TODO
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "type": "object",
+                            }
+                        }
+                    },
+                },
+            },
+        },
+        "/_localstack/info": {},
         "/_localstack/init": {},
+        "/_localstack/init/{stage}": {},
         "/_localstack/plugins": {},
         "/_localstack/stackinfo": {
             "get": {
                 "responses": {
                     "200": {
+                        "description": "",  # TODO
                         "content": {
                             "application/json": {
                                 "schema": {
@@ -86,10 +188,11 @@ OPENAPI = {
                                     },
                                 }
                             }
-                        }
+                        },
                     }
                 }
             }
         },
+        "/_localstack/usage": {},
     },
 }
