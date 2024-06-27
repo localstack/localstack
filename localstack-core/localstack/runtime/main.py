4ed925dab8dd823290f1ab31d1d740a78988c0e1
Dominik Schubert <dominik.schubert91@gmail.com>
2024-06-27 19:34:21 +0200

    Fix docker image digest in startup runtime information for new runtime (#11100)

--- localstack-core/localstack/runtime/main.py
+++ localstack-core/localstack/runtime/main.py
@@ -42,8 +42,9 @@ def print_runtime_information(in_docker: bool = False):
             inspect_result = DOCKER_CLIENT.inspect_container(container_name)
             container_id = inspect_result["Id"]
             print("LocalStack Docker container id: %s" % container_id[:12])
-            image_sha = inspect_result["Image"]
-            print("LocalStack Docker image sha: %s" % image_sha)
+            image_details = DOCKER_CLIENT.inspect_image(inspect_result["Image"])
+            digests = image_details.get("RepoDigests") or ["Unavailable"]
+            print("LocalStack Docker image sha: %s" % digests[0])
         except ContainerException:
             print(
                 "LocalStack Docker container info: Failed to inspect the LocalStack docker container. "
