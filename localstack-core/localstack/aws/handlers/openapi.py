41b8885695ee70e31e92d73bb0e6db142debf80f
Viren Nadkarni <viren.nadkarni@gmail.com>
2024-07-03 14:42:45 +0530

    Log exception instead of propagating

--- localstack-core/localstack/aws/handlers/openapi.py
+++ localstack-core/localstack/aws/handlers/openapi.py
@@ -2,14 +2,19 @@
 Handlers for validating request and response schema against OpenAPI specs.
 """
 
+import logging
+
 from openapi_core import OpenAPI
 from openapi_core.contrib.werkzeug import WerkzeugOpenAPIRequest, WerkzeugOpenAPIResponse
+from openapi_core.exceptions import OpenAPIError
 
 from localstack import spec
 from localstack.aws.api import RequestContext
 from localstack.aws.chain import Handler, HandlerChain
 from localstack.constants import INTERNAL_RESOURCE_PATH
 from localstack.http import Response
+
+LOG = logging.getLogger(__name__)
 
 
 class OpenAPIRequestValidator(Handler):
@@ -20,7 +25,11 @@ class OpenAPIRequestValidator(Handler):
         path = context.request.path
 
         if path.startswith(INTERNAL_RESOURCE_PATH) or path.startswith("/_aws/"):
-            self.openapi.validate_request(WerkzeugOpenAPIRequest(context.request))
+            try:
+                self.openapi.validate_request(WerkzeugOpenAPIRequest(context.request))
+            except OpenAPIError as exc:
+                # TODO: propagate the exception?
+                LOG.error(exc)
 
 
 class OpenAPIResponseValidator(OpenAPIRequestValidator):
