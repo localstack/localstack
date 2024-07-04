655ca21c1f067e17beadcad5e3bafb4f5b36850d
Javier Collado <javier.collado@tutanota.com>
2024-07-04 17:11:34 +0200

    EC2SecurityGroup: add TagSpecifications parameter when Tags present in CloudFormation template (#11089)

--- tests/aws/services/cloudformation/resource_providers/ec2/test_ec2.py
+++ tests/aws/services/cloudformation/resource_providers/ec2/test_ec2.py
@@ -57,6 +57,29 @@ def test_deploy_prefix_list(deploy_cfn_template, aws_client, snapshot):
 
 
 @markers.aws.validated
+def test_deploy_security_group_with_tags(deploy_cfn_template, aws_client, snapshot):
+    """Create security group in default VPC with tags."""
+    stack = deploy_cfn_template(
+        template_path=os.path.join(
+            os.path.dirname(__file__), "../../../../templates/ec2_security_group_with_tags.yml"
+        )
+    )
+
+    snapshot.add_transformer(snapshot.transform.key_value("GroupId"))
+    snapshot.add_transformer(snapshot.transform.key_value("GroupName"))
+    snapshot.add_transformer(snapshot.transform.key_value("VpcId"))
+    snapshot.add_transformer(SortingTransformer("Tags", lambda tag: tag["Key"]))
+    response = aws_client.ec2.describe_security_groups(GroupIds=[stack.outputs["SecurityGroupId"]])
+    security_group = response["SecurityGroups"][0]
+
+    security_group["Tags"] = [
+        tag for tag in security_group["Tags"] if not tag["Key"].startswith("aws:cloudformation")
+    ]
+
+    snapshot.match("security-group", security_group)
+
+
+@markers.aws.validated
 @markers.snapshot.skip_snapshot_verify(
     paths=[
         "$..DnsEntries",
