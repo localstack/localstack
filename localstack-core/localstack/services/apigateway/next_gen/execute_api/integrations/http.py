68d01f5b80770c9522ff4ada708ab5cf66f3a3c0
Benjamin Simon <benjh.simon@gmail.com>
2024-07-05 15:16:16 +0200

    update typing and refactor

--- localstack-core/localstack/services/apigateway/next_gen/execute_api/integrations/http.py
+++ localstack-core/localstack/services/apigateway/next_gen/execute_api/integrations/http.py
@@ -1,4 +1,5 @@
 from http import HTTPMethod
+from typing import Optional, TypedDict
 
 import requests
 from werkzeug.datastructures import Headers
@@ -16,7 +17,35 @@ NO_BODY_METHODS = {
 }
 
 
-class RestApiHttpIntegration(RestApiIntegration):
+class SimpleHttpRequest(TypedDict, total=False):
+    method: HTTPMethod
+    url: str
+    params: Optional[dict[str, str | list[str]]]
+    data: bytes
+    headers: Optional[dict[str, str]]
+    cookies: Optional[dict[str, str]]
+    timeout: Optional[int]
+    allow_redirects: Optional[bool]
+    stream: Optional[bool]
+    verify: Optional[bool]
+    # TODO: check if there was a situation where we'd pass certs?
+    cert: Optional[str | tuple[str, str]]
+
+
+class BaseRestApiHttpIntegration(RestApiIntegration):
+    @staticmethod
+    def _get_default_headers(context: RestApiInvocationContext) -> dict[str, str]:
+        # passing full context as the trace-id will probably also come from it
+        # TODO: get right casing in case we can override them with mappings/override?
+        return {
+            "x-amzn-apigateway-api-id": context.api_id,
+            "x-amzn-trace-id": "",  # TODO
+            "user-agent": f"AmazonAPIGateway_{context.api_id}",
+            "accept-encoding": "gzip,deflate",
+        }
+
+
+class RestApiHttpIntegration(BaseRestApiHttpIntegration):
     """
     This is a REST API integration responsible to send a request to another HTTP API.
     https://docs.aws.amazon.com/apigateway/latest/developerguide/setup-http-integrations.html#api-gateway-set-up-http-proxy-integration-on-proxy-resource
@@ -28,16 +57,10 @@ class RestApiHttpIntegration(RestApiIntegration):
         integration_req: IntegrationRequest = context.integration_request
         method = integration_req["http_method"]
 
-        # TODO: get right casing in case we can override them with mappings/override?
-        default_apigw_headers = {
-            "x-amzn-apigateway-api-id": context.api_id,
-            "x-amzn-trace-id": "",  # TODO
-            "user-agent": f"AmazonAPIGateway_{context.api_id}",
-            "accept-encoding": "gzip,deflate",
-        }
+        default_apigw_headers = self._get_default_headers(context)
         default_apigw_headers.update(integration_req["headers"])
 
-        request_parameters = {
+        request_parameters: SimpleHttpRequest = {
             "method": method,
             "url": integration_req["uri"],
             "params": integration_req["query_string_parameters"],
@@ -47,18 +70,21 @@ class RestApiHttpIntegration(RestApiIntegration):
         if method not in NO_BODY_METHODS:
             request_parameters["data"] = integration_req["body"]
 
+        # TODO: configurable timeout (29 by default)
+        # request_parameters["timeout"] = 29
+        # TODO: check for redirects
+        # request_parameters["allow_redirects"] = False
+
         request_response = requests.request(**request_parameters)
 
-        response = Response(
+        return Response(
             response=request_response.content,
             status=request_response.status_code,
             headers=Headers(dict(request_response.headers)),
         )
 
-        return response
 
-
-class RestApiHttpProxyIntegration(RestApiIntegration):
+class RestApiHttpProxyIntegration(BaseRestApiHttpIntegration):
     """
     This is a simplified REST API integration responsible to send a request to another HTTP API by proxying it almost
     directly.
@@ -69,36 +95,30 @@ class RestApiHttpProxyIntegration(RestApiIntegration):
 
     def invoke(self, context: RestApiInvocationContext) -> Response:
         invocation_req: InvocationRequest = context.invocation_request
+        method = invocation_req["http_method"]
         integration_uri = context.resource_method["methodIntegration"]["uri"]
         uri = render_uri_with_path_parameters(
             integration_uri,
             path_parameters=invocation_req["path_parameters"],
         )
 
-        # TODO: get right casing in case we can override them with mappings/override?
-        default_apigw_headers = {
-            "x-amzn-apigateway-api-id": context.api_id,
-            "x-amzn-trace-id": "",  # TODO
-            "user-agent": f"AmazonAPIGateway_{context.api_id}",
-            "accept-encoding": "gzip,deflate",
-        }
-        headers = {
+        default_apigw_headers = self._get_default_headers(context)
+
+        request_headers = {
             key: ",".join(value) for key, value in invocation_req["multi_value_headers"].items()
         }
         # TODO: check which headers to pop
-        headers.pop("Host", None)
+        request_headers.pop("Host", None)
+        default_apigw_headers.update(request_headers)
 
-        default_apigw_headers.update(headers)
-
-        method = invocation_req["http_method"]
-        request_parameters = {
-            "method": invocation_req["http_method"],
+        request_parameters: SimpleHttpRequest = {
+            "method": method,
             "url": uri,
             "params": invocation_req["multi_value_query_string_parameters"],
             "headers": default_apigw_headers,
         }
 
-        # TODO: validate this
+        # TODO: validate this for HTTP_PROXY
         if method not in NO_BODY_METHODS:
             request_parameters["data"] = invocation_req["body"]
 
@@ -110,10 +130,8 @@ class RestApiHttpProxyIntegration(RestApiIntegration):
             if value := request_response.headers.get(header):
                 response_headers[f"x-amzn-remapped-{header}"] = value
 
-        response = Response(
+        return Response(
             response=request_response.content,
             status=request_response.status_code,
             headers=response_headers,
         )
-
-        return response
