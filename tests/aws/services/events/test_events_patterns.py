c56d16126b33fc109b3981cec45d0c5d1597b68d
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-07-11 16:18:08 +0200

    feat: validate content based pattern test

--- tests/aws/services/events/test_events_patterns.py
+++ tests/aws/services/events/test_events_patterns.py
@@ -9,6 +9,10 @@ import pytest
 
 from localstack.testing.aws.util import is_aws_cloud
 from localstack.testing.pytest import markers
+from localstack.utils.common import short_uid
+from tests.aws.services.events.helper_functions import (
+    sqs_collect_messages,
+)
 
 THIS_FOLDER: str = os.path.dirname(os.path.realpath(__file__))
 REQUEST_TEMPLATE_DIR = os.path.join(THIS_FOLDER, "event_pattern_templates")
@@ -203,3 +207,255 @@ class TestTestEventPattern:
             ),
         )
         snapshot.match("eventbridge-test-event-pattern-response-no-match", response)
+
+
+class TestRuleWithPattern:
+    @markers.aws.validated
+    def test_put_events_with_rule_pattern_anything_but(
+        self, put_events_with_filter_to_sqs, snapshot
+    ):
+        snapshot.add_transformer(
+            [
+                snapshot.transform.key_value("MD5OfBody"),
+                snapshot.transform.key_value("ReceiptHandle"),
+                snapshot.transform.jsonpath("$..EventBusName", "event-bus-name"),
+            ]
+        )
+
+        event_detail_match = {"command": "display-message", "payload": "baz"}
+        event_detail_null = {"command": None, "payload": "baz"}
+        event_detail_no_match = {"command": "no-message", "payload": "baz"}
+        test_event_pattern_anything_but = {
+            "source": ["core.update-account-command"],
+            "detail-type": ["core.update-account-command"],
+            "detail": {"command": [{"anything-but": ["no-message"]}]},
+        }
+        entries_match = [
+            {
+                "Source": test_event_pattern_anything_but["source"][0],
+                "DetailType": test_event_pattern_anything_but["detail-type"][0],
+                "Detail": json.dumps(event_detail_match),
+            }
+        ]
+        entries_match_null = [
+            {
+                "Source": test_event_pattern_anything_but["source"][0],
+                "DetailType": test_event_pattern_anything_but["detail-type"][0],
+                "Detail": json.dumps(event_detail_null),
+            }
+        ]
+        entries_no_match = [
+            {
+                "Source": test_event_pattern_anything_but["source"][0],
+                "DetailType": test_event_pattern_anything_but["detail-type"][0],
+                "Detail": json.dumps(event_detail_no_match),
+            }
+        ]
+
+        entries_asserts = [
+            (entries_match, True),
+            (entries_match_null, True),
+            (entries_no_match, False),
+        ]
+
+        messages = put_events_with_filter_to_sqs(
+            pattern=test_event_pattern_anything_but,
+            entries_asserts=entries_asserts,
+        )
+        snapshot.match("rule-anything-but", messages)
+
+    @markers.aws.validated
+    def test_put_events_with_rule_pattern_exists_true(
+        self, put_events_with_filter_to_sqs, snapshot
+    ):
+        """
+        Exists matching True condition: https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-event-patterns-content-based-filtering.html#eb-filtering-exists-matching
+        """
+        snapshot.add_transformer(
+            [
+                snapshot.transform.key_value("MD5OfBody"),
+                snapshot.transform.key_value("ReceiptHandle"),
+                snapshot.transform.jsonpath("$..EventBusName", "event-bus-name"),
+            ]
+        )
+
+        event_detail_exists = {"key": "value", "payload": "baz"}
+        event_detail_not_exists = {"no-key": "no-value", "payload": "baz"}
+        event_patter_details = ["core.update-account-command"]
+        test_event_pattern_exists = {
+            "source": event_patter_details,
+            "detail-type": event_patter_details,
+            "detail": {"key": [{"exists": True}]},
+        }
+        entries_exists = [
+            {
+                "Source": test_event_pattern_exists["source"][0],
+                "DetailType": test_event_pattern_exists["detail-type"][0],
+                "Detail": json.dumps(event_detail_exists),
+            }
+        ]
+        entries_not_exists = [
+            {
+                "Source": test_event_pattern_exists["source"][0],
+                "DetailType": test_event_pattern_exists["detail-type"][0],
+                "Detail": json.dumps(event_detail_not_exists),
+            }
+        ]
+        entries_asserts = [
+            (entries_exists, True),
+            (entries_not_exists, False),
+        ]
+
+        messages = put_events_with_filter_to_sqs(
+            pattern=test_event_pattern_exists,
+            entries_asserts=entries_asserts,
+        )
+        snapshot.match("rule-exists-true", messages)
+
+    @markers.aws.validated
+    def test_put_events_with_rule_pattern_exists_false(
+        self, put_events_with_filter_to_sqs, snapshot
+    ):
+        """
+        Exists matching False condition: https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-event-patterns-content-based-filtering.html#eb-filtering-exists-matching
+        """
+        snapshot.add_transformer(
+            [
+                snapshot.transform.key_value("MD5OfBody"),
+                snapshot.transform.key_value("ReceiptHandle"),
+                snapshot.transform.jsonpath("$..EventBusName", "event-bus-name"),
+            ]
+        )
+
+        event_detail_exists = {"key": "value", "payload": "baz"}
+        event_detail_not_exists = {"no-key": "no-value", "payload": "baz"}
+        event_patter_details = ["core.update-account-command"]
+        test_event_pattern_not_exists = {
+            "source": event_patter_details,
+            "detail-type": event_patter_details,
+            "detail": {"key": [{"exists": False}]},
+        }
+        entries_exists = [
+            {
+                "Source": test_event_pattern_not_exists["source"][0],
+                "DetailType": test_event_pattern_not_exists["detail-type"][0],
+                "Detail": json.dumps(event_detail_exists),
+            }
+        ]
+        entries_not_exists = [
+            {
+                "Source": test_event_pattern_not_exists["source"][0],
+                "DetailType": test_event_pattern_not_exists["detail-type"][0],
+                "Detail": json.dumps(event_detail_not_exists),
+            }
+        ]
+        entries_asserts_exists_false = [
+            (entries_exists, False),
+            (entries_not_exists, True),
+        ]
+
+        messages_not_exists = put_events_with_filter_to_sqs(
+            pattern=test_event_pattern_not_exists,
+            entries_asserts=entries_asserts_exists_false,
+        )
+        snapshot.match("rule-exists-false", messages_not_exists)
+
+    @markers.aws.validated
+    def test_put_event_with_content_base_rule_in_pattern(
+        self,
+        create_sqs_events_target,
+        events_create_event_bus,
+        events_put_rule,
+        snapshot,
+        aws_client,
+    ):
+        queue_url, queue_arn = create_sqs_events_target()
+
+        # Create event bus
+        event_bus_name = f"event-bus-{short_uid()}"
+        events_create_event_bus(Name=event_bus_name)
+
+        # Put rule
+        rule_name = f"rule-{short_uid()}"
+        # EventBridge apparently converts some fields, for example: Source=>source, DetailType=>detail-type
+        # but the actual pattern matching is case-sensitive by key!
+        pattern = {
+            "source": [{"exists": True}],
+            "detail-type": [{"prefix": "core.app"}],
+            "detail": {
+                "description": ["this-is-event-details"],
+                "amount": [200],
+                "salary": [2000, 4000],
+                "env": ["dev", "prod"],
+                "user": ["user1", "user2", "user3"],
+                "admins": ["skyli", {"prefix": "hey"}, {"prefix": "ad"}],
+                "test1": [{"anything-but": 200}],
+                "test2": [{"anything-but": "test2"}],
+                "test3": [{"anything-but": ["test3", "test33"]}],
+                "test4": [{"anything-but": {"prefix": "test4"}}],
+                # TODO: unsupported in LocalStack
+                # "ip": [{"cidr": "10.102.1.0/24"}],
+                "num-test1": [{"numeric": ["<", 200]}],
+                "num-test2": [{"numeric": ["<=", 200]}],
+                "num-test3": [{"numeric": [">", 200]}],
+                "num-test4": [{"numeric": [">=", 200]}],
+                "num-test5": [{"numeric": [">=", 200, "<=", 500]}],
+                "num-test6": [{"numeric": [">", 200, "<", 500]}],
+                "num-test7": [{"numeric": [">=", 200, "<", 500]}],
+            },
+        }
+
+        events_put_rule(
+            Name=rule_name,
+            EventBusName=event_bus_name,
+            EventPattern=json.dumps(pattern),
+        )
+
+        # Put target
+        target_id = f"target-{short_uid()}"
+        aws_client.events.put_targets(
+            Rule=rule_name,
+            EventBusName=event_bus_name,
+            Targets=[{"Id": target_id, "Arn": queue_arn, "InputPath": "$.detail"}],
+        )
+
+        event = {
+            "EventBusName": event_bus_name,
+            "Source": "core.update-account-command",
+            "DetailType": "core.app.backend",
+            "Detail": json.dumps(
+                {
+                    "description": "this-is-event-details",
+                    "amount": 200,
+                    "salary": 2000,
+                    "env": "prod",
+                    "user": "user3",
+                    "admins": "admin",
+                    "test1": 300,
+                    "test2": "test22",
+                    "test3": "test333",
+                    "test4": "this test4",
+                    "ip": "10.102.1.100",
+                    "num-test1": 100,
+                    "num-test2": 200,
+                    "num-test3": 300,
+                    "num-test4": 200,
+                    "num-test5": 500,
+                    "num-test6": 300,
+                    "num-test7": 300,
+                }
+            ),
+        }
+
+        aws_client.events.put_events(Entries=[event])
+
+        messages = sqs_collect_messages(aws_client, queue_url, expected_events_count=1, retries=3)
+
+        snapshot.add_transformer(
+            [
+                snapshot.transform.key_value("MD5OfBody"),
+                snapshot.transform.key_value("ReceiptHandle"),
+                snapshot.transform.key_value("MessageId"),
+            ]
+        )
+        snapshot.match("messages", messages)
