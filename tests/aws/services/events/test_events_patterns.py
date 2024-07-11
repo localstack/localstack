5a8a4e11f0821feabc1f55f0f2b3b1ce818ff15e
Maximilian Hoheiser <max.hoheiser@gmail.com>
2024-07-11 13:54:28 +0200

    refactor: separate test_event_pattern test into class

--- tests/aws/services/events/test_events_patterns.py
+++ tests/aws/services/events/test_events_patterns.py
@@ -77,85 +77,89 @@ def list_files_with_suffix(directory_path: str, suffix: str) -> List[str]:
 request_template_tuples = load_request_templates(REQUEST_TEMPLATE_DIR)
 
 
-# TODO: extend these test cases based on the open source docs + tests: https://github.com/aws/event-ruler
-#  For example, "JSON Array Matching", "And and Or Relationship among fields with Ruler", rule validation,
-#  and exception handling.
-@pytest.mark.parametrize(
-    "request_template,label", request_template_tuples, ids=[t[1] for t in request_template_tuples]
-)
-@markers.aws.validated
-def test_test_event_pattern(aws_client, snapshot, request_template, label):
-    """This parametrized test handles three outcomes:
-    a) MATCH (default): The EventPattern matches the Event yielding true as result.
-    b) NO MATCH (_NEG suffix): The EventPattern does NOT match the Event yielding false as result.
-    c) EXCEPTION (_EXC suffix): The EventPattern is invalid and raises an exception.
-    """
-    if label in SKIP_LABELS and not is_aws_cloud():
-        pytest.skip("Not yet implemented")
+class TestTestEventPattern:
+    # TODO: extend these test cases based on the open source docs + tests: https://github.com/aws/event-ruler
+    #  For example, "JSON Array Matching", "And and Or Relationship among fields with Ruler", rule validation,
+    #  and exception handling.
+    @pytest.mark.parametrize(
+        "request_template,label",
+        request_template_tuples,
+        ids=[t[1] for t in request_template_tuples],
+    )
+    @markers.aws.validated
+    def test_test_event_pattern(self, aws_client, snapshot, request_template, label):
+        """This parametrized test handles three outcomes:
+        a) MATCH (default): The EventPattern matches the Event yielding true as result.
+        b) NO MATCH (_NEG suffix): The EventPattern does NOT match the Event yielding false as result.
+        c) EXCEPTION (_EXC suffix): The EventPattern is invalid and raises an exception.
+        """
+        if label in SKIP_LABELS and not is_aws_cloud():
+            pytest.skip("Not yet implemented")
 
-    event = request_template["Event"]
-    event_pattern = request_template["EventPattern"]
+        event = request_template["Event"]
+        event_pattern = request_template["EventPattern"]
 
-    if label.endswith("_EXC"):
-        with pytest.raises(Exception) as e:
-            aws_client.events.test_event_pattern(
+        if label.endswith("_EXC"):
+            with pytest.raises(Exception) as e:
+                aws_client.events.test_event_pattern(
+                    Event=json.dumps(event),
+                    EventPattern=json.dumps(event_pattern),
+                )
+            exception_info = {
+                "exception_type": type(e.value),
+                "exception_message": e.value.response,
+            }
+            snapshot.match(label, exception_info)
+        else:
+            response = aws_client.events.test_event_pattern(
                 Event=json.dumps(event),
                 EventPattern=json.dumps(event_pattern),
             )
-        exception_info = {"exception_type": type(e.value), "exception_message": e.value.response}
-        snapshot.match(label, exception_info)
-    else:
-        response = aws_client.events.test_event_pattern(
-            Event=json.dumps(event),
-            EventPattern=json.dumps(event_pattern),
-        )
 
-        # Validate the test intention: The _NEG suffix indicates negative tests (i.e., a pattern not matching the event)
-        if label.endswith("_NEG"):
-            assert not response["Result"]
-        else:
+            # Validate the test intention: The _NEG suffix indicates negative tests (i.e., a pattern not matching the event)
+            if label.endswith("_NEG"):
+                assert not response["Result"]
+            else:
+                assert response["Result"]
+
+    @markers.aws.validated
+    def test_test_event_pattern_with_multi_key(self, aws_client):
+        """Test the special case of a duplicate JSON key separately because it requires working around the
+        uniqueness constraints of the JSON5 library and Python dicts, which would already de-deduplicate the key "location".
+        This example is based on the following AWS documentation:
+        https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-event-patterns-content-based-filtering.html#eb-filtering-complex-example
+        """
+
+        with open(COMPLEX_MULTI_KEY_EVENT, "r") as event_file, open(
+            COMPLEX_MULTI_KEY_EVENT_PATTERN, "r"
+        ) as event_pattern_file:
+            event = event_file.read()
+            event_pattern = event_pattern_file.read()
+
+            response = aws_client.events.test_event_pattern(
+                Event=event,
+                EventPattern=event_pattern,
+            )
             assert response["Result"]
 
+    @markers.aws.validated
+    def test_test_event_pattern_with_escape_characters(self, aws_client):
+        r"""Test the special case of using escape characters separately because it requires working around JSON escaping.
+        Escape characters are explained in the AWS documentation:
+        https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-event-patterns-content-based-filtering.html#eb-filtering-wildcard-matching
+        * "The string \* represents the literal * character"
+        * "The string \\ represents the literal \ character"
+        """
 
-@markers.aws.validated
-def test_test_event_pattern_with_multi_key(aws_client):
-    """Test the special case of a duplicate JSON key separately because it requires working around the
-    uniqueness constraints of the JSON5 library and Python dicts, which would already de-deduplicate the key "location".
-    This example is based on the following AWS documentation:
-    https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-event-patterns-content-based-filtering.html#eb-filtering-complex-example
-    """
-
-    with open(COMPLEX_MULTI_KEY_EVENT, "r") as event_file, open(
-        COMPLEX_MULTI_KEY_EVENT_PATTERN, "r"
-    ) as event_pattern_file:
-        event = event_file.read()
-        event_pattern = event_pattern_file.read()
+        event = r'{"id": "1", "source": "test-source", "detail-type": "test-detail-type", "account": "123456789012", "region": "us-east-2", "time": "2022-07-13T13:48:01Z", "detail": {"escape_star": "*", "escape_backslash": "\\"}}'
+        # TODO: devise better testing strategy for * because the wildcard matches everything and "\\*" does not match.
+        event_pattern = r'{"detail": {"escape_star": ["*"], "escape_backslash": ["\\"]}}'
 
         response = aws_client.events.test_event_pattern(
             Event=event,
             EventPattern=event_pattern,
         )
         assert response["Result"]
-
-
-@markers.aws.validated
-def test_test_event_pattern_with_escape_characters(aws_client):
-    r"""Test the special case of using escape characters separately because it requires working around JSON escaping.
-    Escape characters are explained in the AWS documentation:
-    https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-event-patterns-content-based-filtering.html#eb-filtering-wildcard-matching
-    * "The string \* represents the literal * character"
-    * "The string \\ represents the literal \ character"
-    """
-
-    event = r'{"id": "1", "source": "test-source", "detail-type": "test-detail-type", "account": "123456789012", "region": "us-east-2", "time": "2022-07-13T13:48:01Z", "detail": {"escape_star": "*", "escape_backslash": "\\"}}'
-    # TODO: devise better testing strategy for * because the wildcard matches everything and "\\*" does not match.
-    event_pattern = r'{"detail": {"escape_star": ["*"], "escape_backslash": ["\\"]}}'
-
-    response = aws_client.events.test_event_pattern(
-        Event=event,
-        EventPattern=event_pattern,
-    )
-    assert response["Result"]
 
 
 @markers.aws.validated
