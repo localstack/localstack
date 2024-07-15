600091f4ca0be338482ee301d14ca80ae4025fb4
MEPalma <marcoepalma@tutanota.com>
2024-07-15 11:17:11 +0200

    improve shutdown logic perf

--- localstack-core/localstack/services/stepfunctions/asl/component/state/state_execution/state_task/service/state_task_service_callback.py
+++ localstack-core/localstack/services/stepfunctions/asl/component/state/state_execution/state_task/service/state_task_service_callback.py
@@ -161,42 +161,45 @@ class StateTaskServiceCallback(StateTaskService, abc.ABC):
 
         # Collect the output of the integration pattern.
         outcome: CallbackOutcome | Any
-        if self.resource.condition == ResourceCondition.WaitForTaskToken:
-            outcome = self._eval_wait_for_task_token(
-                env=env,
-                timeout_seconds=timeout_seconds,
-                callback_endpoint=callback_endpoint,
-                heartbeat_endpoint=heartbeat_endpoint,
-            )
-        else:
-            # Sync operations require the task output as input.
-            env.stack.append(task_output)
-            if self.resource.condition == ResourceCondition.Sync:
-                sync_resolver = self._build_sync_resolver(
+        try:
+            if self.resource.condition == ResourceCondition.WaitForTaskToken:
+                outcome = self._eval_wait_for_task_token(
                     env=env,
-                    resource_runtime_part=resource_runtime_part,
-                    normalised_parameters=normalised_parameters,
+                    timeout_seconds=timeout_seconds,
+                    callback_endpoint=callback_endpoint,
+                    heartbeat_endpoint=heartbeat_endpoint,
                 )
             else:
-                sync_resolver = self._build_sync2_resolver(
+                # Sync operations require the task output as input.
+                env.stack.append(task_output)
+                if self.resource.condition == ResourceCondition.Sync:
+                    sync_resolver = self._build_sync_resolver(
+                        env=env,
+                        resource_runtime_part=resource_runtime_part,
+                        normalised_parameters=normalised_parameters,
+                    )
+                else:
+                    sync_resolver = self._build_sync2_resolver(
+                        env=env,
+                        resource_runtime_part=resource_runtime_part,
+                        normalised_parameters=normalised_parameters,
+                    )
+
+                outcome = self._eval_sync(
                     env=env,
-                    resource_runtime_part=resource_runtime_part,
-                    normalised_parameters=normalised_parameters,
+                    timeout_seconds=timeout_seconds,
+                    callback_endpoint=callback_endpoint,
+                    heartbeat_endpoint=heartbeat_endpoint,
+                    sync_resolver=sync_resolver,
                 )
-
-            outcome = self._eval_sync(
-                env=env,
-                timeout_seconds=timeout_seconds,
-                callback_endpoint=callback_endpoint,
-                heartbeat_endpoint=heartbeat_endpoint,
-                sync_resolver=sync_resolver,
-            )
-
-        # Now that the outcome is collected and the process has finished, ensure all waiting
-        # threads on this endpoint (or task) will stop. This is in an effort to release resources
-        # sooner than when these would eventually synchronise with the updated environment state
-        # of this task.
-        callback_endpoint.interrupt_all()
+        except Exception as integration_exception:
+            outcome = integration_exception
+        finally:
+            # Now that the outcome is collected or the exception is about to be passed upstream, and the process has
+            # finished, ensure all waiting # threads on this endpoint (or task) will stop. This is in an effort to
+            # release resources sooner than when these would eventually synchronise with the updated environment
+            # state of this task.
+            callback_endpoint.interrupt_all()
 
         # Handle Callback outcome types.
         if isinstance(outcome, CallbackOutcomeTimedOut):
@@ -208,6 +211,9 @@ class StateTaskServiceCallback(StateTaskService, abc.ABC):
         elif isinstance(outcome, CallbackOutcomeSuccess):
             outcome_output = json.loads(outcome.output)
             env.stack.append(outcome_output)
+        # Pass evaluation exception upstream for error handling.
+        elif isinstance(outcome, Exception):
+            raise outcome
         # Otherwise the outcome is the result of the integration pattern (sync, sync2)
         # therefore push it onto the evaluation stack for the next operations.
         else:
