e21ebada11849a4f0617fe011113537ac8910256
MEPalma <marcoepalma@tutanota.com>
2024-07-15 15:59:05 +0200

    fix request normalisation

--- localstack-core/localstack/services/stepfunctions/asl/component/state/state_execution/state_task/service/state_task_service.py
+++ localstack-core/localstack/services/stepfunctions/asl/component/state/state_execution/state_task/service/state_task_service.py
@@ -97,7 +97,7 @@ class StateTaskService(StateTask, abc.ABC):
             self._to_boto_request(request_value, value_shape)
         elif isinstance(value_shape, ListShape) and isinstance(request_value, list):
             for request_list_value in request_value:
-                self._to_boto_request(request_list_value, value_shape.member)  # noqa
+                self._to_boto_request_value(request_list_value, value_shape.member)  # noqa
         elif isinstance(value_shape, StringShape) and not isinstance(request_value, str):
             boto_request_value = to_json_str(request_value)
         elif value_shape.type_name == "blob" and not isinstance(boto_request_value, bytes):
