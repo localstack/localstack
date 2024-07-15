d8ca773add5278d2adb0762d35484f3fd4ab9d29
Simon Walker <simon.walker@localstack.cloud>
2024-07-15 04:25:19 +0100

    Add missing resources test

--- tests/aws/services/cloudformation/engine/test_process.py
+++ tests/aws/services/cloudformation/engine/test_process.py
@@ -10,10 +10,14 @@ Four possible error locations:
 *: no observed failures so no test
 """
 
+import json
 import textwrap
+from typing import Callable, TypeAlias
 
 import pytest
 import yaml
+from botocore.exceptions import ClientError, WaiterError
+from localstack_snapshot.snapshots.prototype import SnapshotSession
 
 from localstack.aws.connect import ServiceLevelClientFactory
 from localstack.testing.pytest import markers
@@ -23,9 +27,80 @@ pytestmark = [
     markers.aws.validated,
 ]
 
+Template: TypeAlias = str | dict
+
+
+@pytest.fixture
+def stack_name() -> str:
+    return f"stack-{short_uid()}"
+
+
+@pytest.fixture
+def change_set_name() -> str:
+    return f"cs-{short_uid()}"
+
+
+@pytest.fixture
+def run_sample(
+    aws_client: ServiceLevelClientFactory,
+    snapshot: SnapshotSession,
+    stack_name: str,
+    change_set_name: str,
+):
+    client = aws_client.cloudformation
+
+    def inner(template: Template):
+        def deploy():
+            if isinstance(template, dict):
+                template_str = json.dumps(template)
+            elif isinstance(template, str):
+                template_str = template
+            else:
+                raise ValueError()
+
+            client.create_change_set(
+                StackName=stack_name,
+                ChangeSetName=change_set_name,
+                ChangeSetType="CREATE",
+                TemplateBody=template_str,
+            )
+            client.get_waiter("change_set_create_complete").wait(
+                StackName=stack_name,
+                ChangeSetName=change_set_name,
+            )
+
+            client.execute_change_set(
+                StackName=stack_name,
+                ChangeSetName=change_set_name,
+            )
+            client.get_waiter("stack_exists").wait(
+                StackName=stack_name,
+            )
+
+        try:
+            deploy()
+        except ClientError as exc:
+            # snapshot.match("error", exc.response.get("Error", {}))
+            snapshot.match("error", str(exc))
+        except WaiterError as exc:
+            # snapshot.match("error", exc.response.get("Error", {}))
+            snapshot.match("error", str(exc))
+        else:
+            pytest.fail("no error detected")
+
+    yield inner
+
+    try:
+        client.delete_stack(StackName=stack_name)
+    except:  # noqa: E722
+        pass
+
 
 class TestCreateChangesetSync:
-    def test_invalid_yaml(self, aws_client: ServiceLevelClientFactory, snapshot):
+    def test_invalid_yaml(
+        self,
+        run_sample: Callable[..., None],
+    ):
         template = textwrap.dedent("""
         Resources:
         bad-indent
@@ -34,15 +109,11 @@ class TestCreateChangesetSync:
         with pytest.raises(Exception):
             yaml.safe_load(template)
 
-        with pytest.raises(Exception) as exc:
-            aws_client.cloudformation.create_change_set(
-                StackName=f"stack-{short_uid()}",
-                ChangeSetName=f"cs-{short_uid()}",
-                ChangeSetType="CREATE",
-                TemplateBody=template,
-            )
+        run_sample(template)
 
-        snapshot.match("error", str(exc.value))
+    def test_missing_required_top_level_key(self, run_sample: Callable[..., None]):
+        template = {}
+        run_sample(template)
 
 
 class TestCreateChangesetAync:
