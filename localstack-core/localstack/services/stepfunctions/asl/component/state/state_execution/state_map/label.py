df3f18ddb008ee9572898155f82643f608046ef7
Minoru Nakata <minoru@sprocket.bz>
2024-07-20 15:17:12 +0000

    feat: add label validation

--- localstack-core/localstack/services/stepfunctions/asl/component/state/state_execution/state_map/label.py
+++ localstack-core/localstack/services/stepfunctions/asl/component/state/state_execution/state_map/label.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from typing import Final
 
 from localstack.services.stepfunctions.asl.component.component import Component
@@ -7,4 +8,18 @@ class Label(Component):
     label: Final[str]
 
     def __init__(self, label: str):
-        self.label = label
+        self.label = label.encode().decode("unicode-escape")
+
+        if len(self.label) == 0:
+            raise ValueError("Label cannot be empty")
+
+        if len(self.label) > 40:
+            raise ValueError("Label cannot exceed 40 characters")
+
+        for invalid_char in list(' ?*<>{}[]:;,\\|^~$#%&`"') + [
+            chr(i) for i in chain(range(0x00, 0x20), range(0x7F, 0xA0))
+        ]:
+            if invalid_char in self.label:
+                raise ValueError(
+                    f"Label contains invalid character: '{invalid_char.encode("unicode-escape").decode()}'"
+                )
