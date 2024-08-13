f5a2c1121d7d81e5e97f02b3364de9548383dc1e
Benjamin Simon <benjh.simon@gmail.com>
2024-08-14 01:47:19 +0200

    clean up

--- localstack-core/localstack/services/apigateway/next_gen/provider.py
+++ localstack-core/localstack/services/apigateway/next_gen/provider.py
@@ -40,15 +40,16 @@ from .execute_api.router import ApiGatewayEndpoint, ApiGatewayRouter
 
 class ApigatewayNextGenProvider(ApigatewayProvider):
     router: ApiGatewayRouter
-    route_handler: ApiGatewayEndpoint
 
     def __init__(self, router: ApiGatewayRouter = None):
         # we initialize the route handler with a global store with default account and region, because it only ever
         # access values with CrossAccount attributes
-        store = apigateway_stores[DEFAULT_AWS_ACCOUNT_ID][AWS_REGION_US_EAST_1]
-        route_handler = ApiGatewayEndpoint(store=store)
-        super().__init__(router=router or ApiGatewayRouter(ROUTER, handler=route_handler))
-        self.route_handler = route_handler
+        if not router:
+            store = apigateway_stores[DEFAULT_AWS_ACCOUNT_ID][AWS_REGION_US_EAST_1]
+            route_handler = ApiGatewayEndpoint(store=store)
+            router = ApiGatewayRouter(ROUTER, handler=route_handler)
+
+        super().__init__(router=router)
 
     def on_after_init(self):
         apply_patches()
