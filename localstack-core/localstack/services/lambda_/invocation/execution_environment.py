e9c60818b27f19560fd628e04d4e246105911965
MEPalma <marcoepalma@tutanota.com>
2024-08-28 09:53:33 +0200

    update util func name

--- localstack-core/localstack/services/lambda_/invocation/execution_environment.py
+++ localstack-core/localstack/services/lambda_/invocation/execution_environment.py
@@ -408,6 +408,6 @@ class ExecutionEnvironment:
         # Returns the timeout value in seconds to be enforced during lambda container startups.
         # This is the value defined through LAMBDA_RUNTIME_ENVIRONMENT_TIMEOUT or the LAMBDA
         # DEBUG MODE default if this is enabled.
-        if is_lambda_debug_timeout_for(self.function_version.qualified_arn):
+        if is_lambda_debug_timeout_enabled_for(self.function_version.qualified_arn):
             return DEFAULT_LAMBDA_DEBUG_MODE_TIMEOUT_SECONDS
         return STARTUP_TIMEOUT_SEC
