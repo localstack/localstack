8c28fcbcfdeda15ff382e726241e7e3e254d432a
Giovanni Grano <me@giograno.com>
2024-08-30 12:47:51 +0200

    env var to delete the cached cert at startup

--- localstack-core/localstack/plugins.py
+++ localstack-core/localstack/plugins.py
@@ -2,6 +2,8 @@ import logging
 
 from localstack import config
 from localstack.runtime import hooks
+from localstack.utils.files import rm_rf
+from localstack.utils.ssl import get_cert_pem_file_path
 
 LOG = logging.getLogger(__name__)
 
@@ -24,3 +26,10 @@ def deprecation_warnings() -> None:
     from localstack.deprecations import log_deprecation_warnings
 
     log_deprecation_warnings()
+
+
+@hooks.on_infra_start(should_load=lambda: config.REMOVE_SSL_CERT)
+def delete_cached_certificate():
+    LOG.debug("Removing the cached local SSL certificate")
+    target_file = get_cert_pem_file_path()
+    rm_rf(target_file)
