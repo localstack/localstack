dbbc3d618f53149b316bfe1310856c63943d1d68
Alexander Rashed <alexander.rashed@localstack.cloud>
2024-09-02 11:51:14 +0200

    remove ARN partition rewriter

--- localstack-core/localstack/plugins.py
+++ localstack-core/localstack/plugins.py
@@ -8,18 +8,6 @@ from localstack.utils.ssl import get_cert_pem_file_path
 LOG = logging.getLogger(__name__)
 
 
-# Register the ArnPartitionRewriteListener only if the feature flag is enabled
-@hooks.on_infra_start(should_load=lambda: config.ARN_PARTITION_REWRITING)
-def register_partition_adjusting_proxy_listener():
-    LOG.info(
-        "Registering ArnPartitionRewriteListener to dynamically replace partitions in requests and responses."
-    )
-    from localstack.aws import handlers
-    from localstack.aws.handlers.partition_rewriter import ArnPartitionRewriteHandler
-
-    handlers.preprocess_request.append(ArnPartitionRewriteHandler())
-
-
 @hooks.on_infra_start()
 def deprecation_warnings() -> None:
     LOG.debug("Checking for the usage of deprecated community features and configs...")
