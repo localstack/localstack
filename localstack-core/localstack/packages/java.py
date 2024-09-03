c76b4297c2e01b79addf83eb87dab68027e95fae
Viren Nadkarni <viren.nadkarni@gmail.com>
2024-09-03 18:47:31 +0530

    Add Thai locale

--- localstack-core/localstack/packages/java.py
+++ localstack-core/localstack/packages/java.py
@@ -69,8 +69,8 @@ class JavaPackageInstaller(ArchiveDownloadAndExtractInstaller):
             "jdk.httpserver,jdk.management,jdk.management.agent,"
             # Required by Spark and Hadoop
             "java.security.jgss,jdk.security.auth,"
-            # Locales
-            "jdk.localedata --include-locales en "
+            # OpenSearch requires Thai locale for segmentation support
+            "jdk.localedata --include-locales en,th "
             # Supplementary args
             "--compress 2 --strip-debug --no-header-files --no-man-pages "
             # Output directory
