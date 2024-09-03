a325dfc7906d7206f1c86ebfb9259c8e137772b9
Viren Nadkarni <viren.nadkarni@gmail.com>
2024-09-03 16:38:26 +0530

    Optimise JRE for minimise disk footprint

--- localstack-core/localstack/packages/java.py
+++ localstack-core/localstack/packages/java.py
@@ -2,9 +2,11 @@ import logging
 import os
 from typing import List
 
-from localstack.packages import Package
+from localstack.packages import InstallTarget, Package
 from localstack.packages.core import ArchiveDownloadAndExtractInstaller
+from localstack.utils.files import rm_rf
 from localstack.utils.platform import Arch, get_arch, is_linux, is_mac_os
+from localstack.utils.run import run
 
 LOG = logging.getLogger(__name__)
 
@@ -19,7 +21,7 @@ JAVA_VERSIONS = {
     "21": "21.0.4+7",
 }
 
-JRE_DISTRIB_URL = "https://github.com/adoptium/temurin{version}-binaries/releases/download/{tag_slug}/OpenJDK{version}U-jre_{arch}_{os}_hotspot_{semver_safe}.tar.gz"
+JDK_DOWNLOAD_URL = "https://github.com/adoptium/temurin{version}-binaries/releases/download/{tag_slug}/OpenJDK{version}U-jdk_{arch}_{os}_hotspot_{semver_safe}.tar.gz"
 
 
 class JavaPackageInstaller(ArchiveDownloadAndExtractInstaller):
@@ -45,12 +47,50 @@ class JavaPackageInstaller(ArchiveDownloadAndExtractInstaller):
             semver_safe = semver_safe.replace("-", "")
             tag_slug = f"jdk{self.semver}"
 
-        return JRE_DISTRIB_URL.format(
+        return JDK_DOWNLOAD_URL.format(
             version=self.version, tag_slug=tag_slug, os=os, arch=arch, semver_safe=semver_safe
         )
 
     def _get_archive_subdir(self) -> str | None:
-        return f"jdk-{self.semver}-jre"
+        return ""
+
+    def _post_process(self, target: InstallTarget) -> None:
+        target_directory = self._get_install_dir(target)
+        minimal_jre_path = os.path.join(target.value, self.name, "jre-{self.version}-minimal")
+
+        # If jlink is not available, use the environment as is
+        if not os.path.exists(os.path.join(target_directory, "bin/jlink")):
+            LOG.warning("Skipping JRE optimisation because jlink is not available")
+            return
+
+        # Build a custom JRE with only the necessary bits to minimise disk footprint
+        LOG.debug("Optimising JRE installation")
+        cmd = (
+            "bin/jlink --add-modules "
+            # Required modules
+            "java.base,java.desktop,java.instrument,java.management,"
+            "java.naming,java.scripting,java.sql,java.xml,jdk.compiler,"
+            # jdk.unsupported contains sun.misc.Unsafe which is required by some dependencies
+            "jdk.unsupported,"
+            # Additional cipher suites
+            "jdk.crypto.cryptoki,"
+            # Archive support
+            "jdk.zipfs,"
+            # Required by MQ broker
+            "jdk.httpserver,jdk.management,jdk.management.agent,"
+            # Required by Spark and Hadoop
+            "java.security.jgss,jdk.security.auth,"
+            # Locales
+            "jdk.localedata --include-locales en "
+            # Supplementary args
+            "--compress 2 --strip-debug --no-header-files --no-man-pages "
+            # Output directory
+            "--output " + minimal_jre_path
+        )
+        run(cmd, cwd=target_directory)
+
+        rm_rf(target_directory)
+        os.rename(minimal_jre_path, target_directory)
 
     def get_java_home(self) -> str:
         return self.get_installed_dir()
