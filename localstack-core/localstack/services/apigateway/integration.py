69d38987cafe2a9f36f7bef7f5ff8849a1b5ce29
Cyril HAGEGE <cyril.hagege@graneet.fr>
2024-09-09 17:51:02 +0200

    fix: testing condition

--- localstack-core/localstack/services/apigateway/integration.py
+++ localstack-core/localstack/services/apigateway/integration.py
@@ -719,8 +719,10 @@ class S3Integration(BackendIntegration):
         action = None
         invoke_args = {"Bucket": bucket, "Key": object_key}
         integration_method = integration.get("httpMethod") or invocation_context.method
-        if integration_method == HTTPMethod.ANY:
+        LOG.debug("Integration method: %s", integration_method)
+        if integration_method == "ANY":
             integration_method = invocation_context.method
+        LOG.debug("Integration method: %s", integration_method)
 
         match integration_method:
             case HTTPMethod.GET:
