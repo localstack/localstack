c87403784f69e219eb407503124085a49e72a786
Cyril HAGEGE <cyril.hagege@graneet.fr>
2024-09-09 22:31:25 +0200

    fix: simplify logic after review

--- localstack-core/localstack/services/apigateway/integration.py
+++ localstack-core/localstack/services/apigateway/integration.py
@@ -718,8 +718,8 @@ class S3Integration(BackendIntegration):
 
         action = None
         invoke_args = {"Bucket": bucket, "Key": object_key}
-        integration_method = integration.get("httpMethod") or invocation_context.method
-        if integration_method == "ANY":
+        
+        if not (integration_method := integration.get("httpMethod")) or integration_method == "ANY":
             integration_method = invocation_context.method
 
         match integration_method:
