81cc12fb41b477ff0d12933aa554f9355ee4b729
Giovanni Grano <me@giograno.com>
2024-09-12 17:52:41 -0400

    add swagger plugin and route registration

--- localstack-core/localstack/plugins.py
+++ localstack-core/localstack/plugins.py
@@ -1,10 +1,17 @@
+import importlib
 import logging
+
+from rolo.routing import RuleAdapter
 
 from localstack import config
 from localstack.aws.handlers.validation import OASPlugin
+from localstack.http import Response
 from localstack.runtime import hooks
+from localstack.services.edge import ROUTER
+from localstack.services.internal import get_internal_apis
 from localstack.utils.files import rm_rf
 from localstack.utils.ssl import get_cert_pem_file_path
+from localstack.utils.swagger import SwaggerUIApi
 
 LOG = logging.getLogger(__name__)
 
@@ -22,6 +29,17 @@ def delete_cached_certificate():
     LOG.debug("Removing the cached local SSL certificate")
     target_file = get_cert_pem_file_path()
     rm_rf(target_file)
+
+
+@hooks.on_infra_start()
+def register_swagger_endpoints():
+    get_internal_apis().add(SwaggerUIApi())
+
+    def _serve_static_file(_request, path: str):
+        module = importlib.import_module("localstack.static")
+        return Response.for_resource(module, path)
+
+    ROUTER.add(RuleAdapter("/static/<path:path>", _serve_static_file))
 
 
 class CoreOASPlugin(OASPlugin):
