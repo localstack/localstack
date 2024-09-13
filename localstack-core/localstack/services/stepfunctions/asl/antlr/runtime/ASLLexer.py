b84a8c0bc21da70531044aba512a92102f27c668
MEPalma <64580864+MEPalma@users.noreply.github.com>
2024-09-13 17:58:51 +0200

    Step Functions: Bump ANTLR to 4.13.2 (#11499)

--- localstack-core/localstack/services/stepfunctions/asl/antlr/runtime/ASLLexer.py
+++ localstack-core/localstack/services/stepfunctions/asl/antlr/runtime/ASLLexer.py
@@ -1,4 +1,4 @@
-# Generated from ASLLexer.g4 by ANTLR 4.13.1
+# Generated from ASLLexer.g4 by ANTLR 4.13.2
 from antlr4 import *
 from io import StringIO
 import sys
@@ -1278,7 +1278,7 @@ class ASLLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.13.1")
+        self.checkVersion("4.13.2")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
