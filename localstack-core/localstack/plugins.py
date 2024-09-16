564ddc568ca497e7694a54c76b084d71a7b0faa0
Giovanni Grano <me@giograno.com>
2024-09-16 15:41:21 -0400

    build spec at runtime and add it to the router

--- localstack-core/localstack/plugins.py
+++ localstack-core/localstack/plugins.py
@@ -1,6 +1,8 @@
 import importlib
 import logging
 
+import werkzeug
+import yaml
 from rolo.routing import RuleAdapter
 
 from localstack import config
@@ -10,6 +12,7 @@ from localstack.runtime import hooks
 from localstack.services.edge import ROUTER
 from localstack.services.internal import get_internal_apis
 from localstack.utils.files import rm_rf
+from localstack.utils.openapi import get_localstack_openapi_spec
 from localstack.utils.ssl import get_cert_pem_file_path
 from localstack.utils.swagger import SwaggerUIApi
 
@@ -39,6 +42,14 @@ def register_swagger_endpoints():
         module = importlib.import_module("localstack.static")
         return Response.for_resource(module, path)
 
+    def _serve_openapi_spec(_request):
+        spec = get_localstack_openapi_spec()
+        response_body = yaml.dump(spec)
+        return werkzeug.Response(
+            response_body, content_type="application/yaml", direct_passthrough=True
+        )
+
+    ROUTER.add(RuleAdapter("/openapi.yaml", _serve_openapi_spec))
     ROUTER.add(RuleAdapter("/static/<path:path>", _serve_static_file))
 
 
--- localstack-core/localstack/static/swagger-initializer.js
+++ localstack-core/localstack/static/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://localhost:4566/static/openapi.yaml",
+    url: "http://localhost:4566/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
