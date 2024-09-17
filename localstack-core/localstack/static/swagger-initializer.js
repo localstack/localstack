0f44ccc80a7a7aa9c5daaab202e32cb5c57498e0
Giovanni Grano <me@giograno.com>
2024-09-17 12:01:16 -0400

    better way to compute the initializer link

--- localstack-core/localstack/static/swagger-initializer.js
+++ localstack-core/localstack/static/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:4566/openapi.yaml",
+    url: "{{ swagger_url }}",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
--- localstack-core/localstack/utils/swagger.py
+++ localstack-core/localstack/utils/swagger.py
@@ -3,10 +3,16 @@ import os
 from jinja2 import Environment, FileSystemLoader
 from rolo import route
 
+from localstack.config import external_service_url
 from localstack.http import Response
 
 
 class SwaggerUIApi:
+    init_path: str
+
+    def __init__(self) -> None:
+        self.init_path = f"{external_service_url()}/openapi.yaml"
+
     @route("/_localstack/swagger", methods=["GET"])
     def server_swagger_ui(self, _request):
         oas_path = os.path.join(os.path.dirname(__file__), "..", "templates")
@@ -14,3 +20,12 @@ class SwaggerUIApi:
         template = env.get_template("index.html")
         rendered_template = template.render()
         return Response(rendered_template, content_type="text/html")
+
+    @route("/_localstack/swagger-initializer.js", methods=["GET"])
+    def serve_swagger_initializer(self, _request):
+        oas_path = os.path.join(os.path.dirname(__file__), "..", "static")
+        env = Environment(loader=FileSystemLoader(oas_path))
+        template = env.get_template("swagger-initializer.js")
+
+        rendered_template = template.render(swagger_url=self.init_path)
+        return Response(rendered_template, content_type="application/javascript")
