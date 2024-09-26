886247a9d8885b9bacff911822a3cc53032e8ede
Daniel Fangl <daniel.fangl@localstack.cloud>
2024-09-26 11:37:38 +0200

    Strip surrounding quotes from DNS names provided in DNS_PATTERNS_TO_RESOLVE_UPSTREAM

--- tests/bootstrap/test_dns_server.py
+++ tests/bootstrap/test_dns_server.py
@@ -112,6 +112,47 @@ def test_skip_pattern(
     assert constants.LOCALHOST_IP in stdout.decode().splitlines()
 
 
+def test_skip_pattern_with_surrounding_quotes(
+    docker_network,
+    container_factory: ContainerFactory,
+    stream_container_logs,
+    wait_for_localstack_ready,
+    dns_query_from_container,
+):
+    """
+    Add a skip pattern of localhost.localstack.cloud to ensure that we prioritise skips before
+    local name resolution
+    """
+    ls_container = container_factory(
+        configurators=[
+            ContainerConfigurators.debug,
+            ContainerConfigurators.mount_docker_socket,
+            ContainerConfigurators.network(docker_network),
+            ContainerConfigurators.env_vars(
+                {
+                    "DNS_NAME_PATTERNS_TO_RESOLVE_UPSTREAM": r"'.*localhost.localstack.cloud'",
+                }
+            ),
+        ]
+    )
+    running_ls_container = ls_container.start()
+    stream_container_logs(ls_container)
+    wait_for_localstack_ready(running_ls_container)
+
+    container_ip = running_ls_container.ip_address(docker_network=docker_network)
+    stdout, _ = dns_query_from_container(
+        name=LOCALHOST_HOSTNAME, ip_address=container_ip, network=docker_network
+    )
+    assert container_ip not in stdout.decode().splitlines()
+    assert constants.LOCALHOST_IP in stdout.decode().splitlines()
+
+    stdout, _ = dns_query_from_container(
+        name=f"foo.{LOCALHOST_HOSTNAME}", ip_address=container_ip, network=docker_network
+    )
+    assert container_ip not in stdout.decode().splitlines()
+    assert constants.LOCALHOST_IP in stdout.decode().splitlines()
+
+
 def test_resolve_localstack_host(
     container_factory: ContainerFactory,
     stream_container_logs,
