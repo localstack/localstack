50c10c436fcdba9aecab149b63cae6761d67926a
Simon Walker <s.r.walker101@googlemail.com>
2024-09-27 14:39:58 +0100

    Set CodeSigningConfigId for AWS::Lambda::CodeSigningConfig

--- localstack-core/localstack/services/lambda_/resource_providers/aws_lambda_codesigningconfig.py
+++ localstack-core/localstack/services/lambda_/resource_providers/aws_lambda_codesigningconfig.py
@@ -64,6 +64,7 @@ class LambdaCodeSigningConfigProvider(ResourceProvider[LambdaCodeSigningConfigPr
 
         response = lambda_client.create_code_signing_config(**model)
         model["CodeSigningConfigArn"] = response["CodeSigningConfig"]["CodeSigningConfigArn"]
+        model["CodeSigningConfigId"] = response["CodeSigningConfig"]["CodeSigningConfigId"]
 
         return ProgressEvent(
             status=OperationStatus.SUCCESS,
