b75156d3dc95c3f794798acd7b76a88bc70ceeed
Greg Furman <gregfurman99@gmail.com>
2024-09-30 18:51:43 +0200

    fix: Events-v2 do not transform on custom input

--- localstack-core/localstack/services/events/target.py
+++ localstack-core/localstack/services/events/target.py
@@ -140,11 +140,11 @@ class TargetSender(ABC):
 
     def process_event(self, event: FormattedEvent):
         """Processes the event and send it to the target."""
-        if isinstance(event, dict):
-            event.pop("event-bus-name", None)
         if input_ := self.target.get("Input"):
             event = json.loads(input_)
         else:
+            if isinstance(event, dict):
+                event.pop("event-bus-name", None)
             if input_path := self.target.get("InputPath"):
                 event = transform_event_with_target_input_path(input_path, event)
             if input_transformer := self.target.get("InputTransformer"):
