d5cfa97b76828274850c010878d8084b0578d802
Benjamin Simon <benjh.simon@gmail.com>
2024-10-10 12:09:49 +0200

    baseline granian

---
+++ localstack-core/localstack/aws/serving/granian.py
@@ -0,0 +1,13 @@
+from rolo.gateway.wsgi import WsgiGateway
+
+from localstack.runtime.current import initialize_runtime
+
+
+def create_app():
+    gateway = initialize_runtime().components.gateway
+
+    app = WsgiGateway(gateway)
+    return app
+
+
+# granian --interface wsgi --factory --port 4566 --threads 10 localstack.aws.serving.granian:create_app
