495a62357e2ceb410b8aab8b569bac9f414e6fa0
Mathieu Cloutier <cloutier.mat0@gmail.com>
2024-10-14 18:52:16 -0600

    updated tests

--- tests/unit/utils/test_id_generator.py
+++ tests/unit/utils/test_id_generator.py
@@ -1,101 +1,107 @@
 import pytest
 
+from localstack.constants import TAG_KEY_CUSTOM_ID
+from localstack.services.cloudformation.engine.entities import StackIdentifier
 from localstack.testing.config import TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME
 from localstack.utils.id_generator import (
     generate_short_uid,
     generate_str_id,
     generate_uid,
-    set_custom_id,
-    unset_custom_id,
+    localstack_id_manager,
 )
 from localstack.utils.strings import long_uid, short_uid
 
-TEST_SERVICE = "test-service"
 TEST_NAME = "test-name"
-TEST_RESOURCE = "test-resource"
 
 
 @pytest.fixture
-def configure_custom_id(unset_configured_custom_id):
-    def _configure_custom_id(custom_id: str):
-        set_custom_id(
-            account_id=TEST_AWS_ACCOUNT_ID,
-            region=TEST_AWS_REGION_NAME,
-            service=TEST_SERVICE,
-            resource=TEST_RESOURCE,
-            name=TEST_NAME,
-            custom_id=custom_id,
+def default_resource_identifier():
+    return StackIdentifier(TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_NAME)
+
+
+@pytest.fixture
+def configure_custom_id(unset_configured_custom_id, default_resource_identifier):
+    set_identifier = [default_resource_identifier]
+
+    def _configure_custom_id(custom_id: str, resource_identifier=None):
+        localstack_id_manager.set_custom_id(
+            resource_identifier or default_resource_identifier, custom_id=custom_id
         )
+        if resource_identifier:
+            set_identifier.append(resource_identifier)
 
     yield _configure_custom_id
 
     # we reset the ids after each test
-    unset_configured_custom_id()
+    for identifier in set_identifier:
+        unset_configured_custom_id(identifier)
 
 
 @pytest.fixture
-def unset_configured_custom_id():
-    def _unset():
-        unset_custom_id(
-            account_id=TEST_AWS_ACCOUNT_ID,
-            region=TEST_AWS_REGION_NAME,
-            service=TEST_SERVICE,
-            resource=TEST_RESOURCE,
-            name=TEST_NAME,
-        )
+def unset_configured_custom_id(default_resource_identifier):
+    def _unset(resource_identifier=None):
+        localstack_id_manager.unset_custom_id(resource_identifier or default_resource_identifier)
 
     return _unset
 
 
-def test_generate_short_id(configure_custom_id, unset_configured_custom_id):
+def test_generate_short_id(
+    configure_custom_id, unset_configured_custom_id, default_resource_identifier
+):
     custom_id = short_uid()
     configure_custom_id(custom_id)
 
-    generated = generate_short_uid(
-        TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_SERVICE, TEST_RESOURCE, TEST_NAME
-    )
+    generated = generate_short_uid(default_resource_identifier)
     assert generated == custom_id
 
     unset_configured_custom_id()
-    generated = generate_short_uid(
-        TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_SERVICE, TEST_RESOURCE, TEST_NAME
-    )
+    generated = generate_short_uid(default_resource_identifier)
     assert generated != custom_id
 
 
-def test_generate_uid(configure_custom_id, unset_configured_custom_id):
+def test_generate_uid(configure_custom_id, unset_configured_custom_id, default_resource_identifier):
     custom_id = long_uid()
     configure_custom_id(custom_id)
 
-    generated = generate_uid(
-        TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_SERVICE, TEST_RESOURCE, TEST_NAME
-    )
+    generated = generate_uid(default_resource_identifier)
     assert generated == custom_id
 
     unset_configured_custom_id()
 
     # test configured length
-    generated = generate_uid(
-        TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_SERVICE, TEST_RESOURCE, TEST_NAME, length=9
-    )
+    generated = generate_uid(default_resource_identifier, length=9)
     assert generated != custom_id
     assert len(generated) == 9
 
 
-def test_generate_str_id(configure_custom_id, unset_configured_custom_id):
+def test_generate_str_id(
+    configure_custom_id, unset_configured_custom_id, default_resource_identifier
+):
     custom_id = "RandomString"
     configure_custom_id(custom_id)
 
-    generated = generate_str_id(
-        TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_SERVICE, TEST_RESOURCE, TEST_NAME
-    )
+    generated = generate_str_id(default_resource_identifier)
     assert generated == custom_id
 
     unset_configured_custom_id()
 
     # test configured length
-    generated = generate_str_id(
-        TEST_AWS_ACCOUNT_ID, TEST_AWS_REGION_NAME, TEST_SERVICE, TEST_RESOURCE, TEST_NAME, length=9
-    )
+    generated = generate_str_id(default_resource_identifier, length=9)
     assert generated != custom_id
     assert len(generated) == 9
+
+
+def test_generate_with_custom_id_tag(
+    configure_custom_id, unset_configured_custom_id, default_resource_identifier
+):
+    custom_id = "set_id"
+    tag_custom_id = "id_from_tag"
+    configure_custom_id(custom_id)
+
+    # If the tags are passed, they should have priority
+    generated = generate_str_id(
+        default_resource_identifier, tags={TAG_KEY_CUSTOM_ID: tag_custom_id}
+    )
+    assert generated == tag_custom_id
+    generated = generate_str_id(default_resource_identifier)
+    assert generated == custom_id
