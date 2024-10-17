034383f7914abe248b8a7858e495973630eb20e9
Viren Nadkarni <viren.nadkarni@gmail.com>
2024-10-17 12:11:27 +0530

    Keep discard logic

--- localstack-core/localstack/runtime/init.py
+++ localstack-core/localstack/runtime/init.py
@@ -155,6 +155,8 @@ class InitScriptManager:
             for script in scripts:
                 LOG.debug("Running %s script %s", script.stage, script.path)
 
+                env_original = os.environ.copy()
+
                 try:
                     script.state = State.RUNNING
                     runner = self.get_script_runner(script.path)
@@ -167,7 +169,20 @@ class InitScriptManager:
                         LOG.error("Error while running script %s: %s", script, e)
                 else:
                     script.state = State.SUCCESSFUL
-
+                finally:
+                    # Discard env variables overridden in startup script that may cause side-effects
+                    for env_var in (
+                        "AWS_ACCESS_KEY_ID",
+                        "AWS_SECRET_ACCESS_KEY",
+                        "AWS_SESSION_TOKEN",
+                        "AWS_DEFAULT_REGION",
+                        "AWS_PROFILE",
+                        "AWS_REGION",
+                    ):
+                        if env_var in env_original:
+                            os.environ[env_var] = env_original[env_var]
+                        else:
+                            os.environ.pop(env_var, None)
         finally:
             self.stage_completed[stage] = True
 
