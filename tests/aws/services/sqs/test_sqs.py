b878459bcc3840e45f9be1241820e463f292b91d
Kshitij Kohli <kshitijkohli@hotmail.com>
2024-10-20 12:36:31 +0530

    patch(11576): pushing tests

--- tests/aws/services/sqs/test_sqs.py
+++ tests/aws/services/sqs/test_sqs.py
@@ -436,6 +436,15 @@ class TestSqsProvider:
 
         snapshot.match("send_oversized_message_batch", response)
 
+    @markers.aws.unknown
+    def test_send_message_to_standard_queue_with_empty_message_group_id(self, sqs_create_queue, aws_client, snapshot):
+        queue = sqs_create_queue()
+
+        with pytest.raises(ClientError) as e:
+            aws_client.sqs.send_message(QueueUrl=queue, MessageBody="message", MessageGroupId="")
+
+        snapshot.match("error", e.value.response)
+
     @markers.aws.validated
     def test_tag_untag_queue(self, sqs_create_queue, aws_sqs_client, snapshot):
         queue_url = sqs_create_queue()
