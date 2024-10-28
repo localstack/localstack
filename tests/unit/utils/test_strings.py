93579ebe9fb04da22b36c3733efc9b40d652011b
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2024-10-28 20:49:59 +0200

    Add test

--- tests/unit/utils/test_strings.py
+++ tests/unit/utils/test_strings.py
@@ -1,7 +1,12 @@
-from localstack.utils.strings import prepend_with_slash
+from localstack.utils.strings import prepend_with_slash, key_value_pairs_to_dict, short_uid, to_bytes
 
 
 def test_prepend_with_slash():
     assert prepend_with_slash("hello") == "/hello"
     assert prepend_with_slash("/world") == "/world"
     assert prepend_with_slash("//world") == "//world"
+
+def key_value_pairs_to_dict():
+    assert key_value_pairs_to_dict("a=1,b=2,c=3") == {"a": "1", "b": "2", "c": "3"}
+    assert key_value_pairs_to_dict("a=1;b=2;c=3", delimiter=";", separator="=") == {"a": "1", "b": "2", "c": "3"}
+    assert key_value_pairs_to_dict("a=1;b=2;c=3", delimiter=";", separator=":") == {'a=1': '', 'b=2': '', 'c=3': ''}
