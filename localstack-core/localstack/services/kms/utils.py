ac11bbbe2e0b565fa0ac4f52b65243dc04f93c8b
Simon Brugman <sbrugman@users.noreply.github.com>
2024-10-31 19:18:53 +0100

    Fix typo `str.rsplit`

--- localstack-core/localstack/services/kms/utils.py
+++ localstack-core/localstack/services/kms/utils.py
@@ -14,7 +14,7 @@ def get_hash_algorithm(signing_algorithm: str) -> str:
     Return the hashing algorithm for a given signing algorithm.
     eg. "RSASSA_PSS_SHA_512" -> "SHA_512"
     """
-    return "_".join(signing_algorithm.rsplit(sep="_", maxsplit=-2)[-2:])
+    return "_".join(signing_algorithm.rsplit(sep="_", maxsplit=2)[-2:])
 
 
 def parse_key_arn(key_arn: str) -> Tuple[str, str, str]:
