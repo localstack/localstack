efa1c230df87e73d40d6eb07f4fb9b9b4f306ecf
George Tsiolis <tsiolis.g@gmail.com>
2024-11-07 19:20:20 +0200

    Add CLI version command prefix (#11474)

--- localstack-core/localstack/cli/localstack.py
+++ localstack-core/localstack/cli/localstack.py
@@ -155,7 +155,13 @@ _click_format_option = click.option(
         "show_default": True,
     },
 )
-@click.version_option(VERSION, "--version", "-v", message="%(version)s")
+@click.version_option(
+    VERSION,
+    "--version",
+    "-v",
+    message="LocalStack CLI %(version)s",
+    help="Show the version of the LocalStack CLI and exit",
+)
 @click.option("-d", "--debug", is_flag=True, help="Enable CLI debugging mode")
 @click.option("-p", "--profile", type=str, help="Set the configuration profile")
 def localstack(debug, profile) -> None:
