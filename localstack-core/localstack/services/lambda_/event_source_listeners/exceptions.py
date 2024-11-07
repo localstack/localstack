1eddb697e583a24b6e5754b10262404de5aee264
Joel Scheuner <joel.scheuner.dev@gmail.com>
2024-11-07 10:12:19 +0100

    Remove legacy Lambda ESM v1 feature (#11733)

--- localstack-core/localstack/services/lambda_/event_source_listeners/exceptions.py
+++
@@ -1,5 +0,0 @@
-"""Exceptions for lambda event source mapping machinery."""
-
-
-class FunctionNotFoundError(Exception):
-    """Indicates that a function that is part of an existing event source listener does not exist."""
