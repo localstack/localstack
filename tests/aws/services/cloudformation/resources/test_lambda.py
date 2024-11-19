93eb9fc524649bdd846d0934d71a9a232e381ac5
Dominik Schubert <dominik.schubert91@gmail.com>
2024-11-19 21:27:25 +0100

    Prevent usage of java rule engine in tests (#11879)

--- tests/aws/services/cloudformation/resources/test_lambda.py
+++ tests/aws/services/cloudformation/resources/test_lambda.py
@@ -744,14 +744,14 @@ class TestCfnLambdaIntegrations:
 
     # TODO: consider moving into the dedicated DynamoDB => Lambda tests
     #  tests.aws.services.lambda_.test_lambda_integration_dynamodbstreams.TestDynamoDBEventSourceMapping.test_dynamodb_event_filter
+    @pytest.mark.skipif(
+        config.EVENT_RULE_ENGINE != "java",
+        reason="Filtering is broken with the Python rule engine for this specific case (exists:false) in ESM v2",
+    )
     @markers.aws.validated
     def test_lambda_dynamodb_event_filter(
         self, dynamodb_wait_for_table_active, deploy_cfn_template, aws_client, monkeypatch
     ):
-        # TODO: Filtering is broken with the Python rule engine for this specific case (exists:false) in ESM v2
-        #  -> using java engine as workaround for now.
-        monkeypatch.setattr(config, "EVENT_RULE_ENGINE", "java")
-
         function_name = f"test-fn-{short_uid()}"
         table_name = f"ddb-tbl-{short_uid()}"
 
