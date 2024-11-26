e28afddd10cf60cdb29e2ec68517ebc597bd0cc3
Joel Scheuner <joel.scheuner.dev@gmail.com>
2024-11-26 17:16:03 +0100

    Switch to UsageSetCounter

--- tests/unit/utils/analytics/test_usage.py
+++ tests/unit/utils/analytics/test_usage.py
@@ -1,4 +1,4 @@
-from localstack.utils.analytics.usage import UsageMultiSetCounter, UsageSetCounter
+from localstack.utils.analytics.usage import UsageSetCounter
 
 
 def test_set_counter():
@@ -7,23 +7,3 @@ def test_set_counter():
     my_feature_counter.record("nodejs16.x")
     my_feature_counter.record("nodejs16.x")
     assert my_feature_counter.aggregate() == {"python3.7": 1, "nodejs16.x": 2}
-
-
-def test_multi_set_counter():
-    my_feature_counter = UsageMultiSetCounter("pipes:invocation")
-    my_feature_counter.record("aws:sqs", "aws:lambda")
-    my_feature_counter.record("aws:sqs", "aws:lambda")
-    my_feature_counter.record("aws:sqs", "aws:stepfunctions")
-    my_feature_counter.record("aws:kinesis", "aws:lambda")
-    assert my_feature_counter.aggregate() == {
-        "pipes:invocation:aws:sqs": {
-            "aws:lambda": 2,
-            "aws:stepfunctions": 1,
-        },
-        "pipes:invocation:aws:kinesis": {"aws:lambda": 1},
-    }
-    assert my_feature_counter._counters["pipes:invocation:aws:sqs"].state == {
-        "aws:lambda": 2,
-        "aws:stepfunctions": 1,
-    }
-    assert my_feature_counter._counters["pipes:invocation:aws:kinesis"].state == {"aws:lambda": 1}
