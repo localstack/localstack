c32c4010446dbbcddd0aa7a3cf506ce27190da61
Joel Scheuner <joel.scheuner.dev@gmail.com>
2024-12-10 11:40:40 +0100

    Clarify usage counter descriptions (#12009)

--- localstack-core/localstack/services/events/usage.py
+++ localstack-core/localstack/services/events/usage.py
@@ -1,7 +1,7 @@
 from localstack.utils.analytics.usage import UsageSetCounter
 
-# number of pipe invocations per source (e.g. aws:sqs, aws:kafka, SelfManagedKafka) and target (e.g., aws:lambda)
+# number of successful EventBridge rule invocations per target (e.g., aws:lambda)
 rule_invocation = UsageSetCounter("events:rule:invocation")
 
-# number of pipe errors per source (e.g. aws:sqs, aws:kafka, SelfManagedKafka) and target (e.g., aws:lambda)
+# number of EventBridge rule errors per target (e.g., aws:lambda)
 rule_error = UsageSetCounter("events:rule:error")
