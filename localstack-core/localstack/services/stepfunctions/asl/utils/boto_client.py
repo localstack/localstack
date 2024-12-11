d421ab02c7acd559624ba87913df2628d499ce7d
MEPalma <marcoepalma@tutanota.com>
2024-12-11 18:18:38 +0100

    boto pr items

--- localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
+++ localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
@@ -13,9 +13,10 @@ from localstack.utils.aws.client_types import ServicePrincipal
 
 _BOTO_CLIENT_CONFIG = config = Config(
     parameter_validation=False,
-    retries={"max_attempts": 0, "total_max_attempts": 1},
+    retries={"total_max_attempts": 1},
     connect_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
     read_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
+    tcp_keepalive=True,
 )
 
 
