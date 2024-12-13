d5ef268f80a757d11760ce37e4b2f97b99ca0711
Greg Furman <gregfurman99@gmail.com>
2024-12-13 21:05:26 +0200

    Handle TrimmedDataAccessException in DynamoDBStreams Poller

--- localstack-core/localstack/services/lambda_/event_source_mapping/pollers/stream_poller.py
+++ localstack-core/localstack/services/lambda_/event_source_mapping/pollers/stream_poller.py
@@ -264,10 +264,7 @@ class StreamPoller(Poller):
             )
             return get_records_response
         # TODO: test iterator expired with conditional error scenario (requires failure destinations)
-        except (
-            self.source_client.exceptions.ExpiredIteratorException,
-            self.source_client.exceptions.TrimmedDataAccessException,
-        ) as e:
+        except self.source_client.exceptions.ExpiredIteratorExceptionas as e:
             LOG.debug(
                 "Shard iterator %s expired for stream %s, re-initializing shards",
                 shard_iterator,
