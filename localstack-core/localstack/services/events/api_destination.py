f69deedaed6aea04aa3c6a07626fdc1c67bc3569
maxhoheiser <max.hoheiser@gmail.com>
2024-12-20 13:30:06 +0100

    refactor: cleanup api destination

--- localstack-core/localstack/services/events/api_destination.py
+++ localstack-core/localstack/services/events/api_destination.py
@@ -1,5 +1,3 @@
-# TODO Target Helper
-
 import logging
 import re
 
@@ -179,40 +177,3 @@ class APIDestinationService:
 
 
 ApiDestinationServiceDict = dict[Arn, APIDestinationService]
-
-# ##########
-#     # Helper Methods for connections and api destinations
-#     ##########
-
-#
-
-#     def _get_public_parameters(self, auth_type: str, auth_parameters: dict) -> dict:
-#         """Extract public parameters (without secrets) based on auth type."""
-#         public_params = {}
-
-#         if auth_type == "BASIC" and "BasicAuthParameters" in auth_parameters:
-#             public_params["BasicAuthParameters"] = {
-#                 "Username": auth_parameters["BasicAuthParameters"]["Username"]
-#             }
-
-#         elif auth_type == "API_KEY" and "ApiKeyAuthParameters" in auth_parameters:
-#             public_params["ApiKeyAuthParameters"] = {
-#                 "ApiKeyName": auth_parameters["ApiKeyAuthParameters"]["ApiKeyName"]
-#             }
-
-#         elif auth_type == "OAUTH_CLIENT_CREDENTIALS" and "OAuthParameters" in auth_parameters:
-#             oauth_params = auth_parameters["OAuthParameters"]
-#             public_params["OAuthParameters"] = {
-#                 "AuthorizationEndpoint": oauth_params["AuthorizationEndpoint"],
-#                 "HttpMethod": oauth_params["HttpMethod"],
-#                 "ClientParameters": {"ClientID": oauth_params["ClientParameters"]["ClientID"]},
-#             }
-#             if "OAuthHttpParameters" in oauth_params:
-#                 public_params["OAuthParameters"]["OAuthHttpParameters"] = oauth_params.get(
-#                     "OAuthHttpParameters"
-#                 )
-
-#         if "InvocationHttpParameters" in auth_parameters:
-#             public_params["InvocationHttpParameters"] = auth_parameters["InvocationHttpParameters"]
-
-#         return public_params
