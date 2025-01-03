b50b78e89463c9b0ac1bf76f11eab4a6921632c0
maxhoheiser <max.hoheiser@gmail.com>
2025-01-03 16:04:19 +0100

    fix: optionally supply un transformed event to put_events as input

--- localstack-core/localstack/services/events/target.py
+++ localstack-core/localstack/services/events/target.py
@@ -195,11 +195,17 @@ class TargetSender(ABC):
         return self._client
 
     @abstractmethod
-    def send_event(self, event: FormattedEvent | TransformedEvent, trace_header: TraceHeader):
+    def send_event(
+        self,
+        event: FormattedEvent | TransformedEvent,
+        trace_header: TraceHeader,
+        input_event: FormattedEvent | None = None,
+    ):
         pass
 
     def process_event(self, event: FormattedEvent, trace_header: TraceHeader):
         """Processes the event and send it to the target."""
+        input_event = event.copy()
         if input_ := self.target.get("Input"):
             event = json.loads(input_)
         if isinstance(event, dict):
@@ -210,7 +216,7 @@ class TargetSender(ABC):
             if input_transformer := self.target.get("InputTransformer"):
                 event = self.transform_event_with_target_input_transformer(input_transformer, event)
         if event:
-            self.send_event(event, trace_header)
+            self.send_event(event, input_event, trace_header)
         else:
             LOG.info("No event to send to target %s", self.target.get("Id"))
 
@@ -570,15 +576,14 @@ class FirehoseTargetSender(TargetSender):
 
 
 class KinesisTargetSender(TargetSender):
-    def send_event(self, event, trace_header):
+    def send_event(self, event, trace_header, input_event):
         partition_key_path = collections.get_safe(
             self.target,
             "$.KinesisParameters.PartitionKeyPath",
             default_value="$.id",
         )
         stream_name = self.target["Arn"].split("/")[-1]
-        partition_key = collections.get_safe(event, partition_key_path, event["id"])
-
+        partition_key = collections.get_safe(input_event, partition_key_path, input_event["id"])
         self.client.put_record(
             StreamName=stream_name,
             Data=to_bytes(to_json_str(event)),
@@ -659,11 +664,11 @@ class SqsTargetSender(TargetSender):
 class StatesTargetSender(TargetSender):
     """Step Functions Target Sender"""
 
-    def send_event(self, event, trace_header):
+    def send_event(self, event, trace_header, input_event):
         self.service = "stepfunctions"
 
         self.client.start_execution(
-            stateMachineArn=self.target["Arn"], name=event["id"], input=to_json_str(event)
+            stateMachineArn=self.target["Arn"], name=input_event["id"], input=to_json_str(event)
         )
 
     def _validate_input(self, target: Target):
