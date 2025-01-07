3093c5792a598ccce35fb0220ae2b210f198f81e
Joel Scheuner <joel.scheuner.dev@gmail.com>
2025-01-07 17:49:51 +0100

    Unify JRE version with dynamodb-local

--- localstack-core/localstack/services/stepfunctions/packages.py
+++ localstack-core/localstack/services/stepfunctions/packages.py
@@ -9,6 +9,9 @@ class JSONataPackage(Package):
     def __init__(self):
         super().__init__("JSONataLibs", JSONATA_DEFAULT_VERSION)
 
+        # Match the dynamodb-local JRE version to reduce the LocalStack image size by sharing the same JRE version
+        self.java_version = "21"
+
     def _get_installer(self, version: str) -> PackageInstaller:
         return JSONataPackageInstaller(version)
 
