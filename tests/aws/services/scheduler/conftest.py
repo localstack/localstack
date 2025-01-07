65d21ae06d61600a0d21c1aa0066763ac2c3ed86
maxhoheiser <max.hoheiser@gmail.com>
2025-01-07 14:11:42 +0100

    feat: add test for schedule groups

--- tests/aws/services/scheduler/conftest.py
+++ tests/aws/services/scheduler/conftest.py
@@ -3,27 +3,45 @@ import logging
 import pytest
 
 from localstack.utils.strings import short_uid
+from localstack.utils.sync import retry
 
 LOG = logging.getLogger(__name__)
 
 
 @pytest.fixture
 def events_scheduler_create_schedule_group(aws_client):
-    schedule_group_arns = []
+    schedule_group_names = []
 
     def _events_scheduler_create_schedule_group(name, **kwargs):
         if not name:
             name = f"events-test-schedule-groupe-{short_uid()}"
         response = aws_client.scheduler.create_schedule_group(Name=name, **kwargs)
         schedule_group_arn = response["ScheduleGroupArn"]
-        schedule_group_arns.append(schedule_group_arn)
+        schedule_group_names.append(name)
 
         return schedule_group_arn
 
     yield _events_scheduler_create_schedule_group
 
-    for schedule_group_arn in schedule_group_arns:
+    def _wait_for_schedule_group_deletion(schedule_group_name):
         try:
-            aws_client.scheduler.delete_schedule_group(ScheduleGroupArn=schedule_group_arn)
+            aws_client.scheduler.get_schedule_group(Name=schedule_group_name)
         except Exception:
-            LOG.info("Failed to delete schedule group %s", schedule_group_arn)
+            LOG.info("Schedule group %s deleted", schedule_group_name)
+            return
+
+        raise Exception(f"Schedule group {schedule_group_name} not deleted")
+
+    for schedule_group_name in schedule_group_names:
+        try:
+            aws_client.scheduler.delete_schedule_group(Name=schedule_group_name)
+        except Exception:
+            LOG.info("Failed to delete schedule group %s", schedule_group_name)
+
+        # wait for resource to be deleted
+        retry(
+            _wait_for_schedule_group_deletion,
+            retries=20,
+            sleep=2,
+            schedule_group_name=schedule_group_name,
+        )
--- tests/aws/services/scheduler/test_scheduler.py
+++ tests/aws/services/scheduler/test_scheduler.py
@@ -167,3 +167,86 @@ def tests_create_schedule_with_valid_schedule_expression(
     cleanups.append(lambda: aws_client.scheduler.delete_schedule(Name=scheduler_name))
 
     snapshot.match("valid-schedule-expression", response)
+class TestScheduleGroupe:
+    @markers.aws.validated
+    @pytest.mark.parametrize("with_tags", [True, False])
+    @pytest.mark.parametrize("with_client_token", [True, False])
+    def test_create_schedule_group(
+        self, with_tags, with_client_token, events_scheduler_create_schedule_group, snapshot
+    ):
+        name = f"test-schedule-group-{short_uid()}"
+        kwargs = {}
+        if with_client_token:
+            kwargs["ClientToken"] = f"test-client_token-{short_uid()}"
+        if with_tags:
+            kwargs["Tags"] = [
+                {
+                    "Key": "TagKey",
+                    "Value": "TagValue",
+                }
+            ]
+        response = events_scheduler_create_schedule_group(
+            name,
+            **kwargs,
+        )
+
+        snapshot.add_transformer(snapshot.transform.regex(name, "<name>"))
+        snapshot.match("create-schedule-group", response)
+
+    @markers.aws.validated
+    @pytest.mark.parametrize("with_tags", [True, False])
+    def test_get_schedule_group(
+        self, with_tags, aws_client, events_scheduler_create_schedule_group, snapshot
+    ):
+        name = f"test-schedule-group-{short_uid()}"
+        kwargs = {"Tags": [{"Key": "TagKey", "Value": "TagValue"}]} if with_tags else {}
+        schedule_group_arn = events_scheduler_create_schedule_group(name, **kwargs)
+
+        response = aws_client.scheduler.get_schedule_group(Name=name)
+
+        assert response["Arn"] == schedule_group_arn
+        assert response["Name"] == name
+
+        snapshot.add_transformer(snapshot.transform.regex(name, "<name>"))
+        snapshot.match("get-schedule-group", response)
+
+    @markers.aws.validated
+    def test_get_schedule_group_not_found(self, aws_client, snapshot):
+        with pytest.raises(Exception) as exc:
+            aws_client.scheduler.get_schedule_group(Name="not-existing-name")
+
+        assert exc.typename == "ResourceNotFoundException"
+
+        snapshot.match("get-schedule-group-not-found", exc.value.response)
+
+    @markers.aws.validated
+    @pytest.mark.parametrize("with_tags", [True, False])
+    def test_list_schedule_groups(
+        self, with_tags, aws_client, events_scheduler_create_schedule_group, snapshot
+    ):
+        kwargs = {"Tags": [{"Key": "TagKey", "Value": "TagValue"}]} if with_tags else {}
+        name_one = f"test-schedule-groupe-one-{short_uid()}"
+        events_scheduler_create_schedule_group(name_one, **kwargs)
+
+        name_two = f"test-schedule-groupe-two-{short_uid()}"
+        events_scheduler_create_schedule_group(name_two, **kwargs)
+
+        response = aws_client.scheduler.list_schedule_groups()
+
+        assert len(response["ScheduleGroups"]) == 3  # default schedule group + 2 created
+
+        snapshot.add_transformers_list(
+            [
+                snapshot.transform.regex(name_one, "<name-groupe-one>"),
+                snapshot.transform.regex(name_two, "<name-groupe-two>"),
+            ]
+        )
+        snapshot.match("list-schedule-groups", response)
+
+    @markers.aws.validated
+    def test_list_schedule_groups_not_found(self, aws_client, snapshot):
+        response = aws_client.scheduler.list_schedule_groups()
+
+        assert len(response["ScheduleGroups"]) == 1  # default schedule group
+
+        snapshot.match("list-schedule-groups-not-found", response)
