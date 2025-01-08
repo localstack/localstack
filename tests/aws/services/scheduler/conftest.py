80228c97c32ab208fff993916bf50d528591110e
maxhoheiser <max.hoheiser@gmail.com>
2025-01-08 12:29:16 +0100

    feat: add snapshot tests for schedule crud

--- tests/aws/services/scheduler/conftest.py
+++ tests/aws/services/scheduler/conftest.py
@@ -1,11 +1,69 @@
 import logging
+import time
 
 import pytest
 
+from localstack.testing.aws.util import is_aws_cloud
 from localstack.utils.strings import short_uid
 from localstack.utils.sync import retry
 
 LOG = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def events_scheduler_create_schedule(aws_client, sqs_as_events_schedule_target):
+    schedule_names = []
+
+    def _events_scheduler_create_schedule(
+        name, flexible_time_window=None, schedule_expression=None, target=None, **kwargs
+    ):
+        if not name:
+            name = f"events-test-schedule-{short_uid()}"
+        if not flexible_time_window:
+            flexible_time_window = {
+                "Mode": "OFF",
+            }
+        if not schedule_expression:
+            schedule_expression = "rate(1 minute)"
+        if not target:
+            _, queue_arn, role_arn = sqs_as_events_schedule_target()
+            target = {"Arn": queue_arn, "Input": '{"key": "value"}', "RoleArn": role_arn}
+
+        response = aws_client.scheduler.create_schedule(
+            Name=name,
+            FlexibleTimeWindow=flexible_time_window,
+            ScheduleExpression=schedule_expression,
+            Target=target,
+            **kwargs,
+        )
+        schedule_names.append(name)
+
+        return response
+
+    yield _events_scheduler_create_schedule
+
+    def _wait_for_schedule_deletion(schedule_name):
+        try:
+            aws_client.scheduler.get_schedule_group(Name=schedule_name)
+        except Exception:
+            LOG.info("Schedule %s deleted", schedule_name)
+            return
+
+        raise Exception(f"Schedule {schedule_name} not deleted")
+
+    for schedule_name in schedule_names:
+        try:
+            aws_client.scheduler.delete_schedule(Name=schedule_name)
+        except Exception:
+            LOG.info("Failed to delete schedule %s", schedule_name)
+
+        # wait for resource to be deleted
+        retry(
+            _wait_for_schedule_deletion,
+            retries=20,
+            sleep=2,
+            schedule_name=schedule_name,
+        )
 
 
 @pytest.fixture
@@ -16,10 +74,9 @@ def events_scheduler_create_schedule_group(aws_client):
         if not name:
             name = f"events-test-schedule-groupe-{short_uid()}"
         response = aws_client.scheduler.create_schedule_group(Name=name, **kwargs)
-        schedule_group_arn = response["ScheduleGroupArn"]
         schedule_group_names.append(name)
 
-        return schedule_group_arn
+        return response
 
     yield _events_scheduler_create_schedule_group
 
@@ -45,3 +102,60 @@ def events_scheduler_create_schedule_group(aws_client):
             sleep=2,
             schedule_group_name=schedule_group_name,
         )
+
+
+@pytest.fixture
+def sqs_as_events_schedule_target(aws_client, sqs_get_queue_arn, create_iam_role_with_policy):
+    queue_urls = []
+
+    def _sqs_as_events_schedule_target(queue_name: str | None = None) -> tuple[str, str, str]:
+        if not queue_name:
+            queue_name = f"tests-queue-{short_uid()}"
+        sqs_client = aws_client.sqs
+        queue_url = sqs_client.create_queue(QueueName=queue_name)["QueueUrl"]
+        queue_urls.append(queue_url)
+        queue_arn = sqs_get_queue_arn(queue_url)
+
+        role_name = f"events-scheduler-role-{short_uid()}"
+        policy_name = f"events-scheduler-policy-{short_uid()}"
+        role_definition = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {"Service": "scheduler.amazonaws.com"},
+                    "Action": "sts:AssumeRole",
+                }
+            ],
+        }
+        policy_definition = {
+            "Version": "2012-10-17",
+            "Id": f"sqs-eventbridge-{short_uid()}",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "sqs:SendMessage",
+                    "Resource": queue_arn,
+                }
+            ],
+        }
+        role_arn = create_iam_role_with_policy(
+            RoleName=role_name,
+            PolicyName=policy_name,
+            RoleDefinition=role_definition,
+            PolicyDefinition=policy_definition,
+        )
+
+        if is_aws_cloud():
+            # wait for the role to be available
+            time.sleep(10)
+
+        return queue_url, queue_arn, role_arn
+
+    yield _sqs_as_events_schedule_target
+
+    for queue_url in queue_urls:
+        try:
+            aws_client.sqs.delete_queue(QueueUrl=queue_url)
+        except Exception as e:
+            LOG.debug("error cleaning up queue %s: %s", queue_url, e)
--- tests/aws/services/scheduler/test_scheduler.py
+++ tests/aws/services/scheduler/test_scheduler.py
@@ -23,7 +23,7 @@ def test_list_schedules(aws_client):
 @markers.aws.validated
 def test_tag_resource(aws_client, events_scheduler_create_schedule_group, snapshot):
     name = short_uid()
-    schedule_group_arn = events_scheduler_create_schedule_group(name)
+    schedule_group_arn = events_scheduler_create_schedule_group(name)["ScheduleGroupArn"]
 
     response = aws_client.scheduler.tag_resource(
         ResourceArn=schedule_group_arn,
@@ -52,7 +52,7 @@ def test_untag_resource(aws_client, events_scheduler_create_schedule_group, snap
             "Value": "TagValue",
         }
     ]
-    schedule_group_arn = events_scheduler_create_schedule_group(name, Tags=tags)
+    schedule_group_arn = events_scheduler_create_schedule_group(name, Tags=tags)["ScheduleGroupArn"]
 
     response = aws_client.scheduler.untag_resource(
         ResourceArn=schedule_group_arn, TagKeys=["TagKey"]
@@ -167,6 +167,292 @@ def tests_create_schedule_with_valid_schedule_expression(
     cleanups.append(lambda: aws_client.scheduler.delete_schedule(Name=scheduler_name))
 
     snapshot.match("valid-schedule-expression", response)
+class TestSchedule:
+    @markers.aws.validated
+    @pytest.mark.parametrize("with_client_token", [True, False])
+    @pytest.mark.parametrize("with_description", [True, False])
+    @pytest.mark.parametrize("action_after_completion", ["NONE", "DELETE"])
+    @pytest.mark.parametrize("state", ["ENABLED", "DISABLED"])
+    @pytest.mark.parametrize("flexible_time_window_mode", ["OFF", "FLEXIBLE"])
+    def test_create_schedule(
+        self,
+        with_client_token,
+        with_description,
+        action_after_completion,
+        state,
+        flexible_time_window_mode,
+        sqs_as_events_schedule_target,
+        events_scheduler_create_schedule,
+        snapshot,
+    ):
+        name = f"test-schedule-{short_uid()}"
+        kwargs = {"ActionAfterCompletion": action_after_completion, "State": state}
+        if with_client_token:
+            kwargs["ClientToken"] = f"test-client_token-{short_uid()}"
+        if with_description:
+            kwargs["Description"] = "Test description"
+
+        flexible_time_window = {
+            "Mode": flexible_time_window_mode,
+        }
+        if flexible_time_window_mode == "FLEXIBLE":
+            flexible_time_window["MaximumWindowInMinutes"] = 60
+
+        schedule_expression = "rate(1 minute)"
+        queue_url, queue_arn, role_arn = sqs_as_events_schedule_target()
+        target = {"Arn": queue_arn, "Input": '{"key": "value"}', "RoleArn": role_arn}
+
+        response = events_scheduler_create_schedule(
+            name, flexible_time_window, schedule_expression, target, **kwargs
+        )
+
+        snapshot.add_transformer(snapshot.transform.regex(name, "<name>"))
+        snapshot.match("create-schedule", response)
+
+    @markers.aws.validated
+    @pytest.mark.parametrize("with_client_token", [True, False])
+    @pytest.mark.parametrize("with_description", [True, False])
+    @pytest.mark.parametrize("action_after_completion", ["NONE", "DELETE"])
+    @pytest.mark.parametrize("state", ["ENABLED", "DISABLED"])
+    @pytest.mark.parametrize("flexible_time_window_mode", ["OFF", "FLEXIBLE"])
+    def test_get_schedule(
+        self,
+        with_client_token,
+        with_description,
+        action_after_completion,
+        state,
+        flexible_time_window_mode,
+        sqs_as_events_schedule_target,
+        events_scheduler_create_schedule,
+        aws_client,
+        snapshot,
+    ):
+        name = f"test-schedule-{short_uid()}"
+        kwargs = {"ActionAfterCompletion": action_after_completion, "State": state}
+        if with_client_token:
+            kwargs["ClientToken"] = f"test-client_token-{short_uid()}"
+        if with_description:
+            kwargs["Description"] = "Test description"
+
+        flexible_time_window = {
+            "Mode": flexible_time_window_mode,
+        }
+        if flexible_time_window_mode == "FLEXIBLE":
+            flexible_time_window["MaximumWindowInMinutes"] = 60
+
+        schedule_expression = "rate(1 minute)"
+        queue_url, queue_arn, role_arn = sqs_as_events_schedule_target()
+        target = {"Arn": queue_arn, "Input": '{"key": "value"}', "RoleArn": role_arn}
+
+        schedule_arn = events_scheduler_create_schedule(
+            name, flexible_time_window, schedule_expression, target, **kwargs
+        )["ScheduleArn"]
+
+        response = aws_client.scheduler.get_schedule(Name=name)
+
+        assert response["Arn"] == schedule_arn
+        assert response["Name"] == name
+
+        snapshot.add_transformers_list(
+            [
+                snapshot.transform.regex(name, "<name>"),
+                snapshot.transform.regex(role_arn, "<role-arn>"),
+                snapshot.transform.regex(queue_arn, "<queue-arn>"),
+            ]
+        )
+        snapshot.match("get-schedule", response)
+
+    @markers.aws.validated
+    def test_get_schedule_not_found(self, aws_client, snapshot):
+        with pytest.raises(Exception) as exc:
+            aws_client.scheduler.get_schedule(Name="not-existing-schedule-name")
+
+        assert exc.typename == "ResourceNotFoundException"
+
+        snapshot.match("get-schedule-not-found", exc.value.response)
+
+    @markers.aws.validated
+    def test_list_schedules(
+        self, aws_client, sqs_as_events_schedule_target, events_scheduler_create_schedule, snapshot
+    ):
+        name_one = f"test-schedule-one-{short_uid()}"
+        name_two = f"test-schedule-two-{short_uid()}"
+
+        flexible_time_window = {
+            "Mode": "OFF",
+        }
+        schedule_expression = "rate(1 minute)"
+        queue_url, queue_arn, role_arn = sqs_as_events_schedule_target()
+        target = {"Arn": queue_arn, "Input": '{"key": "value"}', "RoleArn": role_arn}
+
+        schedule_arn_one = events_scheduler_create_schedule(
+            name_one, flexible_time_window, schedule_expression, target
+        )["ScheduleArn"]
+        schedule_arn_two = events_scheduler_create_schedule(
+            name_two, flexible_time_window, schedule_expression, target
+        )["ScheduleArn"]
+
+        response = aws_client.scheduler.list_schedules()
+
+        assert len(response["Schedules"]) == 2
+
+        snapshot.add_transformers_list(
+            [
+                snapshot.transform.regex(schedule_arn_one, "<schedule-one-arn>"),
+                snapshot.transform.regex(schedule_arn_two, "<schedule-two-arn>"),
+                snapshot.transform.regex(queue_arn, "<target-sqs-queue-arn>"),
+            ]
+        )
+        snapshot.match("list-schedules", response)
+
+    @markers.aws.validated
+    def test_list_schedules_non(self, aws_client, snapshot):
+        response = aws_client.scheduler.list_schedules()
+
+        assert len(response["Schedules"]) == 0
+
+        snapshot.match("list-schedules-non", response)
+
+    @markers.aws.validated
+    @pytest.mark.parametrize("initially_with_client_token", [True, False])
+    @pytest.mark.parametrize("initially_with_description", [True, False])
+    @pytest.mark.parametrize(
+        "combinations_action_after_completion", [["NONE", "DELETE"], ["NONE", "DELETE"]]
+    )
+    @pytest.mark.parametrize(
+        "combinations_state", [["ENABLED", "DISABLED"], ["ENABLED", "DISABLED"]]
+    )
+    @pytest.mark.parametrize("initial_flexible_time_window_mode", ["OFF", "FLEXIBLE"])
+    def test_update_schedule(
+        self,
+        initially_with_client_token,
+        initially_with_description,
+        combinations_action_after_completion,
+        combinations_state,
+        initial_flexible_time_window_mode,
+        sqs_as_events_schedule_target,
+        events_scheduler_create_schedule,
+        aws_client,
+        snapshot,
+    ):
+        name = f"test-schedule-{short_uid()}"
+        kwargs_initial = {
+            "ActionAfterCompletion": combinations_action_after_completion[0],
+            "State": combinations_state[0],
+        }
+        kwargs_update = {
+            "ActionAfterCompletion": combinations_action_after_completion[1],
+            "State": combinations_state[1],
+        }
+
+        client_token = f"test-client_token-{short_uid()}"
+        if initially_with_client_token:
+            kwargs_initial["ClientToken"] = client_token
+        else:
+            kwargs_update["ClientToken"] = client_token
+
+        description = "Test description"
+        if initially_with_description:
+            kwargs_initial["Description"] = description
+        else:
+            kwargs_update["Description"] = description
+
+        flexible_time_window = {
+            "MaximumWindowInMinutes": 60,
+            "Mode": "FLEXIBLE",
+        }
+        not_flexible_time_window = {
+            "Mode": "OFF",
+        }
+        if initial_flexible_time_window_mode == "FLEXIBLE":
+            initial_flexible_time_window = flexible_time_window
+            update_flexible_time_window = not_flexible_time_window
+        else:
+            initial_flexible_time_window = not_flexible_time_window
+            update_flexible_time_window = flexible_time_window
+
+        initial_schedule_expression = "rate(1 minute)"
+        update_schedule_expression = "rate(2 minute)"
+
+        _, initial_queue_arn, initial_role_arn = sqs_as_events_schedule_target()
+        initial_target = {
+            "Arn": initial_queue_arn,
+            "Input": '{"key": "value"}',
+            "RoleArn": initial_role_arn,
+        }
+
+        _, update_queue_arn, update_role_arn = sqs_as_events_schedule_target()
+        update_target = {
+            "Arn": update_queue_arn,
+            "Input": '{"otherkey": "othervalue"}',
+            "RoleArn": update_role_arn,
+        }
+
+        response = events_scheduler_create_schedule(
+            name,
+            initial_flexible_time_window,
+            initial_schedule_expression,
+            initial_target,
+            **kwargs_initial,
+        )
+
+        # get initial schedule description
+        response = aws_client.scheduler.get_schedule(Name=name)
+
+        snapshot.add_transformers_list(
+            [
+                snapshot.transform.regex(name, "<name>"),
+                snapshot.transform.regex(client_token, "<client-token>"),
+                snapshot.transform.regex(initial_queue_arn, "<initial-target-queue-arn>"),
+                snapshot.transform.regex(initial_role_arn, "<initial-target-role-arn>"),
+            ]
+        )
+        snapshot.match("initial-schedule", response)
+
+        response = aws_client.scheduler.update_schedule(
+            Name=name,
+            FlexibleTimeWindow=update_flexible_time_window,
+            ScheduleExpression=update_schedule_expression,
+            Target=update_target,
+            **kwargs_update,
+        )
+
+        snapshot.match("update-schedule", response)
+
+        # get updated schedule description
+        response = aws_client.scheduler.get_schedule(Name=name)
+
+        snapshot.add_transformers_list(
+            [
+                snapshot.transform.regex(update_queue_arn, "<update-target-queue-arn>"),
+                snapshot.transform.regex(update_role_arn, "<update-target-role-arn>"),
+            ]
+        )
+        snapshot.match("updated-schedule", response)
+
+    @markers.aws.validated
+    def test_delete_schedule(self, aws_client, events_scheduler_create_schedule, snapshot):
+        name = f"test-schedule-{short_uid()}"
+        events_scheduler_create_schedule(name)
+
+        response = aws_client.scheduler.delete_schedule(Name=name)
+
+        response = aws_client.scheduler.list_schedules()
+
+        assert len(response["Schedules"]) == 0
+
+        snapshot.match("delete-schedule", response)
+
+    @markers.aws.validated
+    def test_delete_schedule_not_found(self, aws_client, snapshot):
+        with pytest.raises(Exception) as exc:
+            aws_client.scheduler.delete_schedule(Name="not-existing-schedule-name")
+
+        assert exc.typename == "ResourceNotFoundException"
+
+        snapshot.match("delete-schedule-not-found", exc.value.response)
+
+
 class TestScheduleGroupe:
     @markers.aws.validated
     @pytest.mark.parametrize("with_tags", [True, False])
@@ -200,7 +486,9 @@ class TestScheduleGroupe:
     ):
         name = f"test-schedule-group-{short_uid()}"
         kwargs = {"Tags": [{"Key": "TagKey", "Value": "TagValue"}]} if with_tags else {}
-        schedule_group_arn = events_scheduler_create_schedule_group(name, **kwargs)
+        schedule_group_arn = events_scheduler_create_schedule_group(name, **kwargs)[
+            "ScheduleGroupArn"
+        ]
 
         response = aws_client.scheduler.get_schedule_group(Name=name)
 
