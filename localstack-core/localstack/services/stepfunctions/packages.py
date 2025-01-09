eb6e3dccf9e568b2ca223dee76e311b65abea64a
Joel Scheuner <joel.scheuner.dev@gmail.com>
2025-01-09 14:06:37 +0100

    Ship StepFunction JSONata dependency by default (#12110)

--- localstack-core/localstack/services/stepfunctions/packages.py
+++ localstack-core/localstack/services/stepfunctions/packages.py
@@ -12,6 +12,9 @@ class JSONataPackage(Package):
     def __init__(self):
         super().__init__("JSONataLibs", JSONATA_DEFAULT_VERSION)
 
+        # Match the dynamodb-local JRE version to reduce the LocalStack image size by sharing the same JRE version
+        self.java_version = "21"
+
     def get_versions(self) -> list[str]:
         return list(JSONATA_JACKSON_VERSION_STORE.keys())
 
@@ -24,10 +27,13 @@ class JSONataPackageInstaller(JavaInstallerMixin, MavenPackageInstaller):
         jackson_version = JSONATA_JACKSON_VERSION_STORE[version]
         super().__init__(
             f"pkg:maven/com.dashjoin/jsonata@{version}",
+            # jackson-databind is imported in jsonata.py as "from com.fasterxml.jackson.databind import ObjectMapper"
+            # jackson-annotations and jackson-core are dependencies of jackson-databind:
+            # https://central.sonatype.com/artifact/com.fasterxml.jackson.core/jackson-databind/dependencies
             f"pkg:maven/com.fasterxml.jackson.core/jackson-core@{jackson_version}",
             f"pkg:maven/com.fasterxml.jackson.core/jackson-annotations@{jackson_version}",
             f"pkg:maven/com.fasterxml.jackson.core/jackson-databind@{jackson_version}",
         )
 
 
-jsonata_package = JSONataPackage()
+jpype_jsonata_package = JSONataPackage()
