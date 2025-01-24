0a5cd13f8de8f89156cd0b458f8e8edc4cbaed4a
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-01-24 17:03:24 +0100

    StepFunctions: Clean-up legacy JSONata unit tests (#11921)

--- tests/unit/services/stepfunctions/test_usage_metrics_static_analyser.py
+++ tests/unit/services/stepfunctions/test_usage_metrics_static_analyser.py
@@ -5,33 +5,96 @@ import pytest
 from localstack.services.stepfunctions.asl.static_analyser.usage_metrics_static_analyser import (
     UsageMetricsStaticAnalyser,
 )
-from tests.aws.services.stepfunctions.templates.assign.assign_templates import (
-    AssignTemplate,
+
+BASE_PASS_JSONATA = json.dumps(
+    {
+        "QueryLanguage": "JSONata",
+        "StartAt": "StartState",
+        "States": {
+            "StartState": {"Type": "Pass", "End": True},
+        },
+    }
 )
-from tests.aws.services.stepfunctions.templates.querylanguage.query_language_templates import (
-    QueryLanguageTemplate,
+
+BASE_PASS_JSONPATH = json.dumps(
+    {
+        "QueryLanguage": "JSONPath",
+        "StartAt": "StartState",
+        "States": {
+            "StartState": {"Type": "Pass", "End": True},
+        },
+    }
+)
+
+BASE_PASS_JSONATA_OVERRIDE = json.dumps(
+    {
+        "QueryLanguage": "JSONPath",
+        "StartAt": "StartState",
+        "States": {
+            "StartState": {"QueryLanguage": "JSONata", "Type": "Pass", "End": True},
+        },
+    }
+)
+
+BASE_PASS_JSONATA_OVERRIDE_DEFAULT = json.dumps(
+    {
+        "StartAt": "StartState",
+        "States": {
+            "StartState": {"QueryLanguage": "JSONata", "Type": "Pass", "End": True},
+        },
+    }
+)
+
+JSONPATH_TO_JSONATA_DATAFLOW = json.dumps(
+    {
+        "StartAt": "StateJsonPath",
+        "States": {
+            "StateJsonPath": {"Type": "Pass", "Assign": {"var": 42}, "Next": "StateJsonata"},
+            "StateJsonata": {
+                "QueryLanguage": "JSONata",
+                "Type": "Pass",
+                "Output": "{% $var %}",
+                "End": True,
+            },
+        },
+    }
+)
+
+ASSIGN_BASE_EMPTY = json.dumps(
+    {"StartAt": "State0", "States": {"State0": {"Type": "Pass", "Assign": {}, "End": True}}}
+)
+
+ASSIGN_BASE_SCOPE_MAP = json.dumps(
+    {
+        "StartAt": "State0",
+        "States": {
+            "State0": {
+                "Type": "Map",
+                "ItemProcessor": {
+                    "ProcessorConfig": {"Mode": "INLINE"},
+                    "StartAt": "Inner",
+                    "States": {
+                        "Inner": {
+                            "Type": "Pass",
+                            "Assign": {},
+                            "End": True,
+                        },
+                    },
+                },
+                "End": True,
+            }
+        },
+    }
 )
 
 
 class TestUsageMetricsStaticAnalyser:
-    @staticmethod
-    def _get_query_language_definition(query_language_template_filepath: str) -> str:
-        template = QueryLanguageTemplate.load_sfn_template(query_language_template_filepath)
-        definition = json.dumps(template)
-        return definition
-
-    @staticmethod
-    def _get_variable_sampling_definition(variable_sampling_template_filepath: str) -> str:
-        template = AssignTemplate.load_sfn_template(variable_sampling_template_filepath)
-        definition = json.dumps(template)
-        return definition
-
     @pytest.mark.parametrize(
-        "template_filepath",
+        "definition",
         [
-            QueryLanguageTemplate.BASE_PASS_JSONATA,
-            QueryLanguageTemplate.BASE_PASS_JSONATA_OVERRIDE,
-            QueryLanguageTemplate.BASE_PASS_JSONATA_OVERRIDE_DEFAULT,
+            BASE_PASS_JSONATA,
+            BASE_PASS_JSONATA_OVERRIDE,
+            BASE_PASS_JSONATA_OVERRIDE_DEFAULT,
         ],
         ids=[
             "BASE_PASS_JSONATA",
@@ -39,58 +102,37 @@ class TestUsageMetricsStaticAnalyser:
             "BASE_PASS_JSONATA_OVERRIDE_DEFAULT",
         ],
     )
-    def test_jsonata(self, template_filepath):
-        definition = self._get_query_language_definition(template_filepath)
+    def test_jsonata(self, definition):
         analyser = UsageMetricsStaticAnalyser.process(definition)
         assert analyser.has_jsonata
         assert not analyser.has_variable_sampling
 
-    @pytest.mark.parametrize(
-        "template_filepath",
-        [
-            QueryLanguageTemplate.BASE_PASS_JSONPATH,
-        ],
-        ids=["BASE_PASS_JSONPATH"],
-    )
-    def test_jsonpath(self, template_filepath):
-        definition = self._get_query_language_definition(template_filepath)
+    @pytest.mark.parametrize("definition", [BASE_PASS_JSONPATH], ids=["BASE_PASS_JSONPATH"])
+    def test_jsonpath(self, definition):
         analyser = UsageMetricsStaticAnalyser.process(definition)
         assert not analyser.has_jsonata
         assert not analyser.has_variable_sampling
 
     @pytest.mark.parametrize(
-        "template_filepath",
-        [
-            QueryLanguageTemplate.JSONPATH_TO_JSONATA_DATAFLOW,
-            QueryLanguageTemplate.JSONPATH_ASSIGN_JSONATA_REF,
-        ],
-        ids=["JSONPATH_TO_JSONATA_DATAFLOW", "JSONPATH_ASSIGN_JSONATA_REF"],
+        "definition", [JSONPATH_TO_JSONATA_DATAFLOW], ids=["JSONPATH_TO_JSONATA_DATAFLOW"]
     )
-    def test_jsonata_and_variable_sampling(self, template_filepath):
-        definition = self._get_query_language_definition(template_filepath)
+    def test_jsonata_and_variable_sampling(self, definition):
         analyser = UsageMetricsStaticAnalyser.process(definition)
         assert analyser.has_jsonata
         assert analyser.has_variable_sampling
 
     @pytest.mark.parametrize(
-        "template_filepath",
+        "definition",
         [
-            AssignTemplate.BASE_EMPTY,
-            AssignTemplate.BASE_PATHS,
-            AssignTemplate.BASE_SCOPE_MAP,
-            AssignTemplate.BASE_ASSIGN_FROM_LAMBDA_TASK_RESULT,
-            AssignTemplate.BASE_REFERENCE_IN_LAMBDA_TASK_FIELDS,
+            ASSIGN_BASE_EMPTY,
+            ASSIGN_BASE_SCOPE_MAP,
         ],
         ids=[
-            "BASE_EMPTY",
-            "BASE_PATHS",
-            "BASE_SCOPE_MAP",
-            "BASE_ASSIGN_FROM_LAMBDA_TASK_RESULT",
-            "BASE_REFERENCE_IN_LAMBDA_TASK_FIELDS",
+            "ASSIGN_BASE_EMPTY",
+            "ASSIGN_BASE_SCOPE_MAP",
         ],
     )
-    def test_jsonpath_and_variable_sampling(self, template_filepath):
-        definition = self._get_query_language_definition(template_filepath)
+    def test_jsonpath_and_variable_sampling(self, definition):
         analyser = UsageMetricsStaticAnalyser.process(definition)
         assert not analyser.has_jsonata
         assert analyser.has_variable_sampling
