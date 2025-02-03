84b2f5f352d63aff3ea8466893595c5ec4e5982e
Ben Simon Hartung <42031100+bentsku@users.noreply.github.com>
2025-02-03 14:05:13 +0100

    APIGW: add Binary Media support for response in `AWS_PROXY` (#12199)

--- localstack-core/localstack/services/apigateway/next_gen/execute_api/helpers.py
+++ localstack-core/localstack/services/apigateway/next_gen/execute_api/helpers.py
@@ -148,3 +148,29 @@ def parse_trace_id(trace_id: str) -> dict[str, str]:
             trace_values[key_value[0].capitalize()] = key_value[1]
 
     return trace_values
+
+
+def mime_type_matches_binary_media_types(mime_type: str | None, binary_media_types: list[str]):
+    if not mime_type or not binary_media_types:
+        return False
+
+    mime_type_and_subtype = mime_type.split(",")[0].split(";")[0].split("/")
+    if len(mime_type_and_subtype) != 2:
+        return False
+    mime_type, mime_subtype = mime_type_and_subtype
+
+    for bmt in binary_media_types:
+        type_and_subtype = bmt.split(";")[0].split("/")
+        if len(type_and_subtype) != 2:
+            continue
+        _type, subtype = type_and_subtype
+        if _type == "*":
+            continue
+
+        if subtype == "*" and mime_type == _type:
+            return True
+
+        if mime_type == _type and mime_subtype == subtype:
+            return True
+
+    return False
--- localstack-core/localstack/services/apigateway/next_gen/execute_api/integrations/aws.py
+++ localstack-core/localstack/services/apigateway/next_gen/execute_api/integrations/aws.py
@@ -34,6 +34,7 @@ from ..header_utils import build_multi_value_headers
 from ..helpers import (
     get_lambda_function_arn_from_invocation_uri,
     get_source_arn,
+    mime_type_matches_binary_media_types,
     render_uri_with_stage_variables,
     validate_sub_dict_of_typed_dict,
 )
@@ -392,9 +393,20 @@ class RestApiAwsProxyIntegration(RestApiIntegration):
         response_headers = self._merge_lambda_response_headers(lambda_response)
         headers.update(response_headers)
 
+        # TODO: maybe centralize this flag inside the context, when we are also using it for other integration types
+        #  AWS_PROXY behaves a bit differently, but this could checked only once earlier
+        binary_response_accepted = mime_type_matches_binary_media_types(
+            context.invocation_request["headers"].get("Accept"),
+            context.deployment.rest_api.rest_api.get("binaryMediaTypes", []),
+        )
+        body = self._parse_body(
+            body=lambda_response.get("body"),
+            is_base64_encoded=binary_response_accepted and lambda_response.get("isBase64Encoded"),
+        )
+
         return EndpointResponse(
             headers=headers,
-            body=to_bytes(lambda_response.get("body") or ""),
+            body=body,
             status_code=int(lambda_response.get("statusCode") or 200),
         )
 
@@ -551,6 +563,19 @@ class RestApiAwsProxyIntegration(RestApiIntegration):
             return body.decode("utf-8"), False
         except UnicodeDecodeError:
             return to_str(base64.b64encode(body)), True
+
+    @staticmethod
+    def _parse_body(body: str | None, is_base64_encoded: bool) -> bytes:
+        if not body:
+            return b""
+
+        if is_base64_encoded:
+            try:
+                return base64.b64decode(body)
+            except Exception:
+                raise InternalServerError("Internal server error", status_code=500)
+
+        return to_bytes(body)
 
     @staticmethod
     def _merge_lambda_response_headers(lambda_response: LambdaProxyResponse) -> dict:
