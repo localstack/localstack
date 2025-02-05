406ab29df14de56cc84a7a7274b900f3b5d45840
ackdav <6107913+ackdav@users.noreply.github.com>
2025-02-05 10:50:49 +0100

    mock runtime for tests

--- tests/unit/utils/analytics/conftest.py
+++ tests/unit/utils/analytics/conftest.py
@@ -1,9 +1,23 @@
 import pytest
 
 from localstack import config
+from localstack.runtime.current import set_current_runtime
 
 
 @pytest.fixture(autouse=True)
 def enable_analytics(monkeypatch):
     """Makes sure that all tests in this package are executed with analytics enabled."""
     monkeypatch.setattr(config, "DISABLE_EVENTS", False)
+
+class MockComponents:
+    name = "dummy-product"
+
+class MockRuntime:
+    components = MockComponents()
+
+@pytest.fixture(autouse=True)
+def mock_runtime():
+    runtime = MockRuntime()
+    set_current_runtime(runtime)
+    yield
+    set_current_runtime(None)
