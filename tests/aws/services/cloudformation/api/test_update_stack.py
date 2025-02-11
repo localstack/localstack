f6abf33337b7bdf1bc5e17a5ff968af3212077fd
Simon Walker <s.r.walker101@googlemail.com>
2025-02-11 16:46:55 +0000

    Add fixture and test to capture update process

--- tests/aws/services/cloudformation/api/test_update_stack.py
+++ tests/aws/services/cloudformation/api/test_update_stack.py
@@ -1,15 +1,38 @@
 import json
 import os
 import textwrap
+from collections import defaultdict
+from typing import Callable
 
 import botocore.errorfactory
 import botocore.exceptions
 import pytest
 
+from localstack.aws.api.cloudformation import StackEvent
+from localstack.aws.connect import ServiceLevelClientFactory
 from localstack.testing.pytest import markers
 from localstack.utils.files import load_file
 from localstack.utils.strings import short_uid
 from localstack.utils.testutil import upload_file_to_bucket
+
+PerResourceStackEvents = dict[str, list[StackEvent]]
+
+
+@pytest.fixture
+def capture_per_resource_events(
+    aws_client: ServiceLevelClientFactory,
+) -> Callable[[str], PerResourceStackEvents]:
+    def capture(stack_name: str) -> PerResourceStackEvents:
+        events = aws_client.cloudformation.describe_stack_events(StackName=stack_name)[
+            "StackEvents"
+        ]
+        per_resource_events = defaultdict(list)
+        for event in events:
+            if logical_resource_id := event.get("LogicalResourceId"):
+                per_resource_events[logical_resource_id].append(event)
+        return per_resource_events
+
+    return capture
 
 
 @markers.aws.validated
@@ -452,3 +475,134 @@ def test_diff_after_update(deploy_cfn_template, aws_client, snapshot):
 
     describe_stack_response = aws_client.cloudformation.describe_stacks(StackName=stack.stack_name)
     assert describe_stack_response["Stacks"][0]["StackStatus"] == "UPDATE_COMPLETE"
+
+
+@pytest.fixture
+def capture_update_process(aws_client, snapshot, cleanups, capture_per_resource_events):
+    def try_do(f, *args, **kwargs):
+        try:
+            f(*args, **kwargs)
+        except Exception as e:
+            print(f"Error executing function: {e}")
+
+    stack_name = f"stack-{short_uid()}"
+    change_set_name = f"cs-{short_uid()}"
+
+    def inner(t1: dict | str, t2: dict | str):
+        if isinstance(t1, dict):
+            t1 = json.dumps(t1)
+        elif isinstance(t1, str):
+            with open(t1) as infile:
+                t1 = infile.read()
+        if isinstance(t2, dict):
+            t2 = json.dumps(t2)
+        elif isinstance(t2, str):
+            with open(t2) as infile:
+                t2 = infile.read()
+
+        # deploy original stack
+        change_set_details = aws_client.cloudformation.create_change_set(
+            StackName=stack_name,
+            ChangeSetName=change_set_name,
+            TemplateBody=t1,
+            ChangeSetType="CREATE",
+        )
+        snapshot.match("create-change-set-1", change_set_details)
+        stack_id = change_set_details["StackId"]
+        change_set_id = change_set_details["Id"]
+        aws_client.cloudformation.get_waiter("change_set_create_complete").wait(
+            ChangeSetName=change_set_id
+        )
+        cleanups.append(
+            lambda: try_do(aws_client.cloudformation.delete_change_set, ChangeSetName=change_set_id)
+        )
+
+        describe_change_set_with_prop_values = aws_client.cloudformation.describe_change_set(
+            ChangeSetName=change_set_id, IncludePropertyValues=True
+        )
+        snapshot.match("describe-change-set-1-prop-values", describe_change_set_with_prop_values)
+        describe_change_set_without_prop_values = aws_client.cloudformation.describe_change_set(
+            ChangeSetName=change_set_id, IncludePropertyValues=False
+        )
+        snapshot.match("describe-change-set-1", describe_change_set_without_prop_values)
+
+        execute_results = aws_client.cloudformation.execute_change_set(ChangeSetName=change_set_id)
+        snapshot.match("execute-change-set-1", execute_results)
+        aws_client.cloudformation.get_waiter("stack_create_complete").wait(StackName=stack_id)
+
+        # ensure stack deletion
+        cleanups.append(lambda: try_do(aws_client.cloudformation.delete_stack(StackName=stack_id)))
+
+        describe = aws_client.cloudformation.describe_stacks(StackName=stack_id)["Stacks"][0]
+        snapshot.match("post-create-1-describe", describe)
+
+        # update stack
+        change_set_details = aws_client.cloudformation.create_change_set(
+            StackName=stack_name,
+            ChangeSetName=change_set_name,
+            TemplateBody=t2,
+            ChangeSetType="UPDATE",
+        )
+        snapshot.match("create-change-set-2", change_set_details)
+        stack_id = change_set_details["StackId"]
+        change_set_id = change_set_details["Id"]
+        aws_client.cloudformation.get_waiter("change_set_create_complete").wait(
+            ChangeSetName=change_set_id
+        )
+
+        describe_change_set_with_prop_values = aws_client.cloudformation.describe_change_set(
+            ChangeSetName=change_set_id, IncludePropertyValues=True
+        )
+        snapshot.match("describe-change-set-2-prop-values", describe_change_set_with_prop_values)
+        describe_change_set_without_prop_values = aws_client.cloudformation.describe_change_set(
+            ChangeSetName=change_set_id, IncludePropertyValues=False
+        )
+        snapshot.match("describe-change-set-2", describe_change_set_without_prop_values)
+
+        execute_results = aws_client.cloudformation.execute_change_set(ChangeSetName=change_set_id)
+        snapshot.match("execute-change-set-2", execute_results)
+        aws_client.cloudformation.get_waiter("stack_update_complete").wait(StackName=stack_id)
+
+        describe = aws_client.cloudformation.describe_stacks(StackName=stack_id)["Stacks"][0]
+        snapshot.match("post-create-2-describe", describe)
+
+        events = capture_per_resource_events(stack_name)
+        snapshot.match("per-resource-events", events)
+
+        # delete stack
+        aws_client.cloudformation.delete_stack(StackName=stack_id)
+        aws_client.cloudformation.get_waiter("stack_delete_complete").wait(StackName=stack_id)
+        describe = aws_client.cloudformation.describe_stacks(StackName=stack_id)["Stacks"][0]
+        snapshot.match("delete-describe", describe)
+
+    yield inner
+
+
+@markers.aws.validated
+def test_capture_update_process(
+    capture_update_process,
+):
+    name1 = f"topic-1-{short_uid()}"
+    name2 = f"topic-2-{short_uid()}"
+    t1 = {
+        "Resources": {
+            "Foo": {
+                "Type": "AWS::SNS::Topic",
+                "Properties": {
+                    "TopicName": name1,
+                },
+            },
+        },
+    }
+    t2 = {
+        "Resources": {
+            "Foo": {
+                "Type": "AWS::SNS::Topic",
+                "Properties": {
+                    "TopicName": name2,
+                },
+            },
+        },
+    }
+
+    capture_update_process(t1, t2)
