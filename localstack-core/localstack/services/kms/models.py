dfae403bc92bd8b83368f9fd6b87aaeb33295aa5
Alejandro Garcia Seco <alejandro.garcia@wallapop.com>
2025-03-11 17:56:22 +0100

    Removed unused attribute.

--- localstack-core/localstack/services/kms/models.py
+++ localstack-core/localstack/services/kms/models.py
@@ -284,7 +284,6 @@ class KmsKey:
         self.crypto_key = KmsCryptoKey(self.metadata.get("KeySpec"), custom_key_material)
         self.rotation_period_in_days = 365
         self.next_rotation_date = None
-        self.on_demand_rotation_start_date = None
 
     def calculate_and_set_arn(self, account_id, region):
         self.metadata["Arn"] = kms_key_arn(self.metadata.get("KeyId"), account_id, region)
