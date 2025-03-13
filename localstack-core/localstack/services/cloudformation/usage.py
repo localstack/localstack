3d81b7d1343d1b4a30171b9aeffeca694e735ef2
Simon Walker <simon.walker@localstack.cloud>
2025-03-13 10:32:09 +0000

    Switch to using new usage counters

--- localstack-core/localstack/services/cloudformation/usage.py
+++ localstack-core/localstack/services/cloudformation/usage.py
@@ -1,4 +1,11 @@
-from localstack.utils.analytics.usage import UsageSetCounter
+from localstack.utils.analytics.metrics import Counter
 
-resource_type = UsageSetCounter("cloudformation:resourcetype")
-missing_resource_types = UsageSetCounter("cloudformation:missingresourcetypes")
+COUNTER_NAMESPACE = "cloudformation"
+
+# XXX the name is resourcetype to maintain backwards compatibility with the previous counters (when using UsageSetCounter)
+resource_types = Counter(namespace=COUNTER_NAMESPACE, name="resourcetype", labels=["resource_type"])
+
+# XXX the name is missingresourcetypes to maintain backwards compatibility with the previous counters (when using UsageSetCounter)
+missing_resource_types = Counter(
+    namespace=COUNTER_NAMESPACE, name="missingresourcetypes", labels=["missing_resource_type"]
+)
