a2d0f1f242c182e78bac1d8f4e1be14baad5f809
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-03-13 18:23:31 +0100

    metrics migration and improve query language transparency

--- localstack-core/localstack/services/stepfunctions/asl/static_analyser/usage_metrics_static_analyser.py
+++ localstack-core/localstack/services/stepfunctions/asl/static_analyser/usage_metrics_static_analyser.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import logging
+from typing import Final
 
 import localstack.services.stepfunctions.usage as UsageMetrics
 from localstack.services.stepfunctions.asl.antlr.runtime.ASLParser import ASLParser
@@ -10,20 +13,36 @@ from localstack.services.stepfunctions.asl.static_analyser.static_analyser impor
 LOG = logging.getLogger(__name__)
 
 
+class QueryLanguage(str):
+    JSONPath = QueryLanguageMode.JSONPath.name
+    JSONata = QueryLanguageMode.JSONata.name
+    Both = "JSONPath+JSONata"
+
+
 class UsageMetricsStaticAnalyser(StaticAnalyser):
     @staticmethod
-    def process(definition: str) -> "UsageMetricsStaticAnalyser":
+    def process(definition: str) -> UsageMetricsStaticAnalyser:
         analyser = UsageMetricsStaticAnalyser()
         try:
+            # Run the static analyser.
             analyser.analyse(definition=definition)
 
-            if analyser.has_jsonata:
-                UsageMetrics.jsonata_create_counter.increment()
+            # Determine which query language is being used in this state machine.
+            query_modes = analyser._query_language_modes
+            if len(query_modes) == 2:
+                language_used = QueryLanguage.Both
+            elif QueryLanguageMode.JSONata in query_modes:
+                language_used = QueryLanguage.JSONata
             else:
-                UsageMetrics.jsonpath_create_counter.increment()
+                language_used = QueryLanguage.JSONPath
 
-            if analyser.has_variable_sampling:
-                UsageMetrics.variables_create_counter.increment()
+            # Determine is the state machine uses the variables feature.
+            uses_variables = analyser._uses_variables
+
+            # Count.
+            UsageMetrics.language_features_counter.labels(
+                query_language=language_used, variables=uses_variables
+            ).increment()
         except Exception as e:
             LOG.warning(
                 "Failed to record Step Functions metrics from static analysis",
@@ -31,26 +50,28 @@ class UsageMetricsStaticAnalyser(StaticAnalyser):
             )
         return analyser
 
+    _query_language_modes: Final[set[QueryLanguageMode]]
+    _uses_variables: bool
+
     def __init__(self):
         super().__init__()
-        self.has_jsonata: bool = False
-        self.has_variable_sampling = False
+        self._query_language_modes = set()
+        self._uses_variables = False
 
     def visitQuery_language_decl(self, ctx: ASLParser.Query_language_declContext):
-        if self.has_jsonata:
+        if len(self._query_language_modes) == 2:
+            # Both query language modes have been confirmed to be in use.
             return
-
         query_language_mode_int = ctx.children[-1].getSymbol().type
         query_language_mode = QueryLanguageMode(value=query_language_mode_int)
-        if query_language_mode == QueryLanguageMode.JSONata:
-            self.has_jsonata = True
+        self._query_language_modes.add(query_language_mode)
 
     def visitString_literal(self, ctx: ASLParser.String_literalContext):
         # Prune everything parsed as a string literal.
         return
 
     def visitString_variable_sample(self, ctx: ASLParser.String_variable_sampleContext):
-        self.has_variable_sampling = True
+        self._uses_variables = True
 
     def visitAssign_decl(self, ctx: ASLParser.Assign_declContext):
-        self.has_variable_sampling = True
+        self._uses_variables = True
