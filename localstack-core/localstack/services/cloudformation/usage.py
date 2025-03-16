9ffd3acf8ed3947b8585302f28cd241175d13569
Simon Walker <simon.walker@localstack.cloud>
2025-03-16 17:30:25 +0000

    Merge two counters into one

--- localstack-core/localstack/services/cloudformation/usage.py
+++ localstack-core/localstack/services/cloudformation/usage.py
@@ -2,10 +2,6 @@ from localstack.utils.analytics.metrics import Counter
 
 COUNTER_NAMESPACE = "cloudformation"
 
-# XXX the name is resourcetype to maintain backwards compatibility with the previous counters (when using UsageSetCounter)
-resource_types = Counter(namespace=COUNTER_NAMESPACE, name="resourcetype", labels=["resource_type"])
-
-# XXX the name is missingresourcetypes to maintain backwards compatibility with the previous counters (when using UsageSetCounter)
-missing_resource_types = Counter(
-    namespace=COUNTER_NAMESPACE, name="missingresourcetypes", labels=["missing_resource_type"]
+resources = Counter(
+    namespace=COUNTER_NAMESPACE, name="resources", labels=["resource_type", "missing"]
 )
