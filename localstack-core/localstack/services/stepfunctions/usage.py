b6c4b8a771dfde3dc4741998cc9b2f71f77c15d7
Marco Edoardo Palma <64580864+MEPalma@users.noreply.github.com>
2025-03-18 08:19:54 +0100

    Step Functions: Migrate Usage Metrics to New Counter Standards (#12389)

--- localstack-core/localstack/services/stepfunctions/usage.py
+++ localstack-core/localstack/services/stepfunctions/usage.py
@@ -2,19 +2,11 @@
 Usage reporting for StepFunctions service
 """
 
-from localstack.utils.analytics.usage import UsageCounter
+from localstack.utils.analytics.metrics import Counter
 
-# Count of StepFunctions being created with JSONata QueryLanguage
-jsonata_create_counter = UsageCounter("stepfunctions:jsonata:create")
-
-# Count of StepFunctions being created with JSONPath QueryLanguage
-jsonpath_create_counter = UsageCounter("stepfunctions:jsonpath:create")
-
-# Count of StepFunctions being created that use Variable Sampling or the Assign block
-variables_create_counter = UsageCounter("stepfunctions:variables:create")
-
-# Successful invocations (also including expected error cases in line with AWS behaviour)
-invocation_counter = UsageCounter("stepfunctions:invocation")
-
-# Unexpected errors that we do not account for
-error_counter = UsageCounter("stepfunctions:error")
+# Initialize a counter to record the usage of language features for each state machine.
+language_features_counter = Counter(
+    namespace="stepfunctions",
+    name="language_features_used",
+    labels=["query_language", "uses_variables"],
+)
