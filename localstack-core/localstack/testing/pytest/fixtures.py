3a46145360b0098ba771b1f965024e3b9a1e2a97
Ben Simon Hartung <42031100+bentsku@users.noreply.github.com>
2025-03-19 22:00:20 +0100

    APIGW: fix rendering of $input.body when empty (#12413)
    Co-authored-by: Daniel Fangl <daniel.fangl@localstack.cloud>

--- localstack-core/localstack/testing/pytest/fixtures.py
+++ localstack-core/localstack/testing/pytest/fixtures.py
@@ -2054,30 +2054,33 @@ def sample_stores() -> AccountRegionBundle:
 @pytest.fixture
 def create_rest_apigw(aws_client_factory):
     rest_apis = []
+    retry_boto_config = None
+    if is_aws_cloud():
+        retry_boto_config = botocore.config.Config(
+            # Api gateway can throttle requests pretty heavily. Leading to potentially undeleted apis
+            retries={"max_attempts": 10, "mode": "adaptive"}
+        )
 
     def _create_apigateway_function(**kwargs):
-        region_name = kwargs.pop("region_name", None)
-        client_config = None
-        if is_aws_cloud():
-            client_config = botocore.config.Config(
-                # Api gateway can throttle requests pretty heavily. Leading to potentially undeleted apis
-                retries={"max_attempts": 10, "mode": "adaptive"}
-            )
+        client_region_name = kwargs.pop("region_name", None)
         apigateway_client = aws_client_factory(
-            region_name=region_name, config=client_config
+            region_name=client_region_name, config=retry_boto_config
         ).apigateway
         kwargs.setdefault("name", f"api-{short_uid()}")
 
         response = apigateway_client.create_rest_api(**kwargs)
         api_id = response.get("id")
-        rest_apis.append((api_id, region_name))
+        rest_apis.append((api_id, client_region_name))
 
         return api_id, response.get("name"), response.get("rootResourceId")
 
     yield _create_apigateway_function
 
-    for rest_api_id, region_name in rest_apis:
-        apigateway_client = aws_client_factory(region_name=region_name).apigateway
+    for rest_api_id, _client_region_name in rest_apis:
+        apigateway_client = aws_client_factory(
+            region_name=_client_region_name,
+            config=retry_boto_config,
+        ).apigateway
         # First, retrieve the usage plans associated with the REST API
         usage_plan_ids = []
         usage_plans = apigateway_client.get_usage_plans()
