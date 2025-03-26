1bd4e0af66d9bf1e935e49d76c8b9797eb3ecbbc
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2025-03-26 10:22:03 -0500

    fix port usage

--- localstack-core/localstack/testing/pytest/fixtures.py
+++ localstack-core/localstack/testing/pytest/fixtures.py
@@ -2149,7 +2149,7 @@ def appsync_create_api(aws_client):
 @pytest.fixture
 def assert_host_customisation(monkeypatch):
     initial_host = "localhost"
-    initial_port = config.HostAndPort.port()
+    initial_port = "4566"
 
     localstack_host = "foo.bar"
     monkeypatch.setattr(
