e86e0c4dcddee03cf548d91facbe230f12f8ce8c
Misha Tiurin <650819+tiurin@users.noreply.github.com>
2025-04-03 09:06:46 +0200

    Skip flaky transcribe tests (#12473)

--- tests/aws/services/transcribe/test_transcribe.py
+++ tests/aws/services/transcribe/test_transcribe.py
@@ -138,6 +138,7 @@ class TestTranscribe:
             "$..Error..Code",
         ]
     )
+    @pytest.mark.skip(reason="flaky")
     def test_transcribe_happy_path(self, transcribe_create_job, snapshot, aws_client):
         file_path = os.path.join(BASEDIR, "../../files/en-gb.wav")
         job_name = transcribe_create_job(audio_file=file_path)
@@ -182,6 +183,7 @@ class TestTranscribe:
         ],
     )
     @markers.aws.needs_fixing
+    @pytest.mark.skip(reason="flaky")
     def test_transcribe_supported_media_formats(
         self, transcribe_create_job, media_file, speech, aws_client
     ):
@@ -322,6 +324,7 @@ class TestTranscribe:
             (None, None),  # without output bucket and output key
         ],
     )
+    @pytest.mark.skip(reason="flaky")
     def test_transcribe_start_job(
         self,
         output_bucket,
