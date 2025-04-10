b507eded72d4a84b4ec51ef257166f91fc672858
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-04-10 15:50:56 +0200

    temporarely increase the total max attempts of integrations

--- localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
+++ localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
@@ -10,7 +10,7 @@ from localstack.utils.aws.client_types import ServicePrincipal
 
 _BOTO_CLIENT_CONFIG = config = Config(
     parameter_validation=False,
-    retries={"total_max_attempts": 1},
+    retries={"total_max_attempts": 5},
     connect_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
     read_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
     tcp_keepalive=True,
