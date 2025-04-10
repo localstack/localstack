dbfc57c463e855f831a0695eeb952313339e1cb7
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-04-10 15:56:47 +0200

    comment on temporary adjustment for unblocking

--- localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
+++ localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
@@ -10,6 +10,9 @@ from localstack.utils.aws.client_types import ServicePrincipal
 
 _BOTO_CLIENT_CONFIG = config = Config(
     parameter_validation=False,
+    # Temporary workaround—should be reverted once underlying potential Lambda limitation is resolved.
+    # Increased total boto client retry attempts from 1 to 5 to mitigate transient service issues.
+    # This helps reduce unnecessary state machine retries on non-service-level errors.
     retries={"total_max_attempts": 5},
     connect_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
     read_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
