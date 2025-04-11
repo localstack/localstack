ab2d6a4ec7afd63a42f2343e9ef3108ed14de437
Marco Edoardo Palma <64580864+MEPalma@users.noreply.github.com>
2025-04-11 11:59:44 +0200

    Step Functions: Increase Retry Attempts on Service Integrations for Resilience Against Transient Network Errors (#12512)

--- localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
+++ localstack-core/localstack/services/stepfunctions/asl/utils/boto_client.py
@@ -10,7 +10,10 @@ from localstack.utils.aws.client_types import ServicePrincipal
 
 _BOTO_CLIENT_CONFIG = config = Config(
     parameter_validation=False,
-    retries={"total_max_attempts": 1},
+    # Temporary workaround—should be reverted once underlying potential Lambda limitation is resolved.
+    # Increased total boto client retry attempts from 1 to 5 to mitigate transient service issues.
+    # This helps reduce unnecessary state machine retries on non-service-level errors.
+    retries={"total_max_attempts": 5},
     connect_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
     read_timeout=TimeoutSeconds.DEFAULT_TIMEOUT_SECONDS,
     tcp_keepalive=True,
