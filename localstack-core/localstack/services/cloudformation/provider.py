5458c37ec06996e2d90650e0baf59e8a1fe0ede0
Simon Walker <simon.walker@localstack.cloud>
2025-04-11 17:34:13 +0100

    Add testing commit with single change
    This is so we have some concrete commits to manually test with

--- localstack-core/localstack/services/cloudformation/provider.py
+++ localstack-core/localstack/services/cloudformation/provider.py
@@ -1347,3 +1347,6 @@ class CloudformationProvider(CloudformationApi):
             type_summaries.extend(get_listable_types_summaries(pro_plugin_manager))
 
         return ListTypesOutput(TypeSummaries=type_summaries)
+
+
+foo
