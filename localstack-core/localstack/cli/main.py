a86bbcaf01f6ce5f16f4fe973487bd4e45b07285
Jim Wilkinson <jw2@users.noreply.github.com>
2025-04-15 08:34:23 +0100

    Allow --profile to be specified anywhere on the CLI command line (#12500)
    Prior to this commit, --profile must be specified as a top-level option, e.g. localstack --profile test start.  Now it can be specified at any point, e.g. localstack start --profile test.

--- localstack-core/localstack/cli/main.py
+++ localstack-core/localstack/cli/main.py
@@ -6,9 +6,10 @@ def main():
     os.environ["LOCALSTACK_CLI"] = "1"
 
     # config profiles are the first thing that need to be loaded (especially before localstack.config!)
-    from .profiles import set_profile_from_sys_argv
+    from .profiles import set_and_remove_profile_from_sys_argv
 
-    set_profile_from_sys_argv()
+    # WARNING: This function modifies sys.argv to remove the profile argument.
+    set_and_remove_profile_from_sys_argv()
 
     # initialize CLI plugins
     from .localstack import create_with_plugins
--- tests/unit/cli/test_profiles.py
+++ tests/unit/cli/test_profiles.py
@@ -1,18 +1,148 @@
 import os
 import sys
 
-from localstack.cli.profiles import set_profile_from_sys_argv
+from localstack.cli.profiles import set_and_remove_profile_from_sys_argv
+
+
+def profile_test(monkeypatch, input_args, expected_profile, expected_argv):
+    monkeypatch.setattr(sys, "argv", input_args)
+    monkeypatch.setenv("CONFIG_PROFILE", "")
+    set_and_remove_profile_from_sys_argv()
+    assert os.environ["CONFIG_PROFILE"] == expected_profile
+    assert sys.argv == expected_argv
 
 
 def test_profiles_equals_notation(monkeypatch):
-    monkeypatch.setattr(sys, "argv", ["--profile=non-existing-test-profile"])
-    monkeypatch.setenv("CONFIG_PROFILE", "")
-    set_profile_from_sys_argv()
-    assert os.environ["CONFIG_PROFILE"] == "non-existing-test-profile"
+    profile_test(
+        monkeypatch,
+        input_args=["--profile=non-existing-test-profile"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=[],
+    )
 
 
 def test_profiles_separate_args_notation(monkeypatch):
-    monkeypatch.setattr(sys, "argv", ["--profile", "non-existing-test-profile"])
-    monkeypatch.setenv("CONFIG_PROFILE", "")
-    set_profile_from_sys_argv()
-    assert os.environ["CONFIG_PROFILE"] == "non-existing-test-profile"
+    profile_test(
+        monkeypatch,
+        input_args=["--profile", "non-existing-test-profile"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=[],
+    )
+
+
+def test_p_equals_notation(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=["-p=non-existing-test-profile"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=["-p=non-existing-test-profile"],
+    )
+
+
+def test_p_separate_args_notation(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=["-p", "non-existing-test-profile"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=["-p", "non-existing-test-profile"],
+    )
+
+
+def test_profiles_args_before_and_after(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=["cli", "-D", "--profile=non-existing-test-profile", "start"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=["cli", "-D", "start"],
+    )
+
+
+def test_profiles_args_before_and_after_separate(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=["cli", "-D", "--profile", "non-existing-test-profile", "start"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=["cli", "-D", "start"],
+    )
+
+
+def test_p_args_before_and_after_separate(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=["cli", "-D", "-p", "non-existing-test-profile", "start"],
+        expected_profile="non-existing-test-profile",
+        expected_argv=["cli", "-D", "-p", "non-existing-test-profile", "start"],
+    )
+
+
+def test_profiles_args_multiple(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=[
+            "cli",
+            "--profile",
+            "non-existing-test-profile",
+            "start",
+            "--profile",
+            "another-profile",
+        ],
+        expected_profile="another-profile",
+        expected_argv=["cli", "start"],
+    )
+
+
+def test_p_args_multiple(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=[
+            "cli",
+            "-p",
+            "non-existing-test-profile",
+            "start",
+            "-p",
+            "another-profile",
+        ],
+        expected_profile="non-existing-test-profile",
+        expected_argv=[
+            "cli",
+            "-p",
+            "non-existing-test-profile",
+            "start",
+            "-p",
+            "another-profile",
+        ],
+    )
+
+
+def test_p_and_profile_args(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=[
+            "cli",
+            "-p",
+            "non-existing-test-profile",
+            "start",
+            "--profile",
+            "the_profile",
+            "-p",
+            "another-profile",
+        ],
+        expected_profile="the_profile",
+        expected_argv=[
+            "cli",
+            "-p",
+            "non-existing-test-profile",
+            "start",
+            "-p",
+            "another-profile",
+        ],
+    )
+
+
+def test_trailing_p_argument(monkeypatch):
+    profile_test(
+        monkeypatch,
+        input_args=["cli", "start", "-p"],
+        expected_profile="",
+        expected_argv=["cli", "start", "-p"],
+    )
