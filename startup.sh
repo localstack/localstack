aa50b34cd7c4fecaead9f460283cecbab0dc1594
Daniel Fangl <daniel.fangl@localstack.cloud>
2025-04-23 12:24:55 +0200

    Small usability script changes

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-source .venv/bin/activate
+set -euo pipefail
 
+mkdir -p /tmp/localstack
 rm -rf /tmp/localstack/*.lock
+
+source .venv/bin/activate
 SQS_DISABLE_CLOUDWATCH_METRICS=1 PROVIDER_OVERRIDE_DYNAMODBSTREAMS=v2 DYNAMODB_IN_MEMORY=1 DYNAMODB_LOCAL_PORT=31456 python -m gunicorn --bind 0.0.0.0:4566 -w 10 --threads=10 'localstack.aws.serving.granian:create_app()'
