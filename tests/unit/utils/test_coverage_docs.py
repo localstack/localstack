49dcd934a8983e85f6870a254439884a17434923
Marco Edoardo Palma <64580864+MEPalma@users.noreply.github.com>
2025-04-23 18:16:21 +0200

    Clarify coverage error messages to distinguish license and emulation limits (#12547)

--- tests/unit/utils/test_coverage_docs.py
+++ tests/unit/utils/test_coverage_docs.py
@@ -3,15 +3,17 @@ from localstack.utils.coverage_docs import get_coverage_link_for_service
 
 def test_coverage_link_for_existing_service():
     coverage_link = get_coverage_link_for_service("s3", "random_action")
-    assert (
-        coverage_link
-        == "API action 'random_action' for service 's3' not yet implemented or pro feature - please check https://docs.localstack.cloud/references/coverage/coverage_s3/ for further information"
+    assert coverage_link == (
+        "The API action 'random_action' for service 's3' is either not available in your current "
+        "license plan or has not yet been emulated by LocalStack. "
+        "Please refer to https://docs.localstack.cloud/references/coverage/coverage_s3 for more information."
     )
 
 
 def test_coverage_link_for_non_existing_service():
     coverage_link = get_coverage_link_for_service("dummy_service", "random_action")
-    assert (
-        coverage_link
-        == "API for service 'dummy_service' not yet implemented or pro feature - please check https://docs.localstack.cloud/references/coverage/ for further information"
+    assert coverage_link == (
+        "The API for service 'dummy_service' is either not included in your current license plan or "
+        "has not yet been emulated by LocalStack. "
+        "Please refer to https://docs.localstack.cloud/references/coverage for more details."
     )
