8389ab3c033a290b9818acac591c63e3ca1463af
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-04-23 09:51:24 +0200

    api coverage errors

--- localstack-core/localstack/utils/coverage_docs.py
+++ localstack-core/localstack/utils/coverage_docs.py
@@ -1,8 +1,4 @@
-COVERAGE_LINK_BASE = "https://docs.localstack.cloud/references/coverage/"
-MESSAGE_TEMPLATE = (
-    f"API %sfor service '%s' not yet implemented or pro feature"
-    f" - please check {COVERAGE_LINK_BASE}%s for further information"
-)
+_COVERAGE_LINK_BASE = "https://docs.localstack.cloud/references/coverage/"
 
 
 def get_coverage_link_for_service(service_name: str, action_name: str) -> str:
@@ -11,11 +7,14 @@ def get_coverage_link_for_service(service_name: str, action_name: str) -> str:
     available_services = SERVICE_PLUGINS.list_available()
 
     if service_name not in available_services:
-        return MESSAGE_TEMPLATE % ("", service_name, "")
-
+        return (
+            f"The API for service '{service_name}' is either not included in your current license plan "
+            "or has not yet been emulated by LocalStack. "
+            f"Please refer to {_COVERAGE_LINK_BASE} for more details."
+        )
     else:
-        return MESSAGE_TEMPLATE % (
-            f"action '{action_name}' ",
-            service_name,
-            f"coverage_{service_name}/",
+        return (
+            f"The API action '{action_name}' for service '{service_name}' is either not available in "
+            "your current license plan or has not yet been emulated by LocalStack. "
+            f"Please refer to {_COVERAGE_LINK_BASE} for more information."
         )
