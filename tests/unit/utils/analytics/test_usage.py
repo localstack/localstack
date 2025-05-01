587e5f3f26b6dfe2975415fb65bd7ce8922e9e72
Vittorio Polverino <vittoriopolverino91@gmail.com>
2025-05-01 20:44:43 +0200

    refactor(analytics): remove usage.py module and related unit tests

--- tests/unit/utils/analytics/test_usage.py
+++
@@ -1,9 +0,0 @@
-from localstack.utils.analytics.usage import UsageSetCounter
-
-
-def test_set_counter():
-    my_feature_counter = UsageSetCounter("lambda:runtime")
-    my_feature_counter.record("python3.7")
-    my_feature_counter.record("nodejs16.x")
-    my_feature_counter.record("nodejs16.x")
-    assert my_feature_counter.aggregate() == {"python3.7": 1, "nodejs16.x": 2}
