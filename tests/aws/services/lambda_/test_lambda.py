6c0fdcd65c5f31747a38616a68759505410a6198
Daniel Fangl <daniel.fangl@localstack.cloud>
2025-05-07 13:30:55 +0200

    Add tests for provisioned concurrency created on an alias

--- tests/aws/services/lambda_/test_lambda.py
+++ tests/aws/services/lambda_/test_lambda.py
@@ -2540,6 +2540,57 @@ class TestLambdaConcurrency:
         assert result2 == "on-demand"
 
     @markers.aws.validated
+    def test_provisioned_concurrency_on_alias(self, create_lambda_function, snapshot, aws_client):
+        """
+        Tests provisioned concurrency created and invoked using an alias
+        """
+        min_concurrent_executions = 10 + 5
+        check_concurrency_quota(aws_client, min_concurrent_executions)
+
+        func_name = f"test_lambda_{short_uid()}"
+        alias_name = "live"
+
+        create_lambda_function(
+            func_name=func_name,
+            handler_file=TEST_LAMBDA_INVOCATION_TYPE,
+            runtime=Runtime.python3_12,
+            client=aws_client.lambda_,
+        )
+
+        v1 = aws_client.lambda_.publish_version(FunctionName=func_name)
+        aws_client.lambda_.create_alias(
+            FunctionName=func_name, Name=alias_name, FunctionVersion=v1["Version"]
+        )
+
+        put_provisioned = aws_client.lambda_.put_provisioned_concurrency_config(
+            FunctionName=func_name, Qualifier=alias_name, ProvisionedConcurrentExecutions=5
+        )
+        snapshot.match("put_provisioned_5", put_provisioned)
+
+        get_provisioned_prewait = aws_client.lambda_.get_provisioned_concurrency_config(
+            FunctionName=func_name, Qualifier=alias_name
+        )
+
+        snapshot.match("get_provisioned_prewait", get_provisioned_prewait)
+        assert wait_until(concurrency_update_done(aws_client.lambda_, func_name, alias_name))
+        get_provisioned_postwait = aws_client.lambda_.get_provisioned_concurrency_config(
+            FunctionName=func_name, Qualifier=alias_name
+        )
+        snapshot.match("get_provisioned_postwait", get_provisioned_postwait)
+
+        invoke_result1 = aws_client.lambda_.invoke(FunctionName=func_name, Qualifier=alias_name)
+        result1 = json.load(invoke_result1["Payload"])
+        assert result1 == "provisioned-concurrency"
+
+        invoke_result1 = aws_client.lambda_.invoke(FunctionName=func_name, Qualifier=v1["Version"])
+        result1 = json.load(invoke_result1["Payload"])
+        assert result1 == "provisioned-concurrency"
+
+        invoke_result2 = aws_client.lambda_.invoke(FunctionName=func_name, Qualifier="$LATEST")
+        result2 = json.load(invoke_result2["Payload"])
+        assert result2 == "on-demand"
+
+    @markers.aws.validated
     def test_lambda_provisioned_concurrency_scheduling(
         self, snapshot, create_lambda_function, aws_client
     ):
