2bfc1357fda2d9c3147687d9a52a0f240fd8a0de
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-05-20 18:08:51 +0200

    base support for fn sub

--- tests/aws/services/cloudformation/v2/ported_from_v1/resources/test_apigateway.py
+++ tests/aws/services/cloudformation/v2/ported_from_v1/resources/test_apigateway.py
@@ -553,7 +553,7 @@ def test_api_gateway_with_policy_as_dict(deploy_cfn_template, snapshot, aws_clie
     snapshot.match("rest-api", rest_api)
 
 
-@pytest.mark.skip(reason="No support for Fn::Sub")
+@pytest.mark.skip(reason="No resource provider found for AWS::Serverless::Api")
 @markers.aws.validated
 @markers.snapshot.skip_snapshot_verify(
     paths=[
