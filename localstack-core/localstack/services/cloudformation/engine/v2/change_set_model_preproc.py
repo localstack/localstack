75d12d6d31fe9fe6aab88d28f0e6e1518b3db4b6
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-05-26 16:59:06 +0200

    base support for fn::transform

--- localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
+++ localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
@@ -3,6 +3,11 @@ from __future__ import annotations
 import re
 from typing import Any, Final, Generic, Optional, TypeVar
 
+from localstack.services.cloudformation.engine.transformers import (
+    Transformer,
+    execute_macro,
+    transformers,
+)
 from localstack.services.cloudformation.engine.v2.change_set_model import (
     ChangeSetEntity,
     ChangeType,
@@ -30,6 +35,7 @@ from localstack.services.cloudformation.engine.v2.change_set_model import (
 from localstack.services.cloudformation.engine.v2.change_set_model_visitor import (
     ChangeSetModelVisitor,
 )
+from localstack.services.cloudformation.stores import get_cloudformation_store
 from localstack.services.cloudformation.v2.entities import ChangeSet
 from localstack.utils.aws.arns import get_partition
 from localstack.utils.urls import localstack_host
@@ -487,6 +493,78 @@ class ChangeSetModelPreproc(ChangeSetModelVisitor):
         else:
             after = None
         # Implicit change type computation.
+        return PreprocEntityDelta(before=before, after=after)
+
+    def _compute_fn_transform(self, args: dict[str, Any]) -> Any:
+        # TODO: add typing to arguments before this level.
+        # TODO: add schema validation
+        # TODO: add support for other transform types
+
+        account_id = self._change_set.account_id
+        region_name = self._change_set.region_name
+        transform_name: str = args.get("Name")
+        if not isinstance(transform_name, str):
+            raise RuntimeError("Invalid or missing Fn::Transform 'Name' argument")
+        transform_parameters: dict = args.get("Parameters")
+        if not isinstance(transform_parameters, dict):
+            raise RuntimeError("Invalid or missing Fn::Transform 'Parameters' argument")
+
+        if transform_name in transformers:
+            # TODO: port and refactor this 'transformers' logic to this package.
+            builtin_transformer_class = transformers[transform_name]
+            builtin_transformer: Transformer = builtin_transformer_class()
+            transform_output: Any = builtin_transformer.transform(
+                account_id=account_id, region_name=region_name, parameters=transform_parameters
+            )
+            return transform_output
+
+        macros_store = get_cloudformation_store(
+            account_id=account_id, region_name=region_name
+        ).macros
+        if transform_name in macros_store:
+            # TODO: this formatting of stack parameters is odd but required to integrate with v1 execute_macro util.
+            #  consider porting this utils and passing the plain list of parameters instead.
+            stack_parameters = {
+                parameter["ParameterKey"]: parameter
+                for parameter in self._change_set.stack.parameters
+            }
+            transform_output: Any = execute_macro(
+                account_id=account_id,
+                region_name=region_name,
+                parsed_template=dict(),  # TODO: review the requirements for this argument.
+                macro=args,  # TODO: review support for non dict bindings (v1).
+                stack_parameters=stack_parameters,
+                transformation_parameters=transform_parameters,
+                is_intrinsic=True,
+            )
+            return transform_output
+
+        raise RuntimeError(
+            f"Unsupported transform function '{transform_name}' in '{self._change_set.stack.stack_name}'"
+        )
+
+    def visit_node_intrinsic_function_fn_transform(
+        self, node_intrinsic_function: NodeIntrinsicFunction
+    ) -> PreprocEntityDelta:
+        arguments_delta = self.visit(node_intrinsic_function.arguments)
+        arguments_before = arguments_delta.before
+        arguments_after = arguments_delta.after
+
+        # TODO: review the use of cache in self.precessed from the 'before' run to
+        #  ensure changes to the lambda (such as after UpdateFunctionCode) do not
+        #  generalise tot he before value at this depth (thus making it seems as
+        #  though for this transformation before==after). Another options may be to
+        #  have specialised caching for transformations.
+
+        # TODO: add tests to review the behaviour of CFN with changes to transformation
+        #  function code and no changes to the template.
+
+        before = None
+        if arguments_before:
+            before = self._compute_fn_transform(args=arguments_before)
+        after = None
+        if arguments_after:
+            after = self._compute_fn_transform(args=arguments_after)
         return PreprocEntityDelta(before=before, after=after)
 
     def visit_node_intrinsic_function_fn_sub(
