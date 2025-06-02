5f014c9a1aadc40d922c24c7864112894a5894de
MEPalma <64580864+MEPalma@users.noreply.github.com>
2025-06-02 19:45:32 +0200

    missing type hint

--- localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
+++ localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
@@ -709,7 +709,7 @@ class ChangeSetModelPreproc(ChangeSetModelVisitor):
 
     def visit_node_intrinsic_function_fn_get_a_zs(
         self, node_intrinsic_function: NodeIntrinsicFunction
-    ):
+    ) -> PreprocEntityDelta:
         # TODO: add further support for schema validation
         arguments_delta = self.visit(node_intrinsic_function.arguments)
         arguments_before = arguments_delta.before
