76dede227c22d4693ae0577d133ded6344cd6fb6
Victor Martin <victor.martin.santiago@gmail.com>
2025-06-09 15:20:24 +0200

    lint: fix linter errors

--- tests/aws/services/kms/test_kms.py
+++ tests/aws/services/kms/test_kms.py
@@ -911,11 +911,11 @@ class TestKMS:
         assert base64.b64decode(destination_key_plaintext) == message
 
     @markers.aws.validated
-    def test_re_encrypt_incorrect_source_key(self, kms_create_key, aws_client, snapshot, algo="SYMMETRIC_DEFAULT"):
+    def test_re_encrypt_incorrect_source_key(
+        self, kms_create_key, aws_client, snapshot, algo="SYMMETRIC_DEFAULT"
+    ):
         message = b"test message 123 !%$@ 1234567890"
-        source_key_id = kms_create_key(KeyUsage="ENCRYPT_DECRYPT", KeySpec=algo)[
-            "KeyId"
-        ]
+        source_key_id = kms_create_key(KeyUsage="ENCRYPT_DECRYPT", KeySpec=algo)["KeyId"]
         ciphertext = aws_client.kms.encrypt(
             KeyId=source_key_id, Plaintext=base64.b64encode(message), EncryptionAlgorithm=algo
         )["CiphertextBlob"]
