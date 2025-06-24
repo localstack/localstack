ed54458dd0949a8077df6dcaceb2ffa53632ead9
ArthurAkh <48313237+ArthurAkh@users.noreply.github.com>
2025-06-24 22:07:20 +0200

    refactor

--- localstack-core/localstack/services/apigateway/legacy/provider.py
+++ localstack-core/localstack/services/apigateway/legacy/provider.py
@@ -2064,31 +2064,33 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
             raise ValidationException(
                 "1 validation error detected: Value 'wrong' at 'statusCode' failed to satisfy constraint: Member must satisfy regular expression pattern: [1-5]\\d\\d"
             )
-
         try:
             moto_rest_api = get_moto_rest_api(context, rest_api_id)
         except NotFoundException:
             raise NotFoundException("Invalid Resource identifier specified")
+
         if not (moto_resource := moto_rest_api.resources.get(resource_id)):
             raise NotFoundException("Invalid Resource identifier specified")
 
         if not (moto_method := moto_resource.resource_methods.get(http_method)):
             raise NotFoundException("Invalid Method identifier specified")
+
         integration_response = None
-        try:
-            integration_response = moto_method.method_integration.integration_responses.get(status_code)
+        if integration_responses := moto_method.method_integration.integration_responses:
+            integration_response = integration_responses.get(status_code)
             if not integration_response:
                 raise NotFoundException("Invalid Integration Response identifier specified")
-        except NotFoundException:
-            pass
 
         response: IntegrationResponse = call_moto(context)
         remove_empty_attributes_from_integration_response(response)
         # moto does not return selectionPattern is set to an empty string
         # TODO: fix upstream
-        if "selectionPattern" not in response:
-            if integration_response is not None and integration_response.selection_pattern is not None:
-                response["selectionPattern"] = integration_response.selection_pattern
+        if (
+            "selectionPattern" not in response
+            and integration_response
+            and integration_response.selection_pattern is not None
+        ):
+            response["selectionPattern"] = integration_response.selection_pattern
         return response
 
     @handler("PutIntegrationResponse", expand=False)
@@ -2102,7 +2104,6 @@ class ApigatewayProvider(ApigatewayApi, ServiceLifecycleHook):
             raise ValidationException(
                 "1 validation error detected: Value 'wrong' at 'statusCode' failed to satisfy constraint: Member must satisfy regular expression pattern: [1-5]\\d\\d"
             )
-
         try:
             # put integration response doesn't return the right exception compared to AWS
             moto_rest_api = get_moto_rest_api(context=context, rest_api_id=request.get("restApiId"))
