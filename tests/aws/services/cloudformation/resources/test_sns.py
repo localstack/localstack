090ae348c576394283c30085c544911a19c6db80
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2025-07-31 13:20:14 -0500

    move test_sns

--- tests/aws/services/cloudformation/resources/test_sns.py
+++ tests/aws/services/cloudformation/resources/test_sns.py
@@ -4,6 +4,7 @@ import aws_cdk as cdk
 import pytest
 from tests.aws.services.cloudformation.conftest import skip_if_v2_provider
 
+from aws.services.cloudformation.conftest import skip_if_v2_provider
 from localstack.testing.aws.util import is_aws_cloud
 from localstack.testing.pytest import markers
 from localstack.utils.aws.arns import parse_arn
@@ -80,6 +81,7 @@ def test_sns_subscription(deploy_cfn_template, aws_client):
     assert len(subscriptions["Subscriptions"]) > 0
 
 
+skip_if_v2_provider(reason="CFNV2:Engine")
 @skip_if_v2_provider(reason="CFNV2:Engine")
 @markers.aws.validated
 def test_deploy_stack_with_sns_topic(deploy_cfn_template, aws_client):
