41d7a49f8bb0834a4559407341eb46162cdc3cbf
Silvio Vasiljevic <silvio.vasiljevic@gmail.com>
2025-08-04 13:57:51 +0200

    Use analytics.log.event

--- localstack-core/localstack/utils/analytics/service_providers.py
+++ localstack-core/localstack/utils/analytics/service_providers.py
@@ -6,25 +6,14 @@ def publish_provider_assignment():
     """
     Publishes the service provider assignment to the analytics service.
     """
-    from datetime import datetime
 
     from localstack.config import SERVICE_PROVIDER_CONFIG
     from localstack.services.plugins import SERVICE_PLUGINS
-    from localstack.utils.analytics import get_session_id
-    from localstack.utils.analytics.events import Event, EventMetadata
-    from localstack.utils.analytics.publisher import AnalyticsClientPublisher
+    from localstack.utils.analytics import log
 
     provider_assignment = {
         service: f"localstack.aws.provider/{service}:{SERVICE_PROVIDER_CONFIG[service]}"
         for service in SERVICE_PLUGINS.list_available()
     }
-    metadata = EventMetadata(
-        session_id=get_session_id(),
-        client_time=str(datetime.now()),
-    )
 
-    event = Event(
-        name="ls_service_provider_assignment", metadata=metadata, payload=provider_assignment
-    )
-
-    AnalyticsClientPublisher().publish([event])
+    log.event("ls_service_provider_assignment", provider_assignment)
