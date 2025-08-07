8f67d570dadabf7ca9b8a9ee0c1f582c43937d94
Simon Walker <simon.walker@localstack.cloud>
2025-08-07 14:48:08 +0100

    Correct use of visiting condition value

--- localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
+++ localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
@@ -554,16 +554,6 @@ class ChangeSetModelPreproc(ChangeSetModelVisitor):
     def visit_node_intrinsic_function_fn_if(
         self, node_intrinsic_function: NodeIntrinsicFunction
     ) -> PreprocEntityDelta:
-        def _compute_delta_for_if_statement(condition_name: str) -> PreprocEntityDelta:
-            node_condition = self._get_node_condition_if_exists(condition_name=condition_name)
-            condition_value = self.visit(node_condition).before
-            if condition_value:
-                arg_delta = self.visit(node_intrinsic_function.arguments.array[1])
-            else:
-                arg_delta = self.visit(node_intrinsic_function.arguments.array[2])
-
-            return arg_delta
-
         # `if` needs to be short-circuiting i.e. if the condition is True we don't evaluate the
         # False branch. If the condition is False, we don't evaluate the True branch.
         if len(node_intrinsic_function.arguments.array) != 3:
@@ -574,11 +564,25 @@ class ChangeSetModelPreproc(ChangeSetModelVisitor):
         condition_delta = self.visit(node_intrinsic_function.arguments.array[0])
         if_delta = PreprocEntityDelta()
         if not is_nothing(condition_delta.before):
-            arg_delta = _compute_delta_for_if_statement(condition_delta.before)
+            node_condition = self._get_node_condition_if_exists(
+                condition_name=condition_delta.before
+            )
+            condition_value = self.visit(node_condition).before
+            if condition_value:
+                arg_delta = self.visit(node_intrinsic_function.arguments.array[1])
+            else:
+                arg_delta = self.visit(node_intrinsic_function.arguments.array[2])
             if_delta.before = arg_delta.before
 
         if not is_nothing(condition_delta.after):
-            arg_delta = _compute_delta_for_if_statement(condition_delta.after)
+            node_condition = self._get_node_condition_if_exists(
+                condition_name=condition_delta.after
+            )
+            condition_value = self.visit(node_condition).after
+            if condition_value:
+                arg_delta = self.visit(node_intrinsic_function.arguments.array[1])
+            else:
+                arg_delta = self.visit(node_intrinsic_function.arguments.array[2])
             if_delta.after = arg_delta.after
 
         return if_delta
