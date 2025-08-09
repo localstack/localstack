7ea6b104dd120b6131cab8f305630c51d71f35d1
Simon Walker <simon.walker@localstack.cloud>
2025-08-09 15:12:08 +0100

    Add reporter line in status

--- tests/aws/services/cloudformation/conftest.py
+++ tests/aws/services/cloudformation/conftest.py
@@ -27,3 +27,14 @@ _T = TypeVar("_T")
 
 def skipped_v2_items(*items: Iterable[_T]) -> list[_T]:
     return optional_list(is_v2_engine(), items)
+
+
+def pytest_report_collectionfinish(config, start_path, startdir, items):
+    v1_items, v2_items = [], []
+    for item in items:
+        if skip := item.get_closest_marker("skipif"):
+            if "CFNV2" in skip.kwargs.get("reason"):
+                v2_items.append(item.nodeid)
+            elif "CFNV1" in skip.kwargs.get("reason"):
+                v1_items.append(item.nodeid)
+    return f"CFNV2: skipped {len(v1_items)} v1 skips and {len(v2_items)} v2 skips"
