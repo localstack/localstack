fc59d6b39590502543897f7a8f19d3cf6fe73982
Ben Simon Hartung <42031100+bentsku@users.noreply.github.com>
2025-08-19 17:32:47 +0200

    S3/ASF parser: fix parsing header `list` shape type with multivalue headers (#13022)

--- localstack-core/localstack/aws/protocol/parser.py
+++ localstack-core/localstack/aws/protocol/parser.py
@@ -234,13 +234,21 @@ class RequestParser(abc.ABC):
         if location is not None:
             if location == "header":
                 header_name = shape.serialization.get("name")
-                payload = request.headers.get(header_name)
-                if payload and shape.type_name == "list":
+                if shape.type_name == "list":
                     # headers may contain a comma separated list of values (e.g., the ObjectAttributes member in
                     # s3.GetObjectAttributes), so we prepare it here for the handler, which will be `_parse_list`.
                     # Header lists can contain optional whitespace, so we strip it
                     # https://www.rfc-editor.org/rfc/rfc9110.html#name-lists-rule-abnf-extension
-                    payload = [value.strip() for value in payload.split(",")]
+                    # It can also directly contain a list of headers
+                    # See https://datatracker.ietf.org/doc/html/rfc2616
+                    payload = request.headers.getlist(header_name) or None
+                    if payload:
+                        headers = ",".join(payload)
+                        payload = [value.strip() for value in headers.split(",")]
+
+                else:
+                    payload = request.headers.get(header_name)
+
             elif location == "headers":
                 payload = self._parse_header_map(shape, request.headers)
                 # shapes with the location trait "headers" only contain strings and are not further processed
