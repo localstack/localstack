b297c40f27ae742553dbbec92db7783bae9e1d81
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2025-08-20 18:43:37 -0500

    detect custom type

--- localstack-core/localstack/services/cloudformation/resource_provider.py
+++ localstack-core/localstack/services/cloudformation/resource_provider.py
@@ -563,6 +563,8 @@ class ResourceProviderExecutor:
     @staticmethod
     def try_load_resource_provider(resource_type: str) -> ResourceProvider | None:
         # TODO: unify namespace of plugins
+        if resource_type.startswith("Custom"):
+            resource_type = "AWS::CloudFormation::CustomResource"
 
         # 1. try to load pro resource provider
         # prioritise pro resource providers
