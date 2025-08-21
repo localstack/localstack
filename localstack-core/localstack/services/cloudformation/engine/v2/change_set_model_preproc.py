0221c45ad4a102d6cefc1a656167c5ffe04bd43f
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2025-08-21 17:43:13 -0500

    add port to aws suffix

--- localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
+++ localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
@@ -60,7 +60,7 @@ from localstack.utils.run import to_str
 from localstack.utils.strings import to_bytes
 from localstack.utils.urls import localstack_host
 
-_AWS_URL_SUFFIX = localstack_host().host  # The value in AWS is "amazonaws.com"
+_AWS_URL_SUFFIX = localstack_host().host_and_port()  # The value in AWS is "amazonaws.com"
 
 _PSEUDO_PARAMETERS: Final[set[str]] = {
     "AWS::Partition",
