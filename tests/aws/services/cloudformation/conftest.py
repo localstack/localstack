7e45ad4525c56d499963dc1e7155b1572fb4bd4f
Simon Walker <simon.walker@localstack.cloud>
2025-09-04 21:44:16 +0100

    Remove cfnv2 skip report

--- tests/aws/services/cloudformation/conftest.py
+++ tests/aws/services/cloudformation/conftest.py
@@ -1,5 +1,4 @@
 import re
-from collections import Counter
 from collections.abc import Iterable
 from typing import TypeVar
 
@@ -23,27 +22,3 @@ _T = TypeVar("_T")
 
 def skipped_v2_items(*items: Iterable[_T]) -> list[_T]:
     return optional_list(is_v2_engine(), items)
-
-
-def pytest_report_collectionfinish(config, start_path, startdir, items):
-    if not is_v2_engine():
-        return
-
-    v1_items, v2_skip_types = [], []
-    for item in items:
-        if skip := item.get_closest_marker("skipif"):
-            if reason := skip.kwargs.get("reason"):
-                if "CFNV2" in reason:
-                    if match := SKIP_TYPE_RE.match(reason):
-                        v2_skip_types.append(match.group("reason"))
-                    else:
-                        v2_skip_types.append("Other")
-                elif "CFNV1" in reason:
-                    v1_items.append(item.nodeid)
-    header = f"CFNV2: skipped {len(v1_items)} v1 skips and {len(v2_skip_types)} v2 skips"
-    types_count_parts = []
-    for skip_type, count in sorted(
-        Counter(v2_skip_types).items(), key=lambda e: e[1], reverse=True
-    ):
-        types_count_parts.append("- " + ": ".join([f"CFNV2({skip_type})", str(count)]))
-    return [header] + types_count_parts
