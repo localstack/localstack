cd98bf91705335840ccb878b3d89121e969fe459
Benjamin Simon <benjh.simon@gmail.com>
2025-09-10 22:17:32 +0200

    implement RpcV2CBORRequestParser

--- localstack-core/localstack/aws/protocol/parser.py
+++ localstack-core/localstack/aws/protocol/parser.py
@@ -1245,6 +1245,113 @@ class CBORRequestParser(BaseCBORRequestParser, JSONRequestParser):
         return super()._parse_timestamp(request, shape, node, uri_params)
 
 
+class BaseRpcV2RequestParser(RequestParser):
+    def __init__(self, service: ServiceModel) -> None:
+        super().__init__(service)
+        # self.ignore_get_body_errors = False
+        self._operation_router = RestServiceOperationRouter(service)
+
+    @_handle_exceptions
+    def parse(self, request: Request) -> tuple[OperationModel, Any]:
+        # TODO: add this special path handling to the ServiceNameParser to allow RPC v2 service to be properly extracted
+        #  path = '/service/{service_name}/operation/{operation_name}'
+        rpc_v2_params = request.path.lstrip("/").split("/")
+        if len(rpc_v2_params) != 4 or not (
+            operation := self.service.operation_model(rpc_v2_params[-1])
+        ):
+            raise OperationNotFoundParserError(
+                f"Unable to find operation for request to service "
+                f"{self.service.service_name}: {request.method} {request.path}"
+            )
+
+        # there are no URI params in RPC v2
+        uri_params = {}
+        shape: StructureShape = operation.input_shape
+        final_parsed = self._do_parse(request, shape, uri_params)
+        return operation, final_parsed
+
+    @_handle_exceptions
+    def _do_parse(
+        self, request: Request, shape: Shape, uri_params: Mapping[str, Any] = None
+    ) -> dict[str, Any]:
+        parsed = {}
+        if shape is not None:
+            event_stream_name = shape.event_stream_name
+            if event_stream_name:
+                parsed = self._handle_event_stream(request, shape, event_stream_name)
+            else:
+                parsed = {}
+                self._parse_payload(request, shape, parsed, uri_params)
+
+        return parsed
+
+    def _handle_event_stream(self, request: Request, shape: Shape, event_name: str):
+        # TODO handle event streams
+        raise NotImplementedError
+
+    def _parse_structure(
+        self,
+        request: Request,
+        shape: StructureShape,
+        node: dict | None,
+        uri_params: Mapping[str, Any] = None,
+    ):
+        if shape.is_document_type:
+            final_parsed = node
+        else:
+            if node is None:
+                # If the comes across the wire as "null" (None in python),
+                # we should be returning this unchanged, instead of as an
+                # empty dict.
+                return None
+            final_parsed = {}
+            members = shape.members
+            if shape.is_tagged_union:
+                cleaned_value = node.copy()
+                cleaned_value.pop("__type", None)
+                cleaned_value = {k: v for k, v in cleaned_value.items() if v is not None}
+                if len(cleaned_value) != 1:
+                    raise ProtocolParserError(
+                        f"Invalid service response: {shape.name} must have one and only one member set."
+                    )
+
+            for member_name, member_shape in members.items():
+                member_value = node.get(member_name)
+                if member_value is not None:
+                    final_parsed[member_name] = self._parse_shape(
+                        request, member_shape, member_value, uri_params
+                    )
+
+        return final_parsed
+
+    def _parse_payload(
+        self,
+        request: Request,
+        shape: Shape,
+        final_parsed: dict,
+        uri_params: Mapping[str, Any] = None,
+    ) -> None:
+        original_parsed = self._initial_body_parse(request)
+        body_parsed = self._parse_shape(request, shape, original_parsed, uri_params)
+        final_parsed.update(body_parsed)
+
+    def _initial_body_parse(self, request: Request):
+        # This method should do the initial parsing of the
+        # body.  We still need to walk the parsed body in order
+        # to convert types, but this method will do the first round
+        # of parsing.
+        raise NotImplementedError("_initial_body_parse")
+
+
+class RpcV2CBORRequestParser(BaseRpcV2RequestParser, BaseCBORRequestParser):
+    def _initial_body_parse(self, request: Request):
+        body_contents = request.data
+        if body_contents == b"":
+            return body_contents
+        body_contents_stream = self.get_peekable_stream_from_bytes(body_contents)
+        return self.parse_data_item(body_contents_stream)
+
+
 class EC2RequestParser(QueryRequestParser):
     """
     The ``EC2RequestParser`` is responsible for parsing incoming requests for services which use the ``ec2``
@@ -1446,6 +1553,7 @@ def create_parser(service: ServiceModel, protocol: ProtocolName | None = None) -
         "rest-json": RestJSONRequestParser,
         "rest-xml": RestXMLRequestParser,
         "ec2": EC2RequestParser,
+        "smithy-rpc-v2-cbor": RpcV2CBORRequestParser,
         # TODO: implement multi-protocol support for Kinesis, so that it can uses the `cbor` protocol and remove
         #  CBOR handling from JSONRequestParser
         # this is not an "official" protocol defined from the spec, but is derived from ``json``
