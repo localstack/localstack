7b399f01642479715b4870d73e3503fb9bc2647a
Viren Nadkarni <viren.nadkarni@gmail.com>
2025-09-11 17:13:09 +0530

    Add unit test

--- tests/unit/test_moto.py
+++ tests/unit/test_moto.py
@@ -1,6 +1,8 @@
 import pytest
+from moto.core.exceptions import ServiceException
 
-from localstack.services.moto import get_dispatcher
+from localstack.aws.api import CommonServiceException
+from localstack.services.moto import ServiceExceptionTranslator, get_dispatcher
 
 
 def test_get_dispatcher_for_path_with_optional_slashes():
@@ -11,3 +13,22 @@ def test_get_dispatcher_for_path_with_optional_slashes():
 def test_get_dispatcher_for_non_existing_path_raises_not_implemented():
     with pytest.raises(NotImplementedError):
         get_dispatcher("route53", "/non-existing")
+
+
+def test_service_exception_translator_context_manager():
+    class WeirdException(ServiceException):
+        code = "WeirdErrorCode"
+
+    # Ensure Moto ServiceExceptions are translated to ASF CommonServiceException
+    with pytest.raises(CommonServiceException) as exc:
+        with ServiceExceptionTranslator():
+            raise WeirdException()
+    assert exc.value.code == "WeirdErrorCode"
+
+    # Ensure other exceptions are not affected
+    with pytest.raises(RuntimeError):
+        raise RuntimeError()
+
+    with pytest.raises(RuntimeError):
+        with ServiceExceptionTranslator():
+            raise RuntimeError()
