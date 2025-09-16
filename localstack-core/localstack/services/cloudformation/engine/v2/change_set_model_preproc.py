42d9d15e1f80c40022ec505e6dc785d37c7b9283
Simon Walker <simon.walker@localstack.cloud>
2025-09-16 11:03:21 +0100

    Add fix for pro test

--- localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
+++ localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_preproc.py
@@ -844,21 +844,20 @@ class ChangeSetModelPreproc(ChangeSetModelVisitor):
         # TODO: add further support for schema validation
         def _compute_fn_select(args: list[Any]) -> Any:
             values = args[1]
+            # defer evaluation if the selection list contains unresolved elements (e.g., unresolved intrinsics)
+            if isinstance(values, list) and not all(isinstance(value, str) for value in values):
+                raise RuntimeError("Fn::Select list contains unresolved elements")
+
             if not isinstance(values, list) or not values:
                 raise ValidationError(
                     "Template error: Fn::Select requires a list argument with two elements: an integer index and a list"
                 )
-            values_len = len(values)
             try:
                 index: int = int(args[0])
             except ValueError as e:
                 raise ValidationError(
                     "Template error: Fn::Select requires a list argument with two elements: an integer index and a list"
                 ) from e
-
-            # defer evaluation if the selection list contains unresolved elements (e.g., unresolved intrinsics)
-            if any(v is None for v in values):
-                raise RuntimeError("Fn::Select list contains unresolved elements")
 
             values_len = len(values)
             if index < 0 or index >= values_len:
