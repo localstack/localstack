698ab308d03b2af1c4e7adc5ede1330d91fb27b1
Simon Walker <simon.walker@localstack.cloud>
2025-09-19 15:14:18 +0100

    WIP

--- localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_static_preproc.py
+++ localstack-core/localstack/services/cloudformation/engine/v2/change_set_model_static_preproc.py
@@ -30,7 +30,6 @@ from localstack.services.cloudformation.engine.v2.change_set_model import (
     NodeProperties,
     NodeProperty,
     NodeResource,
-    NodeTemplate,
     Nothing,
     NothingType,
     Scope,
@@ -55,7 +54,6 @@ from localstack.services.cloudformation.stores import (
 from localstack.services.cloudformation.v2.entities import ChangeSet
 from localstack.services.cloudformation.v2.types import ResolvedResource
 from localstack.utils.aws.arns import get_partition
-from localstack.utils.objects import get_value_from_path
 from localstack.utils.run import to_str
 from localstack.utils.strings import to_bytes
 from localstack.utils.urls import localstack_host
@@ -83,6 +81,33 @@ REGEX_OUTPUT_APIGATEWAY = re.compile(
 MOCKED_REFERENCE = "unknown"
 
 VALID_LOGICAL_RESOURCE_ID_RE = re.compile(r"^[A-Za-z0-9]+$")
+
+
+class UnComputableType:
+    _singleton = None
+    __slots__ = ()
+
+    def __new__(cls):
+        if cls._singleton is None:
+            cls._singleton = super().__new__(cls)
+        return cls._singleton
+
+    def __str__(self) -> str:
+        return "Uncomputable"
+
+    def __repr__(self) -> str:
+        return f"<{self}>"
+
+    def __bool__(self) -> bool:
+        return False
+
+
+MaybeComputable = _T | UnComputableType
+UnComputable = UnComputableType()
+
+
+def is_computable(value: Any) -> bool:
+    return value is not UnComputable
 
 
 class PreprocEntityDelta(Generic[TBefore, TAfter]):
@@ -227,81 +252,6 @@ class ChangeSetModelStaticPreproc(ChangeSetModelVisitor):
         self.visit(node_template)
         self._save_runtime_cache()
 
-    def _get_node_resource_for(
-        self, resource_name: str, node_template: NodeTemplate
-    ) -> NodeResource:
-        # TODO: this could be improved with hashmap lookups if the Node contained bindings and not lists.
-        for node_resource in node_template.resources.resources:
-            if node_resource.name == resource_name:
-                self.visit(node_resource)
-                return node_resource
-        raise ValidationError(
-            f"Template format error: Unresolved resource dependencies [{resource_name}] in the Resources block of the template"
-        )
-
-    def _get_node_property_for(
-        self, property_name: str, node_resource: NodeResource
-    ) -> NodeProperty | None:
-        # TODO: this could be improved with hashmap lookups if the Node contained bindings and not lists.
-        for node_property in node_resource.properties.properties:
-            if node_property.name == property_name:
-                self.visit(node_property)
-                return node_property
-        return None
-
-    def _deployed_property_value_of(
-        self, resource_logical_id: str, property_name: str, resolved_resources: dict
-    ) -> Any:
-        # We have to override this function to make sure it does not try to access the
-        # resolved resource
-
-        # Before we can obtain deployed value for a resource, we need to first ensure to
-        # process the resource if this wasn't processed already. Ideally, values should only
-        # be accessible through delta objects, to ensure computation is always complete at
-        # every level.
-        _ = self._get_node_resource_for(
-            resource_name=resource_logical_id,
-            node_template=self._change_set.update_model.node_template,
-        )
-        resolved_resource = resolved_resources.get(resource_logical_id)
-        if resolved_resource is None:
-            raise RuntimeError(
-                f"No deployed instances of resource '{resource_logical_id}' were found"
-            )
-        properties = resolved_resource.get("Properties", {})
-        # TODO support structured properties, e.g. NestedStack.Outputs.OutputName
-        property_value: Any | None = get_value_from_path(properties, property_name)
-
-        if property_value:
-            if not isinstance(property_value, (str, list)):
-                # TODO: is this correct? If there is a bug in the logic here, it's probably
-                #  better to know about it with a clear error message than to receive some form
-                #  of message about trying to use a dictionary in place of a string
-                raise RuntimeError(
-                    f"Accessing property '{property_name}' from '{resource_logical_id}' resulted in a non-string value nor list"
-                )
-            return property_value
-        elif config.CFN_IGNORE_UNSUPPORTED_RESOURCE_TYPES:
-            return MOCKED_REFERENCE
-
-        return property_value
-
-    def _before_deployed_property_value_of(
-        self, resource_logical_id: str, property_name: str
-    ) -> Any:
-        return self._deployed_property_value_of(
-            resource_logical_id=resource_logical_id,
-            property_name=property_name,
-            resolved_resources=self._before_resolved_resources,
-        )
-
-    def _after_deployed_property_value_of(
-        self, resource_logical_id: str, property_name: str
-    ) -> str | None:
-        return self._before_deployed_property_value_of(
-            resource_logical_id=resource_logical_id, property_name=property_name
-        )
-
     def _get_node_mapping(self, map_name: str) -> NodeMapping:
         mappings: list[NodeMapping] = self._change_set.update_model.node_template.mappings.mappings
         # TODO: another scenarios suggesting property lookups might be preferable.
@@ -359,7 +309,7 @@ class ChangeSetModelStaticPreproc(ChangeSetModelVisitor):
             case _:
                 raise RuntimeError(f"The use of '{pseudo_parameter_name}' is currently unsupported")
 
-    def _resolve_reference(self, logical_id: str) -> PreprocEntityDelta:
+    def _resolve_reference(self, logical_id: str) -> MaybeComputable[PreprocEntityDelta]:
         if logical_id in _PSEUDO_PARAMETERS:
             pseudo_parameter_value = self._resolve_pseudo_parameter(
                 pseudo_parameter_name=logical_id
@@ -372,13 +322,7 @@ class ChangeSetModelStaticPreproc(ChangeSetModelVisitor):
             parameter_delta = self.visit(node_parameter)
             return parameter_delta
 
-        node_resource = self._get_node_resource_for(
-            resource_name=logical_id, node_template=self._change_set.update_model.node_template
-        )
-        resource_delta = self.visit(node_resource)
-        before = resource_delta.before
-        after = resource_delta.after
-        return PreprocEntityDelta(before=before, after=after)
+        return UnComputable
 
     def _resolve_mapping(
         self, map_name: str, top_level_key: str, second_level_key
@@ -552,60 +496,6 @@ class ChangeSetModelStaticPreproc(ChangeSetModelVisitor):
                 after[name] = delta_after
         return PreprocEntityDelta(before=before, after=after)
 
-    def _resolve_attribute(self, arguments: str | list[str], select_before: bool) -> str:
-        # TODO: add arguments validation.
-        arguments_list: list[str]
-        if isinstance(arguments, str):
-            arguments_list = arguments.split(".")
-        else:
-            arguments_list = arguments
-        logical_name_of_resource = arguments_list[0]
-        attribute_name = arguments_list[1]
-
-        node_resource = self._get_node_resource_for(
-            resource_name=logical_name_of_resource,
-            node_template=self._change_set.update_model.node_template,
-        )
-        node_property: NodeProperty | None = self._get_node_property_for(
-            property_name=attribute_name, node_resource=node_resource
-        )
-        if node_property is not None:
-            # The property is statically defined in the template and its value can be computed.
-            property_delta = self.visit(node_property)
-            value = property_delta.before if select_before else property_delta.after
-        else:
-            # The property is not statically defined and must therefore be available in
-            # the properties deployed set.
-            if select_before:
-                value = self._before_deployed_property_value_of(
-                    resource_logical_id=logical_name_of_resource,
-                    property_name=attribute_name,
-                )
-            else:
-                value = self._after_deployed_property_value_of(
-                    resource_logical_id=logical_name_of_resource,
-                    property_name=attribute_name,
-                )
-        return value
-
-    def visit_node_intrinsic_function_fn_get_att(
-        self, node_intrinsic_function: NodeIntrinsicFunction
-    ) -> PreprocEntityDelta:
-        # TODO: validate the return value according to the spec.
-        arguments_delta = self.visit(node_intrinsic_function.arguments)
-        before_arguments: Maybe[str | list[str]] = arguments_delta.before
-        after_arguments: Maybe[str | list[str]] = arguments_delta.after
-
-        before = self._before_cache.get(node_intrinsic_function.scope, Nothing)
-        if is_nothing(before) and not is_nothing(before_arguments):
-            before = self._resolve_attribute(arguments=before_arguments, select_before=True)
-
-        after = self._after_cache.get(node_intrinsic_function.scope, Nothing)
-        if is_nothing(after) and not is_nothing(after_arguments):
-            after = self._resolve_attribute(arguments=after_arguments, select_before=False)
-
-        return PreprocEntityDelta(before=before, after=after)
-
     def visit_node_intrinsic_function_fn_equals(
         self, node_intrinsic_function: NodeIntrinsicFunction
     ) -> PreprocEntityDelta:
@@ -705,76 +595,73 @@ class ChangeSetModelStaticPreproc(ChangeSetModelVisitor):
         )
         return delta
 
-    def visit_node_intrinsic_function_fn_sub(
-        self, node_intrinsic_function: NodeIntrinsicFunction
-    ) -> PreprocEntityDelta:
-        def _compute_sub(args: str | list[Any], select_before: bool) -> str:
-            # TODO: add further schema validation.
-            string_template: str
-            sub_parameters: dict
-            if isinstance(args, str):
-                string_template = args
-                sub_parameters = {}
-            elif (
-                isinstance(args, list)
-                and len(args) == 2
-                and isinstance(args[0], str)
-                and isinstance(args[1], dict)
-            ):
-                string_template = args[0]
-                sub_parameters = args[1]
+    def _compute_sub(self, args: str | list[Any], select_before: bool) -> str:
+        # TODO: add further schema validation.
+        string_template: str
+        sub_parameters: dict
+        if isinstance(args, str):
+            string_template = args
+            sub_parameters = {}
+        elif (
+            isinstance(args, list)
+            and len(args) == 2
+            and isinstance(args[0], str)
+            and isinstance(args[1], dict)
+        ):
+            string_template = args[0]
+            sub_parameters = args[1]
+        else:
+            raise RuntimeError(
+                "Invalid arguments shape for Fn::Sub, expected a String "
+                f"or a Tuple of String and Map but got '{args}'"
+            )
+        sub_string = string_template
+        template_variable_names = re.findall("\\${([^}]+)}", string_template)
+        for template_variable_name in template_variable_names:
+            template_variable_value = Nothing
+
+            # Try to resolve the variable name as pseudo parameter.
+            if template_variable_name in _PSEUDO_PARAMETERS:
+                template_variable_value = self._resolve_pseudo_parameter(
+                    pseudo_parameter_name=template_variable_name
+                )
+
+            # Try to resolve the variable name as an entry to the defined parameters.
+            elif template_variable_name in sub_parameters:
+                template_variable_value = sub_parameters[template_variable_name]
+
+            # Try to resolve the variable name as GetAtt.
+            elif "." in template_variable_name:
+                try:
+                    template_variable_value = self._resolve_attribute(
+                        arguments=template_variable_name, select_before=select_before
+                    )
+                except RuntimeError:
+                    pass
+
+            # Try to resolve the variable name as Ref.
             else:
+                try:
+                    resource_delta = self._resolve_reference(logical_id=template_variable_name)
+                    template_variable_value = (
+                        resource_delta.before if select_before else resource_delta.after
+                    )
+                    if isinstance(template_variable_value, PreprocResource):
+                        template_variable_value = template_variable_value.physical_resource_id
+                except RuntimeError:
+                    pass
+
+            if is_nothing(template_variable_value):
                 raise RuntimeError(
-                    "Invalid arguments shape for Fn::Sub, expected a String "
-                    f"or a Tuple of String and Map but got '{args}'"
+                    f"Undefined variable name in Fn::Sub string template '{template_variable_name}'"
                 )
-            sub_string = string_template
-            template_variable_names = re.findall("\\${([^}]+)}", string_template)
-            for template_variable_name in template_variable_names:
-                template_variable_value = Nothing
 
-                # Try to resolve the variable name as pseudo parameter.
-                if template_variable_name in _PSEUDO_PARAMETERS:
-                    template_variable_value = self._resolve_pseudo_parameter(
-                        pseudo_parameter_name=template_variable_name
-                    )
+            if not isinstance(template_variable_value, str):
+                template_variable_value = str(template_variable_value)
 
-                # Try to resolve the variable name as an entry to the defined parameters.
-                elif template_variable_name in sub_parameters:
-                    template_variable_value = sub_parameters[template_variable_name]
-
-                # Try to resolve the variable name as GetAtt.
-                elif "." in template_variable_name:
-                    try:
-                        template_variable_value = self._resolve_attribute(
-                            arguments=template_variable_name, select_before=select_before
-                        )
-                    except RuntimeError:
-                        pass
-
-                # Try to resolve the variable name as Ref.
-                else:
-                    try:
-                        resource_delta = self._resolve_reference(logical_id=template_variable_name)
-                        template_variable_value = (
-                            resource_delta.before if select_before else resource_delta.after
-                        )
-                        if isinstance(template_variable_value, PreprocResource):
-                            template_variable_value = template_variable_value.physical_resource_id
-                    except RuntimeError:
-                        pass
-
-                if is_nothing(template_variable_value):
-                    raise RuntimeError(
-                        f"Undefined variable name in Fn::Sub string template '{template_variable_name}'"
-                    )
-
-                if not isinstance(template_variable_value, str):
-                    template_variable_value = str(template_variable_value)
-
-                sub_string = sub_string.replace(
-                    f"${{{template_variable_name}}}", template_variable_value
-                )
+            sub_string = sub_string.replace(
+                f"${{{template_variable_name}}}", template_variable_value
+            )
 
             # FIXME: the following type reduction is ported from v1; however it appears as though such
             #        reduction is not performed by the engine, and certainly not at this depth given the
@@ -793,15 +680,18 @@ class ChangeSetModelStaticPreproc(ChangeSetModelVisitor):
                     result = sub_string
             return result
 
+    def visit_node_intrinsic_function_fn_sub(
+        self, node_intrinsic_function: NodeIntrinsicFunction
+    ) -> PreprocEntityDelta:
         arguments_delta = self.visit(node_intrinsic_function.arguments)
         arguments_before = arguments_delta.before
         arguments_after = arguments_delta.after
         before = self._before_cache.get(node_intrinsic_function.scope, Nothing)
         if is_nothing(before) and not is_nothing(arguments_before):
-            before = _compute_sub(args=arguments_before, select_before=True)
+            before = self._compute_sub(args=arguments_before, select_before=True)
         after = self._after_cache.get(node_intrinsic_function.scope, Nothing)
         if is_nothing(after) and not is_nothing(arguments_after):
-            after = _compute_sub(args=arguments_after, select_before=False)
+            after = self._compute_sub(args=arguments_after, select_before=False)
         return PreprocEntityDelta(before=before, after=after)
 
     def visit_node_intrinsic_function_fn_join(
