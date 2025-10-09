213dd95ec59a14f4477a72ad763623f321489596
Mathieu Cloutier <cloutier.mat0@gmail.com>
2025-10-08 21:41:20 -0600

    added test

--- tests/unit/utils/test_json.py
+++ tests/unit/utils/test_json.py
@@ -1,9 +1,15 @@
 import json
 
-from localstack.utils.json import BytesEncoder
+from localstack.utils.json import BytesEncoder, assign_to_path
 
 
 def test_json_encoder():
     payload = {"foo": b"foobar"}
     result = json.dumps(payload, cls=BytesEncoder)
     assert result == '{"foo": "Zm9vYmFy"}'
+
+
+def test_assign_to_path_single_path():
+    target = {}
+    assign_to_path(target, "foo", "bar")
+    assert target == {"foo": "bar"}
