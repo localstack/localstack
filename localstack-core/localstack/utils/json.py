1a48eb6577d8c6fe156272294803aafd1bc57cab
Mathieu Cloutier <79954947+cloutierMat@users.noreply.github.com>
2025-10-20 10:39:21 -0600

    fix json.assign_to_path with non nested path (#13245)

--- localstack-core/localstack/utils/json.py
+++ localstack-core/localstack/utils/json.py
@@ -169,10 +169,24 @@ def extract_jsonpath(value, path):
     return result
 
 
-def assign_to_path(target, path: str, value, delimiter: str = "."):
+def assign_to_path(target: dict, path: str, value: any, delimiter: str = ".") -> dict:
+    """Assign the given value to a dict. If the path doesn't exist in the target dict, it will be created.
+    The delimiter can be used to provide a path with a different delimiter.
+
+    Examples:
+     - assign_to_path({}, "a", "b") => {"a": "b"}
+     - assign_to_path({}, "a.b.c", "d") => {"a": {"b": {"c": "d"}}}
+     - assign_to_path({}, "a.b/c", "d", delimiter="/") => {"a.b": {"c": "d"}}
+
+    """
     parts = path.strip(delimiter).split(delimiter)
+
+    if len(parts) == 1:
+        target[parts[0]] = value
+        return target
+
     path_to_parent = delimiter.join(parts[:-1])
-    parent = extract_from_jsonpointer_path(target, path_to_parent, auto_create=True)
+    parent = extract_from_jsonpointer_path(target, path_to_parent, delimiter, auto_create=True)
     if not isinstance(parent, dict):
         LOG.debug(
             'Unable to find parent (type %s) for path "%s" in object: %s',
--- tests/unit/utils/test_json.py
+++ tests/unit/utils/test_json.py
@@ -1,9 +1,27 @@
 import json
 
-from localstack.utils.json import BytesEncoder
+from localstack.utils.json import BytesEncoder, assign_to_path
 
 
 def test_json_encoder():
     payload = {"foo": b"foobar"}
     result = json.dumps(payload, cls=BytesEncoder)
     assert result == '{"foo": "Zm9vYmFy"}'
+
+
+def test_assign_to_path_single_path():
+    target = {}
+    assign_to_path(target, "a", "bar")
+    assert target == {"a": "bar"}
+
+
+def test_assign_multi_nested_path():
+    target = {}
+    assign_to_path(target, "a.b.foo", "bar")
+    assert target == {"a": {"b": {"foo": "bar"}}}
+
+
+def test_assign_to_path_mixed_delimiters():
+    target = {}
+    assign_to_path(target, "a.b/c", "d", delimiter="/")
+    assert target == {"a.b": {"c": "d"}}
