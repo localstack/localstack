06902e059dd6eb6572366c5fa262a887944c3cd0
Misha Tiurin <misha.tiurin@localstack.cloud>
2025-11-17 16:46:58 +0100

    Add mock result validation according to field validation modes
    TestState validates mocked responses against AWS API models with three validation modes: STRICT (this is the default and validates all required fields), PRESENT (validates field types and names), and NONE (no validation).
    
    STRICT field validation mode checks the presence of required fields and validates the type and format of the fields in the mock against the respective service shape in AWS API definitions.
    PRESENT field validation mode doesn't check the presence of required fields. It only validates the type and format of the fields that are present in the user-provided mock.
    NONE mode means no format validation will be performed. A check that the result is a valid JSON string is still performed even in NONE field validation mode.
    
    Note: fields that are not present in the API spec but are supplied in the mock will be allowed, because of forward-compatibility.

---
+++ tests/aws/services/stepfunctions/v2/test_state/test_field_validation_mode_none.py
@@ -0,0 +1,65 @@
+import json
+
+import pytest
+
+from localstack.aws.api.stepfunctions import InspectionLevel
+from localstack.testing.pytest import markers
+from localstack.utils.aws import arns
+from tests.aws.services.stepfunctions.templates.test_state.test_state_templates import (
+    TestStateTemplate as TST,
+)
+
+
+class TestFieldValidationModeNone:
+    SFN_VALIDATION_ERRORS = [
+        pytest.param(
+            {},
+            id="missing_required_fields",
+        ),
+        pytest.param(
+            {"ExecutionArn": "stringValueExecutionArn", "StartDate": True},
+            id="wrong_timestamp_type_bool",
+        ),
+    ]
+
+    @markers.aws.validated
+    @pytest.mark.parametrize("result", SFN_VALIDATION_ERRORS)
+    def test_none_mode_sfn_task(
+        self,
+        aws_client,
+        aws_client_no_sync_prefix,
+        create_state_machine_iam_role,
+        account_id,
+        region_name,
+        sfn_snapshot,
+        result,
+    ):
+        template = TST.load_sfn_template(TST.BASE_SFN_START_EXECUTION_TASK_STATE)
+        definition = json.dumps(template)
+
+        target_state_machine_arn = arns.stepfunctions_state_machine_arn(
+            name="TargetStateMachine", account_id=account_id, region_name=region_name
+        )
+
+        target_execution_name = "TestStartTarget"
+
+        exec_input = json.dumps(
+            {
+                "stateMachineArn": target_state_machine_arn,
+                "targetInput": None,
+                "name": target_execution_name,
+            }
+        )
+
+        mock = {"result": json.dumps(result), "fieldValidationMode": "NONE"}
+
+        sfn_role_arn = create_state_machine_iam_role(aws_client)
+
+        test_state_response = aws_client_no_sync_prefix.stepfunctions.test_state(
+            definition=definition,
+            roleArn=sfn_role_arn,
+            input=exec_input,
+            inspectionLevel=InspectionLevel.INFO,
+            mock=mock,
+        )
+        sfn_snapshot.match("test_state_response", test_state_response)
