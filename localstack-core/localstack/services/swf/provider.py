328d204cd7ea404310aa03606eefb29369f7a252
Benjamin Simon <benjh.simon@gmail.com>
2025-11-27 18:20:48 +0100

    define state container for swf

--- localstack-core/localstack/services/swf/provider.py
+++ localstack-core/localstack/services/swf/provider.py
@@ -1,7 +1,11 @@
 from abc import ABC
 
 from localstack.aws.api.swf import SwfApi
+from localstack.state import StateVisitor
 
 
 class SWFProvider(SwfApi, ABC):
-    pass
+    def accept_state_visitor(self, visitor: StateVisitor):
+        from moto.swf.models import swf_backends
+
+        visitor.visit(swf_backends)
