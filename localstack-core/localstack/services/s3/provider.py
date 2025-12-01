5e13ab2c2627725a1fac3bae57d76d81f9893fab
Aiden Huynh <aiden.huynh@localstack.cloud>
2025-12-01 22:02:26 +0000

    add bucket arn to list buckets response

--- localstack-core/localstack/services/s3/provider.py
+++ localstack-core/localstack/services/s3/provider.py
@@ -659,6 +659,7 @@ class S3Provider(S3Api, ServiceLifecycleHook):
                 Name=bucket.name,
                 CreationDate=bucket.creation_date,
                 BucketRegion=bucket.bucket_region,
+                BucketArn=bucket.bucket_arn
             )
             buckets.append(output_bucket)
             count += 1
