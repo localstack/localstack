bad88084552364a8203a92e673c85e5682335114
Bernhard Matyas <90144234+baermat@users.noreply.github.com>
2025-12-15 13:33:36 +0100

    SNS: v2 permission operations (#13516)

--- localstack-core/localstack/services/sns/v2/provider.py
+++ localstack-core/localstack/services/sns/v2/provider.py
@@ -10,6 +10,7 @@ from rolo import Request, Router, route
 
 from localstack.aws.api import CommonServiceException, RequestContext
 from localstack.aws.api.sns import (
+    ActionsList,
     AmazonResourceName,
     BatchEntryIdsNotDistinctException,
     CheckIfPhoneNumberIsOptedOutResponse,
@@ -17,6 +18,7 @@ from localstack.aws.api.sns import (
     CreateEndpointResponse,
     CreatePlatformApplicationResponse,
     CreateTopicResponse,
+    DelegatesList,
     Endpoint,
     EndpointDisabledException,
     GetEndpointAttributesResponse,
@@ -60,6 +62,7 @@ from localstack.aws.api.sns import (
     attributeValue,
     authenticateOnUnsubscribe,
     endpoint,
+    label,
     message,
     messageStructure,
     nextToken,
@@ -88,6 +91,7 @@ from localstack.services.sns.constants import (
     SUBSCRIPTION_TOKENS_ENDPOINT,
     VALID_APPLICATION_PLATFORMS,
     VALID_MSG_ATTR_NAME_CHARS,
+    VALID_POLICY_ACTIONS,
     VALID_SUBSCRIPTION_ATTR_NAME,
 )
 from localstack.services.sns.filter import FilterPolicyValidator
@@ -1113,6 +1117,61 @@ class SnsProvider(SnsApi, ServiceLifecycleHook):
         if phone_number in store.PHONE_NUMBERS_OPTED_OUT:
             store.PHONE_NUMBERS_OPTED_OUT.remove(phone_number)
         return OptInPhoneNumberResponse()
+
+    #
+    # Permission operations
+    #
+
+    def add_permission(
+        self,
+        context: RequestContext,
+        topic_arn: topicARN,
+        label: label,
+        aws_account_id: DelegatesList,
+        action_name: ActionsList,
+        **kwargs,
+    ) -> None:
+        topic: Topic = self._get_topic(topic_arn, context)
+        policy = json.loads(topic["attributes"]["Policy"])
+        statement = next(
+            (statement for statement in policy["Statement"] if statement["Sid"] == label),
+            None,
+        )
+
+        if statement:
+            raise InvalidParameterException("Invalid parameter: Statement already exists")
+
+        if any(action not in VALID_POLICY_ACTIONS for action in action_name):
+            raise InvalidParameterException(
+                "Invalid parameter: Policy statement action out of service scope!"
+            )
+
+        principals = [
+            f"arn:{get_partition(context.region)}:iam::{account_id}:root"
+            for account_id in aws_account_id
+        ]
+        actions = [f"SNS:{action}" for action in action_name]
+
+        statement = {
+            "Sid": label,
+            "Effect": "Allow",
+            "Principal": {"AWS": principals[0] if len(principals) == 1 else principals},
+            "Action": actions[0] if len(actions) == 1 else actions,
+            "Resource": topic_arn,
+        }
+
+        policy["Statement"].append(statement)
+        topic["attributes"]["Policy"] = json.dumps(policy)
+
+    def remove_permission(
+        self, context: RequestContext, topic_arn: topicARN, label: label, **kwargs
+    ) -> None:
+        topic = self._get_topic(topic_arn, context)
+        policy = json.loads(topic["attributes"]["Policy"])
+        statements = policy["Statement"]
+        statements = [statement for statement in statements if statement["Sid"] != label]
+        policy["Statement"] = statements
+        topic["attributes"]["Policy"] = json.dumps(policy)
 
     def list_tags_for_resource(
         self, context: RequestContext, resource_arn: AmazonResourceName, **kwargs
