becaa2b3992e108f9fd6fa06505ba440463f714e
Giovanni Grano <me@giograno.com>
2025-12-16 19:05:26 +0100

    fix Any annotations

--- localstack-core/localstack/services/stepfunctions/asl/eval/evaluation_details.py
+++ localstack-core/localstack/services/stepfunctions/asl/eval/evaluation_details.py
@@ -18,10 +18,12 @@ class ExecutionDetails:
     arn: Final[LongArn]
     name: Final[str]
     role_arn: Final[Arn]
-    inpt: Final[Any | None]
+    inpt: Final[dict[str, Any] | None]
     start_time: Final[str]
 
-    def __init__(self, arn: LongArn, name: str, role_arn: Arn, inpt: Any | None, start_time: str):
+    def __init__(
+        self, arn: LongArn, name: str, role_arn: Arn, inpt: dict[str, Any] | None, start_time: str
+    ):
         self.arn = arn
         self.name = name
         self.role_arn = role_arn
