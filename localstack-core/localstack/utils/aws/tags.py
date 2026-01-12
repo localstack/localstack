2f1354d204b664ea8ebc09d9dd1196740731e415
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2026-01-12 10:50:45 -0500

    fix utility

--- localstack-core/localstack/utils/aws/tags.py
+++ localstack-core/localstack/utils/aws/tags.py
@@ -3,4 +3,4 @@ def tag_list_to_dict(tag_list: list[dict[str, str]]) -> dict[str, str]:
 
 
 def tag_dict_to_list(tag_dict: dict[str, str]) -> list[dict[str, str]]:
-    return [{"Key": key, "Value": value} for key, value in dict.items()]
+    return [{"Key": key, "Value": value} for key, value in tag_dict.items()]
