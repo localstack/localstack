1d67e582b0da2fbffb728112f02789891fa07be5
Simon Walker <simon.walker@localstack.cloud>
2026-01-14 14:01:09 +0000

    CFn: correctly detect createOnlyProperties
    # Motivation
    
    While experimenting with the AWS::SQS::Queue resource, Peter found that the following does not have parity with AWS:
    
    1. Create queue without speccifying `QueueName`
    2. Add queue name to the resource and redeploy
    
    On AWS this replaces the resource. On LocalStack we incorrectly try to do an in-place update, which does not work correctly as our `update` method does not handle this case. (Note: it _shouldn't_ handle this case, the engine should replace the resource).
    
    # Changes
    
    * Instead of only considering properties when they are `MODIFIED` i.e. they change value, but both before and after states include the value, we determine a change if the resource property is not `UNCHANGED`, which should include `ADDED` and `REMOVED` as well.
    * Validated test asserting the events of the resource as well as that the queue name changes after the update (which was also not the case before)

---
+++ tests/aws/services/cloudformation/engine/test_replacement.py
@@ -0,0 +1,67 @@
+import json
+
+from localstack.testing.pytest import markers
+from localstack.utils.strings import short_uid
+
+
+@markers.aws.validated
+@markers.snapshot.skip_snapshot_verify(paths=["$..PhysicalResourceId"])
+def test_requires_replacement(
+    deploy_cfn_template,
+    capture_per_resource_events,
+    snapshot,
+):
+    snapshot.add_transformer(snapshot.transform.cloudformation_api())
+
+    t1 = {
+        "Resources": {
+            "MyQueue": {
+                "Type": "AWS::SQS::Queue",
+            },
+        },
+        "Outputs": {
+            "QueueName": {
+                "Value": {"Fn::GetAtt": ["MyQueue", "QueueName"]},
+            },
+        },
+    }
+    t2 = {
+        "Parameters": {
+            "QueueName": {
+                "Type": "String",
+            },
+        },
+        "Resources": {
+            "MyQueue": {
+                "Type": "AWS::SQS::Queue",
+                "Properties": {
+                    "QueueName": {"Ref": "QueueName"},
+                },
+            },
+        },
+        "Outputs": {
+            "QueueName": {
+                "Value": {"Fn::GetAtt": ["MyQueue", "QueueName"]},
+            },
+        },
+    }
+
+    deploy_result = deploy_cfn_template(template=json.dumps(t1))
+    given_queue_name = deploy_result.outputs["QueueName"]
+    snapshot.add_transformer(snapshot.transform.regex(given_queue_name, "<queue-name-1>"))
+
+    new_queue_name = f"queue-{short_uid()}"
+    deploy_result_2 = deploy_cfn_template(
+        template=json.dumps(t2),
+        is_update=True,
+        stack_name=deploy_result.stack_id,
+        parameters={"QueueName": new_queue_name},
+    )
+
+    assert deploy_result_2.outputs["QueueName"] == new_queue_name
+    assert given_queue_name != new_queue_name
+
+    per_resource_events = capture_per_resource_events(
+        deploy_result.stack_id,
+    )
+    snapshot.match("queue-events", per_resource_events["MyQueue"])
