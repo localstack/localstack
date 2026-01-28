1075e754fc160fdadd167ca9ea5774f423bc3b3d
Anastasia Dusak <61540676+k-a-il@users.noreply.github.com>
2026-01-28 12:01:20 +0100

    Reduce scope of try-except

--- localstack-core/localstack/aws/handlers/service_plugin.py
+++ localstack-core/localstack/aws/handlers/service_plugin.py
@@ -56,24 +56,24 @@ class ServiceLoader(Handler):
         try:
             # Ensure the Service is loaded and set to ServiceState.RUNNING if not in an erroneous state.
             service_plugin: Service = self.service_manager.require(service_name)
-
-            with self.service_locks[context.service.service_name]:
-                # try again to avoid race conditions
-                if service_name in self.loaded_services:
-                    return
-                self.loaded_services.add(service_name)
-                if isinstance(service_plugin, Service):
-                    request_router.add_skeleton(service_plugin.skeleton)
-                else:
-                    LOG.warning(
-                        "found plugin for '%s', but cannot attach service plugin of type '%s'",
-                        service_name,
-                        type(service_plugin),
-                    )
         except PluginDisabled as e:
             if e.reason == "This feature is not part of the active license agreement":
                 raise PluginNotIncludedInUserLicenseError()
             raise
+
+        with self.service_locks[context.service.service_name]:
+            # try again to avoid race conditions
+            if service_name in self.loaded_services:
+                return
+            self.loaded_services.add(service_name)
+            if isinstance(service_plugin, Service):
+                request_router.add_skeleton(service_plugin.skeleton)
+            else:
+                LOG.warning(
+                    "found plugin for '%s', but cannot attach service plugin of type '%s'",
+                    service_name,
+                    type(service_plugin),
+                )
 
 
 class ServiceLoaderForDataPlane(Handler):
