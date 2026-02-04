66088323d689b32551f609a7586c160755038bbe
Aiden Huynh <115724666+aidehn@users.noreply.github.com>
2026-02-04 15:30:43 +0800

    refactor tag validation logic

--- localstack-core/localstack/services/s3control/provider.py
+++ localstack-core/localstack/services/s3control/provider.py
@@ -10,8 +10,8 @@ from localstack.aws.api.s3control import (
     UntagResourceResult,
 )
 from localstack.aws.forwarder import NotImplementedAvoidFallbackError
-from localstack.services.s3.models import s3_stores
-from localstack.services.s3control.validation import validate_tags
+from localstack.services.s3.models import s3_stores, S3Store
+from localstack.services.s3control.validation import validate_tags, validate_arn_for_tagging
 from localstack.state import StateVisitor
 from localstack.utils.tagging import TaggingService
 
@@ -33,59 +33,25 @@ class S3ControlProvider(S3ControlApi):
     """
 
     @staticmethod
-    def _get_tagging_service_for_bucket(
-        resource_arn: S3ResourceArn,
-        partition: str,
-        region: str,
-        account_id: str,
-    ) -> TaggingService:
-        s3_prefix = f"arn:{partition}:s3:::"
-        if not resource_arn.startswith(s3_prefix):
-            # Moto does not support Tagging operations for S3 Control, so we should not forward those operations back
-            # to it
-            raise NotImplementedAvoidFallbackError(
-                "LocalStack only support Bucket tagging operations for S3Control"
-            )
-
-        store = s3_stores[account_id][region]
-        bucket_name = resource_arn.removeprefix(s3_prefix)
-        if bucket_name not in store.global_bucket_map:
-            raise NoSuchResource("The specified resource doesn't exist.")
-
-        return store.TAGS
+    def get_s3_store(account_id: str, region: str) -> S3Store:
+        return s3_stores[account_id][region]
 
     def _tag_bucket_resource(
         self, resource_arn: str, partition: str, region: str, account_id: str, tags: TagList
     ) -> None:
-        tagging_service = self._get_tagging_service_for_bucket(
-            resource_arn=resource_arn,
-            partition=partition,
-            region=region,
-            account_id=account_id,
-        )
-        validate_tags(tags)
+        tagging_service = self.get_s3_store(account_id, region).TAGS
         tagging_service.tag_resource(resource_arn, tags)
 
     def _untag_bucket_resource(
         self, resource_arn: str, partition: str, region: str, account_id: str, tag_keys: TagKeyList
     ) -> None:
-        tagging_service = self._get_tagging_service_for_bucket(
-            resource_arn=resource_arn,
-            partition=partition,
-            region=region,
-            account_id=account_id,
-        )
+        tagging_service = self.get_s3_store(account_id, region).TAGS
         tagging_service.untag_resource(resource_arn, tag_keys)
 
     def _list_bucket_tags(
         self, resource_arn: str, partition: str, region: str, account_id: str
     ) -> TagList:
-        tagging_service = self._get_tagging_service_for_bucket(
-            resource_arn=resource_arn,
-            partition=partition,
-            region=region,
-            account_id=account_id,
-        )
+        tagging_service = self.get_s3_store(account_id, region).TAGS
         return tagging_service.list_tags_for_resource(resource_arn)["Tags"]
 
     def tag_resource(
@@ -97,6 +63,9 @@ class S3ControlProvider(S3ControlApi):
         **kwargs,
     ) -> TagResourceResult:
         # Currently S3Control only supports tagging buckets
+        validate_arn_for_tagging(resource_arn, context.partition, account_id, context.region)
+        validate_tags(tags)
+
         self._tag_bucket_resource(resource_arn, context.partition, context.region, account_id, tags)
         return TagResourceResult()
 
@@ -109,6 +78,8 @@ class S3ControlProvider(S3ControlApi):
         **kwargs,
     ) -> UntagResourceResult:
         # Currently S3Control only supports tagging buckets
+        validate_arn_for_tagging(resource_arn, context.partition, account_id, context.region)
+
         self._untag_bucket_resource(
             resource_arn, context.partition, context.region, account_id, tag_keys
         )
@@ -118,5 +89,7 @@ class S3ControlProvider(S3ControlApi):
         self, context: RequestContext, account_id: AccountId, resource_arn: S3ResourceArn, **kwargs
     ) -> ListTagsForResourceResult:
         # Currently S3Control only supports tagging buckets
+        validate_arn_for_tagging(resource_arn, context.partition, account_id, context.region)
+
         tags = self._list_bucket_tags(resource_arn, context.partition, context.region, account_id)
         return ListTagsForResourceResult(Tags=tags)
--- localstack-core/localstack/services/s3control/validation.py
+++ localstack-core/localstack/services/s3control/validation.py
@@ -1,7 +1,35 @@
 from localstack.aws.api.s3 import InvalidTag
 from localstack.aws.api.s3control import Tag, TagList
+from localstack.aws.forwarder import NotImplementedAvoidFallbackError
 from localstack.services.s3.exceptions import MalformedXML
+from localstack.services.s3.models import s3_stores
 from localstack.services.s3.utils import TAG_REGEX
+from localstack.services.s3control.provider import NoSuchResource
+
+
+def validate_arn_for_tagging(resource_arn: str, partition: str, account_id: str, region: str) -> None:
+    """
+    Validates the resource ARN for the resource being tagged.
+
+    :param resource_arn: The ARN of the resource being tagged.
+    :param partition: The partition the request is originating from.
+    :param account_id: The account ID of the target resource.
+    :param region: The region the request is originating from.
+    :return: None
+    """
+
+    s3_prefix = f"arn:{partition}:s3:::"
+    if not resource_arn.startswith(s3_prefix):
+        # Moto does not support Tagging operations for S3 Control, so we should not forward those operations back
+        # to it
+        raise NotImplementedAvoidFallbackError(
+            "LocalStack only support Bucket tagging operations for S3Control"
+        )
+
+    store = s3_stores[account_id][region]
+    bucket_name = resource_arn.removeprefix(s3_prefix)
+    if bucket_name not in store.global_bucket_map:
+        raise NoSuchResource("The specified resource doesn't exist.")
 
 
 def validate_tags(tags: TagList):
