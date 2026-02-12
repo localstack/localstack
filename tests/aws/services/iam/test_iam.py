9c45220767587ed123f0bd9e7f60d0009ad8f1a7
Daniel Fangl <daniel.fangl@localstack.cloud>
2026-02-12 11:19:16 +0100

    Migrate IAM user tests from moto to LocalStack (#13742)

--- tests/aws/services/iam/test_iam.py
+++ tests/aws/services/iam/test_iam.py
@@ -31,10 +31,6 @@ GET_USER_POLICY_DOC = """{
 }"""
 
 
-class TestUsers:
-    pass
-
-
 class TestIAMExtensions:
     @markers.aws.validated
     def test_get_user_without_username_as_user(self, create_user, aws_client, region_name):
