c22cbfcf64d2a5fe60c804a1062508ab54911ae3
Simon Walker <simon.walker@localstack.cloud>
2026-02-19 16:43:28 +0000

    Use external url for TEST_AWS_ENDPOINT_URL and add helper
    The helper will be used to skip tests if they are targeting a different host

--- localstack-core/localstack/testing/config.py
+++ localstack-core/localstack/testing/config.py
@@ -11,7 +11,7 @@ TEST_AWS_ACCOUNT_ID = os.getenv("TEST_AWS_ACCOUNT_ID") or DEFAULT_AWS_ACCOUNT_ID
 TEST_AWS_ACCESS_KEY_ID = os.getenv("TEST_AWS_ACCESS_KEY_ID") or "test"
 TEST_AWS_SECRET_ACCESS_KEY = os.getenv("TEST_AWS_SECRET_ACCESS_KEY") or "test"
 TEST_AWS_REGION_NAME = os.getenv("TEST_AWS_REGION_NAME") or "us-east-1"
-TEST_AWS_ENDPOINT_URL = os.getenv("TEST_AWS_ENDPOINT_URL") or config.internal_service_url()
+TEST_AWS_ENDPOINT_URL = os.getenv("TEST_AWS_ENDPOINT_URL") or config.external_service_url()
 
 # Secondary test AWS profile - only used for testing against AWS
 SECONDARY_TEST_AWS_PROFILE = os.getenv("SECONDARY_TEST_AWS_PROFILE")
@@ -24,3 +24,10 @@ SECONDARY_TEST_AWS_REGION_NAME = os.getenv("SECONDARY_TEST_AWS_REGION_NAME") or 
 
 TEST_SKIP_LOCALSTACK_START = is_env_true("TEST_SKIP_LOCALSTACK_START")
 TEST_FORCE_LOCALSTACK_START = is_env_true("TEST_FORCE_LOCALSTACK_START")
+
+
+def external_localstack_host() -> bool:
+    """
+    Returns true if the tests are running on a separate host than LocalStack
+    """
+    return bool(os.getenv("TEST_AWS_ENDPOINT_URL"))
