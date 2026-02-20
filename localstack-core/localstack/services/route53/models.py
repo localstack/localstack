50510e13e890b4395649f21ee1d7545565739a95
Aiden Huynh <115724666+aidehn@users.noreply.github.com>
2026-02-20 14:20:19 +0800

    update route53 store to have tags and no longer validate region

--- localstack-core/localstack/services/route53/models.py
+++ localstack-core/localstack/services/route53/models.py
@@ -1,10 +1,12 @@
 from localstack.aws.api.route53 import DelegationSet
 from localstack.services.stores import AccountRegionBundle, BaseStore, LocalAttribute
+from localstack.utils.tagging import Tags
 
 
 class Route53Store(BaseStore):
     # maps delegation set ID to reusable delegation set details
     reusable_delegation_sets: dict[str, DelegationSet] = LocalAttribute(default=dict)
+    tags: Tags = LocalAttribute(default=Tags)
 
 
-route53_stores = AccountRegionBundle("route53", Route53Store)
+route53_stores = AccountRegionBundle("route53", Route53Store, validate=False)
