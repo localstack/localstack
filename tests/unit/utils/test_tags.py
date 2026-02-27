84387883596a64bb08567475ca20c89c83c2f0a9
Viren Nadkarni <viren.nadkarni@localstack.cloud>
2026-02-27 17:00:22 +0530

    Consolidate tag utils (#13839)

--- tests/unit/utils/test_tags.py
+++ tests/unit/utils/test_tags.py
@@ -1,11 +1,18 @@
-from localstack.utils.aws import tags
+from localstack.utils import tagging
 
 
 def test_convert_tag_list_to_dictionary():
     tags_list = [{"Key": "key", "Value": "value"}]
-    assert tags.tag_list_to_dict(tags_list) == {"key": "value"}
+    assert tagging.tag_list_to_map(tags_list) == {"key": "value"}
+    tags_list = [{"key": "key", "value": "value"}]
+    assert tagging.tag_list_to_map(tags_list, key_field="key", value_field="value") == {
+        "key": "value"
+    }
 
 
 def test_convert_tag_dictionary_to_list():
     tags_dict = {"key": "value"}
-    assert tags.tag_dict_to_list(tags_dict) == [{"Key": "key", "Value": "value"}]
+    assert tagging.tag_map_to_list(tags_dict) == [{"Key": "key", "Value": "value"}]
+    assert tagging.tag_map_to_list(tags_dict, key_field="key", value_field="value") == [
+        {"key": "key", "value": "value"}
+    ]
